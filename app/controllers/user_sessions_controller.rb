d1de6adfae7f327711f6ed7beaa0653c4df7a6de
Daniel Weinmann <danielweinmann@gmail.com>
2010-11-26 01:29:01 -0200

    Basic layout

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,20 @@
+class UserSessionsController < ApplicationController
+  def new
+    @user_session = UserSession.new
+  end
+
+  def create
+    @user_session = UserSession.new(params[:user_session])
+    if @user_session.save
+      redirect_to account_url
+    else
+      render :action => :new
+    end
+  end
+
+  def destroy
+    current_user_session.destroy
+    redirect_to new_user_session_url
+  end
+end
+
