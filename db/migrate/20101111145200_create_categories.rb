858c57ce71bdcd7206ee1a993d212080e8778a83
Daniel Weinmann <danielweinmann@gmail.com>
2010-11-26 22:20:15 -0200

    Demo home page done

--- db/migrate/20101111145200_create_categories.rb
+++ db/migrate/20101111145200_create_categories.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 class CreateCategories < ActiveRecord::Migration
   def self.up
     create_table :categories do |t|
@@ -9,21 +11,22 @@ class CreateCategories < ActiveRecord::Migration
     #  t.name :not_blank => true, :unique => true
     #end
     add_index :categories, :name
-    # TODO find out why doesn't it work with special characters
     execute "INSERT INTO categories (name) VALUES ('Arte')"
+    execute "INSERT INTO categories (name) VALUES ('Artes plásticas')"
+    execute "INSERT INTO categories (name) VALUES ('Circo')"
     execute "INSERT INTO categories (name) VALUES ('Comunidade')"
-    execute "INSERT INTO categories (name) VALUES ('Feito a mao')"
+    execute "INSERT INTO categories (name) VALUES ('Feito à mão')"
     execute "INSERT INTO categories (name) VALUES ('Humor')"
     execute "INSERT INTO categories (name) VALUES ('Quadrinhos')"
-    execute "INSERT INTO categories (name) VALUES ('Danca')"
+    execute "INSERT INTO categories (name) VALUES ('Dança')"
     execute "INSERT INTO categories (name) VALUES ('Design')"
     execute "INSERT INTO categories (name) VALUES ('Eventos')"
     execute "INSERT INTO categories (name) VALUES ('Moda')"
     execute "INSERT INTO categories (name) VALUES ('Comida')"
-    execute "INSERT INTO categories (name) VALUES ('Cinema & Video')"
+    execute "INSERT INTO categories (name) VALUES ('Cinema & Vídeo')"
     execute "INSERT INTO categories (name) VALUES ('Jogos')"
     execute "INSERT INTO categories (name) VALUES ('Jornalismo')"
-    execute "INSERT INTO categories (name) VALUES ('Musica')"
+    execute "INSERT INTO categories (name) VALUES ('Música')"
     execute "INSERT INTO categories (name) VALUES ('Fotografia')"
     execute "INSERT INTO categories (name) VALUES ('Tecnologia')"
     execute "INSERT INTO categories (name) VALUES ('Teatro')"
