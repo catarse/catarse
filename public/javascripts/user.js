ea3ae8a36360c4c517f05d660610538abf295c73
Daniel Weinmann <danielweinmann@gmail.com>
2010-11-30 04:03:57 -0200

    Improving user menu's CSS and JS

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -1,5 +1,10 @@
 $('#user').hover(function(){
   $('#user_menu').show()
+  if($('#user_menu').css('width') > $('#user_menu_link').css('width')) {
+    $('#user_menu').addClass('user_menu_border')
+  } else {
+    $('#user_menu').css('width', $('#user_menu_link').css('width'))
+  }
   $('#user_menu_link').addClass('hover')
 }, function(){
   $('#user_menu').hide()
