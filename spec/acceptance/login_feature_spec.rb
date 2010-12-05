556ebc3090a6e3c621d5e5e9d326188d76e85ed7
Diogo Biazus <diogob@gmail.com>
2010-12-05 18:51:02 -0200

    Added click_login helper and started google authentication test

--- spec/acceptance/login_feature_spec.rb
+++ spec/acceptance/login_feature_spec.rb
@@ -3,13 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
 feature "Login Feature" do
 
   scenario "I'm new to the site and I want to signup with Twitter" do
-
-    visit homepage
-    
-    page.should have_no_css('#user')
-    page.should have_no_link('Catarse Test')
-    
-    click_link 'Login'
+    click_login
     click_link 'Twitter'
 
     fill_in 'username_or_email', :with => 'catarsetest'
@@ -24,5 +18,25 @@ feature "Login Feature" do
     page.should have_link('Catarse Test')
 
   end
+
+  scenario "I'm new to the site and I want to signup with Google" do
+=begin
+    click_login
+    click_link 'Google'
+
+    fill_in 'username_or_email', :with => 'catarsetest'
+    fill_in 'session[password]', :with => 'testcatarse'
+    click_button 'Sign in'
+    
+    click_button 'Allow' if page.has_button?('Allow')
+    
+    current_path.should == homepage
+    
+    page.should have_css('#user')
+    page.should have_link('Catarse Test')
+=end
+  end
+
+end
 
 end
