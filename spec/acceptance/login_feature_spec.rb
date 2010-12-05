3ae81c60b35d9dda66db22779b102d592c4f70ee
Diogo Biazus <diogob@gmail.com>
2010-12-05 18:54:11 -0200

    fixed ugly syntax error. sorry.

--- spec/acceptance/login_feature_spec.rb
+++ spec/acceptance/login_feature_spec.rb
@@ -20,23 +20,7 @@ feature "Login Feature" do
   end
 
   scenario "I'm new to the site and I want to signup with Google" do
-=begin
     click_login
     click_link 'Google'
-
-    fill_in 'username_or_email', :with => 'catarsetest'
-    fill_in 'session[password]', :with => 'testcatarse'
-    click_button 'Sign in'
-    
-    click_button 'Allow' if page.has_button?('Allow')
-    
-    current_path.should == homepage
-    
-    page.should have_css('#user')
-    page.should have_link('Catarse Test')
-=end
   end
-
-end
-
 end
