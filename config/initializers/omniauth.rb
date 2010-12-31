327479b6505d6c7e44d169dc0f9aee9e84386035
Diogo Biazus <diogob@gmail.com>
2010-12-31 12:49:27 -0200

    Put some error handling in omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,12 @@
 require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do  
   provider :open_id, OpenID::Store::Filesystem.new("#{Rails.root}/tmp"), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
-  OauthProvider.all.each do |p|
-    provider p.name.to_sym, p.key, p.secret
+  begin
+    OauthProvider.all.each do |p|
+      provider p.name.to_sym, p.key, p.secret
+    end
+  rescue Exception => e
+    # We should initialize even with errors during providers setup
+    Rails.logger.error "Error while setting omniauth providers: #{e.inspect}"
   end
 end
