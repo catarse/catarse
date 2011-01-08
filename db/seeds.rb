0d7febbf6bea6cf3a59ecabb46bb252acb8f72c4
Daniel Weinmann <danielweinmann@gmail.com>
2011-01-08 19:08:09 -0200

    Changes in the project form and show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -25,3 +27,4 @@ ActiveRecord::Base.connection.execute "INSERT INTO categories (name) VALUES ('Ar
  ('Tecnologia'),
  ('Teatro'),
  ('Literatura');"
+
