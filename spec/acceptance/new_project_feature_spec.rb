7493106da10c2bd6f8e82c9c03d7e64555175202
Diogo Biazus <diogob@gmail.com>
2011-01-14 12:10:52 -0200

    removed transactional fixtures. now all the acceptance tests are working fine. We also have a complete new project spec

--- spec/acceptance/new_project_feature_spec.rb
+++ spec/acceptance/new_project_feature_spec.rb
@@ -7,6 +7,7 @@ feature "New Project Feature" do
     find("#login").visible?.should be_true
   end
   scenario "I am logged in and I want to send a project" do
+    c = Factory(:category) 
     visit homepage
     fake_login
     click_link 'Envie seu projeto'
@@ -31,6 +32,20 @@ feature "New Project Feature" do
       end
     end
     within '#content' do
+      fill_in 'project_name', :with => 'test project'
+      fill_in 'project_video_url', :with => 'http://vimeo.com/18210052'
+      fill_in 'project_about', :with => 'about this very cool project'
+      fill_in 'project_headline', :with => 'this is our nice headline'
+      fill_in 'project_goal', :with => '1000'
+      fill_in 'project_expires_at', :with => '21/12/2012'
+      fill_in 'project_rewards_attributes_0_description', :with => 'this is an exciting reward'
+      fill_in 'project_rewards_attributes_0_minimum_value', :with => '10'
+      select c.name, :from => 'project_category_id'
+      check 'accept'
+      click_button 'project_submit'
     end
+    p = Project.all.first
+    p.name.should == 'test project'
+    page.should have_content("test project")
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,7 +23,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
