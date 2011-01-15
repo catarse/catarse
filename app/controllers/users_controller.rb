19d5047f6116fad4d1e5d1dbfdeb66a3e0806831
Daniel Weinmann <danielweinmann@gmail.com>
2011-01-14 22:29:35 -0200

    Redirecting to primary if existent on show user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   can_edit_on_the_spot
   def show
     show!{
+      return redirect_to(user_path(@user.primary)) if @user.primary
       @title = "#{@user.display_name}"
       @backs = @user.backs.confirmed.order(:confirmed_at)
       @projects = @user.projects.order("updated_at DESC")
