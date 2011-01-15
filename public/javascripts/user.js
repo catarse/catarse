4b785a1e0a5b44942797a58dfa6d7e68a54005e6
Daniel Weinmann <danielweinmann@gmail.com>
2011-01-15 12:58:45 -0200

    Adding preventDefault() to #user_menu_link click

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -1,4 +1,5 @@
-$('#user').click(function(){
+$('#user_menu_link').click(function(e){
+  e.preventDefault()
   if($('#user_menu').is(':hidden')) {
     $('#user_menu').show()
     if($('#user_menu').css('width') > $('#user_menu_link').css('width')) {
