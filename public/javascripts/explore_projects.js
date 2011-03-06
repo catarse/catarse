311ced47c9153644d3179becfba3f080a3a56989
Daniel Weinmann <danielweinmann@gmail.com>
2011-03-06 19:53:03 -0300

    Making the explore project page work on IE

--- public/javascripts/explore_projects.js
+++ public/javascripts/explore_projects.js
@@ -2,9 +2,8 @@ $('#explore_quick a').click(function(e){
   e.preventDefault()
   $('a.selected').removeClass('selected')
   $(this).addClass('selected')
-  id = /^menu_(\w+)$/.exec($(this).attr('id'))
+  var id = /^menu_(\w+)$/.exec($(this).attr('id'))
   id = id[1]
-  console.log(id)
   $('#explore_results .results').hide()
   $('#explore_'+id).show()
 })
@@ -12,7 +11,7 @@ $('#explore_categories a').click(function(e){
   e.preventDefault()
   $('a.selected').removeClass('selected')
   $(this).addClass('selected')
-  category = $(this).html()
+  var category = $(this).html()
   $('#explore_results .results').hide()
   $('#explore_all .project_box').show()
   $('#explore_all .project_category').each(function(){
