1da242f7d11da458059d1d748a1c8b0209acbb16
Daniel Weinmann <danielweinmann@gmail.com>
2011-03-11 08:59:28 -0300

    Making MoIP initializer create the Configuration records in case they dont exist yet

--- config/initializers/moip.rb
+++ config/initializers/moip.rb
@@ -1,4 +1,11 @@
 MoIP.setup do |config|
-  config.token = Configuration.find_by_name('moip_token').value
-  config.key = Configuration.find_by_name('moip_key').value
+  begin
+    config.token = Configuration.find_by_name('moip_token').value
+    config.key = Configuration.find_by_name('moip_key').value
+  rescue
+    Configuration.create(:name => 'moip_token', :value => '')
+    Configuration.create(:name => 'moip_key', :value => '')
+    config.token = Configuration.find_by_name('moip_token').value
+    config.key = Configuration.find_by_name('moip_key').value
+  end
 end
