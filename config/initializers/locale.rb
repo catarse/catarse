f87fb34279aa9c5f4bdd2e24997a00f1f8139d7e
Daniel Walmsley <goldsounds@gmail.com>
2011-04-21 09:36:53 -0400

    begin translations to English

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,5 +1,6 @@
 # tell the I18n library where to find your translations
 I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+I18n.load_path += Dir[Rails.root.join('config', 'locale', '*.{rb,yml}')]
  
 # set default locale to something other than :en
 I18n.default_locale = :pt
