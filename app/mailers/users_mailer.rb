bda5dc4e74817fa9784c8a1b48d41ca7518b9e88
Daniel Weinmann <danielweinmann@gmail.com>
2011-05-01 18:04:59 -0300

    More internationalization of the views. Almost done, just need to internationalize the site specific views now

--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -1,6 +1,9 @@
 class UsersMailer < ActionMailer::Base
   def notification_email(notification)
     @notification = notification
+    old_locale = I18n.locale
+    I18n.locale = @notification.user.locale
     mail(:from => "\"#{@notification.site.name}\" <#{@notification.site.email}>", :to => @notification.user.email, :subject => @notification.email_subject)
+    I18n.locale = old_locale
   end
 end
