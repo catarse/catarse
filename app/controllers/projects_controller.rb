c68db13aca3fb188af0dec3e369df2e748d1f3d3
Daniel Weinmann <danielweinmann@gmail.com>
2011-06-23 00:53:32 -0300

    Putting a limit on recent and expiring projects on explore

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -31,12 +31,12 @@ class ProjectsController < ApplicationController
   end
   def explore
     @title = t('projects.explore.title')
-    @categories = Category.with_projects(current_site).order(:name)
-    @recommended = current_site.present_projects.visible.recommended.order('created_at DESC')
-    @expiring = current_site.present_projects.visible.expiring.order('expires_at')
-    @recent = current_site.present_projects.visible.recent.order('created_at DESC')
-    @successful = current_site.present_projects.visible.successful.order('expires_at DESC')
-    @all = current_site.present_projects.visible.order('created_at DESC')
+    @categories = Category.with_projects(current_site).order(:name).all
+    @recommended = current_site.present_projects.visible.recommended.order('created_at DESC').all
+    @expiring = current_site.present_projects.visible.expiring.limit(16).order('expires_at').all
+    @recent = current_site.present_projects.visible.recent.limit(16).order('created_at DESC').all
+    @successful = current_site.present_projects.visible.successful.order('expires_at DESC').all
+    @all = current_site.present_projects.visible.order('created_at DESC').all
   end
   def start
     @title = t('projects.start.title')
