fc926362158020a29c41ab3acc417363e6a0e8a2
Paulo Geyer <paulogeyer@gmail.com>
2011-09-01 18:35:44 -0300

    adjusts to project admin form

--- app/admin/projects.rb
+++ app/admin/projects.rb
@@ -4,10 +4,33 @@ ActiveAdmin.register Project do
 
   index do
     column :name do |project|
-      link_to project.title, admin_project_path(project)
+      link_to project.name, admin_project_path(project)
     end
     column :headline
     default_actions
   end
+
+  form do |f|
+    f.inputs do
+      f.input :user
+      f.input :category
+      f.input :name, :as => :string
+      f.input :goal
+      f.input :expires_at
+      f.input :about
+      f.input :headline
+      f.input :video_url, :as => :string
+      f.input :image_url, :as => :string
+      f.input :short_url, :as => :string
+      f.input :can_finish
+      f.input :finished
+      f.input :about_html
+    end
+
+    f.buttons do
+      f.submit
+    end
+  end
+  
 
 end
