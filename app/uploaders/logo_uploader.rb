840c6eae30c51b897d482bcea5e91c74546330d7
Paulo Geyer <paulogeyer@gmail.com>
2011-09-12 02:33:44 -0300

    using rmagick to resize logo image

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 class LogoUploader < CarrierWave::Uploader::Base
+  include CarrierWave::RMagick
 
   if Rails.env.production? and Configuration.find_by_name('aws_access_key')
     storage :s3
@@ -10,5 +11,14 @@ class LogoUploader < CarrierWave::Uploader::Base
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
+
+  version :thumb do
+    process :resize_to_fill => [260,170]
+    process :convert => :png
   end
 end
