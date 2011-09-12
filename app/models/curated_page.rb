d219465e60f4d1751d96adbc5c9e8607d46a36b9
Paulo Geyer <paulogeyer@gmail.com>
2011-09-12 11:20:46 -0300

    validating presence of site for CuratedPage

--- app/models/curated_page.rb
+++ app/models/curated_page.rb
@@ -3,7 +3,7 @@ class CuratedPage < ActiveRecord::Base
   belongs_to :site
 
   validates_uniqueness_of :permalink, :scope => :site_id
-  validates_presence_of :permalink, :name
+  validates_presence_of :permalink, :name, :site
 
   mount_uploader :logo, LogoUploader
 
