8364dbfb1a220affd77fee46c01684d888bd3b3e
Paulo Geyer <paulogeyer@gmail.com>
2011-09-14 15:55:02 -0300

    making easier to add descriptions to projects on curated pages

--- app/admin/projects_curated_pages.rb
+++ app/admin/projects_curated_pages.rb
@@ -1,2 +1,11 @@
 ActiveAdmin.register ProjectsCuratedPage do
+  index do
+    column :project do |pcp|
+      pcp.project.name
+    end
+    column :curated_page do |pcp|
+      pcp.curated_page.name
+    end
+    default_actions
+  end
 end
