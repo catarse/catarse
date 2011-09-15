39840a1002f9cdcfab808fa0454835a4c56ba387
Antonio Roberto <forevertonny@gmail.com>
2011-09-15 15:09:55 -0300

    move acceptance specs to root folder

---
+++ acceptance/logout_feature_spec.rb
@@ -0,0 +1,10 @@
+require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
+feature "Logout Feature" do
+  scenario "Given I'm logged in, I must be able to logout" do
+    fake_login
+    page.should have_link(user.display_name)
+    click_link user.display_name
+    click_link "Sair"
+    page.should have_no_link(user.display_name)
+  end
+end
