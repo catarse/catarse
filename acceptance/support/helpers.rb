3cd5e1b4e1723b41378207bd9dc11407b58e95d5
Antonio Roberto <forevertonny@gmail.com>
2011-09-15 18:50:08 -0300

    acceptance helpers to build admin user and some projects site

--- acceptance/support/helpers.rb
+++ acceptance/support/helpers.rb
@@ -6,16 +6,33 @@ module HelperMethods
     @current_site = Factory(:site, :name => "Catarse", :path => "catarse") unless @current_site
     @current_site
   end
+
   def fake_login
     visit fake_login_path
   end
+
   def user
     User.find_by_uid 'fake_login'
   end
+  alias_method :current_user, :user
+
   def click_login
     visit homepage
     page.should have_no_css('#user')
     click_link 'Entrar'
+  end
+
+  def user_to_admin user
+    user.admin=true
+    user.save
+    user.reload
+  end
+
+  def add_some_projects_to_current_site(num_of_projects=5)
+    num_of_projects.times do |n|
+      project = create(:project, :name => "Foo bar #{n}", :site => current_site)
+      create(:projects_site, :site => current_site, :project => project)
+    end
   end
 end
 RSpec.configuration.include HelperMethods, :type => :acceptance
