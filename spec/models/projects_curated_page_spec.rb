521a016bc90bee32aa3407c9c18fa9146acd2dc1
Paulo Geyer <paulogeyer@gmail.com>
2011-09-28 17:07:46 -0300

    tests for ProjectsCuratedPage model

--- spec/models/projects_curated_page_spec.rb
+++ spec/models/projects_curated_page_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe ProjectsCuratedPage do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a project" do
+    pcp = Factory.build(:projects_curated_page, :project => nil)
+    pcp.should_not be_valid
+  end
+
+  it "should have a curated page" do
+    pcp = Factory.build(:projects_curated_page, :curated_page => nil)
+    pcp.should_not be_valid
+  end
+
+  it "should be valid when associated to a project and a curated page" do
+    pcp = Factory.build(:projects_curated_page)
+    pcp.should be_valid
+  end
 end
