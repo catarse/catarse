d1a8e7bca7f0db37fb7e275f465e541178c3bd87
Antonio Roberto <forevertonny@gmail.com>
2011-09-28 22:22:17 -0300

    annotate

--- spec/models/payment_log_spec.rb
+++ spec/models/payment_log_spec.rb
@@ -4,6 +4,8 @@ describe PaymentLog do
   it { should belong_to(:backer) }
 end
 
+
+
 # == Schema Information
 #
 # Table name: payment_logs
@@ -11,13 +13,13 @@ end
 #  id             :integer         not null, primary key
 #  backer_id      :integer
 #  status         :integer
-#  almost         :float
+#  amount         :float
 #  payment_status :integer
 #  moip_id        :integer
 #  payment_method :integer
-#  payment_type   :integer
 #  consumer_email :string(255)
 #  created_at     :datetime
 #  updated_at     :datetime
+#  payment_type   :string(255)
 #
 
