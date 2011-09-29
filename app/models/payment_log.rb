57217837d4bee14220f25fec0a7d6019368429cc
Antonio Roberto <forevertonny@gmail.com>
2011-09-28 22:22:17 -0300

    annotate

--- app/models/payment_log.rb
+++ app/models/payment_log.rb
@@ -2,6 +2,8 @@ class PaymentLog < ActiveRecord::Base
   belongs_to :backer
 end
 
+
+
 # == Schema Information
 #
 # Table name: payment_logs
@@ -9,13 +11,13 @@ end
 #  id             :integer         not null, primary key
 #  backer_id      :integer
 #  status         :integer
-#  almost         :float
+#  amount         :float
 #  payment_status :integer
 #  moip_id        :integer
 #  payment_method :integer
-#  payment_type   :integer
 #  consumer_email :string(255)
 #  created_at     :datetime
 #  updated_at     :datetime
+#  payment_type   :string(255)
 #
 
