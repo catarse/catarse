dda92b6a46663c6c3a06b8a40d3ccfedcb20ccdd
Antonio Roberto <forevertonny@gmail.com>
2011-09-30 11:05:02 -0300

    move thank_you action to payment_stream controller and add some tests case.

--- spec/controllers/payment_stream_controller_spec.rb
+++ spec/controllers/payment_stream_controller_spec.rb
@@ -1,20 +1,48 @@
 require 'spec_helper'
 describe PaymentStreamController do
-  it "should confirm backer in moip payment" do
-    backer = Factory(:backer, :confirmed => false)
-    post :moip, post_moip_params.merge!({:id_transacao => backer.key, :status_pagamento => '1', :valor => backer.moip_value})
-    response.should be_successful
-    backer.reload.confirmed.should be_true
+  render_views
+
+  describe '/moip' do
+    it "should confirm backer in moip payment" do
+      backer = Factory(:backer, :confirmed => false)
+      post :moip, post_moip_params.merge!({:id_transacao => backer.key, :status_pagamento => '1', :valor => backer.moip_value})
+      response.should be_successful
+      backer.reload.confirmed.should be_true
+    end
+
+    it "should not confirm in case of error in moip payment" do
+      backer = Factory(:backer, :confirmed => false)
+      post :moip, post_moip_params.merge!({:id_transacao => -1, :status_pagamento => '1', :valor => backer.moip_value})
+      response.should_not be_successful
+      backer.reload.confirmed.should_not be_true
+    end
+
+    it "should return 422 when moip params is empty" do
+      post :moip, {}
+      response.should_not be_successful
+      response.code.should == '422'
+    end
   end
-  it "should not confirm in case of error in moip payment" do
-    backer = Factory(:backer, :confirmed => false)
-    post :moip, post_moip_params.merge!({:id_transacao => -1, :status_pagamento => '1', :valor => backer.moip_value})
-    response.should_not be_successful
-    backer.reload.confirmed.should_not be_true
-  end
-  it "should return 422 when moip params is empty" do
-    post :moip, {}
-    response.should_not be_successful
-    response.code.should == '422'
+
+  describe '/thank_you' do
+    it "without project / session, should redirect" do
+      request.session[:thank_you_id]=nil
+      get :thank_you, {:locale => :en}
+      request.flash[:failure].should == I18n.t('payment_stream.thank_you.error')
+      response.should be_redirect
+    end
+
+    it "with project / session, should not redirect" do
+      project=create(:project)
+      request.session[:thank_you_id] = project.id
+      get :thank_you, {:locale => :en}
+
+      response.should render_template("payment_stream/thank_you")
+      response.should be_success
+      response.should_not be_redirect
+      response.body.should =~ /#{I18n.t('payment_stream.thank_you.title')}/
+      response.body.should =~ /#{I18n.t('payment_stream.thank_you.header_title')}/
+      response.body.should =~ /#{I18n.t('payment_stream.thank_you.header_subtitle')}/
+    end
   end
 end
