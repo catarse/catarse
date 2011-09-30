9bc208f253c3cb15346ad3c4008401938cc6185a
Antonio Roberto <forevertonny@gmail.com>
2011-09-30 15:46:07 -0300

    annotate and spec helper methods

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,3 +40,28 @@ def post_moip_params
     :email_consumidor => 'some@email.com'
   }
 end
+
+def moip_query_response
+  {
+    "ID"=>"201109300946542390000012428473", "Status"=>"Sucesso",
+    "Autorizacao"=>{
+      "Pagador"=>{
+        "Nome"=>"Lorem Ipsum", "Email"=>"some@email.com"
+      },
+      "EnderecoCobranca"=>{
+        "Logradouro"=>"Some Address ,999", "Numero"=>"999",
+        "Complemento"=>"Address A", "Bairro"=>"Hello World", "CEP"=>"99999-000",
+        "Cidade"=>"Some City", "Estado"=>"MG", "Pais"=>"BRA",
+        "TelefoneFixo"=>"(31)3666-6666"
+      },
+      "Recebedor"=>{
+        "Nome"=>"Happy Guy", "Email"=>"happy@email.com"
+      },
+      "Pagamento"=>{
+        "Data"=>"2011-09-30T09:33:37.000-03:00", "TotalPago"=>"999.00", "TaxaParaPagador"=>"0.00",
+        "TaxaMoIP"=>"19.37", "ValorLiquido"=>"979.63", "FormaPagamento"=>"BoletoBancario",
+        "InstituicaoPagamento"=>"Itau", "Status"=>"BoletoImpresso", "CodigoMoIP"=>"0000.0728.5285"
+      }
+    }
+  }
+end
