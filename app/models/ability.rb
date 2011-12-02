97efa1f53fe08769be53797bd480929be6466e57
Daniel Weinmann <danielweinmann@gmail.com>
2011-12-02 20:22:14 -0200

    Dynamic json attributes for backers and users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,12 +6,12 @@ class Ability
     
     if user.admin?
       can :manage, :all
-    elsif not user.manages_projects.empty?
+    elsif user.projects.present? or user.manages_projects.present?
       can :manage, Project do |project|
-        user.manages_projects.include? project
+        user.manages_projects.include?(project) or project.user == user
       end
       can :manage, Reward do |reward|
-        user.manages_projects.include? reward.project
+        user.manages_projects.include?(reward.project) or reward.project.user == user
       end      
     else
       can :read, :all
