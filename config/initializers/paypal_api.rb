fda5a10e6ae5508d10081056baf8cf47f7a93d72
Antonio Roberto <forevertonny@gmail.com>
2011-12-09 00:49:36 -0200

    added class to get some transaction details from paypal api

--- config/initializers/paypal_api.rb
+++ config/initializers/paypal_api.rb
@@ -1,5 +1,7 @@
-PaypalApi.configure do |config|
-  config.username   = Configuration.find_by_name('paypal_username').value
-  config.password   = Configuration.find_by_name('paypal_password').value
-  config.signature  = Configuration.find_by_name('paypal_signature').value
+unless Rails.env == 'test'
+  PaypalApi.configure do |config|
+    config.username   = Configuration.find_by_name('paypal_username').value
+    config.password   = Configuration.find_by_name('paypal_password').value
+    config.signature  = Configuration.find_by_name('paypal_signature').value
+  end
 end
