fa54b577647d79b899ceed84103b56628d3b1fc8
Antonio Roberto <forevertonny@gmail.com>
2011-12-19 21:50:09 -0200

    adjust routes instead to use back action use backers new

--- app/controllers/projects/backers_controller.rb
+++ app/controllers/projects/backers_controller.rb
@@ -26,6 +26,19 @@ class Projects::BackersController < ApplicationController
     end
   end
 
+  def checkout
+    @title = t('projects.review.title')
+    params[:backer][:reward_id] = nil if params[:backer][:reward_id] == '0'
+    params[:backer][:user_id] = current_user.id
+    @project = Project.find params[:id]
+    @backer = @project.backers.new(params[:backer])
+    unless @backer.save
+      flash[:failure] = t('projects.review.error')
+      return redirect_to new_project_backer_path(@project)
+    end
+    session[:thank_you_id] = @project.id
+  end
+
   private
 
   def load_project
