5e8859db3619de11fed1d3a26f840ebf8e0d044a
Juan Maiz <juanmaiz@gmail.com>
2011-12-21 14:32:26 -0200

    changed the way titles works - needs full project refactoring

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,19 +1,4 @@
 class StaticController < ApplicationController
-  def guidelines
-    @title = t('static.guidelines.title')
-  end
-
-  def faq
-    @title = t('static.faq.title')
-  end
-
-  def terms
-    @title = t('static.terms.title')
-  end
-
-  def privacy
-    @title = t('static.privacy.title')
-  end
 
   def sitemap
     @home_page = Project.includes(:user, :category).visible.home_page.limit(6).order('"order"').all
@@ -21,4 +6,5 @@ class StaticController < ApplicationController
     @recent = Project.includes(:user, :category).visible.not_home_page.not_expiring.not_successful.not_unsuccessful.where("projects.user_id <> 7329").order('created_at DESC').limit(3).all
     @successful = Project.includes(:user, :category).visible.not_home_page.successful.order('expires_at DESC').limit(3).all
   end
+
 end
