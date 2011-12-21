3959d0a4bc33e8549451c127d81991cdc8f10b4a
Juan Maiz <juanmaiz@gmail.com>
2011-12-21 13:36:28 -0200

    finished refactoring tests in order to avoid internet connections

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -14,4 +14,12 @@ class StaticController < ApplicationController
   def privacy
     @title = t('static.privacy.title')
   end
+
+  def sitemap
+    @home_page = Project.includes(:user, :category).visible.home_page.limit(6).order('"order"').all
+    @expiring = Project.includes(:user, :category).visible.expiring.not_home_page.not_successful.not_unsuccessful.order('expires_at, created_at DESC').limit(3).all
+    @recent = Project.includes(:user, :category).visible.not_home_page.not_expiring.not_successful.not_unsuccessful.where("projects.user_id <> 7329").order('created_at DESC').limit(3).all
+    @successful = Project.includes(:user, :category).visible.not_home_page.successful.order('expires_at DESC').limit(3).all
+
+  end
 end
