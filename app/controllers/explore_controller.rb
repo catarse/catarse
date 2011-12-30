96a1511422b588d570c47c3654ddb7cdb3d4ce24
Antonio Roberto <forevertonny@gmail.com>
2011-12-29 23:01:53 -0200

    instead use .all use .length because the view use whe instace variable only to verify

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -3,10 +3,10 @@ class ExploreController < ApplicationController
   def index
     @title = t('explore.title')
     @categories = Category.with_projects.order(:name).all
-    @recommended = Project.visible.not_expired.recommended.order('expires_at').all
-    @expiring = Project.visible.expiring.limit(16).order('expires_at').all
-    @recent = Project.visible.recent.not_expired.limit(16).order('created_at DESC').all
-    @successful = Project.visible.successful.order('expires_at DESC').all
+    @recommended = (Project.visible.not_expired.recommended.order('expires_at').length > 0)
+    @expiring = (Project.visible.expiring.limit(16).order('expires_at').length > 0)
+    @recent = (Project.visible.recent.not_expired.limit(16).order('created_at DESC').length > 0)
+    @successful = (Project.visible.successful.order('expires_at DESC').length > 0)
   end
 
 end
