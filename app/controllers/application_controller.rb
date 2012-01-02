570aec1925bafb2552f908ac25b797e6a1ea9411
Antonio Roberto <forevertonny@gmail.com>
2012-01-02 17:17:08 -0200

    with slim attributes can't be a empty string, change to null.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,10 @@ class ApplicationController < ActionController::Base
 
   def namespace
     names = self.class.to_s.split('::')
-    return "" if names.length < 2
+    return "null" if names.length < 2
     names[0..(names.length-2)].map(&:downcase).join('_')
   end
-  
+
   def is_homepage?
     controller_name == 'projects' && action_name == 'index'
   end
