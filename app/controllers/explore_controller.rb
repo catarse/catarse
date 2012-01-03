2423f16df9ea5c17ff505e15af7ef9fb40cba6a0
Antonio Roberto <forevertonny@gmail.com>
2012-01-03 09:05:03 -0200

    adjust explore home and search

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -4,10 +4,10 @@ class ExploreController < ApplicationController
   def index
     @title = t('explore.title')
     @categories = Category.with_projects.order(:name).all
-    @recommended = (Project.visible.not_expired.recommended.order('expires_at').length > 0)
-    @expiring = (Project.visible.expiring.limit(16).order('expires_at').length > 0)
-    @recent = (Project.visible.recent.not_expired.limit(16).order('created_at DESC').length > 0)
-    @successful = (Project.visible.successful.order('expires_at DESC').length > 0)
+    @recommended = Project.visible.not_expired.recommended.order('expires_at').limit(3)
+    @expiring = Project.visible.expiring.limit(3).order('expires_at')
+    @recent = Project.visible.recent.not_expired.limit(3).order('created_at DESC')
+    @successful = Project.visible.successful.order('expires_at DESC').limit(3)
   end
 
 end
