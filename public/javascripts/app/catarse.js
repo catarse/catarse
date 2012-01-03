18c16ddf0cd8a9eeb08506396a41ca650c5fe089
Daniel Weinmann <danielweinmann@gmail.com>
2012-01-02 22:58:04 -0200

    Refactoring projects#show javascripts

--- public/javascripts/app/catarse.js
+++ public/javascripts/app/catarse.js
@@ -3,8 +3,25 @@ var CATARSE = {
   loader: CATARSE_LOADER,
   locale: $(document.body).data("locale"),
   currentUser: $(document.body).data("user"),
+      
+  requireLogin: function(event, customUrl){
+    event.preventDefault()
+    var url = null
+    if(typeof(customUrl) != 'undefined') {
+      url = customUrl
+    } else {
+      if($(this).is('a')){
+        url = $(this).attr('href')
+      } else {
+        url = $(this).parentsUntil('form').parent().attr('action')
+      }
+    }
+    $('#return_to').val(url)
+    $('#login_overlay').show()
+    $('#login').fadeIn()
+  },
   
-  common:{
+  common: {
     init: function(){
       // Common init for every action
       CATARSE.router = new CATARSE.Router()
