929e2645f492773e6a8029fc094bff88a109f559
Daniel Weinmann <danielweinmann@gmail.com>
2012-01-04 10:12:21 -0200

    A few tweaks on explore

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,31 +1,24 @@
 class StaticController < ApplicationController
 
   def new_home
-    render :layout => 'redesign'
   end
 
   def new_blog
-    render :layout => 'redesign'
   end
 
   def new_profile
-    render :layout => 'redesign'
   end
 
   def new_project_profile
-    render :layout => 'redesign'
   end
 
   def new_discover
-    render :layout => 'redesign'
   end
 
   def new_payment
-    render :layout => 'redesign'
   end
 
   def new_opendata
-    render :layout => 'redesign'
   end
 
   def guidelines
