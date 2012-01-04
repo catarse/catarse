05caa530aee9a8baba31712e65cc5ef8c7cec5e1
Paulo Geyer <paulogeyer@gmail.com>
2012-01-04 19:05:37 -0200

    adjusts for user profile contents, and content selection

--- public/javascripts/app/views/users/show.js
+++ public/javascripts/app/views/users/show.js
@@ -1,16 +1,38 @@
 CATARSE.UsersShowView = Backbone.View.extend({
 
 	initialize: function() {
+		_.bindAll(this, "index", "backs", "projects", "credits")
 		CATARSE.router.route("", "index", this.index)
 		CATARSE.router.route("backs", "backs", this.backs)
+		CATARSE.router.route("projects", "projects", this.projects)
+		CATARSE.router.route("credits", "credits", this.credits)
 	},
-	
+
 	index: function() {
+		this.backs()
+    CATARSE.router.navigate("backs")
 	},
-	
+
 	backs: function() {
-		
+		this.selectItem("backed_projects")
+	},
+
+	projects: function() {
+		this.selectItem("created_projects")
+	},
+
+	credits: function() {
+		this.selectItem("credits")
+	},
+
+	selectItem: function(item) {
+		this.$("#user_profile_content .content").hide()
+		this.$("#user_profile_content #user_" + item + ".content").show()
+		var link = this.$("#user_profile_menu #" + item + "_link")
+		link.parent().children().removeClass('selected')
+    link.addClass('selected')
 	}
+
 })
 
 $('input,textarea').live('keypress', function(e){
@@ -18,6 +40,7 @@ $('input,textarea').live('keypress', function(e){
     e.preventDefault();
   }
 })
+
 $('#user_feed input').live('keyup', function(){
   var value = $(this).val()
   var re = /^[a-z0-9\._-]+@([a-z0-9][a-z0-9-_]*[a-z0-9-_]\.)+([a-z-_]+\.)?([a-z-_]+)$/
@@ -29,6 +52,7 @@ $('#user_feed input').live('keyup', function(){
     $("button:contains('OK')").attr('disabled', true)
   }
 })
+
 $('#content_header textarea').live('keyup', function(){
   var value = $(this).val()
   if(value.length <= 140){
@@ -39,6 +63,7 @@ $('#content_header textarea').live('keyup', function(){
     $("button:contains('OK')").attr('disabled', true)
   }
 })
+
 $('input[type=checkbox]').click(function(){
   $.post('/users/update_attribute_on_the_spot', {
     id: 'user__' + $(this).attr('id') + '__' + $('#id').val(),
