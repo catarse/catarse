2db3770a9ec5147369444de0f3af75df5d709f93
Paulo Geyer <paulogeyer@gmail.com>
2012-01-11 17:08:42 -0200

    using cancan to check permissions for user profile

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,19 +2,21 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
+  def initialize(current_user)
     user ||= User.new
     
     if user.admin?
       can :manage, :all
-    elsif user.projects.present? or user.manages_projects.present?
+    elsif current_user.projects.present? or current_user.manages_projects.present?
+      can :manage, User, :id => current_user.id
       can :manage, Project do |project|
-        user.manages_projects.include?(project) or project.user == user
+        current_user.manages_projects.include?(project) or project.user == current_user
       end
       can :manage, Reward do |reward|
-        user.manages_projects.include?(reward.project) or reward.project.user == user
+        current_user.manages_projects.include?(reward.project) or reward.project.user == current_user
       end      
     else
+      can :manage, User, :id => current_user.id
       can :read, :all
     end
   end
