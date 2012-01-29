7470027cefefb568a66f186e686517494e5fca14
Antonio Roberto <forevertonny@gmail.com>
2012-01-29 18:01:37 -0200

    change devise error redirect to include custom param

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,9 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.warden do |manager|
+    manager.failure_app = CustomFailure
+  end
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
