f497f10612f87bfa1505b67093d1e4f170dd99d1
Antonio Roberto <forevertonny@gmail.com>
2012-01-29 18:14:13 -0200

    login, register and forgot password forms moved to login modal

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -39,5 +39,69 @@ $('a.provider').click(function(e){
 
 $("#login_with_another_social").click(function(e){
   e.preventDefault();
-  $('.another_social').slideDown('fast');
+  if($('#login_form_with_email').css('display') == 'block') {
+    $('#login_form_with_email').slideUp('fast', function(){
+      $('.another_social').slideDown('fast');
+    })
+  } else if ($('#new_email_account').css('display') == 'block') {
+    $('#new_email_account').slideUp('fast', function(){
+      $('.another_social').slideDown('fast');
+    })
+  } else if ($('#forgot_password_form').css('display') == 'block') {
+    $('#forgot_password_form').slideUp('fast', function(){
+      $('.another_social').slideDown('fast');
+    })
+  } else {
+    $('.another_social').slideDown('fast');
+  }
+});
+
+$("#login_with_mail").click(function(e){
+  e.preventDefault();
+  if($('.another_social').css('display') == 'block') {
+    $('.another_social').slideUp('fast', function(){
+      $('#login_form_with_email').slideDown('fast');
+    })
+  } else {
+    $('#login_form_with_email').slideDown('fast');
+  }
+});
+
+$('a.new_registration_link').click(function(e){
+  e.preventDefault();
+  if($('#login_form_with_email').css('display') == 'block') {
+    $('#login_form_with_email').slideUp('fast', function(){
+      $('#new_email_account').slideDown('fast');
+    });
+  } else if($('#forgot_password_form').css('display') == 'block') {
+    $('#forgot_password_form').slideUp('fast', function(){
+      $('#new_email_account').slideDown('fast');
+    });
+  } else { $('#new_email_account').slideDown('fast'); }
+});
+
+$('a.new_session_link').click(function(e){
+  e.preventDefault();
+  if($('#new_email_account').css('display') == 'block') {
+    $('#new_email_account').slideUp('fast', function(){
+      $('#login_form_with_email').slideDown('fast');
+    });
+  } else if($('#forgot_password_form').css('display') == 'block') {
+    $('#forgot_password_form').slideUp('fast', function(){
+      $('#login_form_with_email').slideDown('fast');
+    });
+  } else { $('#login_form_with_email').slideDown('fast'); }
+});
+
+$('a.forgot_password_link').click(function(e){
+  e.preventDefault();
+  if($('#new_email_account').css('display') == 'block') {
+    $('#new_email_account').slideUp('fast', function(){
+      $('#forgot_password_form').slideDown('fast');
+    });
+  } else if($('#login_form_with_email').css('display') == 'block') {
+    $('#login_form_with_email').slideUp('fast', function(){
+      $('#forgot_password_form').slideDown('fast');
+    });
+  } else { $('#forgot_password_form').slideDown('fast'); }
 });
