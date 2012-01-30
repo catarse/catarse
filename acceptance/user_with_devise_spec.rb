9ff94777f6e7385a55b723a7b1b8d947f8b0fd29
Antonio Roberto <forevertonny@gmail.com>
2012-01-30 15:35:14 -0200

    some style adjust and test the login modal

--- acceptance/user_with_devise_spec.rb
+++ acceptance/user_with_devise_spec.rb
@@ -1,15 +1,70 @@
 require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
 
-feature "Devise" do
-
-  scenario "I open the login modal and click in login with email" do
+feature "with login modal" do
+  scenario "Login with user that use another provider" do
     click_login
     verify_translations
     find("#login").visible?.should be_true
     find("a#login_with_mail").click
 
-    verify_translations
+    user_facebook = Factory.create(:user, :provider => 'facebook', :uid => '1234566',:email => 'lorem@lorem.com', :password => 'somepassword', :password_confirmation => 'somepassword')
+    fill_in 'user_email', :with => user_facebook.email
+    fill_in 'user_password', :with => user_facebook.password
+    click_button 'user_submit'
+
+    page.should have_no_css('#user')
+    page.should have_css('.alert.wrapper')
   end
+
+  scenario "Login with devise user" do
+    click_login
+    verify_translations
+    find("#login").visible?.should be_true
+    find("a#login_with_mail").click
+
+    user = Factory.create(:user, :provider => 'devise',:email => 'lorem@lorem.com', :password => '123lorem', :password_confirmation => '123lorem')
+    fill_in 'user_email', :with => user.email
+    fill_in 'user_password', :with => user.password
+    click_button 'user_submit'
+
+    page.should have_css('#user')
+    page.should have_link(user.name)
+  end
+
+  scenario "Register new user account" do
+    click_login
+    verify_translations
+    find("#login").visible?.should be_true
+    find("a#login_with_mail").click
+    find("a.new_registration_link").click
+    verify_translations
+
+    fill_in 'user_email', :with => 'lorem@lorem.com'
+    fill_in 'user_password', :with => '123lorem'
+    fill_in 'user_password_confirmation', :with => '123lorem'
+
+    click_button 'user_submit'
+    page.should have_css('#user')
+  end
+
+  scenario "Request new password" do
+    ActionMailer::Base.deliveries.should be_empty
+
+    user = Factory.create(:user, :provider => 'devise',:email => 'lorem@lorem.com', :password => '123lorem', :password_confirmation => '123lorem')
+    click_login
+    verify_translations
+    find("#login").visible?.should be_true
+    find("a#login_with_mail").click
+    find("a.forgot_password_link").click
+    verify_translations
+    fill_in 'user_email', :with => user.email
+    click_button 'user_submit'
+
+    ActionMailer::Base.deliveries.should_not be_empty
+  end
+end
+
+feature "with devise routes" do
 
   scenario "Try login with email that another provider" do
     visit new_user_session_path
@@ -49,6 +104,7 @@ feature "Devise" do
   end
 
   scenario "Request new password" do
+    ActionMailer::Base.deliveries = []
     ActionMailer::Base.deliveries.should be_empty
 
     user = Factory.create(:user, :provider => 'devise',:email => 'lorem@lorem.com', :password => '123lorem', :password_confirmation => '123lorem')
