a736bd46fce41ea005d58dfccc93f0df1beccba6
Antonio Roberto <forevertonny@gmail.com>
2012-02-08 10:46:58 -0200

    move project index logic to presenter

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -20,45 +20,18 @@ class ProjectsController < ApplicationController
     index! do |format|
       format.html do
         @title = t("site.title")
-        collection_projects = Project.includes(:user, :category).
-                                  with_homepage_comment.
-                                  recommended.
-                                  visible.
-                                  not_expired.
-                                  order('"order"')
-
-        unless collection_projects.empty?
-          home_page_projects = collection_projects.home_page
-          if current_user and current_user.recommended_project
-            @recommended_project = current_user.recommended_project
-            home_page_projects = home_page_projects.where("id != #{current_user.recommended_project.id}")
-            collection_projects = collection_projects.where("id != #{current_user.recommended_project.id}")
-          end
-          category_projects = collection_projects
-          category_projects = category_projects.where("category_id != #{@recommended_project.category_id}") if @recommended_project
-          @project_of_day = home_page_projects.first
-          category_projects = category_projects.where("id != #{@project_of_day.id}") if @project_of_day
-          @second_project = category_projects.all[rand(category_projects.length)]
-          category_projects = category_projects.where("category_id != #{@second_project.category_id}") if @second_project
-          @third_project = category_projects.all[rand(category_projects.length)]
-          unless @recommended_project
-            category_projects = category_projects.where("category_id != #{@third_project.category_id}") if @third_project
-            @fourth_project = category_projects.all[rand(category_projects.length)]
-          end
-        end
-
-        project_ids = []
-        project_ids << @recommended_project.id if @recommended_project
-        project_ids << @project_of_the_day.id if @project_of_the_day
-        project_ids << @second_project.id if @second_project
-        project_ids << @third_project.id if @third_project
-        project_ids << @fourth_project.id if @fourth_project
-        project_ids = project_ids.join(',')
-        project_ids = "id NOT IN (#{project_ids})" unless project_ids.blank?
-
-        @expiring = Project.includes(:user, :category).where(project_ids).visible.expiring.not_expired.order('date(expires_at), random()').limit(3).all
-        @recent = Project.includes(:user, :category).where(project_ids).recent.visible.not_expiring.not_expired.order('date(created_at), random()').limit(3).all
-        @curated_pages = CuratedPage.visible.order("created_at desc").limit(6)
+        presenter = ProjectPresenter::Home.new({:current_user => current_user})
+        presenter.fetch_projects        
+        
+        @recommended_project  = presenter.recommended_project
+        @project_of_day       = presenter.project_of_day
+        @second_project       = presenter.second_project
+        @third_project        = presenter.third_project
+        @fourth_project       = presenter.fourth_project
+        @expiring             = presenter.expiring
+        @recend               = presenter.recent
+        
+        @curated_pages = CuratedPage.visible.order("created_at desc").limit(6)            
         @last_tweet = Rails.cache.fetch('last_tweet', :expires_in => 30.minutes) do
             JSON.parse(Net::HTTP.get(URI("http://api.twitter.com/1/statuses/user_timeline.json?screen_name=#{t('site.twitter')}"))).first
         end
---
+++ app/presenters/project_presenter/home.rb
@@ -0,0 +1,57 @@
+module ProjectPresenter
+  class Home
+    
+    attr_accessor :recommended_project, :project_of_day, :second_project, 
+                  :third_project, :fourth_project, :expiring, :recent, :current_user
+    
+    
+    def initialize(options)
+      @current_user ||= options[:current_user]
+    end
+    
+    def current_user
+      @current_user
+    end
+    
+    def fetch_projects
+      collection_projects = Project.includes(:user, :category).
+                                    with_homepage_comment.
+                                    recommended.
+                                    visible.
+                                    not_expired.
+                                    order('"order"')
+          
+      unless collection_projects.empty?
+        home_page_projects = collection_projects.home_page
+        if current_user and current_user.recommended_project
+          @recommended_project = current_user.recommended_project
+          home_page_projects = home_page_projects.where("id != #{current_user.recommended_project.id}")
+          collection_projects = collection_projects.where("id != #{current_user.recommended_project.id}")
+        end
+        category_projects = collection_projects
+        category_projects = category_projects.where("category_id != #{@recommended_project.category_id}") if @recommended_project
+        @project_of_day = home_page_projects.first
+        category_projects = category_projects.where("id != #{@project_of_day.id}") if @project_of_day
+        @second_project = category_projects.all[rand(category_projects.length)]
+        category_projects = category_projects.where("category_id != #{@second_project.category_id}") if @second_project
+        @third_project = category_projects.all[rand(category_projects.length)]
+        unless @recommended_project
+          category_projects = category_projects.where("category_id != #{@third_project.category_id}") if @third_project
+          @fourth_project = category_projects.all[rand(category_projects.length)]
+        end
+      end
+      
+      project_ids = []
+      project_ids << @recommended_project.id if @recommended_project
+      project_ids << @project_of_the_day.id if @project_of_the_day
+      project_ids << @second_project.id if @second_project
+      project_ids << @third_project.id if @third_project
+      project_ids << @fourth_project.id if @fourth_project
+      project_ids = project_ids.join(',')
+      project_ids = "id NOT IN (#{project_ids})" unless project_ids.blank?
+      
+      @expiring = Project.includes(:user, :category).where(project_ids).visible.expiring.not_expired.order('date(expires_at), random()').limit(3).all
+      @recent = Project.includes(:user, :category).where(project_ids).recent.visible.not_expiring.not_expired.order('date(created_at), random()').limit(3).all
+    end    
+  end
+end
