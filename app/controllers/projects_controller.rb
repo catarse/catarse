1a005adbb792772babcf5876f90449c8a0bc5cb1
Antonio Roberto <forevertonny@gmail.com>
2012-02-10 12:10:35 -0200

    whitespace

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 class ProjectsController < ApplicationController
-  
+
   include ActionView::Helpers::DateHelper
   inherit_resources
   actions :index, :show, :new, :create
@@ -10,7 +10,7 @@ class ProjectsController < ApplicationController
   skip_before_filter :detect_locale, :only => [:backers]
   before_filter :can_update_on_the_spot?, :only => :update_attribute_on_the_spot
   before_filter :date_format_convert, :only => [:create]
-  
+
   def date_format_convert
     # TODO localize here and on the datepicker on project_form.js
     params["project"]["expires_at"] = Date.strptime(params["project"]["expires_at"], '%d/%m/%Y')
@@ -22,7 +22,7 @@ class ProjectsController < ApplicationController
         @title = t("site.title")
         presenter = ProjectPresenter::Home.new({:current_user => current_user})
         presenter.fetch_projects
-        
+
         @recommended_project  = presenter.recommended_project
         @project_of_day       = presenter.project_of_day
         @second_project       = presenter.second_project
@@ -30,8 +30,8 @@ class ProjectsController < ApplicationController
         @fourth_project       = presenter.fourth_project
         @expiring             = presenter.expiring
         @recend               = presenter.recent
-        
-        @curated_pages = CuratedPage.visible.order("created_at desc").limit(6)            
+
+        @curated_pages = CuratedPage.visible.order("created_at desc").limit(6)
         @last_tweet = Rails.cache.fetch('last_tweet', :expires_in => 30.minutes) do
             JSON.parse(Net::HTTP.get(URI("http://api.twitter.com/1/statuses/user_timeline.json?screen_name=#{t('site.twitter')}"))).first
         end
@@ -42,18 +42,18 @@ class ProjectsController < ApplicationController
       end
     end
   end
-  
+
   def start
     @title = t('projects.start.title')
   end
-  
+
   def send_mail
     current_user.update_attribute :email, params[:contact] if current_user.email.nil?
     ProjectsMailer.start_project_email(params[:about], params[:rewards], params[:links], params[:contact], current_user, "#{I18n.t('site.base_url')}#{user_path(current_user)}").deliver
     flash[:success] = t('projects.send_mail.success')
     redirect_to :root
   end
-  
+
   def new
     return unless require_login
     new! do
@@ -61,7 +61,7 @@ class ProjectsController < ApplicationController
       @project.rewards.build
     end
   end
-  
+
   def create
     params[:project][:expires_at] += (23.hours + 59.minutes + 59.seconds) if params[:project][:expires_at]
     validate_rewards_attributes if params[:project][:rewards_attributes].present?
@@ -73,7 +73,7 @@ class ProjectsController < ApplicationController
       @project.update_attribute :short_url, bitly
     end
   end
-  
+
   def show
     show!{
       @title = @project.name
@@ -82,7 +82,7 @@ class ProjectsController < ApplicationController
       fb_admins_add(@project.user.facebook_id) if @project.user.facebook_id
     }
   end
-  
+
   def vimeo
     project = Project.new(:video_url => params[:url])
     if project.vimeo.info
@@ -91,7 +91,7 @@ class ProjectsController < ApplicationController
       render :json => {:id => false}.to_json
     end
   end
-  
+
   def cep
     address = BuscaEndereco.por_cep(params[:cep])
     render :json => {
@@ -123,15 +123,15 @@ class ProjectsController < ApplicationController
     @search = Project.search(params[:search])
     @projects = @search.order('projects.created_at DESC').page(params[:page])
   end
-  
+
   def pending_backers
     return unless require_admin
     @title = t('projects.pending_backers.title')
     @search = Backer.search(params[:search])
     @backers = @search.order("created_at DESC").page(params[:page])
   end
-  
-  
+
+
   private
 
   # Just to fix a minor bug,
@@ -150,7 +150,7 @@ class ProjectsController < ApplicationController
     data = JSON.parse(res.body)['data']
     data['url'] if data
   end
-  
+
   def can_update_on_the_spot?
     project_fields = []
     project_admin_fields = ["name", "about", "headline", "can_finish", "expires_at", "user_id", "image_url", "video_url", "visible", "rejected", "recommended", "home_page", "order"]
@@ -176,5 +176,5 @@ class ProjectsController < ApplicationController
       return render_error unless current_user.admin
     end
   end
-  
+
 end
