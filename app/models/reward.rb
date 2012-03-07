9785286f4af666fd700a4f6aae22da3acf09eaac
Antonio Roberto <forevertonny@gmail.com>
2012-03-07 18:00:55 -0300

    use html_safe instead h

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -22,7 +22,7 @@ class Reward < ActiveRecord::Base
     I18n.t('reward.display_remaining', :remaining => remaining, :maximum => maximum_backers)
   end
   def name
-    "<div class='reward_minimum_value'>#{(minimum_value > 0 ? display_minimum+'+' : I18n.t('reward.dont_want'))}</div><div class='reward_description'>#{h(description)}</div>#{'<div class="sold_out">' + I18n.t('reward.sold_out') + '</div>' if sold_out?}<div class='clear'></div>".html_safe
+    "<div class='reward_minimum_value'>#{(minimum_value > 0 ? display_minimum+'+' : I18n.t('reward.dont_want'))}</div><div class='reward_description'>#{(description.html_safe)}</div>#{'<div class="sold_out">' + I18n.t('reward.sold_out') + '</div>' if sold_out?}<div class='clear'></div>".html_safe
   end
   def display_minimum
     number_to_currency minimum_value, :unit => 'R$', :precision => 2, :delimiter => '.'
