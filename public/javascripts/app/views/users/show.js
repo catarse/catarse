2d9f5bbebdf453fb400f4e5945054a69735eaf3f
Antonio Roberto <forevertonny@gmail.com>
2012-03-12 03:43:14 -0300

    implemented my backs view in profile

--- public/javascripts/app/views/users/show.js
+++ public/javascripts/app/views/users/show.js
@@ -1,15 +1,15 @@
 CATARSE.UsersShowView = Backbone.View.extend({
 
-	initialize: function() {
-		_.bindAll(this, "index", "backs", "projects", "credits", "comments", "request_refund")
-		CATARSE.router.route("", "index", this.index)
-		CATARSE.router.route("backs", "backs", this.backs)
-		CATARSE.router.route("projects", "projects", this.projects)
-		CATARSE.router.route("credits", "credits", this.credits)
-		CATARSE.router.route("comments", "comments", this.comments)
-		CATARSE.router.route("request_refund/:back_id", "request_refund", this.request_refund)
-		this.render()
-	},
+  initialize: function() {
+    _.bindAll(this, "index", "backs", "projects", "credits", "comments", "request_refund")
+    CATARSE.router.route("", "index", this.index)
+    CATARSE.router.route("backs", "backs", this.backs)
+    CATARSE.router.route("projects", "projects", this.projects)
+    CATARSE.router.route("credits", "credits", this.credits)
+    CATARSE.router.route("comments", "comments", this.comments)
+    CATARSE.router.route("request_refund/:back_id", "request_refund", this.request_refund)
+    this.render()
+  },
 
   user: new CATARSE.User($('#user_profile').data("user")),
 
@@ -18,71 +18,70 @@ CATARSE.UsersShowView = Backbone.View.extend({
   }),
 
   BacksView: CATARSE.PaginatedView.extend({
-  	emptyTemplate: _.template(this.$('#empty_user_back_template').html()),
-		afterUpdate: function() {
-			FB.XFBML.parse()
-		}
+    emptyTemplate: _.template(this.$('#empty_user_back_template').html()),
+    afterUpdate: function() {
+      FB.XFBML.parse()
+    }
   }),
 
   ProjectView: CATARSE.ModelView.extend({
     template: _.template(this.$('#user_project_template').html())
   }),
-  
+
   ProjectsView: CATARSE.PaginatedView.extend({
-  	emptyTemplate: _.template(this.$('#empty_user_project_template').html()),
-		afterUpdate: function() {
-			FB.XFBML.parse()
-		}
+    emptyTemplate: _.template(this.$('#empty_user_project_template').html()),
+    afterUpdate: function() {
+      FB.XFBML.parse()
+    }
   }),
 
-	index: function() {
-		this.backs()
-    CATARSE.router.navigate("backs")
-	},
+  index: function() {
+    CATARSE.router.navigate("backs", {trigger: true})
+  },
 
-	backs: function() {
-		this.selectItem("backed_projects")
-		this.backsView = new this.BacksView({
-			modelView: this.BackView,
-			collection: this.user.backs,
-			loading: this.$("#loading"),
-			el: this.$("#user_backed_projects")
-		})
-	},
+  backs: function() {
+    this.selectItem("backed_projects")
+    this.backsView = new this.BacksView({
+      modelView: this.BackView,
+      collection: this.user.backs,
+      loading: this.$("#loading"),
+      el: this.$("#user_backed_projects")
+    })
+  },
 
-	projects: function() {
-		this.selectItem("created_projects")
-		this.projectsView = new this.ProjectsView({
-			modelView: this.ProjectView,
-			collection: this.user.projects,
-			loading: this.$("#loading"),
-			el: this.$("#user_created_projects")
-		})
-	},
+  projects: function() {
+    this.selectItem("created_projects")
+    this.projectsView = new this.ProjectsView({
+      modelView: this.ProjectView,
+      collection: this.user.projects,
+      loading: this.$("#loading"),
+      el: this.$("#user_created_projects")
+    })
+  },
 
-	credits: function() {
-		this.selectItem("credits")
-	},
+  credits: function() {
+    this.selectItem("credits")
+  },
 
-	comments: function() {
-		this.selectItem("comments")
-	},
+  comments: function() {
+    this.selectItem("comments")
+  },
 
-	request_refund: function(back_id) {
-		url = '/users/'+this.user.id+'/request_refund/'+back_id;
-		$.post(url, function(result) {
-			alert(result['status'])
-			$("tr#back_"+back_id+" td.status").text(result['status'])
-		})
-	},
+  request_refund: function(back_id) {
+    url = '/users/'+this.user.id+'/request_refund/'+back_id;
+    $.post(url, function(result) {
+      alert(result['status'])
+      $("tr#back_"+back_id+" td.status").text(result['status'])
+    })
+  },
 
-	selectItem: function(item) {
-		this.$("#user_profile_content .content").hide()
-		this.$("#user_profile_content #user_" + item + ".content").show()
-		var link = this.$("#user_profile_menu #" + item + "_link")
-		link.parent().children().removeClass('selected')
+  selectItem: function(item) {
+    this.$("#user_profile_content .content").hide()
+    this.$("#user_profile_content #user_" + item + ".content").show()
+    var link = this.$("#user_profile_menu #" + item + "_link")
+    link.parent().children().removeClass('selected')
     link.addClass('selected')
-	}
+  }
 
 })
 
