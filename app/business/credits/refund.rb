9cad0e4938cd95f5bb51fa1a617ea6962c3b07ba
Antonio Roberto <forevertonny@gmail.com>
2012-03-19 09:59:56 -0300

    don't need to do this user verification because cancan do this.

--- app/business/credits/refund.rb
+++ app/business/credits/refund.rb
@@ -8,18 +8,14 @@ module Credits
     end
 
     def make_request!
-      if @user == @backer.user or @user.admin?
-        check_refunded
-        check_requested
-        check_total_of_credits
-        check_can_refund
-        @backer.update_attribute :requested_refund, true
-        @backer.user.update_attribute :credits, @backer.user.credits - @backer.value
-        CreditsMailer.request_refund_from(@backer).deliver
-        @message = I18n.t('credits.index.refunded')
-      else
-        raise I18n.t('credits.refund.cannot_refund')
-      end
+      check_refunded
+      check_requested
+      check_total_of_credits
+      check_can_refund
+      @backer.update_attribute :requested_refund, true
+      @backer.user.update_attribute :credits, @backer.user.credits - @backer.value
+      CreditsMailer.request_refund_from(@backer).deliver
+      @message = I18n.t('credits.index.refunded')
     end
 
     private
