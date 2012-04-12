00f2eef6b0f6ede4b190d2a6367f930ac2ebec25
Diogo Biazus <diogob@gmail.com>
2012-04-12 09:27:59 -0300

    tidying up the code

--- app/models/backer.rb
+++ app/models/backer.rb
@@ -19,53 +19,64 @@ class Backer < ActiveRecord::Base
   scope :can_refund, where(:can_refund => true)
   scope :within_refund_deadline, where("current_timestamp < (created_at + interval '180 days')")
   after_create :define_key, :define_payment_method
+
   def define_key
     self.update_attribute :key, Digest::MD5.new.update("#{self.id}###{self.created_at}###{Kernel.rand}").to_s
   end
+
   def define_payment_method
     self.update_attribute :payment_method, 'MoIP'
   end
-  # after_save :update_user_credits
-  # def update_user_credits
-  #   self.user.update_credits
-  # end
+
   before_save :confirm?
+
   def confirm?
     if confirmed and confirmed_at.nil?
       self.confirmed_at = Time.now
       self.display_notice = true
     end
   end
+
   def confirm!
     update_attribute :confirmed, true
     update_attribute :confirmed_at, Time.now
   end
+
   def reward_must_be_from_project
     return unless reward
     errors.add(:reward, I18n.t('backer.reward_must_be_from_project')) unless reward.project == project
   end
+
   validate :value_must_be_at_least_rewards_value
+
   def value_must_be_at_least_rewards_value
     return unless reward
     errors.add(:value, I18n.t('backer.value_must_be_at_least_rewards_value', :minimum_value => reward.display_minimum)) unless value >= reward.minimum_value
   end
+
   validate :should_not_back_if_maximum_backers_been_reached, :on => :create
+
   def should_not_back_if_maximum_backers_been_reached
     return unless reward and reward.maximum_backers and reward.maximum_backers > 0
     errors.add(:reward, I18n.t('backer.should_not_back_if_maximum_backers_been_reached')) unless reward.backers.confirmed.count < reward.maximum_backers
   end
+
   def display_value
     number_to_currency value, :unit => "R$", :precision => 0, :delimiter => '.'
   end
+
   def display_confirmed_at
     I18n.l(confirmed_at.to_date) if confirmed_at
   end
+
   def platform_fee(fee=7.5)
     (value.to_f * fee)/100
   end
+
   def display_platform_fee(fee=7.5)
     number_to_currency platform_fee(fee), :unit => "R$", :precision => 2, :delimiter => '.'
   end
+
   def payment_service_fee
     if payment_detail
       payment_detail.service_tax_amount.to_f
@@ -74,13 +85,10 @@ class Backer < ActiveRecord::Base
       payment_detail.service_tax_amount.to_f
     end
   end
+
   def moip_value
     "%0.0f" % (value * 100)
   end
-
-  #def refund!
-    #::Credits::Refund.new(self).make_request!
-  #end
 
   def cancel_refund_request!
     raise I18n.t('credits.cannot_cancel_refund_reques') unless self.requested_refund
@@ -95,11 +103,12 @@ class Backer < ActiveRecord::Base
     self.user.update_attribute :credits, self.user.credits + self.value
     self.update_attribute :can_refund, true
   end
+
   def refund_deadline
     created_at + 180.days
   end
-  def as_json(options={})
 
+  def as_json(options={})
     json_attributes = {
       :id => id,
       :anonymous => anonymous,
@@ -108,23 +117,18 @@ class Backer < ActiveRecord::Base
       :value => display_value,
       :user => user.as_json(options.merge(:anonymous => anonymous))
     }
-
     if options and options[:can_manage]
       json_attributes.merge!({
         :display_value => display_value,
         :reward => reward
       })
     end
-
     if options and options[:include_project]
       json_attributes.merge!({:project => project})
     end
-
     if options and options[:include_reward]
       json_attributes.merge!({:reward => reward})
     end
-
     json_attributes
-
   end
 end
