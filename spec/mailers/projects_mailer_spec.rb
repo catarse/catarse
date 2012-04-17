eef80b58347ea7336c4530f2c5ea9c39823e4239
Antônio Roberto Silva <forevertonny@gmail.com>
2012-04-17 13:38:44 -0300

    start project view

--- spec/mailers/projects_mailer_spec.rb
+++ spec/mailers/projects_mailer_spec.rb
@@ -2,16 +2,25 @@ require "spec_helper"
 
 describe ProjectsMailer do
   it "should send project, with HTML-safe fields and converting new lines to <br>" do
+    how_much_you_need = "1000 <javascript>"
+    category = "art <javascript>"
     about = "About the project\n<javascript>"
     rewards = "Rewards of the project\n<javascript>"
+    video = "http://vimeo.com/9090 <javascript>"
+    twitter = "username <javascript>"
+    blog = "www.lorem.com <javascript>"
     links = "Links of the project\n<javascript>"
+    know_us_via = "My friends\n<javascript>"
+    how_works = "Through social networks\n<javascript>"
     contact = "foo@bar.com"
     user = Factory(:user)
-    email = ProjectsMailer.start_project_email(about, rewards, links, contact, user, "#{I18n.t('site.base_url')}#{user_path(user)}").deliver
+    email = ProjectsMailer.start_project_email(how_much_you_need, category, about, rewards, video, twitter, blog, links, know_us_via, how_works, contact, user, "#{I18n.t('site.base_url')}#{user_path(user)}").deliver
     ActionMailer::Base.deliveries.should_not be_empty
+    email.encoded.should =~ /1000 &lt;javascript&gt;/
     email.encoded.should =~ /About the project\<br\>&lt;javascript&gt;/
     email.encoded.should =~ /Rewards of the project\<br\>&lt;javascript&gt;/
     email.encoded.should =~ /Links of the project\<br\>&lt;javascript&gt;/
+    email.encoded.should =~ /Through social networks\<br\>&lt;javascript&gt;/
     email.encoded.should =~ /foo@bar.com/
     email[:from].to_s.should == "#{I18n.t('site.name')} <#{I18n.t('site.email.system')}>"
   end
