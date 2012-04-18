31f0f5ba604da67a190e6630813f0edbb4bdcdc8
Diogo Biazus <diogob@gmail.com>
2012-04-18 20:06:53 -0300

    now the devise also redirects to session[:return_to] after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -112,6 +112,12 @@ class ApplicationController < ActionController::Base
     session[:return_to] = nil
   end
 
+  def after_sign_in_path_for(resource_or_scope)
+    return_to = session[:return_to]
+    session[:return_to] = nil
+    (return_to || root_path)
+  end
+
   def require_condition(condition, message)
     unless condition
       session[:return_to] = request.env['REQUEST_PATH']
