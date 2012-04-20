b7a27a5ec795c008f685e012fb3366c389ad9226
Diogo Biazus <diogob@gmail.com>
2012-04-20 10:11:20 -0300

    tidying up and removing inject

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -34,12 +34,7 @@ class ProjectsController < ApplicationController
 
         @curated_pages = CuratedPage.visible.order("created_at desc").limit(6)
         @last_tweets = Rails.cache.fetch('last_tweets', :expires_in => 30.minutes) do
-            JSON.parse(Net::HTTP.get(URI("http://api.twitter.com/1/statuses/user_timeline.json?screen_name=#{t('site.twitter')}"))).inject([]) do |total, item|
-              if total.size <= 1
-                total << item
-              end
-              total
-            end
+          JSON.parse(Net::HTTP.get(URI("http://api.twitter.com/1/statuses/user_timeline.json?screen_name=#{t('site.twitter')}")))[0..2]
         end
       end
       format.json do
