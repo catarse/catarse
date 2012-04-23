68749d0739b8d03aa436d714f2bdd941a0918413
Diogo Biazus <diogob@gmail.com>
2012-04-23 12:24:22 -0300

    removing comments and tidying up spec of user controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,16 +5,23 @@ describe UsersController do
   render_views
   subject{ response }
 
-  describe "User request refund" do
+  let(:backer){ Factory.create(:backer) }
+  let(:user){ backer.user }
+
+  describe "GET show" do
     before do
-      @backer = Factory.create(:backer)
-      @user = @backer.user
+      request.session[:user_id] = user.id
+      get :show, :id => user.id
     end
 
+    it{ assigns(:fb_admins).should include(user.facebook_id) }
+  end
+
+  describe "POST request_refund" do
     context "without user" do
       it 'should raise a exception' do
         lambda { 
-          post :request_refund, { id: @user.id, back_id: @backer.id }
+          post :request_refund, { id: user.id, back_id: backer.id }
         }.should raise_exception CanCan::AccessDenied, 'You are not authorized to access this page.'
       end
     end
@@ -22,40 +29,40 @@ describe UsersController do
     context "with user" do
       context "when current_user request to refund your backer" do
         before do
-          request.session[:user_id]=@user.id
+          request.session[:user_id]=user.id
         end
 
         it "success requested" do
-          @backer.can_refund = true
-          @user.credits = 100
-          @user.save
-          @backer.save
-          post :request_refund, { id: @user.id, back_id: @backer.id }
+          backer.can_refund = true
+          user.credits = 100
+          user.save
+          backer.save
+          post :request_refund, { id: user.id, back_id: backer.id }
 
           ActiveSupport::JSON.decode(subject.body)['status'].should == I18n.t('credits.index.refunded')
         end
 
         it "when user doesn't have a necessary value" do
-          @user.credits = 4
-          @user.save
-          @user.reload
-          post :request_refund, { id: @user.id, back_id: @backer.id }
+          user.credits = 4
+          user.save
+          user.reload
+          post :request_refund, { id: user.id, back_id: backer.id }
 
           ActiveSupport::JSON.decode(subject.body)['status'].should == I18n.t('credits.refund.no_credits')
         end
 
         it "when backer cannot refunded" do
-          @backer.update_attribute :refunded, true
-          @backer.reload
-          post :request_refund, { id: @user.id, back_id: @backer.id }
+          backer.update_attribute :refunded, true
+          backer.reload
+          post :request_refund, { id: user.id, back_id: backer.id }
 
           ActiveSupport::JSON.decode(subject.body)['status'].should == I18n.t('credits.refund.refunded')
         end
 
         it "when backer already requested to refund" do
-          @backer.update_attribute :requested_refund, true
-          @backer.reload
-          post :request_refund, { id: @user.id, back_id: @backer.id }
+          backer.update_attribute :requested_refund, true
+          backer.reload
+          post :request_refund, { id: user.id, back_id: backer.id }
 
           ActiveSupport::JSON.decode(subject.body)['status'].should == I18n.t('credits.refund.requested_refund')
         end
