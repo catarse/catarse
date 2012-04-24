c069cc1fee3153b4027d4f03f72092b4a5364a2a
Diogo Biazus <diogob@gmail.com>
2012-04-24 12:07:42 -0300

    removing tumblr posts from codebase, lets replace with Update model

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -7,14 +7,7 @@ describe ProjectsController do
 
   describe "GET show" do
     let(:project){ Factory(:project) }
-    before{
-      mock_tumblr
-      get :show, id: project, locale: :pt
-    }
-
+    before{ get :show, :id => project, :locale => :pt }
     it{ should be_success }
-    context "with posts" do
-      its(:body){ should =~ /Belo Monte de Vozes/ }
-    end
   end
 end
