606d0eb5d762dfd958f600a4322d0336cfb7f4b1
Diogo Biazus <diogob@gmail.com>
2012-04-24 12:39:32 -0300

    added validations and associations to the Update model

--- app/models/update.rb
+++ app/models/update.rb
@@ -1,2 +1,5 @@
 class Update < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :project
+  validates_presence_of :user_id, :project_id, :comment, :comment_html
 end
--- spec/models/update_spec.rb
+++ spec/models/update_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Update do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it{ should validate_presence_of :project_id }
+    it{ should validate_presence_of :user_id }
+    it{ should validate_presence_of :comment }
+    it{ should validate_presence_of :comment_html }
+  end
+
+  describe "associations" do
+    it{ should belong_to :user }
+    it{ should belong_to :project }
+  end
 end
