f666059c2887574e5d856333e9ecafff87eca7a0
Diogo Biazus <diogob@gmail.com>
2012-04-25 15:57:01 -0300

    created project updates creation form

--- spec/controllers/updates_controller_spec.rb
+++ spec/controllers/updates_controller_spec.rb
@@ -3,19 +3,33 @@ require 'spec_helper'
 describe UpdatesController do
   subject{ response }
   describe "GET index" do
-    before do
-      @update = Factory(:update)
-      get :index, :project_id => @update.project.id, :locale => 'pt', :format => 'json'
+    context "in html format" do
+      before do
+        @update = Factory(:update)
+        get :index, :project_id => @update.project.id, :locale => 'pt', :format => 'html'
+      end
+      its(:status){ should == 200 }
     end
-    its(:status){ should == 200 }
-    its(:body){ should == [@update].to_json }
+
+    context "in json format" do
+      before do
+        @update = Factory(:update)
+        get :index, :project_id => @update.project.id, :locale => 'pt', :format => 'json'
+      end
+      its(:status){ should == 200 }
+      its(:body){ should == [@update].to_json }
+    end
   end
 
   describe "POST create" do
+    let(:user){ Factory(:user) }
     before do
-      
+      request.session[:user_id] = user.id
+      @project = Factory(:project)
+      post :create, :project_id => @project.id, :locale => 'pt', :update => {:title => 'title', :comment => 'update comment'}
     end
-  
+    it{ should redirect_to project_updates_path(@project) }
+    it{ Update.where(:user_id => user.id, :project_id => @project.id).count.should == 1 }
   end
 
 end
