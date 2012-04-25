8a684922df231ad477a86ac868c85b25d6ef8bdb
Antônio Roberto Silva <forevertonny@gmail.com>
2012-04-25 08:39:17 -0300

    start project form adjusts

--- app/mailers/projects_mailer.rb
+++ app/mailers/projects_mailer.rb
@@ -1,17 +1,17 @@
 class ProjectsMailer < ActionMailer::Base
   include ERB::Util
 
-  def start_project_email(how_much_you_need, category, about, rewards, video, twitter, blog, links, know_us_via, how_works, contact, user, user_url)
+  def start_project_email(how_much_you_need, category, about, rewards, video, facebook,twitter, blog, links, know_us_via, contact, user, user_url)
     @how_much_you_need = h(how_much_you_need)
     @category = h(category)
     @about = h(about).gsub("\n", "<br>").html_safe
     @rewards = h(rewards).gsub("\n", "<br>").html_safe
     @video = h(video)
+    @facebook = h(facebook)
     @twitter = h(twitter)
     @blog = h(blog)
     @links = h(links).gsub("\n", "<br>").html_safe
     @know_us_via = h(know_us_via).gsub("\n", "<br>").html_safe
-    @how_works = h(how_works).gsub("\n", "<br>").html_safe
     @contact = contact
     @user = user
     @user_url = user_url
