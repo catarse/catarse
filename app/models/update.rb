90cbcfe1854ee8b30e33fb8769a58114ca8fca36
Diogo Biazus <diogob@gmail.com>
2012-04-25 14:42:05 -0300

    added auto_html to comment field

--- app/models/update.rb
+++ app/models/update.rb
@@ -2,4 +2,14 @@ class Update < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
   validates_presence_of :user_id, :project_id, :comment, :comment_html
+
+  auto_html_for :comment do
+    html_escape :map => {
+      '&' => '&amp;',
+      '>' => '&gt;',
+      '<' => '&lt;',
+      '"' => '"' }
+    redcloth :target => :_blank
+    link :target => :_blank
+  end
 end
