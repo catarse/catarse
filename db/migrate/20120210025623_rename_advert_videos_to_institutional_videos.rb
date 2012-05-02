c7b67f9772c4356395b21a3e10639f0475e6fd70
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-05-02 15:20:43 -0300

    fixed bug in 'rename_advert_videos_to_institutional_videos' migration

--- db/migrate/20120210025623_rename_advert_videos_to_institutional_videos.rb
+++ db/migrate/20120210025623_rename_advert_videos_to_institutional_videos.rb
@@ -1,12 +1,12 @@
 class RenameAdvertVideosToInstitutionalVideos < ActiveRecord::Migration
   def self.up
-    if ActiveRecord::Base.connection.tables.include?(:advert_videos)
+    if ActiveRecord::Base.connection.tables.include?("advert_videos")
       rename_table :advert_videos, :institutional_videos
     end
   end
 
   def self.down
-    if ActiveRecord::Base.connection.tables.include?(:institutional_videos)
+    if ActiveRecord::Base.connection.tables.include?("institutional_videos")
       rename_table :institutional_videos, :advert_videos
     end
   end
