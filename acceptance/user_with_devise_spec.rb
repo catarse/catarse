dda5ba41770e95d22b11282308571c5324609a37
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-05-11 17:03:48 -0300

    spec acceptance: login with devise is passing

--- acceptance/user_with_devise_spec.rb
+++ acceptance/user_with_devise_spec.rb
@@ -1,50 +1,50 @@
 require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
 
-feature "with login modal" do
+feature "with login page" do
   scenario "Login with user that use another provider" do
     click_login
     verify_translations
-    find("#login").visible?.should be_true
-    find("a#login_with_mail").click
 
     user_facebook = Factory.create(:user, :provider => 'facebook', :uid => '1234566',:email => 'lorem@lorem.com', :password => 'somepassword', :password_confirmation => 'somepassword')
-    fill_in 'user_email', :with => user_facebook.email
-    fill_in 'user_password', :with => user_facebook.password
-    click_button 'user_submit'
+    within ".new_session" do
+      fill_in 'user_email', :with => user_facebook.email
+      fill_in 'user_password', :with => user_facebook.password
+      click_button 'user_submit'
+    end
+    verify_translations
 
-    page.should have_no_css('#user')
+    page.should have_no_css('.user')
     page.should have_css('.alert.wrapper')
   end
 
   scenario "Login with devise user" do
     click_login
     verify_translations
-    find("#login").visible?.should be_true
-    find("a#login_with_mail").click
 
     user = Factory.create(:user, :provider => 'devise',:email => 'lorem@lorem.com', :password => '123lorem', :password_confirmation => '123lorem')
-    fill_in 'user_email', :with => user.email
-    fill_in 'user_password', :with => user.password
-    click_button 'user_submit'
+    within ".new_session" do
+      fill_in 'user_email', :with => user.email
+      fill_in 'user_password', :with => user.password
+      click_button 'user_submit'
+    end
 
-    page.should have_css('#user')
+    page.should have_css('.user')
     page.should have_link(user.name)
   end
 
   scenario "Register new user account" do
     click_login
     verify_translations
-    find("#login").visible?.should be_true
-    find("a#login_with_mail").click
-    find("a.new_registration_link").click
+
+    within ".new_registration" do
+      fill_in "Nome", with: "Foo Bar"
+      fill_in "Email", with: "foo@bar.com"
+      fill_in "Senha", with: "foo!bar"
+      fill_in "user_password_confirmation", with: "foo!bar"
+      click_button "Efetuar cadastro"
+    end
     verify_translations
-
-    fill_in 'user_email', :with => 'lorem@lorem.com'
-    fill_in 'user_password', :with => '123lorem'
-    fill_in 'user_password_confirmation', :with => '123lorem'
-
-    click_button 'user_submit'
-    page.should have_css('#user')
+    page.should have_css('.user')
   end
 
   scenario "Request new password" do
@@ -53,9 +53,8 @@ feature "with login modal" do
     user = Factory.create(:user, :provider => 'devise',:email => 'lorem@lorem.com', :password => '123lorem', :password_confirmation => '123lorem')
     click_login
     verify_translations
-    find("#login").visible?.should be_true
-    find("a#login_with_mail").click
-    find("a.forgot_password_link").click
+
+    click_link("Esqueceu sua senha?")
     verify_translations
     fill_in 'user_email', :with => user.email
     click_button 'user_submit'
@@ -88,7 +87,7 @@ feature "with devise routes" do
     fill_in 'user_password', :with => user.password
     click_button 'user_submit'
 
-    page.should have_css('#user')
+    page.should have_css('.user')
     page.should have_link(user.name)
   end
 
@@ -96,11 +95,15 @@ feature "with devise routes" do
     visit new_user_registration_path
     verify_translations
 
-    fill_in 'user_email', :with => 'lorem@lorem.com'
-    fill_in 'user_password', :with => '123lorem'
-    fill_in 'user_password_confirmation', :with => '123lorem'
-    click_button 'user_submit'
-    page.should have_css('#user')
+    within ".new_registration" do
+      fill_in "Nome", with: "Foo Bar"
+      fill_in "Email", with: "foo@bar.com"
+      fill_in "Senha", with: "foo!bar"
+      fill_in "user_password_confirmation", with: "foo!bar"
+      click_button "Efetuar cadastro"
+    end
+    verify_translations
+    page.should have_css('.user')
   end
 
   scenario "Request new password" do
