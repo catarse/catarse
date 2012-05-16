7ed0a00aaac5a7ea247162b2240b22022bb338f9
Diogo Biazus <diogob@gmail.com>
2012-05-16 11:14:08 -0300

    now we redirect to permalink if project has one in project show

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -6,8 +6,16 @@ describe ProjectsController do
   subject{ response }
 
   describe "GET show" do
-    let(:project){ Factory(:project) }
-    before{ get :show, :id => project, :locale => :pt }
-    it{ should be_success }
+    context "when we have permalink and do not pass permalink in the querystring" do
+      let(:project){ Factory(:project, :permalink => 'test') }
+      before{ get :show, :id => project, :locale => :pt }
+      it{ should redirect_to project_by_slug_path(project.permalink) }
+    end
+
+    context "when we do not have permalink and do not pass permalink in the querystring" do
+      let(:project){ Factory(:project, :permalink => nil) }
+      before{ get :show, :id => project, :locale => :pt }
+      it{ should be_success }
+    end
   end
 end
