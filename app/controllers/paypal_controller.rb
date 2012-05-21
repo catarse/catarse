63b5c8088b3e3edd0ec81761238513ea29ebe73b
Juan Maiz <juanmaiz@gmail.com>
2012-05-21 14:58:54 -0300

    redirect working as expected with mocks and shit

--- app/controllers/paypal_controller.rb
+++ app/controllers/paypal_controller.rb
@@ -3,22 +3,38 @@ class PaypalController < ApplicationController
   before_filter :initialize_paypal
 
   def pay
+    # raise "!!!"
     backer = Backer.find params[:id]
-    begin
-      paypal_response = @paypal.setup(
-        paypal_payment(backer),
-        success_paypal_url(backer),
-        cancel_paypal_url(backer),
-        :no_shipping => true
-      )
-      backer.update_attribute :payment_method, 'PayPal'
-      redirect_to paypal_response.redirect_uri
-    #rescue Paypal::Exception::APIError => e
-    #  raise "Message: #{e.message}<br/>Response: #{e.response.inspect}<br/>Details: #{e.response.details.inspect}"
-    rescue
-      flash[:failure] = t('projects.pay.paypal_error')
-      return redirect_to new_project_backer_path(backer.project)
-    end
+
+    # this sets the data for whom to pay (the project owner)
+    recipients = [{email: backer.project.user.email, amount: backer.value, primary: true}]
+
+    response = @gateway.setup_purchase(
+      return_url: success_paypal_url(backer),
+      cancel_url: cancel_paypal_url(backer),
+      # ipn_notification_url: url_for(:action => 'notify_action', :only_path => false),
+      receiver_list: recipients,
+      currency_code: :BRL,
+      description: t('projects.pay.paypal_description'),
+      :items => [{
+          :name => backer.project.name,
+          :description => t('projects.pay.paypal_description'),
+          :amount => backer.value#,
+          #:category => :Digital
+        }]
+
+    )
+    backer.update_attribute :payment_method, 'PayPal'
+    # for redirecting the customer to the actual paypal site to finish the payment.
+    redirect_to @gateway.redirect_url_for(response["payKey"])
+
+    # #rescue Paypal::Exception::APIError => e
+    # #  raise "Message: #{e.message}<br/>Response: #{e.response.inspect}<br/>Details: #{e.response.details.inspect}"
+    # rescue
+    #   flash[:failure] = t('projects.pay.paypal_error')
+    #   return redirect_to new_project_backer_path(backer.project)
+    # end
+
   end
 
   def success
@@ -56,30 +72,26 @@ class PaypalController < ApplicationController
   protected
 
   def initialize_paypal
-
-    # TODO remove the sandbox! when ready
-    #Paypal.sandbox!
-    # TODO remove the sandbox! when ready
-
-    @paypal = Paypal::Express::Request.new(
-      :username   => ::Configuration[:paypal_username],
-      :password   => ::Configuration[:paypal_password],
-      :signature  => ::Configuration[:paypal_signature]
+    @gateway ||=  ActiveMerchant::Billing::PaypalAdaptivePayment.new(
+      login: ::Configuration[:paypal_username],
+      password: ::Configuration[:paypal_password],
+      signature: ::Configuration[:paypal_signature],
+      appid: ::Configuration[:paypal_appid]
     )
   end
 
-  def paypal_payment(backer)
-    Paypal::Payment::Request.new(
-      :currency_code => :BRL,
-      :amount => backer.value,
-      :description => t('projects.pay.paypal_description'),
-      :items => [{
-          :name => backer.project.name,
-          :description => t('projects.pay.paypal_description'),
-          :amount => backer.value#,
-          #:category => :Digital
-        }]
-    )
-  end
+  # def paypal_payment(backer)
+  #   Paypal::Payment::Request.new(
+  #     :currency_code => :BRL,
+  #     :amount => backer.value,
+  #     :description => t('projects.pay.paypal_description'),
+  #     :items => [{
+  #         :name => backer.project.name,
+  #         :description => t('projects.pay.paypal_description'),
+  #         :amount => backer.value#,
+  #         #:category => :Digital
+  #       }]
+  #   )
+  # end
 
 end
