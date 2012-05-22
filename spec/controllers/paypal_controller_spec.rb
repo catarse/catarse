2e3757cafb402ab8a5ea8ee1ea3ba2ad8b0da474
Juan Maiz <juanmaiz@gmail.com>
2012-05-22 13:20:47 -0300

    cherry-picking paypal commits

--- spec/controllers/paypal_controller_spec.rb
+++ spec/controllers/paypal_controller_spec.rb
@@ -8,10 +8,25 @@ describe PaypalController do
       ActiveMerchant::Billing::PaypalAdaptivePayment.any_instance.stubs(:setup_purchase).returns({"payKey" => "12345"})
       ActiveMerchant::Billing::PaypalAdaptivePayment.any_instance.stubs(:redirect_url_for).with("12345").returns("https://paypal.com/ok")
 
-      backer = Factory(:backer, :confirmed => false)
+      backer = Factory(:backer, confirmed: false)
       get :pay, id: backer.id, locale: :pt
     }      
     it{ should redirect_to("https://paypal.com/ok") }
 
   end
+
+  describe 'GET /success' do
+    # let( }
+    before{
+      ActiveMerchant::Billing::PaypalAdaptivePayment.any_instance.stubs(:setup_purchase).returns({"payKey" => "12345"})
+      ActiveMerchant::Billing::PaypalAdaptivePayment.any_instance.stubs(:redirect_url_for).with("12345").returns("https://paypal.com/ok")
+      @backer = Factory(:backer, confirmed: false)
+      get :success, id: @backer.id, locale: :pt
+    }      
+    it("backer should be confirmed"){ @backer.reload; @backer.should be_confirmed }
+    it{ should redirect_to(thank_you_path) }
+    
+
+  end
+
 end
