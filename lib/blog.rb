86d8b670d1420633acfb492ed894e43f97e32bf1
Antônio Roberto Silva <forevertonny@gmail.com>
2012-06-04 11:36:41 -0300

    update blog link

--- lib/blog.rb
+++ lib/blog.rb
@@ -2,7 +2,7 @@ class Blog
   class << self
     def fetch_last_posts
       Rails.cache.fetch('blog_posts', expires_in: 10.minutes) do
-        feed = Feedzirra::Feed.fetch_and_parse("http://webmais.net.br/catarse/?feed=rss2")
+        feed = Feedzirra::Feed.fetch_and_parse("#{I18n.t('site.blog')}?feed=rss2")
         feed.entries
       end
     end
