17c487b4d44b2349918bb5a81e8a096291f1ae37
Diogo Biazus <diogob@gmail.com>
2012-06-05 11:30:01 -0300

    fixed explore duplicated items and put some useful comments

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -3,10 +3,12 @@ class ExploreController < ApplicationController
   def index
     @title = t('explore.title')
     @categories = Category.with_projects.order(:name).all
-    @recommended = Project.visible.not_expired.recommended.order('expires_at').limit(3)
-    @expiring = Project.visible.expiring.limit(3).order('expires_at')
+
+    # Just to know if we should present the menu entries, the actual projects are fetched via AJAX
+    @recommended = Project.visible.not_expired.recommended.limit(3)
+    @expiring = Project.visible.expiring.limit(3)
     @recent = Project.visible.recent.not_expired.limit(3).order('created_at DESC')
-    @successful = Project.visible.successful.order('expires_at DESC').limit(3)
+    @successful = Project.visible.successful.limit(3)
   end
 
 end
