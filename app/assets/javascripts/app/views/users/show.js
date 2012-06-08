801a00f390fa9fd8c55dffad3cad217c3dc9f8d9
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-06-08 13:27:32 -0300

    placing some user javascript into backbone initialize

--- app/assets/javascripts/app/views/users/show.js
+++ app/assets/javascripts/app/views/users/show.js
@@ -11,6 +11,31 @@ CATARSE.UsersShowView = Backbone.View.extend({
     CATARSE.router.route("request_refund/:back_id", "request_refund", this.request_refund)
     this.user = new CATARSE.User($('#user_profile').data("user"))
     this.render()
+
+    $('input,textarea').live('keypress', function(e){
+      if (e.which == '13' && $("button:contains('OK')").attr('disabled')) {
+        e.preventDefault();
+      }
+    })
+
+    $('#user_feed input').live('keyup', function(){
+      var value = $(this).val()
+      var re = /^[a-z0-9\._-]+@([a-z0-9][a-z0-9-_]*[a-z0-9-_]\.)+([a-z-_]+\.)?([a-z-_]+)$/
+      if(value.match(re)){
+        $(this).addClass("ok").removeClass("error")
+        $("button:contains('OK')").attr('disabled', false)
+      } else {
+        $(this).addClass("error").removeClass("ok")
+        $("button:contains('OK')").attr('disabled', true)
+      }
+    })
+
+    $('input[type=checkbox]').click(function(){
+      $.post('/users/update_attribute_on_the_spot', {
+        id: 'user__' + $(this).attr('id') + '__' + $('#id').val(),
+        value: ($(this).is(':checked') ? 1 : null)
+      })
+    })
   },
 
   events: {
@@ -130,40 +155,4 @@ CATARSE.UsersShowView = Backbone.View.extend({
     link.addClass('selected')
   }
 
-})
-
-$('input,textarea').live('keypress', function(e){
-  if (e.which == '13' && $("button:contains('OK')").attr('disabled')) {
-    e.preventDefault();
-  }
-})
-
-$('#user_feed input').live('keyup', function(){
-  var value = $(this).val()
-  var re = /^[a-z0-9\._-]+@([a-z0-9][a-z0-9-_]*[a-z0-9-_]\.)+([a-z-_]+\.)?([a-z-_]+)$/
-  if(value.match(re)){
-    $(this).addClass("ok").removeClass("error")
-    $("button:contains('OK')").attr('disabled', false)
-  } else {
-    $(this).addClass("error").removeClass("ok")
-    $("button:contains('OK')").attr('disabled', true)
-  }
-})
-
-$('#content_header textarea').live('keyup', function(){
-  var value = $(this).val()
-  if(value.length <= 140){
-    $(this).addClass("ok").removeClass("error")
-    $("button:contains('OK')").attr('disabled', false)
-  } else {
-    $(this).addClass("error").removeClass("ok")
-    $("button:contains('OK')").attr('disabled', true)
-  }
-})
-
-$('input[type=checkbox]').click(function(){
-  $.post('/users/update_attribute_on_the_spot', {
-    id: 'user__' + $(this).attr('id') + '__' + $('#id').val(),
-    value: ($(this).is(':checked') ? 1 : null)
-  })
 })
