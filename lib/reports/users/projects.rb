273943c0b6d764c73e496aae8faa6ae5e2cb2056
Antônio Roberto Silva <forevertonny@gmail.com>
2012-06-20 19:45:16 -0300

    report adjust

--- lib/reports/users/projects.rb
+++ lib/reports/users/projects.rb
@@ -4,11 +4,13 @@ module Reports
     class Projects
       class << self
         def all_project_owners
-          @collection = User.joins(:projects).includes(:projects)
+          @collection = User.joins(:projects).where('projects.visible is true').includes(:projects)
 
           @csv = CSV.generate(:col_sep => ',') do |csv_string|
             csv_string << [
               'ID projeto',
+              'Nome do projeto',
+              'Projeto bem sucedido?',
               'Nome do realizador',
               'Email do realizador',
               'Estado',
@@ -17,14 +19,18 @@ module Reports
             ]
 
             @collection.each do |resource|
-              csv_string << [
-                resource.projects.map(&:id).join(', '),
-                resource.name,
-                resource.email,
-                resource.address_state,
-                resource.address_city,
-                resource.phone_number
-              ]
+              resource.projects.each do |project|
+                csv_string << [
+                  project.id,
+                  project.name,
+                  (project.successful? ? 'Sim' : 'Nao'),
+                  resource.name,
+                  resource.email,
+                  resource.address_state,
+                  resource.address_city,
+                  resource.phone_number
+                ]
+              end
             end
           end
         end
