7526db4a09dfc50d0b34a44783b8c7ae6407554c
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-06-20 17:09:51 -0300

    fixed bug into search form

--- app/assets/javascripts/app/views/layouts/application.js
+++ app/assets/javascripts/app/views/layouts/application.js
@@ -64,7 +64,7 @@ CATARSE.LayoutsApplicationView = Backbone.View.extend({
   
   search: function(event) {
     var query = this.$(event.target).find("#search").val()
-    if(!(CATARSE.loader.namespace.text == "" && CATARSE.loader.controller == "explore" && CATARSE.loader.action == "index") && query.length > 0)       
+    if(!($('#main_content').data("controller-name") == "explore" && $('#main_content').data("action") == "index") && query.length > 0)
       location.href = "/explore#search/" + query
     return false
   },
