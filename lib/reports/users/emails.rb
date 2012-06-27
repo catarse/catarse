fb175bf01e9c185a729862c3154bb77b15a2e075
Antônio Roberto Silva <forevertonny@gmail.com>
2012-06-27 15:49:28 -0300

    show only emails

--- lib/reports/users/emails.rb
+++ lib/reports/users/emails.rb
@@ -4,19 +4,15 @@ module Reports
     class Emails
       class << self
         def all_emails
-          @collection= User.primary.select('name, nickname, email').group('name, nickname, email')
+          @collection= User.primary.select('email').group('email')
 
           @csv = CSV.generate(:col_sep => ',') do |csv_string|
             csv_string << [
-              'Nome',
-              'Apelido',
               'Email'
             ]
 
             @collection.each do |resource|
               csv_string << [
-                resource.name,
-                resource.nickname,
                 resource.email
               ]
             end
