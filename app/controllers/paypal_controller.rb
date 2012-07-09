5b216c32b3639be1566138e6b377fc5f217751c8
Diogo Biazus <diogob@gmail.com>
2012-07-09 17:58:32 -0300

    using business/payment_gateway

--- app/controllers/paypal_controller.rb
+++ app/controllers/paypal_controller.rb
@@ -94,17 +94,11 @@ class PaypalController < ApplicationController
   end
 
   def initialize_paypal
-    if Rails.env.test?
-      Configuration.create!(name: "paypal_username", value: "usertest_api1.teste.com")
-      Configuration.create!(name: "paypal_password", value: "HVN4PQBGZMHKFVGW")
-      Configuration.create!(name: "paypal_signature", value: "AeL-u-Ox.N6Jennvu1G3BcdiTJxQAWdQcjdpLTB9ZaP0-Xuf-U0EQtnS")
-    end
-
     if Configuration[:paypal_username] and Configuration[:paypal_password] and Configuration[:paypal_signature]
-      @@express_gateway ||= ActiveMerchant::Billing::PaypalExpressGateway.new({
-        login: Configuration[:paypal_username],
-        password: Configuration[:paypal_password],
-        signature: Configuration[:paypal_signature]
+      @@express_gateway ||= PaymentGateway({
+        :login => Configuration[:paypal_username],
+        :password => Configuration[:paypal_password],
+        :signature => Configuration[:paypal_signature]
       })
     else
       puts "[PayPal] An API Certificate or API Signature is required to make requests to PayPal"
