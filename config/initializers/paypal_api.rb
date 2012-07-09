c704c548692eaed105075b076c4248baba45aa69
Diogo Biazus <diogob@gmail.com>
2012-07-09 16:11:11 -0300

    moving express_gateway configs to the paypal_controller. the express_gateway does not need to be global nor to be in an initializer

--- config/initializers/paypal_api.rb
+++ config/initializers/paypal_api.rb
@@ -6,18 +6,4 @@ unless Rails.env.test?
     config.password = Configuration[:paypal_password]
     config.signature = Configuration[:paypal_signature]
   end
-else
-  Configuration.create!(name: "paypal_username", value: "usertest_api1.teste.com")
-  Configuration.create!(name: "paypal_password", value: "HVN4PQBGZMHKFVGW")
-  Configuration.create!(name: "paypal_signature", value: "AeL-u-Ox.N6Jennvu1G3BcdiTJxQAWdQcjdpLTB9ZaP0-Xuf-U0EQtnS")
-end
-
-if Configuration[:paypal_username] and Configuration[:paypal_password] and Configuration[:paypal_signature]
-  EXPRESS_GATEWAY = ActiveMerchant::Billing::PaypalExpressGateway.new({
-    login: Configuration[:paypal_username],
-    password: Configuration[:paypal_password],
-    signature: Configuration[:paypal_signature]
-  })
-else
-  puts "[PayPal] An API Certificate or API Signature is required to make requests to PayPal"
 end
