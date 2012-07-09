09ecadca608a087bb621ebf07526c56b4b703b96
Diogo Biazus <diogob@gmail.com>
2012-07-09 16:48:40 -0300

    removing commented out code

--- app/controllers/paypal_controller.rb
+++ app/controllers/paypal_controller.rb
@@ -75,33 +75,6 @@ class PaypalController < ApplicationController
       paypal_flash_error
       return redirect_to new_project_backer_path(backer.project)
     end
-
-
-
-    #backer = Backer.find params[:id]
-    #begin
-      #details = @paypal.details params[:token]
-      #payment = paypal_payment(backer)
-      #checkout = @paypal.checkout!(params[:token], details.payer.identifier, payment)
-      #if checkout.payment_info.first.payment_status == "Completed"
-        #backer.update_attributes({
-          #:key => checkout.payment_info.first.transaction_id,
-          #:payment_token => params[:token]
-        #})
-        #backer.build_payment_detail.update_from_service
-        #backer.confirm!
-        #paypal_flash_success
-        #redirect_to thank_you_path
-      #else
-        #Airbrake.notify({ :error_class => "Paypal Error", :error_message => "Paypal Error: #{checkout.payment_info.first.inspect}", :parameters => params}) rescue nil
-        #paypal_flash_error
-        #return redirect_to new_project_backer_path(backer.project)
-      #end
-    #rescue Exception => e
-      #Airbrake.notify({ :error_class => "Paypal Error", :error_message => "Paypal Error: #{e.message}", :parameters => params}) rescue nil
-      #paypal_flash_error
-      #return redirect_to new_project_backer_path(backer.project)
-    #end
   end
 
   def cancel
