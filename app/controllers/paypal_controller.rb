54eb8bca155cdbe423e47b8da4efb45e275c9bf5
Antônio Roberto Silva <forevertonny@gmail.com>
2012-07-10 11:10:44 -0300

    change paypal description and currency

--- app/controllers/paypal_controller.rb
+++ app/controllers/paypal_controller.rb
@@ -29,7 +29,7 @@ class PaypalController < ApplicationController
         ip: request.remote_ip,
         return_url: success_paypal_url(backer),
         cancel_return_url: cancel_paypal_url(backer),
-        currency_code: :USD,
+        currency_code: :BRL,
         description: t('paypal_description', scope: SCOPE, :project_name => backer.project.name, :value => backer.display_value),
         notify_url: notifications_paypal_url(backer)
       })
@@ -109,11 +109,11 @@ class PaypalController < ApplicationController
 
   def paypal_payment(backer)
     {
-      currency_code: :USD,
-      description: t('paypal_description', scope: SCOPE),
+      currency_code: :BRL,
+      description: t('paypal_description', scope: SCOPE, :project_name => backer.project.name, :value => backer.display_value),
       items: [{
         name: backer.project.name,
-        description: t('paypal_description', scope: SCOPE),
+        description: t('paypal_description', scope: SCOPE, :project_name => backer.project.name, :value => backer.display_value),
         amount: backer.value
       }]
     }
