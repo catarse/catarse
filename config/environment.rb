2f1e725f632f079e8ec61cb76acec63d1c756e74
Antônio Roberto Silva <forevertonny@gmail.com>
2012-07-11 10:22:37 -0300

    default currency

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Catarse::Application.initialize!
 
+ActiveMerchant::Billing::PaypalExpressGateway.default_currency = 'BRL'
+
