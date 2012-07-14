aed136aeaeb858c525193e5355a229943de7395f
Diogo Biazus <diogob@gmail.com>
2012-07-14 16:44:15 -0300

    fixed view for older versions of PostgreSQL

--- db/migrate/20120714183447_create_statistics.rb
+++ db/migrate/20120714183447_create_statistics.rb
@@ -12,7 +12,7 @@ class CreateStatistics < ActiveRecord::Migration
       total_projects_online
     FROM
       (SELECT count(*) AS total_backs, count(DISTINCT user_id) AS total_backers, sum(value) AS total_backed FROM backers WHERE confirmed) AS backers_totals,
-      (SELECT count(*) AS total_projects, count(CASE WHEN successful THEN 1 ELSE NULL END) total_projects_success, count(CASE WHEN finished = false AND expires_at >= current_timestamp THEN 1 ELSE NULL END) AS total_projects_online FROM projects WHERE visible) AS projects_totals
+      (SELECT count(*) AS total_projects, count(CASE WHEN successful THEN 1 ELSE NULL END) AS total_projects_success, count(CASE WHEN finished = false AND expires_at >= current_timestamp THEN 1 ELSE NULL END) AS total_projects_online FROM projects WHERE visible) AS projects_totals
 SQL
   end
 
