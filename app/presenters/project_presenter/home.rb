657da4ede7a77bc027f74f81fa32ac63b5f90709
Diogo Biazus <diogob@gmail.com>
2012-07-19 12:37:23 -0300

    removing project_of_day from projects controller (it was not used in any view). Also removed home_page scope from static controller, so we can remove the scope in the near future

--- app/presenters/project_presenter/home.rb
+++ app/presenters/project_presenter/home.rb
@@ -1,7 +1,7 @@
 module ProjectPresenter
   class Home
 
-    attr_accessor :recommended_project, :project_of_day, :second_project,
+    attr_accessor :recommended_project, :second_project,
                   :third_project, :fourth_project, :expiring, :recent, :current_user, :first_project
 
 
@@ -20,16 +20,12 @@ module ProjectPresenter
                                     not_expired.
                                     order('"order"')
       unless collection_projects.empty?
-        home_page_projects = collection_projects.home_page
         if current_user and current_user.recommended_project
           @recommended_project = current_user.recommended_project
-          home_page_projects = home_page_projects.where("id != #{current_user.recommended_project.id}")
           collection_projects = collection_projects.where("id != #{current_user.recommended_project.id}")
         end
         category_projects = collection_projects
         category_projects = category_projects.where("category_id != #{@recommended_project.category_id}") if @recommended_project
-        @project_of_day = home_page_projects.first
-        category_projects = category_projects.where("id != #{@project_of_day.id}") if @project_of_day
         @first_project = category_projects.all[rand(category_projects.length)]
         category_projects = category_projects.where("id != #{@first_project.id}") if @first_project
         @second_project = category_projects.all[rand(category_projects.length)]
@@ -41,7 +37,6 @@ module ProjectPresenter
 
       project_ids = []
       project_ids << @recommended_project.id if @recommended_project
-      project_ids << @project_of_the_day.id if @project_of_the_day
       project_ids << @first_project.id if @first_project
       project_ids << @second_project.id if @second_project
       project_ids << @third_project.id if @third_project
