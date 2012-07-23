348df5dd814f7a07116ecebf96ab52b7754c018c
Antônio Roberto Silva <forevertonny@gmail.com>
2012-07-23 09:00:52 -0300

    exception rescue on blog

--- lib/blog.rb
+++ lib/blog.rb
@@ -2,8 +2,12 @@ class Blog
   class << self
     def fetch_last_posts
       Rails.cache.fetch('blog_posts', expires_in: 10.minutes) do
-        feed = Feedzirra::Feed.fetch_and_parse("#{I18n.t('site.blog')}?feed=rss2")
-        feed.entries
+        begin
+          feed = Feedzirra::Feed.fetch_and_parse("#{I18n.t('site.blog')}?feed=rss2")
+          feed.entries
+        rescue
+          []
+        end
       end
     end
   end
