1ed2741edbfcb13629e9fe8693bf097becb7b81d
Diogo Biazus <diogob@gmail.com>
2012-07-30 22:32:26 -0300

    initial Adm::Backers structure

--- app/controllers/adm/backers_controller.rb
+++ app/controllers/adm/backers_controller.rb
@@ -1,4 +1,14 @@
 class Adm::BackersController < Adm::BaseController
   inherit_resources
-  menu "Apoiadores" => Rails.application.routes.url_helpers.adm_backers_path
+  menu I18n.t("admin.backers.menu") => Rails.application.routes.url_helpers.adm_backers_path
+  before_filter :set_title
+
+  protected
+  def set_title
+    @title = t("admin.backers.title")
+  end
+
+  def collection
+    @backers ||= end_of_association_chain.all
+  end
 end
