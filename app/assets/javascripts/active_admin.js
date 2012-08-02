2d83749b90cdcc134584125750e770f946674340
Antônio Roberto Silva <forevertonny@gmail.com>
2012-08-02 15:22:44 -0300

    active_admin things

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,11 +1,1 @@
-/* Active Admin JS */
-//= require ./lib/jquery-1.7.1.min.js
-//= require ./lib/jquery-ui-1.8.6.custom.min.js
-$(function(){
-  $(".datepicker").datepicker({dateFormat: 'yy-mm-dd'});
-
-  $(".clear_filters_btn").click(function(){
-    window.location.search = "";
-    return false;
-  });
-});
+//= require active_admin/base
