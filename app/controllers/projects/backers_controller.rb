787916775e432c9015c8ffd978ef57a5d9a575ea
Antônio Roberto Silva <forevertonny@gmail.com>
2012-08-08 09:25:25 -0300

    adjust exception

--- app/controllers/projects/backers_controller.rb
+++ app/controllers/projects/backers_controller.rb
@@ -93,7 +93,7 @@ class Projects::BackersController < ApplicationController
           backer.update_attribute :payment_token, response["Token"]
           session[:_payment_token] = response["Token"]
           redirect_to MoIP::Client.moip_page(response["Token"])
-        rescue
+        rescue Exception => e
           Airbrake.notify({ :error_class => "Checkout MOIP Error", :error_message => "MOIP Error: #{e.inspect}", :parameters => params}) rescue nil
           Rails.logger.info "-----> #{e.inspect}"
           flash[:failure] = t('projects.backers.checkout.moip_error')
