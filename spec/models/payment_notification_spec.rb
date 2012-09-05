802b4ecf42454563ae12342eca766036583c6d5e
Diogo Biazus <diogob@gmail.com>
2012-09-05 16:22:24 -0300

    removed refund and confirm methods from payment_notifications

--- spec/models/payment_notification_spec.rb
+++ spec/models/payment_notification_spec.rb
@@ -10,41 +10,4 @@ describe PaymentNotification do
     subject{ @p.extra_data }
     it{ should == test_hash }
   end
-
-  describe "#save!" do
-    before do
-      @backer = create(:backer, confirmed: false, refunded: false)
-      @payment = PaymentNotification.new backer: @backer
-    end
-
-    context "when status is confirmed" do
-      before do
-        @payment.status = 'confirmed'
-        @payment.save!
-      end
-
-      it "should not confirm the backer" do
-        @backer.confirmed?.should be_true
-      end
-
-      it "should refund the backer" do
-        @backer.refunded?.should be_false
-      end
-    end
-
-    context "when status is refunded" do
-      before do
-        @payment.status = 'refunded'
-        @payment.save!
-      end
-
-      it "should not confirm the backer" do
-        @backer.confirmed?.should be_false
-      end
-
-      it "should refund the backer" do
-        @backer.refunded?.should be_true
-      end
-    end
-  end
 end
