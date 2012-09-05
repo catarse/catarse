dcb683b69be77e990bb9a829412611136fd560f1
Diogo Biazus <diogob@gmail.com>
2012-09-05 15:25:22 -0300

    implemented confirm and refund behaviour in payment_notifications

--- app/models/payment_notification.rb
+++ app/models/payment_notification.rb
@@ -1,4 +1,16 @@
 class PaymentNotification < ActiveRecord::Base
   belongs_to :backer
   serialize :extra_data, JSON
+
+  after_save :refund_backer
+  after_save :confirm_backer
+
+  protected
+  def confirm_backer
+    backer.confirm! if self.status == 'confirmed'
+  end
+
+  def refund_backer
+    backer.update_attributes({refunded: true}) if self.status == 'refunded'
+  end
 end
