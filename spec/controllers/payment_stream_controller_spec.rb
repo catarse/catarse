29123ea2c4eeca2904bc83e50742004b75923db4
Diogo Biazus <diogob@gmail.com>
2012-09-05 16:49:57 -0300

    removed payment_history/moip class and fixed spec

--- spec/controllers/payment_stream_controller_spec.rb
+++ spec/controllers/payment_stream_controller_spec.rb
@@ -3,25 +3,6 @@ describe PaymentStreamController do
   render_views
   subject{ response }
 
-  describe 'POST /moip' do
-    before do
-      dummy_params = {'testing' => true}
-      dummy_moip.expects(:process_request!).returns(dummy_moip)
-      PaymentHistory::Moip.expects(:new).with(dummy_params.merge('controller' => 'payment_stream', 'action' => 'moip')).returns(dummy_moip)
-      post :moip, dummy_params
-    end
-
-    context "when moip's response_code is not successful" do
-      let(:dummy_moip){ stub(:process_request! => nil, :response_code => 500) }
-      it{ should_not be_successful }
-    end
-
-    context "when moip's response_code is successful" do
-      let(:dummy_moip){ stub(:process_request! => nil, :response_code => 200) }
-      it{ should be_successful }
-    end
-  end
-
   describe 'GET /thank_you' do
     context "when we do not have a without project / session" do
       before do
--- spec/models/payment_notification_spec.rb
+++ spec/models/payment_notification_spec.rb
@@ -4,7 +4,7 @@ describe PaymentNotification do
   describe "#extra_data" do
     let(:test_hash){{"test_hash" => 1}}
     before do
-      @p = PaymentNotification.new(backer_id: Factory(:backer).id, status: 'pending', extra_data: test_hash)
+      @p = PaymentNotification.new(backer_id: Factory(:backer).id, extra_data: test_hash)
       @p.save!
     end
     subject{ @p.extra_data }
