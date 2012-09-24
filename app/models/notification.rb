fc368a70e77c27cd9e102cfd043dfa04116f096c
Diogo Biazus <diogob@gmail.com>
2012-09-24 14:54:49 -0300

    now the notify_backer method receive the NotificationType name as a symbol

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -6,8 +6,13 @@ class Notification < ActiveRecord::Base
   validates_presence_of :user, :text
   scope :not_dismissed, where(:dismissed => false)
 
-  def self.notify_backer backer, notification_type
-    create! :backer => backer, :user => backer.user, :email_text => 'this will be removed', :text => 'this will be removed', :notification_type => notification_type
+  def self.notify_backer backer, notification_type_name
+    nt = NotificationType.where(:name => notification_type_name.to_s).first
+    if nt
+      create! :backer => backer, :user => backer.user, :email_text => 'this will be removed', :text => 'this will be removed', :notification_type_id => nt.id
+    else
+      raise "There is no NotificationType with name #{notification_type_name}"
+    end
   end
 
   def send_email
