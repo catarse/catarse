b4f19b52363b89de52a86da6225a579b423822f5
Diogo Biazus <diogob@gmail.com>
2012-09-24 17:17:38 -0300

    adjusting seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,26 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
-ActiveRecord::Base.connection.execute "INSERT INTO categories (name) VALUES ('Arte'),
- ('Artes plásticas'),
- ('Circo'),
- ('Comunidade'),
- ('Feito à mão'),
- ('Humor'),
- ('Quadrinhos'),
- ('Dança'),
- ('Design'),
- ('Eventos'),
- ('Moda'),
- ('Comida'),
- ('Cinema & Vídeo'),
- ('Jogos'),
- ('Jornalismo'),
- ('Música'),
- ('Fotografia'),
- ('Tecnologia'),
- ('Teatro'),
- ('Literatura');"
+ActiveRecord::Base.connection.execute "INSERT INTO categories (name, created_at, updated_at) VALUES 
+ ('Arte', current_timestamp, current_timestamp),
+ ('Artes plásticas', current_timestamp, current_timestamp),
+ ('Circo', current_timestamp, current_timestamp),
+ ('Comunidade', current_timestamp, current_timestamp),
+ ('Feito à mão', current_timestamp, current_timestamp),
+ ('Humor', current_timestamp, current_timestamp),
+ ('Quadrinhos', current_timestamp, current_timestamp),
+ ('Dança', current_timestamp, current_timestamp),
+ ('Design', current_timestamp, current_timestamp),
+ ('Eventos', current_timestamp, current_timestamp),
+ ('Moda', current_timestamp, current_timestamp),
+ ('Comida', current_timestamp, current_timestamp),
+ ('Cinema & Vídeo', current_timestamp, current_timestamp),
+ ('Jogos', current_timestamp, current_timestamp),
+ ('Jornalismo', current_timestamp, current_timestamp),
+ ('Música', current_timestamp, current_timestamp),
+ ('Fotografia', current_timestamp, current_timestamp),
+ ('Tecnologia', current_timestamp, current_timestamp),
+ ('Teatro', current_timestamp, current_timestamp),
+ ('Literatura', current_timestamp, current_timestamp);"
 
-ActiveRecord::Base.connection.execute "INSERT INTO notification_types (name) VALUES ('confirm_backer'), ('payment_slip');"
+ActiveRecord::Base.connection.execute "INSERT INTO notification_types (name, created_at, updated_at) VALUES ('confirm_backer', current_timestamp, current_timestamp), ('payment_slip', current_timestamp, current_timestamp);"
