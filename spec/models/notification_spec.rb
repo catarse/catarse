f13aa0a81537b33fbd4fcea84fd582e22f52fccf
Diogo Biazus <diogob@gmail.com>
2012-09-25 12:15:09 -0300

    now we use dismiss to avoid duplicate notifications

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -7,10 +7,28 @@ describe Notification do
   it{ should belong_to :backer }
 
   let(:backer){ Factory(:backer) }
+  let(:notification_type){ Factory(:notification_type, :name => 'confirm_backer') }
+
+  before{ ActionMailer::Base.deliveries.clear }
+
+  describe "#send_email" do
+    context "when dismissed is true" do
+      let(:notification){ Factory(:notification, :dismissed => true, :notification_type => notification_type) }
+      before{ notification.send_email }
+      it("should not send email"){ ActionMailer::Base.deliveries.should be_empty }
+    end
+
+    context "when dismissed is false" do
+      let(:notification){ Factory(:notification, :dismissed => false, :notification_type => notification_type) }
+      before{ notification.send_email }
+      it("should send email"){ ActionMailer::Base.deliveries.should_not be_empty }
+      it("should dismiss the notification"){ notification.dismissed.should be_true }
+    end
+  end
 
   describe ".notify_backer" do
     before do
-      Factory(:notification_type, :name => 'confirm_backer')
+      notification_type
     end
 
     context "when NotificationType with the provided name does not exist" do
@@ -20,6 +38,7 @@ describe Notification do
 
     context "when NotificationType with the provided name exists" do
       subject{ Notification.notify_backer(backer, :confirm_backer) }
+      its(:dismissed){ should be_true }
       its(:backer){ should == backer }
     end
   end
