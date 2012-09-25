af6b3289fa5e6422503e1dd0863044ee48588a95
Diogo Biazus <diogob@gmail.com>
2012-09-25 12:15:09 -0300

    now we use dismiss to avoid duplicate notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -8,13 +8,13 @@ class Notification < ActiveRecord::Base
 
   def self.find_notification notification_type_name
     nt = NotificationType.where(:name => notification_type_name.to_s).first
-    raise "There is no NotificationType with name #{notification_type_name}" if !nt
+    raise "There is no NotificationType with name #{notification_type_name}" unless nt
     return nt
   end
 
   def self.notify_backer backer, notification_type_name
     nt = find_notification notification_type_name
-    create! :project => project, :backer => backer, :user => backer.user, :email_text => 'this will be removed', :text => 'this will be removed', :notification_type_id => nt.id
+    create! :project => backer.project, :backer => backer, :user => backer.user, :email_text => 'this will be removed', :text => 'this will be removed', :notification_type_id => nt.id
   end
 
   def self.notify_project_owner project, notification_type_name
@@ -23,6 +23,9 @@ class Notification < ActiveRecord::Base
   end
 
   def send_email
-    NotificationsMailer.notify(self).deliver
+    unless dismissed
+      self.update_attributes :dismissed => true
+      NotificationsMailer.notify(self).deliver
+    end
   end
 end
