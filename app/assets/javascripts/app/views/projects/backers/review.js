c88150ac2a66d413f497fbfb96ca174b43f54feb
Diogo Biazus <diogob@gmail.com>
2012-10-02 17:12:40 -0300

    removed some unused comments

--- app/assets/javascripts/app/views/projects/backers/review.js
+++ app/assets/javascripts/app/views/projects/backers/review.js
@@ -7,12 +7,8 @@ CATARSE.BackersReviewView = Backbone.View.extend({
       if($('#backer_credits').val() == "false"){
         if(!ok('#user_full_name'))
           all_ok = false
-        //if(!cpf_ok())
-          //all_ok = false
         if(!email_ok())
           all_ok = false
-        //if(!zip_code_ok())
-          //all_ok = false
         if(!ok('#user_address_street'))
           all_ok = false
         if(!ok('#user_address_number'))
@@ -21,10 +17,6 @@ CATARSE.BackersReviewView = Backbone.View.extend({
           all_ok = false
         if(!ok('#user_address_city'))
           all_ok = false
-        //if(!ok('#user_address_state'))
-          //all_ok = false
-        //if(!phone_number_ok())
-          //all_ok = false
       }
       if(!accepted_terms())
         all_ok = false
