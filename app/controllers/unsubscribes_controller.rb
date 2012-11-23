8e2bee29e7554e41e9ba2d1a31585ab312386918
Diogo Biazus <diogob@gmail.com>
2012-11-23 17:24:26 -0200

    using create instead of new+save

--- app/controllers/unsubscribes_controller.rb
+++ app/controllers/unsubscribes_controller.rb
@@ -7,7 +7,7 @@ class UnsubscribesController < ApplicationController
       if u[:subscribed] == '1' && !u[:id].nil? #change from unsubscribed to subscribed
         parent.unsubscribes.where(project_id: u[:project_id], notification_type_id: u[:notification_type_id]).destroy_all
       elsif u[:subscribed] == '0' && u[:id].nil? #change from subscribed to unsubscribed
-        parent.unsubscribes.new(project_id: u[:project_id], notification_type_id: u[:notification_type_id]).save!
+        parent.unsubscribes.create!(project_id: u[:project_id], notification_type_id: u[:notification_type_id])
       end
     end
     flash[:notice] = t('users.current_user_fields.updated')
