06d15e6c7d05f2a1e5d8d09b86a209712e68221f
Antônio Roberto Silva <forevertonny@gmail.com>
2012-12-05 15:52:15 -0200

    adjust project controller spec to new cancan abilities

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -6,78 +6,45 @@ describe ProjectsController do
   render_views
   subject{ response }
 
-  shared_examples_for "access on :read :projects" do
-    describe "GET ProjectsController#show [projects/1]" do
-      before { get :show, id: project.id, locale: :pt}
-      it { should be_success }
-
-      context "when we have update_id in the querystring" do
-        let(:project){ Factory(:project, :permalink => nil) }
-        let(:update){ Factory(:update, :project => project) }
-        before{ get :show, :id => project, :update_id => update.id, :locale => :pt }
-        it("should assign update to @update"){ assigns(:update).should == update }
-      end
-
-      context "when we have permalink and do not pass permalink in the querystring" do
-        let(:project){ Factory(:project, :permalink => 'test') }
-        before{ get :show, :id => project, :locale => :pt }
-        it{ should redirect_to project_by_slug_path(project.permalink) }
-      end
-
-      context "when we do not have permalink and do not pass permalink in the querystring" do
-        let(:project){ Factory(:project, :permalink => nil) }
-        before{ get :show, :id => project, :locale => :pt }
-        it{ should be_success }
-      end
-    end
+  shared_examples_for "GET index/show" do
+    before { get :show, id: project.id, locale: :pt}
+    it { should be_success }
   end
 
-  shared_examples_for "access on :create :projects" do
-    describe "GET ProjectsController#new [projects/new]" do
-      before { get :new, locale: :pt }
-      it { should be_success }
-    end
+  shared_examples_for "GET new" do
+    before { get :new, locale: :pt }
+    it { should be_success }
   end
 
-  shared_examples_for "no access on :create :projects" do
-    describe "GET ProjectsController#new [projects/new]" do
-      before { get :new, locale: :pt }
-      it { should_not be_success }
-    end
+  shared_examples_for "GET new without permission" do
+    before { get :new, locale: :pt }
+    it { should_not be_success }
   end
 
-  shared_examples_for "access on :update :projects" do
-    describe "PUT ProjectsController#update [projects/1]" do
-      before { put :update, id: project.id, project: { name: 'My Updated Title' },locale: :pt }
-      it { 
-        project.reload
-        project.name.should == 'My Updated Title' 
-      }
-    end
+  shared_examples_for "PUT update" do
+    before { put :update, id: project.id, project: { name: 'My Updated Title' },locale: :pt }
+    it { 
+      project.reload
+      project.name.should == 'My Updated Title' 
+    }
   end
 
-  shared_examples_for "no access on :update :projects" do
-    describe "PUT ProjectsController#update [projects/1]" do
-      before { put :update, id: project.id, project: { name: 'My Updated Title' },locale: :pt }
-      it { 
-        project.reload
-        project.name.should == 'Foo bar' 
-      }
-    end
+  shared_examples_for "PUT update without permission" do
+    before { put :update, id: project.id, project: { name: 'My Updated Title' },locale: :pt }
+    it { 
+      project.reload
+      project.name.should == 'Foo bar' 
+    }
   end
 
-  shared_examples_for "access on :delete :projects" do
-    describe "DELETE ProjectsController#destroy [projects/1]" do
-      before { delete :destroy, id: project.id, locale: :pt }
-      it { Project.all.include?(project).should be_false }
-    end
+  shared_examples_for "DELETE destroy" do
+    before { delete :destroy, id: project.id, locale: :pt }
+    it { Project.all.include?(project).should be_false }
   end
 
-  shared_examples_for "no access on :delete :projects" do
-    describe "DELETE ProjectsController#destroy [projects/1]" do
-      before { delete :destroy, id: project.id, locale: :pt }
-      it { Project.all.include?(project).should be_true }
-    end
+  shared_examples_for "DELETE destroy without permission" do
+    before { delete :destroy, id: project.id, locale: :pt }
+    it { Project.all.include?(project).should be_true }
   end
 
   context "When current_user is a guest" do
@@ -87,13 +54,10 @@ describe ProjectsController do
       controller.stubs(:current_user).returns(nil)
     end
 
-    it_should_behave_like "access on :read :projects"
-
-    it_should_behave_like "no access on :create :projects"
-
-    it_should_behave_like "no access on :update :projects"
-
-    it_should_behave_like "no access on :delete :projects"
+    it_should_behave_like "GET index/show"
+    it_should_behave_like "GET new without permission"
+    it_should_behave_like "PUT update without permission"
+    it_should_behave_like "DELETE destroy without permission"
   end
 
   context "When current_user is a project owner" do
@@ -103,13 +67,10 @@ describe ProjectsController do
       controller.stubs(:current_user).returns(project.user)
     end
 
-    it_should_behave_like "access on :read :projects"
-
-    it_should_behave_like "access on :create :projects"
-
-    it_should_behave_like "access on :update :projects"
-
-    it_should_behave_like "no access on :delete :projects"
+    it_should_behave_like "GET index/show"
+    it_should_behave_like "GET new"
+    it_should_behave_like "PUT update"
+    it_should_behave_like "DELETE destroy without permission"
   end
 
   context "When current_user is admin" do
@@ -119,13 +80,10 @@ describe ProjectsController do
       controller.stubs(:current_user).returns(Factory(:user, admin: true))
     end
 
-    it_should_behave_like "access on :read :projects"
-
-    it_should_behave_like "access on :create :projects"
-
-    it_should_behave_like "access on :update :projects"
-
-    it_should_behave_like "access on :delete :projects"
+    it_should_behave_like "GET index/show"
+    it_should_behave_like "GET new"
+    it_should_behave_like "PUT update"
+    it_should_behave_like "DELETE destroy"
   end
 
   context "When current_user is a registered user" do
@@ -135,44 +93,63 @@ describe ProjectsController do
       controller.stubs(:current_user).returns(Factory(:user, admin: false))
     end
 
-    it_should_behave_like "access on :read :projects"
-
-    it_should_behave_like "access on :create :projects"
-
-    it_should_behave_like "no access on :update :projects"
-
-    it_should_behave_like "no access on :delete :projects"
+    it_should_behave_like "GET index/show"
+    it_should_behave_like "GET new"
+    it_should_behave_like "PUT update without permission"
+    it_should_behave_like "DELETE destroy without permission"
   end
 
-  #describe "GET new" do
-    #let(:user){ Factory(:user) }
-    #context "when I'm not logged in" do
-      #before{ get :new, :locale => :pt }
-      #it{ should redirect_to(login_path) }
-    #end
+  context "When project is online" do
+    let(:project) { Factory(:project, permalink: nil, state: 'online') }
 
-    #context "when I'm logged in" do
-      #before do
-        #controller.stubs(:current_user).returns(user)
-        #get :new, :locale => :pt
-      #end
-      #it{ should be_success }
-    #end
+    before do
+      controller.stubs(:current_user).returns(project.user)
+    end
 
-  #end
+    it "is not updated when pass attributes that is not allowed to update in some states" do
+      put :update, id: project.id, project: { name: 'new_title', about: 'new_description' }, locale: :pt
+      project.reload
+      project.about.should == 'Foo bar'
+    end
 
-  #describe "POST send_mail" do
-    #let(:project){ Factory(:project) }
-    #Factory(:notification_type, :name => 'project_received')
-    #let(:name){ project.name }
-    #let(:user){ project.user }
+    it "owner can update only about the project" do
+      put :update, id: project.id, project: { about: 'new_description' }, locale: :pt
+      project.reload
+      project.about.should == 'new_description'
+    end
+  end
 
-    #before do
-      #controller.stubs(:current_user).returns(user)
-      #Notification.expects(:create_notification).with(:project_received, user)
-      #post :send_mail, :locale => :pt
-    #end
+  context "when we have update_id in the querystring" do
+    let(:project){ Factory(:project, :permalink => nil) }
+    let(:update){ Factory(:update, :project => project) }
+    before{ get :show, :id => project, :update_id => update.id, :locale => :pt }
+    it("should assign update to @update"){ assigns(:update).should == update }
+  end
 
-    #it{ should redirect_to root_path }
-  #end
+  context "when we have permalink and do not pass permalink in the querystring" do
+    let(:project){ Factory(:project, :permalink => 'test') }
+    before{ get :show, :id => project, :locale => :pt }
+    it{ should redirect_to project_by_slug_path(project.permalink) }
+  end
+
+  context "when we do not have permalink and do not pass permalink in the querystring" do
+    let(:project){ Factory(:project, :permalink => nil) }
+    before{ get :show, :id => project, :locale => :pt }
+    it{ should be_success }
+  end
+
+  describe "POST send_mail" do
+    let(:project){ Factory(:project) }
+    Factory(:notification_type, :name => 'project_received')
+    let(:name){ project.name }
+    let(:user){ project.user }
+
+    before do
+      controller.stubs(:current_user).returns(user)
+      Notification.expects(:create_notification).with(:project_received, user)
+      post :send_mail, :locale => :pt
+    end
+
+    it{ should redirect_to root_path }
+  end
 end
