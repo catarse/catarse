8fbca1058d921185215f0be53620d1593d3a0943
Antônio Roberto Silva <forevertonny@gmail.com>
2012-12-05 09:16:54 -0200

    remove on_spot callbacks from ProjectsController

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,9 +6,7 @@ class ProjectsController < ApplicationController
   actions :index, :show, :new, :create, :update
   respond_to :html, :except => [:backers]
   respond_to :json, :only => [:index, :show, :backers, :update]
-  can_edit_on_the_spot
   skip_before_filter :detect_locale, :only => [:backers]
-  before_filter :can_update_on_the_spot?, :only => :update_attribute_on_the_spot
   before_filter :date_format_convert, :only => [:create]
 
   def udpate
@@ -220,30 +218,5 @@ class ProjectsController < ApplicationController
     res = Net::HTTP.start("api.bit.ly", 80) { |http| http.get("/v3/shorten?login=diogob&apiKey=R_76ee3ab860d76d0d1c1c8e9cc5485ca1&longUrl=#{CGI.escape(project_url(@project))}") }
     data = JSON.parse(res.body)['data']
     data['url'] if data
-  end
-
-  def can_update_on_the_spot?
-    project_fields = []
-    project_admin_fields = ["name", "about", "headline", "can_finish", "expires_at", "user_id", "image_url", "video_url", "visible", "rejected", "recommended", "permalink"]
-    backer_admin_fields = ["confirmed", "requested_refund", "refunded", "anonymous", "user_id"]
-    reward_fields = []
-    reward_admin_fields = ["description"]
-    def render_error; render :text => t('require_permission'), :status => 422; end
-    return render_error unless current_user
-    klass, field, id = params[:id].split('__')
-    return render_error unless klass == 'project' or klass == 'backer' or klass == 'reward'
-    if klass == 'project'
-      return render_error unless project_fields.include?(field) or (current_user.admin and project_admin_fields.include?(field))
-      project = Project.find id
-      return render_error unless current_user.id == project.user.id or current_user.admin
-    elsif klass == 'backer'
-      return render_error unless backer_fields.include?(field) or (current_user.admin and backer_admin_fields.include?(field))
-      backer = Backer.find id
-      return render_error unless current_user.admin or (backer.user == current_user)
-    elsif klass == 'reward'
-      return render_error unless reward_fields.include?(field) or (current_user.admin and reward_admin_fields.include?(field))
-      reward = Reward.find id
-      return render_error unless current_user.admin
-    end
   end
 end
