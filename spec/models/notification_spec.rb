ade2f59e68b489824e97868fe17b981b68bbfb88
Diogo Biazus <diogob@gmail.com>
2012-12-08 19:10:54 -0200

    now we store the update_id in the notification to avoid sending multiple notifications for the same update

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -5,9 +5,10 @@ describe Notification do
   it{ should belong_to :project }
   it{ should belong_to :notification_type }
   it{ should belong_to :backer }
+  it{ should belong_to :project_update }
 
   let(:backer){ Factory(:backer) }
-  let(:notification_type){ Factory(:notification_type, :name => 'confirm_backer') }
+  let(:notification_type){ Factory(:notification_type, name: 'confirm_backer') }
 
   before do
     Notification.unstub(:create_notification)
@@ -17,13 +18,13 @@ describe Notification do
 
   describe "#send_email" do
     context "when dismissed is true" do
-      let(:notification){ Factory(:notification, :dismissed => true, :notification_type => notification_type) }
+      let(:notification){ Factory(:notification, dismissed: true, notification_type: notification_type) }
       before{ notification.send_email }
       it("should not send email"){ ActionMailer::Base.deliveries.should be_empty }
     end
 
     context "when dismissed is false" do
-      let(:notification){ Factory(:notification, :dismissed => false, :notification_type => notification_type) }
+      let(:notification){ Factory(:notification, dismissed: false, notification_type: notification_type) }
       before{ notification.send_email }
       it("should send email"){ ActionMailer::Base.deliveries.should_not be_empty }
       it("should dismiss the notification"){ notification.dismissed.should be_true }
@@ -31,7 +32,7 @@ describe Notification do
   end
 
   describe ".create_notification" do
-    subject{ Notification.create_notification(:confirm_backer, backer.user, :backer => backer,  :project_name => backer.project.name) }
+    subject{ Notification.create_notification(:confirm_backer, backer.user, backer: backer,  project_name: backer.project.name) }
 
     context "when NotificationType with the provided name exists" do
       before{ notification_type }
@@ -42,13 +43,21 @@ describe Notification do
     context "when NotificationType with the provided name does not exist" do
       it{ should be_nil }
     end
+
+    context "when an update is provided" do
+      let(:update){ Factory(:update) }
+      before{ notification_type }
+      subject{ Notification.create_notification(:confirm_backer, backer.user, update: update, backer: backer,  project_name: backer.project.name) }
+      it{ should be_persisted }
+      its(:project_update){ should == update }
+    end
   end
 
   describe ".notify_backer" do
     before{ notification_type }
 
     context "when NotificationType with the provided name exists" do
-      subject{ Notification.create_notification(:confirm_backer, backer.user, :backer => backer,  :project_name => backer.project.name) }
+      subject{ Notification.create_notification(:confirm_backer, backer.user, backer: backer,  project_name: backer.project.name) }
       its(:dismissed){ should be_true }
       its(:backer){ should == backer }
     end
