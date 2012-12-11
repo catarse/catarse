67b5e202b8e558ecb14aee41728ba5ebd8e0ac28
Antônio Roberto Silva <forevertonny@gmail.com>
2012-12-11 11:31:45 -0200

    added urlRoot attribute on project model js

--- app/assets/javascripts/app/models/project.js
+++ app/assets/javascripts/app/models/project.js
@@ -1,4 +1,5 @@
 CATARSE.Project = Backbone.Model.extend({
+  urlRoot: '/projects',
 	initialize: function() {
 		this.backers = new CATARSE.Backers()
 		this.backers.url = '/' + CATARSE.locale + '/projects/' + this.id + '/backers'
