7152b3f69de774cfc5addd1a228b46a903953455
Diogo Biazus <diogob@gmail.com>
2012-12-18 23:44:51 -0200

    fixing specs and tidying up factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,15 +1,19 @@
 Factory.sequence :name do |n|
   "Foo bar #{n}"
 end
+
 Factory.sequence :email do |n|
   "person#{n}@example.com"
 end
+
 Factory.sequence :uid do |n|
   "#{n}"
 end
+
 Factory.sequence :permalink do |n|
   "foo_page_#{n}"
 end
+
 Factory.define :user do |f|
   f.provider "twitter"
   f.uid { Factory.next(:uid) }
@@ -17,9 +21,11 @@ Factory.define :user do |f|
   f.email { Factory.next(:email) }
   f.bio "This is Foo bar's biography."
 end
+
 Factory.define :category do |f|
   f.name { Factory.next(:name) }
 end
+
 Factory.define :project do |f|
   f.name "Foo bar"
   f.permalink { Factory.next(:permalink) }
@@ -31,25 +37,30 @@ Factory.define :project do |f|
   f.expires_at { 1.month.from_now }
   f.video_url 'http://vimeo.com/17298435'
 end
+
 Factory.define :notification_type do |f|
   f.name "confirm_backer"
 end
+
 Factory.define :unsubscribe do |f|
   f.association :user, :factory => :user
   f.association :project, :factory => :project
   f.association :notification_type, :factory => :notification_type
 end
+
 Factory.define :notification do |f|
   f.association :user, :factory => :user
   f.association :backer, :factory => :backer
   f.association :project, :factory => :project
   f.association :notification_type, :factory => :notification_type
 end
+
 Factory.define :reward do |f|
   f.association :project, :factory => :project
   f.minimum_value 1.00
   f.description "Foo bar"
 end
+
 Factory.define :backer do |f|
   f.association :project, :factory => :project
   f.association :user, :factory => :user
@@ -57,10 +68,12 @@ Factory.define :backer do |f|
   f.confirmed_at Time.now
   f.value 10.00
 end
+
 Factory.define :payment_notification do |f|
   f.association :backer, :factory => :backer
   f.extra_data {}
 end
+
 Factory.define :oauth_provider do |f|
   f.name 'GitHub'
   f.strategy 'GitHub'
@@ -68,17 +81,20 @@ Factory.define :oauth_provider do |f|
   f.key 'test_key'
   f.secret 'test_secret'
 end
+
 Factory.define :configuration do |f|
   f.name 'Foo'
   f.value 'Bar'
 end
+
 Factory.define :curated_page do |f|
   f.name 'Foo Page'
   f.permalink { Factory.next(:permalink) }
   f.description 'foo description'
-  f.logo File.open("#{Rails.root.to_s}/lib/fixtures/engage.png")
+  f.logo File.open("#{Rails.root}/spec/fixtures/image.png")
   f.video_url 'http://vimeo.com/28220980'
 end
+
 Factory.define :projects_curated_page do |f|
   f.association :project, :factory => :project
   f.association :curated_page, :factory => :curated_page
