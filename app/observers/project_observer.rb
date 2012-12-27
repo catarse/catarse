e1b32eda00ba7f0c9e9b99b3dbd6dd8b77d6d864
Adriano Benin <adriano.benin@gmail.com>
2012-12-27 18:01:10 -0200

    use create_notification_once where it makes sense to

--- app/observers/project_observer.rb
+++ app/observers/project_observer.rb
@@ -7,21 +7,39 @@ class ProjectObserver < ActiveRecord::Observer
   end
 
   def notify_owner_that_project_is_online(project)
-    Notification.create_notification(:project_visible, project.user, project: project)
+    Notification.create_notification_once(:project_visible,
+      project.user,
+      {project_id: project.id},
+      project: project)
   end
 
   def notify_users(project)
     project.backers.confirmed.each do |backer|
       unless backer.can_refund? or backer.notified_finish
         if project.successful?
-          Notification.create_notification(:backer_project_successful, backer.user, :backer => backer, :project => project, :project_name => project.name)
+          Notification.create_notification_once(
+            :backer_project_successful,
+            backer.user,
+            {project_id: project.id, user_id: backer.user.id},
+            backer: backer,
+            project: project,
+            project_name: project.name)
         else
-          Notification.create_notification(:backer_project_unsuccessful, backer.user, :backer => backer, :project => project, :project_name => project.name)
+          Notification.create_notification_once(
+            :backer_project_unsuccessful,
+            backer.user,
+            {project_id: project.id, user_id: backer.user.id},
+            backer: backer,
+            project: project,
+            project_name: project.name)
         end
         backer.update_attributes({ notified_finish: true })
       end
     end
-    Notification.create_notification( :project_unsuccessful, project.user, :project => project) unless project.successful?
+    Notification.create_notification_once(:project_unsuccessful,
+      project.user,
+      {project_id: project.id, user_id: project.user.id},
+      project: project) unless project.successful?
     project.update_attributes finished: true, successful: project.successful?
   end
 
