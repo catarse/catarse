15c20f21f206e1edb6c78fa3706519004ccf6376
Antônio Roberto Silva <forevertonny@gmail.com>
2013-01-05 11:05:17 -0200

    new project/new form

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,10 @@ class Category < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
   def self.with_projects
-    where("id IN (SELECT DISTINCT category_id FROM projects WHERE visible)")
+    where("id IN (SELECT DISTINCT category_id FROM projects WHERE state <> 'draft' AND state <> 'rejected')")
+  end
+
+  def self.array
+    order('name ASC').collect { |c| [c.name, c.id] }
   end
 end
