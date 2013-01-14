b504d4b5f105a3bb76b120e5e7b5c73599c4659c
Adriano Benin <adriano.benin@gmail.com>
2013-01-14 17:13:32 -0200

    move load_default_config task to db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,3 +21,19 @@
 'project_visible','processing_payment','new_draft_project'].each do |name|
   NotificationType.find_or_create_by_name name
 end
+
+{host: 'catarse.me',
+base_url: "http://catarse.me",
+blog_url: "http://blog.catarse.me",
+email_contact: 'contato@catarse.me',
+email_payments: 'financeiro@catarse.me',
+email_projects: 'projetos@catarse.me',
+email_system: 'system@catarse.me',
+email_no_reply: 'no-reply@catarse.me',
+facebook_url: "http://facebook.com/catarse.me",
+facebook_app_id: '173747042661491',
+twitter_username: "Catarse_",
+mailchimp_url: "http://catarse.us5.list-manage.com/subscribe/post?u=ebfcd0d16dbb0001a0bea3639&amp;id=149c39709e",
+}.each do |name, value|
+  Configuration.find_or_create_by_name_and_value name, value
+end
