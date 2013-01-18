5ece8c8255632fa3bdb64c10f2b046f33caa4bef
Adriano Benin <adriano.benin@gmail.com>
2013-01-18 16:51:16 -0200

    fix blog posts not displaying

--- lib/blog.rb
+++ lib/blog.rb
@@ -3,8 +3,7 @@ class Blog
     def fetch_last_posts
       Rails.cache.fetch('blog_posts', expires_in: 10.minutes) do
         begin
-          raise 'test'
-          feed = Feedzirra::Feed.fetch_and_parse("#{I18n.t('site.blog')}?feed=rss2")
+          feed = Feedzirra::Feed.fetch_and_parse("#{Configuration[:blog_url]}?feed=rss2")
           feed.entries
         rescue
           []
