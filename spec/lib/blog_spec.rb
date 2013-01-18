583c1acd90939a212242479495a34a3edc874d33
Diogo Biazus <diogob@gmail.com>
2013-01-18 14:29:19 -0200

    fixed can_back? and finish bugs in Project

---
+++ spec/lib/blog_spec.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe Blog do
+  describe ".fetch_last_posts" do
+    it "should fetch last posts from the blog url in configuration" do
+      Configuration[:blog_url] = 'test'
+      #Feedzirra::Feed.expects(:fetch_and_parse).with("test?feed=rss2")
+      Blog.fetch_last_posts
+    end
+  end
+end
