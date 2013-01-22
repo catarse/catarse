7ae60fbf2b1e113c9e9e82c201b54426260822c9
Diogo Biazus <diogob@gmail.com>
2013-01-22 17:56:35 -0200

    removing more active_admin stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,8 +51,6 @@ Catarse::Application.configure do
   # Enable threaded mode
   # config.threadsafe!
 
-  config.assets.precompile += %w[active_admin.js active_admin.css]
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
