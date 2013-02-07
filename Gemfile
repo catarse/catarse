29db1f36090e75df69a43fbc6e82431eff0dbb2f
Adriano Benin <adriano.benin@gmail.com>
2013-02-07 15:05:19 -0200

    remove on_the_spot

--- Gemfile
+++ Gemfile
@@ -69,7 +69,6 @@ gem 'spectator-validates_email', require: 'validates_email'
 gem 'has_vimeo_video', '~> 0.0.5'
 gem 'memoist', '~> 0.2.0'
 gem 'wirble'
-gem "on_the_spot"
 gem 'weekdays'
 gem 'brcep'
 gem "RedCloth"
--- app/controllers/adm/backers_controller.rb
+++ app/controllers/adm/backers_controller.rb
@@ -1,14 +1,26 @@
 class Adm::BackersController < Adm::BaseController
   inherit_resources
-  can_edit_on_the_spot
   menu I18n.t("adm.backers.index.menu") => Rails.application.routes.url_helpers.adm_backers_path
   before_filter :set_title
-  before_filter :can_update_on_the_spot?, :only => :update_attribute_on_the_spot
 
   def confirm
     resource.confirm!
     flash[:notice] = I18n.t('adm.backers.messages.successful.confirm')
     redirect_to adm_backers_path
+  end
+
+  def update
+    @backer = Backer.find params[:id]
+
+    respond_to do |format|
+      if @backer.update_attributes(params[:backer])
+        format.html { redirect_to(adm_backers_path) }
+        format.json { respond_with_bip(@backer) }
+      else
+        format.html { render :action => "edit" }
+        format.json { respond_with_bip(@user) }
+      end
+    end
   end
 
   def unconfirm
@@ -18,32 +30,6 @@ class Adm::BackersController < Adm::BaseController
   end
 
   protected
-  def can_update_on_the_spot?
-    project_fields = []
-    backer_fields = []
-    project_admin_fields = ["name", "about", "headline", "can_finish", "expires_at", "user_id", "image_url", "video_url", "visible", "rejected", "recommended", "permalink"]
-    backer_admin_fields = ["confirmed", "requested_refund", "refunded", "anonymous", "user_id"]
-    reward_fields = []
-    reward_admin_fields = ["description"]
-    def render_error; render :text => t('require_permission'), :status => 422; end
-    return render_error unless current_user
-    klass, field, id = params[:id].split('__')
-    return render_error unless klass == 'project' or klass == 'backer' or klass == 'reward'
-    if klass == 'project'
-      return render_error unless project_fields.include?(field) or (current_user.admin and project_admin_fields.include?(field))
-      project = Project.find id
-      return render_error unless current_user.id == project.user.id or current_user.admin
-    elsif klass == 'backer'
-      return render_error unless backer_fields.include?(field) or (current_user.admin and backer_admin_fields.include?(field))
-      backer = Backer.find id
-      return render_error unless current_user.admin or (backer.user == current_user)
-    elsif klass == 'reward'
-      return render_error unless reward_fields.include?(field) or (current_user.admin and reward_admin_fields.include?(field))
-      reward = Reward.find id
-      return render_error unless current_user.admin
-    end
-  end
-
   def set_title
     @title = t("adm.backers.index.title")
   end
