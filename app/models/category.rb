06f99a83936d11ce499f8c271ebd5c6a4efd3120
Adriano Benin <adriano.benin@gmail.com>
2013-02-12 13:58:13 -0200

    rename category name column for code simplicity

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,21 +1,17 @@
 class Category < ActiveRecord::Base
   has_many :projects
-  validates_presence_of :name
-  validates_uniqueness_of :name
+  validates_presence_of :name_pt
+  validates_uniqueness_of :name_pt
 
   def self.with_projects
     where("id IN (SELECT DISTINCT category_id FROM projects WHERE state <> 'draft' AND state <> 'rejected')")
   end
 
   def self.array
-    if I18n.locale == :pt
-      order('name ASC').collect { |c| [c.name, c.id] }
-    elsif I18n.locale == :en
-      order('name_en ASC').collect { |c| [c.name_en, c.id] }
-    end
+    order('name_'+ I18n.locale.to_s + ' ASC').collect { |c| [c.send('name_' + I18n.locale.to_s), c.id] }
   end
 
   def to_s
-    I18n.locale == :pt ? name : name_en
+    self.send('name_' + I18n.locale.to_s)
   end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,13 +6,13 @@ describe Category do
     c.should be_valid
   end
   it "should have a name" do
-    c = Factory.build(:category, :name => nil)
+    c = Factory.build(:category, :name_pt => nil)
     c.should_not be_valid
   end
   it "should have an unique name" do
-    c = Factory(:category, :name => "foo")
+    c = Factory(:category, :name_pt => "foo")
     c.should be_valid
-    c2 = Factory.build(:category, :name => "foo")
+    c2 = Factory.build(:category, :name_pt => "foo")
     c2.should_not be_valid
   end
 end
