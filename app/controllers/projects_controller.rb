2eab3ac3d959c5c6ec6ef410855b02580e2d0379
Diogo Biazus <diogob@gmail.com>
2013-02-13 18:37:45 -0200

    replaced every project_path and project_url by project_bt_slug_path and project_by_slug_url respectively

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -159,7 +159,7 @@ class ProjectsController < ApplicationController
   def bitly
     return unless Rails.env.production?
     require 'net/http'
-    res = Net::HTTP.start("api.bit.ly", 80) { |http| http.get("/v3/shorten?login=diogob&apiKey=R_76ee3ab860d76d0d1c1c8e9cc5485ca1&longUrl=#{CGI.escape(project_url(@project))}") }
+    res = Net::HTTP.start("api.bit.ly", 80) { |http| http.get("/v3/shorten?login=diogob&apiKey=R_76ee3ab860d76d0d1c1c8e9cc5485ca1&longUrl=#{CGI.escape(project_by_slug_url(permalink: @project.permalink))}") }
     data = JSON.parse(res.body)['data']
     data['url'] if data
   end
