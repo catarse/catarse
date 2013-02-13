ea0b26ca16f560ceea62f2b942de7ccccf0e444d
Adriano Benin <adriano.benin@gmail.com>
2013-02-13 15:14:56 -0200

    refactor adm controllers

--- app/controllers/adm/projects_controller.rb
+++ app/controllers/adm/projects_controller.rb
@@ -1,25 +1,10 @@
 class Adm::ProjectsController < Adm::BaseController
-  inherit_resources
   menu I18n.t("adm.projects.index.menu") => Rails.application.routes.url_helpers.adm_projects_path
 
   has_scope :by_id, :pg_search, :user_name_contains, :order_table
 
   before_filter do
     @total_projects = Project.count
-  end
-
-  def update
-    @project = Project.find params[:id]
-
-    respond_to do |format|
-      if @project.update_attributes(params[:project])
-        format.html { redirect_to(adm_projects_path) }
-        format.json { respond_with_bip(@project) }
-      else
-        format.html { render :action => "edit" }
-        format.json { respond_with_bip(@user) }
-      end
-    end
   end
 
   [:approve, :reject, :push_to_draft].each do |name|
