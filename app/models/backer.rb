b6893b5afdcc4caccc2189297fb9412f9ca0afb3
Antônio Roberto Silva <forevertonny@gmail.com>
2013-02-21 09:50:04 -0300

    when click on user email the adm/backers should respond with user_id search and not backer_id search

--- app/models/backer.rb
+++ app/models/backer.rb
@@ -11,6 +11,7 @@ class Backer < ActiveRecord::Base
   validate :reward_must_be_from_project
   scope :by_id, ->(id) { where(id: id) }
   scope :by_key, ->(key) { where(key: key) }
+  scope :by_user_id, ->(user_id) { where(user_id: user_id) }
   scope :user_name_contains, ->(term) { joins(:user).where("unaccent(upper(users.name)) LIKE ('%'||unaccent(upper(?))||'%')", term) }
   scope :project_name_contains, ->(term) { joins(:project).where("unaccent(upper(projects.name)) LIKE ('%'||unaccent(upper(?))||'%')", term) }
   scope :anonymous, where(:anonymous => true)
