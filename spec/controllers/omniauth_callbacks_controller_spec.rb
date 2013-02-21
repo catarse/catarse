7ee7e0da3f8be6af5493b8bcde309a57cd96976a
Diogo Biazus <diogob@gmail.com>
2013-02-21 13:01:26 -0300

    put warning in README about oficial repo

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe OmniauthCallbacksController do
-
+  describe "GET facebook" do
+#let(:extra){ bio: "I simply am not there", email: "diogob@gmail.com", first_name: "Diogo", gender: "male", id: "547955110", last_name: "Biazus", link: "http://www.facebook.com/diogo.biazus", locale: "pt_BR", name: "Diogo Biazus", timezone: -3, updated_time: "2012-08-01T18:22:50+0000" username="diogo.biazus" verified=true>> info=#<OmniAuth::AuthHash::InfoHash description="I simply am not there" email="diogob@gmail.com" first_name="Diogo" image="http://graph.facebook.com/547955110/picture?type=square" last_name="Biazus" name="Diogo Biazus" nickname="diogo.biazus" urls=#<Hashie::Mash Facebook="http://www.facebook.com/diogo.biazus"> verified=true> provider="facebook" uid="547955110"
+    #before do
+      #get :facebook
+    #end
+  end
 end
