c258a5cc05bb7887a8034d84908100f635466165
Diogo Biazus <diogob@gmail.com>
2013-02-22 12:40:25 -0300

    fixed problem with schema_associations and oauth_providers

--- app/models/oauth_provider.rb
+++ app/models/oauth_provider.rb
@@ -1,3 +1,6 @@
 class OauthProvider < ActiveRecord::Base
-  schema_associations rescue puts "problem loading schema_associations, maybe it has not been defined yet"
+  # schema_associations was not working well here, 
+  # maybe because we need this model during the application initialization
+  # Not a big deal since we have only one association
+  has_many :authorizations 
 end
