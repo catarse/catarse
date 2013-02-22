f18740608663a48402c942247e084dd36c1248fa
Diogo Biazus <diogob@gmail.com>
2013-02-22 13:48:40 -0300

    put some error handling in notifications delivery

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -25,8 +25,14 @@ class Notification < ActiveRecord::Base
 
   def send_email
     unless dismissed
-      self.update_attributes dismissed: true
-      NotificationsMailer.notify(self).deliver
+      begin
+        NotificationsMailer.notify(self).deliver
+        self.update_attributes dismissed: true
+      rescue Exception => e
+        Rails.logger.error "Error while delivering email (#{e}).\n
+        Check your configurations sendgrid and sendgrid_user_name\n
+        Your notification was stored in the database with dismissed field set to false"
+      end
     end
   end
 
