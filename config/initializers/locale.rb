e06f49fce85489ad9225ce3d0dc14391f312fd13
Mike Pence <mike.pence@gmail.com>
2013-03-07 16:39:53 -0500

    Don't default to Portugese

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -4,6 +4,3 @@ I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
 # set available locales from config/locales files
 I18n.available_locales = Dir[Rails.root.join('config', 'locales', '*.{yml}')].collect {|l| locale = l.scan(/.+\/(..)\.yml/)[0]; locale[0].to_sym unless locale.nil?}.delete_if {|locale| locale.nil?}
-
-# set default locale to something other than :en
-I18n.default_locale = :pt
