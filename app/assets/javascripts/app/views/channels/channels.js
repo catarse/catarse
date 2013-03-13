39ee6808dd14062c41fcbb51ac68f857bc320fe1
Luiz Fonseca <runeroniek@gmail.com>
2013-03-13 04:56:18 -0300

    The user has now ability to create projects within a Channel
    Inherit views and resources from ProjectController and insert
    after_filter for projects in channels. This way, it's working out of
    the box.

---
+++ app/assets/javascripts/app/views/channels/channels.js
@@ -0,0 +1,31 @@
+CATARSE.channels = {
+  profiles: {
+
+    show: Backbone.View.extend({
+
+      el: 'body',
+
+      initialize: function(){
+        this.banner = this.$('.call_to_action'),
+        this.setupBackground();
+      },
+
+      setupBackground: function(){
+        this.banner.css({"background": 'url("' + 
+                        this.banner.data('background') + '") no-repeat center',
+                      'opacity': 1 
+        });
+      }
+    }), 
+    // End Show
+  },
+
+  projects: {
+    new: function() {
+          $('#project_form').attr('action', window.location.pathname.slice(0,-4));
+          return new CATARSE.ProjectsNewView();
+    },
+  }
+}
+
+
