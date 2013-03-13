ca969985e0dc21e4bd9c1aed26f862a7b5d01d5d
Luiz Fonseca <runeroniek@gmail.com>
2013-03-13 01:31:47 -0300

    Instead of using match, use resources on channels profiles
    And let the resource :profiles do the trick. With path as nil,
    channels/:permalink is the same, but much better (and we can use finder
    method from inherited resources).

--- app/controllers/channels/profiles_controller.rb
+++ app/controllers/channels/profiles_controller.rb
@@ -1,12 +1,9 @@
 class Channels::ProfilesController < Channels::BaseController
   inherit_resources
+  defaults resource_class: Channel, finder: :find_by_permalink!
   actions :show
 
 
-  def show
-    @profile = Channel::Profile.find_by_permalink!(params[:permalink])
-    show!
-  end
 
 
 
