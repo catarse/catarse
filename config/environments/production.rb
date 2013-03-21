11f323f6a0ea094a76b206c71ff3ccfe623d5e2b
Diogo Biazus <diogob@gmail.com>
2013-03-21 04:52:28 -0300

    sorry, wrong cache_store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,6 @@ Catarse::Application.configure do
   # config.logger = SyslogLogger.new
 
   # Use a different cache store in production
-  config.cache_store = :mem_cache_store
   #config.cache_store = :dalli_store
 
   # Disable Rails's static asset server
