56db5d67359a4e74c49329600b935c219391bc9f
Antônio Roberto Silva <forevertonny@gmail.com>
2013-04-01 13:03:35 -0300

    remove new from Statistics on initializer because when project is new the migrations broken

--- config/initializers/statistics_loader_development.rb
+++ config/initializers/statistics_loader_development.rb
@@ -1,4 +1,4 @@
 # Loading the Statistic model before it tries to read from the cache
 # In development, so we can avoid the `Undefined class/module Statistics` 
 #
-Statistics.new if Rails.env.development?
+Statistics if Rails.env.development?
