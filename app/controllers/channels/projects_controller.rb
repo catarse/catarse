b28d2cd5623c9bc68a2f2a661e200fe68b19ecfa
Diogo Biazus <diogob@gmail.com>
2013-04-12 11:57:35 -0300

    added the subdomain to routes (still hardcoded) and put subdomain logic into controllers and views [#47977937]

--- app/controllers/channels/projects_controller.rb
+++ app/controllers/channels/projects_controller.rb
@@ -5,6 +5,7 @@ class Channels::ProjectsController < ProjectsController
   # Inheriting from the original Projects controller
   # With one addition: we include the project into the current channel
   after_filter only: [:create] { @project.channels << @channel }
+  prepend_before_filter{ params[:profile_id] = request.subdomain }
 
 end
 
