4cffb59eb61978f0c5c60f27b77a908280c2d89b
Adriano Benin <adriano.benin@gmail.com>
2013-04-22 15:57:38 -0300

    csv search report on adm/financials #changelog [Fix #46463469]

--- app/controllers/adm/financials_controller.rb
+++ app/controllers/adm/financials_controller.rb
@@ -1,14 +1,48 @@
 class Adm::FinancialsController < Adm::BaseController
+  require 'csv'
   inherit_resources
   defaults  resource_class: Project, collection_name: 'projects', instance_name: 'project'
   menu I18n.t("adm.financials.index.menu") => Rails.application.routes.url_helpers.adm_financials_path
   has_scope :by_permalink, :name_contains, :user_name_contains, :financial, :by_state, :by_progress
   has_scope :between_expires_at, using: [ :start_at, :ends_at ], allow_blank: true
-    
+
   actions :index
 
-  def collection
-    @search = apply_scopes(Project).includes(:user)
-    @projects = @search.order("CASE state WHEN 'successful' THEN 1 WHEN 'waiting_funds' THEN 2 ELSE 3 END, expires_at::date").page(params[:page])
+  def projects
+    @search = apply_scopes(Project).includes(:user).order("CASE state WHEN 'successful' THEN 1 WHEN 'waiting_funds' THEN 2 ELSE 3 END, expires_at::date")
   end
+
+  def collection
+    @projects = projects.page(params[:page])
+  end
+
+  def index
+    respond_to do |format|
+      format.html {collection}
+      format.csv do
+        csv_string = CSV.generate do |csv|
+          # header row
+          csv << ["name", "moip", "goal", "reached", "moip_tax", "catarse_fee", "expires_at", "backer_report", "state"]
+          # data rows
+          projects.each do |project|
+            catarse_fee = ::Configuration[:catarse_fee].to_f * project.pledged
+            csv << [project.name,
+                    project.user.moip_login,
+                    project.display_goal,
+                    "#{project.display_pledged} (#{project.progress}%)",
+                    "#{view_context.number_to_currency project.backers.confirmed.sum('payment_service_fee'), :unit => 'R$', :precision => 2, :delimiter => '.'}",
+                    "#{view_context.number_to_currency catarse_fee, :unit => 'R$', :precision => 0, :delimiter => '.' } (#{view_context.number_to_currency project.pledged - catarse_fee, :unit => 'R$', :precision => 0, :delimiter => '.'})",
+                     "#{project.display_expires_at} (#{I18n.l(8.weekdays_from(project.expires_at).to_date)})",
+                     "#{adm_reports_backer_reports_url(project_id: project.id, format: :csv)}",
+                     project.state
+            ]
+          end
+        end
+        send_data csv_string,
+                  :type => 'text/csv; charset=iso-8859-1; header=present',
+                  :disposition => "attachment; filename=financials.csv"
+      end
+    end
+  end
+
 end
