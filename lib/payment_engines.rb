ff6ac2c7ac46558d1f6ca1cb6132f9f00f2c5aa4
Diogo Biazus <diogob@gmail.com>
2013-05-14 17:36:29 -0300

    implemented a new interface to isolate methods needed by payment_engines

--- lib/payment_engines.rb
+++ lib/payment_engines.rb
@@ -11,4 +11,29 @@ class PaymentEngines
   def self.engines
     @@engines.sort{|a,b| (a[:locale] == I18n.locale.to_s ? -1 : 1) }
   end
+
+  def self.confirm! filter
+    find_payment(filter).confirm!
+  end
+
+  def self.refund! filter
+    find_payment(filter).refund!
+  end
+
+  def self.cancel! filter
+    find_payment(filter).cancel!
+  end
+
+  def self.create_payment_notification filter, data
+    find_payment(filter).payment_notifications.create extra_data: data
+  end
+
+  def self.update_payment_data filter, attributes
+    find_payment(filter).update_attributes attributes
+  end
+
+  protected
+  def self.find_payment filter
+    Backer.where(filter).first
+  end
 end
