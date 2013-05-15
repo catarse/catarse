300e930086c7397972499f853f69194b363591b8
Diogo Biazus <diogob@gmail.com>
2013-05-14 23:10:31 -0300

    lets make a simpler interface for payment engine. Other than that we cannot trust class variables for concurrency in Rails AFAIK

--- lib/payment_engines.rb
+++ lib/payment_engines.rb
@@ -1,4 +1,6 @@
 class PaymentEngines
+  cattr_accessor :backer
+
   def self.register options
     @@engines ||= []
     @@engines.push(options)
@@ -12,28 +14,7 @@ class PaymentEngines
     @@engines.sort{|a,b| (a[:locale] == I18n.locale.to_s ? -1 : 1) }
   end
 
-  def self.confirm! filter
-    find_payment(filter).confirm!
-  end
-
-  def self.refund! filter
-    find_payment(filter).refund!
-  end
-
-  def self.cancel! filter
-    find_payment(filter).cancel!
-  end
-
-  def self.create_payment_notification filter, data
-    find_payment(filter).payment_notifications.create extra_data: data
-  end
-
-  def self.update_payment_data filter, attributes
-    find_payment(filter).update_attributes attributes
-  end
-
-  protected
   def self.find_payment filter
-    Backer.where(filter).first
+    self.backer = Backer.where(filter).first
   end
 end
--- spec/lib/payment_engines_spec.rb
+++ spec/lib/payment_engines_spec.rb
@@ -23,67 +23,13 @@ describe PaymentEngines do
     it{ should be_empty }
   end
 
-  describe ".refund!" do
-    let(:backer){ FactoryGirl.create(:backer, state: 'confirmed') }
+  describe ".find_payment" do
     before do
-      PaymentEngines.refund!({ id: backer.id })
+      PaymentEngines.find_payment({ id: backer.id })
     end
 
-    it "should change backer status to refunded" do
-      backer.reload.refunded?.should be_true
-    end
-  end
-
-  describe ".confirm!" do
-    before do
-      PaymentEngines.confirm!({ id: backer.id })
-    end
-
-    it "should change backer status to confirmed" do
-      backer.reload.confirmed?.should be_true
-    end
-  end
-
-  describe ".cancel!" do
-    before do
-      PaymentEngines.cancel!({ id: backer.id })
-    end
-
-    it "should change backer status to canceled" do
-      backer.reload.canceled?.should be_true
-    end
-  end
-
-  describe ".create_payment_notification" do
-    let(:data){ { 'test' => true } }
-    before do
-      PaymentEngines.create_payment_notification({ id: backer.id }, data)
-    end
-
-    it "should create payment notification with data" do
-      backer.reload.payment_notifications.first.extra_data.should == data
-    end
-  end
-
-  describe ".update_payment_data" do
-    before do
-      PaymentEngines.update_payment_data({ id: backer.id }, {
-        payment_id: 'payment id',
-        payment_choice: 'payment choice',
-        payment_service_fee: 99.6
-      })
-    end
-
-    it "should update payment id" do
-      backer.reload.payment_id.should == 'payment id'
-    end
-
-    it "should update payment choice" do
-      backer.reload.payment_choice.should == 'payment choice'
-    end
-
-    it "should update payment service fee" do
-      backer.reload.payment_service_fee.should == 99.6
+    it "it should assign backer to self.backer" do
+      PaymentEngines.backer.should == backer
     end
   end
 
