d64750fb7ac92ac24547c2e0542c72adb492777a
Antônio Roberto Silva <forevertonny@gmail.com>
2013-05-29 16:20:05 -0300

    use user_url and avatar on uservoice [Fix #41820863]

--- lib/uservoice_sso.rb
+++ lib/uservoice_sso.rb
@@ -7,16 +7,14 @@ module Uservoice
   class Token
     attr_accessor :data
 
-    def initialize(options = {})
+    def self.generate(options = {})
       options.merge!({expires: (Time.zone.now.utc+5*60).to_s})
 
       key = EzCrypto::Key.with_password ::Configuration[:uservoice_subdomain], ::Configuration[:uservoice_sso_key]
       encrypted = key.encrypt(options.to_json)
       @data = Base64.encode64(encrypted).gsub(/\n/, '')
-    end
 
-    def to_s
-      @data
+      @data.to_s
     end
   end
 end
