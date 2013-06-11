a582fe4eb75ac82d322b503b7ab9c3d51bc4733a
Diogo Biazus <diogob@gmail.com>
2013-06-11 20:10:56 -0300

    added statistics controller to adm [fix #51498935]

--- spec/controllers/adm/users_controller_spec.rb
+++ spec/controllers/adm/users_controller_spec.rb
@@ -2,12 +2,7 @@ require 'spec_helper'
 
 describe Adm::UsersController do
   subject{ response }
-  let(:admin) do 
-    u = FactoryGirl.create(:user)
-    u.admin = true
-    u.save!
-    u
-  end
+  let(:admin) { create(:user, admin: true) }
 
   describe "GET index" do
     context "when I'm not logged in" do
