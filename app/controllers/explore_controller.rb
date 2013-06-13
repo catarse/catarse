e799ad2148abac440fcffd839c3a25231b80fabc
Diogo Biazus <diogob@gmail.com>
2013-06-13 16:28:01 -0300

    removed old project template and replaced it with the project box template

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -5,10 +5,10 @@ class ExploreController < ApplicationController
     @categories = Category.with_projects.order(:name_pt).all
 
     # Just to know if we should present the menu entries, the actual projects are fetched via AJAX
-    @recommended = Project.visible.not_expired.recommended.limit(3)
-    @expiring = Project.visible.expiring.limit(3)
-    @recent = Project.visible.recent.not_expired.limit(3).order('created_at DESC')
-    @successful = Project.visible.successful.limit(3)
+    @recommended = Project.visible.not_expired.recommended.limit(1)
+    @expiring = Project.visible.expiring.limit(1)
+    @recent = Project.visible.recent.not_expired.limit(1).order('created_at DESC')
+    @successful = Project.visible.successful.limit(1)
   end
 
 end
