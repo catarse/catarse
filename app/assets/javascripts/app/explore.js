ba9e91d6264419055976494d64e883e3a228ed9f
Diogo Biazus <diogob@gmail.com>
2013-06-26 09:23:28 -0300

    implemented scrolling event in explore

--- app/assets/javascripts/app/explore.js
+++ app/assets/javascripts/app/explore.js
@@ -5,18 +5,20 @@ App.addChild('Explore', {
     this.filter = {
       recommended: true,
       not_expired: true,
-      page: 0 
+      page: 1 
     };
     this.$loader = this.$("#loading img");
     this.$loaderDiv = this.$("#loading");
     this.$results = this.$(".results");
     this.projectsPath = this.$("#explore_results").data('projects-path');
     this.fetchPage();
+    this.$window().scroll(this.onScroll);
   },
 
   fetchPage: function(){
     this.$loader.show();
     $.get(this.projectsPath, this.filter).success(this.onSuccess);
+    this.filter.page += 1;
   },
 
   onSuccess: function(data){
@@ -33,5 +35,8 @@ App.addChild('Explore', {
   },
 
   onScroll: function(event){
+    if(this.isLoaderVisible()){
+      this.fetchPage();
+    }
   }
 });
