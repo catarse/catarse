970c5e64a54818eda92c80ed2e0b1acde81534a1
Diogo Biazus <diogob@gmail.com>
2013-07-01 12:03:37 -0300

    moved new update form to updates/new template. Now we are going to use infinitescroll to load the updates list

--- app/assets/javascripts/app/project_backers.js
+++ app/assets/javascripts/app/project_backers.js
@@ -2,8 +2,8 @@ App.views.Project.addChild('ProjectBackers', _.extend({
   el: '#project_backers',
 
   activate: function(){
-    this.$loader = this.$("#loading img");
-    this.$loaderDiv = this.$("#loading");
+    this.$loader = this.$("#backers-loading img");
+    this.$loaderDiv = this.$("#backers-loading");
     this.$results = this.$(".results");
     this.path = this.$el.data('path');
     this.filter = {};
