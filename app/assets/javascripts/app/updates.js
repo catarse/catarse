866a3481c2da12dae6c98f4e7ce2c343d4fd1ca0
Diogo Biazus <diogob@gmail.com>
2013-07-02 16:27:02 -0300

    added updates JS view spec and implemented update delete

--- app/assets/javascripts/app/updates.js
+++ app/assets/javascripts/app/updates.js
@@ -1,6 +1,10 @@
 App.views.Project.addChild('Updates', _.extend({
   el: '#project_updates',
 
+  events: {
+    'ajax:success .results .update' : 'onUpdateDestroy'
+  },
+
   activate: function(){
     this.$loader = this.$("#updates-loading img");
     this.$loaderDiv = this.$("#updates-loading");
@@ -8,6 +12,16 @@ App.views.Project.addChild('Updates', _.extend({
     this.path = this.$el.data('path');
     this.filter = {};
     this.setupScroll();
+  },
+
+  updates: function(){
+    return this.$('.results .update');
+  },
+
+  onUpdateDestroy: function(e){
+    var $target = $(e.currentTarget);
+    $target.remove();
+    this.parent.$('a#updates_link .count').html(' (' + this.updates().length + ')');
   }
 
 }, Skull.InfiniteScroll));
---
+++ spec/javascripts/updates_spec.js
@@ -0,0 +1,32 @@
+describe("Updates", function() {
+  var view;
+
+  beforeEach(function() {
+    view = new App.views.Project.views.Updates();
+  });
+
+  describe("#onUpdateDestroy", function() {
+    var $target;
+    var $count;
+    beforeEach(function() {
+      view.parent = { $: function(){} };
+      $target = $('<div class="update">');
+      $count = $('<div class="count">');
+      spyOn(window, "$").andReturn($target);
+      spyOn($target, "remove");
+      spyOn(view.parent, "$").andReturn($count);
+      spyOn($count, "html");
+      
+      view.onUpdateDestroy({currentTarget: $target});
+    });
+
+    it("should get currentTarget and remove it", function() {
+      expect(window.$).wasCalledWith($target);
+      expect($target.remove).wasCalled();
+    });
+    
+     it("should update count", function() {
+       expect($count.html).wasCalledWith(' (0)');
+     });
+  });  
+});
