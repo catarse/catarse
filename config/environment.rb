cf8ec1aa9b284a6452340971d9aeb429b88ec5c5
Adriano Benin <adriano.benin@gmail.com>
2013-07-10 15:15:20 -0300

    set encoding to utf-8

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Catarse::Application.initialize!
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
 
