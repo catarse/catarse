f6da88f87a12b88bfacfe18e771d7e365f426cec
Adriano Benin <adriano.benin@gmail.com>
2013-07-23 16:00:29 -0300

    move sort_rewards code to new js framework

--- app/assets/javascripts/app/project_sidebar.js
+++ app/assets/javascripts/app/project_sidebar.js
@@ -4,6 +4,40 @@ App.views.Project.addChild('ProjectSidebar', {
   events:{
     "click .show_reward_form": "showRewardForm"
   },
+  
+  activate: function() {
+    if(this.$("#rewards_authorization").data("can_update") == true){
+      this.$('#rewards').sortable({
+          axis: 'y',
+          placeholder: "ui-state-highlight",
+          start: function(e, ui) {
+            return ui.placeholder.height(ui.item.height());
+          },
+          stop: function(e, ui) {
+            return ui.item.effect('highlight', {}, 1000);
+          },
+          update: function(e, ui) {
+            var csrfToken, position;
+            position = ui.item.index();
+            csrfToken = $("meta[name='csrf-token']").attr("content");
+            return $.ajax({
+              type: 'POST',
+              url: ui.item.data('update_url'),
+              dataType: 'json',
+              headers: {
+                'X-CSRF-Token': csrfToken
+              },
+              data: {
+                reward: {
+                  row_order_position: position
+                }
+              }
+            });
+          }
+    })    
+  }
+  },
+
 
   showRewardForm: function(event) {
     event.preventDefault();
