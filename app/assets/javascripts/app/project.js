5fa446cd1ad9cc8b355f364f56c84a05e2cefaaf
Diogo Biazus <diogob@gmail.com>
2013-07-25 16:46:39 -0300

    moving tab code to a separate Skull.Tabs mixin

--- app/assets/javascripts/app/project.js
+++ app/assets/javascripts/app/project.js
@@ -1,4 +1,4 @@
-App.addChild('Project', {
+App.addChild('Project', _.extend({
   el: '#main_content[data-action="show"][data-controller-name="projects"]',
 
   events: {
@@ -31,35 +31,5 @@ App.addChild('Project', {
         that.$embed.html(data);
       });
     }
-  },
-
-  selectTab: function($el){
-    this.trigger('onSelectTab');
-    this.$('nav#project_menu a').removeClass('selected');
-    $el.addClass('selected');
-  },
-
-  toggleTab: function($tab){
-    this.$('#project_content .content').hide();
-    $tab.show();
-  },
-
-  onTabClick: function(event){
-    var $target = $(event.target);
-    var $tab = this.$($target.data('target'));
-    this.loadTab($tab);
-    this.selectTab($target);
-    this.toggleTab($tab);
-    return false;
-  },
-
-  loadTab: function($tab){
-    var that = this;
-    if($.trim($tab.html()) == '' && $tab.data('path')){
-      $.get($tab.data('path')).success(function(data){
-        $tab.html(data);
-      });
-    }
-  },
-});
-
+  }
+}, Skull.Tabs));
