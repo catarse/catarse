ccc57ca866042add334a4ae441672750fa1af86f
Adriano Benin <adriano.benin@gmail.com>
2013-07-25 16:11:21 -0300

    refactor users model and create temporary password notification

--- app/observers/user_observer.rb
+++ app/observers/user_observer.rb
@@ -1,6 +1,19 @@
 class UserObserver < ActiveRecord::Observer
   observe :user
 
+  def before_validation(user)
+    user.password = SecureRandom.hex(4) unless user.password || user.persisted?
+  end
+
+  def after_create(user)
+    if user.has_facebook_authentication?
+      Notification.create_notification_once(:temporary_password,
+        user,
+        {id: user.id},
+        password: user.password)
+    end
+  end
+
   def before_save(user)
     user.fix_twitter_user
   end
