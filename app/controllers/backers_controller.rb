7d27278c2fda83ea904e22b5b8c48770824ffbe9
Diogo Biazus <diogob@gmail.com>
2013-07-29 16:35:04 -0300

    adjusting backers controller to render index as HTML, removed some unnecessary code and specs

--- app/controllers/backers_controller.rb
+++ app/controllers/backers_controller.rb
@@ -3,34 +3,27 @@ class BackersController < ApplicationController
   defaults resource_class: Backer, collection_name: 'backs', instance_name: 'back'
   belongs_to :user
   actions :index
-  respond_to :json, only: [:index]
 
   def index
-    index! do |format|
-      format.json{ return render json: @backs.includes(:user, :reward, project: [:user, :category, :project_total]).to_json({include_project: true, can_manage: (can? :manage, @user)}) }
-      format.html{ return render nothing: true, status: 404 }
-    end
+    render layout: false
   end
 
   def request_refund
-    back = Backer.find(params[:id])
-
-    authorize! :request_refund, back
-    if back.value > back.user.user_total.credits
+    authorize! :request_refund, resource
+    if resource.value > resource.user.user_total.credits
       flash[:failure] = I18n.t('credits.index.insufficient_credits')
-    elsif can?(:request_refund, back) && back.can_request_refund?
-      back.request_refund!
+    elsif can?(:request_refund, resource) && resource.can_request_refund?
+      resource.request_refund!
       flash[:notice] = I18n.t('credits.index.refunded')
     end
 
     redirect_to user_path(parent, anchor: 'credits')
-    # render json: {status: status, credits: current_user.reload.display_credits}
   end
 
   protected
   def collection
     @backs = end_of_association_chain.available_to_count.order("confirmed_at DESC")
-    @backs = @backs.not_anonymous unless @user == current_user or (current_user and current_user.admin)
-    @backs = @backs.page(params[:page]).per(10)
+    @backs = @backs.not_anonymous if can? :manage, @user
+    @backs = @backs.includes(:user, :reward, project: [:user, :category, :project_total]).page(params[:page]).per(10)
   end
 end
