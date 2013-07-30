a68d0a77e17b8d2822f0c298f01684fe7aec6de5
Antônio Roberto Silva <forevertonny@gmail.com>
2013-07-30 20:50:10 -0300

    Adjust project order on adm/financials [#52825561]

--- app/controllers/adm/financials_controller.rb
+++ app/controllers/adm/financials_controller.rb
@@ -9,7 +9,7 @@ class Adm::FinancialsController < Adm::BaseController
   actions :index
 
   def projects
-    @search = apply_scopes(Project).includes(:user).order("CASE state WHEN 'successful' THEN 1 WHEN 'waiting_funds' THEN 2 ELSE 3 END, (projects.expires_at)::date")
+    @search = apply_scopes(Project).includes(:user).order("CASE state WHEN 'successful' THEN 1 WHEN 'waiting_funds' THEN 2 ELSE 3 END, (projects.expires_at)::date DESc")
   end
 
   def collection
