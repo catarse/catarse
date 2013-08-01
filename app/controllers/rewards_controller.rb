acddff9595b333c00f944290ce8984db5650d87c
Antônio Roberto Silva <forevertonny@gmail.com>
2013-08-01 15:58:13 -0300

    Now to create or edit a reward we showup a modal and show the errors when have it [Fix #54305600]

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,12 +1,19 @@
 class RewardsController < ApplicationController
   load_and_authorize_resource
   inherit_resources
-  actions :index, :create, :update, :destroy, :sort
+  belongs_to :project
   respond_to :html, :json
 
   def index
-    @rewards = Reward.find_by_project_id(params[:project_id])
-    render json: @rewards
+    render layout: false
+  end
+
+  def new
+    render layout: false
+  end
+
+  def edit
+    render layout: false
   end
 
   def show
@@ -15,17 +22,16 @@ class RewardsController < ApplicationController
   end
 
   def update
-    update! do |format|
-      format.html { redirect_to project_by_slug_path(permalink: resource.project.permalink) }
-      format.json { render json: resource.to_json }
+    update! do |success, failure|
+      success.html { render nothing: true, status: 200 }
+      failure.html { render :edit, layout: nil }
     end
   end
 
   def create
     create! do |success, failure|
-      success.html { flash[:notice] = I18n.t('controllers.rewards.create.notice') }
-      failure.html { flash[:alert] = I18n.t('controllers.rewards.create.alert') }
-      return redirect_to project_by_slug_path(permalink: resource.project.permalink)
+      success.html { render nothing: true, status: 200 }
+      failure.html { render :new, layout: nil }
     end
   end
 
@@ -39,13 +45,4 @@ class RewardsController < ApplicationController
 
     render nothing: true
   end
-
-  protected
-
-  def begin_of_association_chain
-    unless action_name == 'show'
-      Project.find(params[:project_id])
-    end
-  end
-
 end
