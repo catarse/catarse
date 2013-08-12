daeb80b387d7ca3620e4b2369c17a96fff9462d1
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-08-12 14:03:36 -0500

    Add secret key base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,4 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Catarse::Application.config.secret_token = '8f3af8dc08112fa30556493dffc1e8ce971209c8e40f41499ba5c7b8fc62fd1a40418451cffba9a18fdfb19e04b891fc671d183934bcafbd314553462524e71f'
-
+Catarse::Application.config.secret_key_base = 'da6b3fed87ffed935dbcf6fcede402313e4f18a5f4757d45658c20b3467056f76eb08a03969a40b6f10a88b06e0f1769151466cb0b048c42c0546eb79b425dc2'
