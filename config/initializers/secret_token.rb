2e639ed3a94c990fdae19d0e5a571b596f52b96f
argerim <argerim@gmail.com>
2013-08-12 14:26:05 -0300

    Generate the Rails secret token on first run.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,8 +1,19 @@
 # Be sure to restart your server when you modify this file.
-
+require 'securerandom'
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Catarse::Application.config.secret_token = '8f3af8dc08112fa30556493dffc1e8ce971209c8e40f41499ba5c7b8fc62fd1a40418451cffba9a18fdfb19e04b891fc671d183934bcafbd314553462524e71f'
+def find_secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist? token_file
+    File.read(token_file).chomp
+  else
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Catarse::Application.config.secret_token = find_secure_token 
 
