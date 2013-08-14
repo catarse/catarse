49ac42c04ca6ce416304bc170fe58e16a876a551
Antônio Roberto Silva <forevertonny@gmail.com>
2013-08-14 00:06:15 -0300

    rename routes and remove unused files

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 require 'sidekiq/web'
 
 Catarse::Application.routes.draw do
+  devise_for :users, path: '', 
+    path_names:   { sign_in: :login, sign_out: :logout }, 
+    controllers:  { omniauth_callbacks: :omniauth_callbacks }
+
   # Root path
   root to: 'projects#index'
 
   match '/thank_you' => "static#thank_you"
-
-  devise_for :users, path: '', 
-    path_names:   { sign_in: :login, sign_out: :logout, sign_up: :register }, 
-    controllers:  { omniauth_callbacks: :omniauth_callbacks, sessions: :sessions, registrations: :registrations }
 
   check_user_admin = lambda { |request| request.env["warden"].authenticate? and request.env['warden'].user.admin }
 
