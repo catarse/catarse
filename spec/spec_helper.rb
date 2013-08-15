0d3e4376c0c4c0361496d6fa0d91af746fd08b16
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-08-15 16:11:52 -0500

    Check for pending migrations before tests are run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,10 @@ require 'sidekiq/testing'
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
+# Checks for pending migrations before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include ActionView::Helpers::TextHelper
