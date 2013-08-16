cb259727e7513e76c1c42004652f29bc3fc36478
Antônio Roberto Silva <forevertonny@gmail.com>
2013-08-15 22:42:47 -0300

    fix controllers spec to new redirect

--- spec/controllers/projects/backers_controller_spec.rb
+++ spec/controllers/projects/backers_controller_spec.rb
@@ -19,7 +19,7 @@ describe Projects::BackersController do
     end
 
     context "when no user is logged in" do
-      it{ should redirect_to(new_user_session_path) }
+      it{ should redirect_to(user_user_registration_path) }
     end
 
     context "when backer don't exist in current_user" do
@@ -40,7 +40,7 @@ describe Projects::BackersController do
     end
 
     context "without user" do
-      it{ should redirect_to(new_user_session_path) }
+      it{ should redirect_to(new_user_registration_path) }
     end
 
     context "when backer don't exist in current_user" do
@@ -77,7 +77,7 @@ describe Projects::BackersController do
     end
 
     context "when no user is logged" do
-      it{ should redirect_to new_user_session_path }
+      it{ should redirect_to new_user_registration_path }
       it('should set the session[:return_to]'){ session[:return_to].should == "/test_path" }
     end
 
@@ -113,7 +113,7 @@ describe Projects::BackersController do
 
     context "when no user is logged" do
       let(:user){ nil }
-      it{ should redirect_to new_user_session_path }
+      it{ should redirect_to new_user_registration_path }
     end
 
     context "when user is logged in but project.online? is false" do
@@ -147,7 +147,7 @@ describe Projects::BackersController do
     end
 
     context "when no user is logged in" do
-      it{ should redirect_to new_user_session_path }
+      it{ should redirect_to new_user_registration_path }
       it('should set flash failure'){ request.flash[:alert].should_not be_empty }
     end
 
