106b74daf68fe41f654fdee6e72c1ccc9ac5db22
Antônio Roberto Silva <forevertonny@gmail.com>
2013-08-15 22:28:54 -0300

    redirect user to registration page when they try access a page when need an account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
     message = exception.message 
 
     if current_user.nil?
-      redirect_to new_user_session_path, alert: I18n.t('devise.failure.unauthenticated')
+      redirect_to new_user_registration_path, alert: I18n.t('devise.failure.unauthenticated')
     elsif request.env["HTTP_REFERER"]
       redirect_to :back, alert: message 
     else
