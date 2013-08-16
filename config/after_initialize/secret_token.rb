2ada3861f45a9d1e7e4e82e3141ce1889b80181e
Antônio Roberto Silva <forevertonny@gmail.com>
2013-08-16 17:03:12 -0300

    create a after_initialize page to load config files that need to use database

---
+++ config/after_initialize/secret_token.rb
@@ -0,0 +1,24 @@
+# Be sure to restart your server when you modify this file.
+require 'securerandom'
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+def find_secure_token
+  ::Configuration[:secret_token] = SecureRandom.hex(64) unless ::Configuration[:secret_token]
+  ::Configuration[:secret_token]
+rescue
+  # Just to ensure that we can run migrations and create the configurations table
+  nil
+end
+
+def find_secure_key_base
+  ::Configuration[:secret_key_base] = SecureRandom.hex(64) unless ::Configuration[:secret_key_base]
+  ::Configuration[:secret_key_base]
+rescue
+  # Just to ensure that we can run migrations and create the configurations table
+  nil
+end
+
+Catarse::Application.config.secret_token = find_secure_token
+Catarse::Application.config.secret_key_base = find_secure_key_base
