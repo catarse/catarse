b2ba7d9eee33dbd2d9f1c534ffa215643dfbe985
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-08-16 16:06:17 -0500

    Remove X-Sendfile from production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Catarse::Application.configure do
   config.assets.version = '1.0'
 
   # Specifies the header that your server uses for sending files.
-  config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
