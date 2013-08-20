ca9e4b709fb2d98d341af90c586d98c586cd9c7e
Antônio Roberto Silva <forevertonny@gmail.com>
2013-08-20 14:53:21 -0300

    added notification when user sign up [Fix #55482082]

--- app/observers/user_observer.rb
+++ app/observers/user_observer.rb
@@ -6,6 +6,8 @@ class UserObserver < ActiveRecord::Observer
   end
 
   def after_create(user)
+    Notification.create_notification_once(:new_user_registration, user, {user_id: user.id}, {user: user})
+
     if user.has_facebook_authentication?
       Notification.create_notification_once(:temporary_password,
         user,
