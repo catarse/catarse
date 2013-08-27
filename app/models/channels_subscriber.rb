c5f99e8d4bcef6b5b6a585389a01eece3798f54a
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-08-27 16:02:05 -0500

    Add back the attr_accessible

--- app/models/channels_subscriber.rb
+++ app/models/channels_subscriber.rb
@@ -1,6 +1,5 @@
 class ChannelsSubscriber < ActiveRecord::Base
-  # TODO:
-  #attr_accessible :user_id, :channel_id, :user, :channel
+  attr_accessible :user_id, :channel_id, :user, :channel
   schema_associations
   validates_presence_of :user_id, :channel_id
 end
