044d193669db76409bd3e88b3bcc93d07ee0b279
Diogo Biazus <diogob@gmail.com>
2013-08-29 13:48:32 -0300

    fixing last migration that broke travis

--- db/migrate/20130828160026_use_expires_at_in_view.rb
+++ db/migrate/20130828160026_use_expires_at_in_view.rb
@@ -1,6 +1,8 @@
 class UseExpiresAtInView < ActiveRecord::Migration
   def up
-    execute "CREATE or replace VIEW financial_reports AS
+    execute "
+    DROP VIEW if exists financial_reports ;
+    CREATE or replace VIEW financial_reports AS
       SELECT p.name, u.moip_login, p.goal, p.expires_at, p.state FROM (projects p JOIN users u ON ((u.id = p.user_id)));"
   end
 
