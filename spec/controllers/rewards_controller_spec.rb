6674cfbf03d8e7773c985b06db36e89028fe0ddd
Antônio Roberto Silva <forevertonny@gmail.com>
2013-09-04 13:09:57 -0300

    Project owner now can edit they rewards

--- spec/controllers/rewards_controller_spec.rb
+++ spec/controllers/rewards_controller_spec.rb
@@ -22,17 +22,17 @@ describe RewardsController do
 
   shared_examples_for "PUT rewards update" do
     before { put :update, project_id: project.id, id: reward.id, reward: { description: 'Amenori ipsum' }, locale: :pt }
-    it { 
+    it {
       reward.reload
-      reward.description.should == 'Amenori ipsum' 
+      reward.description.should == 'Amenori ipsum'
     }
   end
 
   shared_examples_for "PUT rewards update without permission" do
     before { put :update, project_id: project.id, id: reward.id, reward: { description: 'Amenori ipsum' }, locale: :pt }
-    it { 
+    it {
       reward.reload
-      reward.description.should == 'Foo bar' 
+      reward.description.should == 'Foo bar'
     }
   end
 
@@ -73,6 +73,17 @@ describe RewardsController do
           reward.minimum_value.should_not eq(15.0)
         }
       end
+
+      context "can update the description and maximum backers" do
+        before do
+          put :update, project_id: project.id, id: reward.id, reward: { maximum_backers: 99, description: 'lorem ipsum'}, locale: :pt
+          reward.reload
+        end
+
+        it { expect(reward.description).to eq('lorem ipsum') }
+        it { expect(reward.maximum_backers).to eq(99) }
+      end
+
     end
   end
 
