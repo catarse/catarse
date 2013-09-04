c0a009ef87fd993655a819ed7b304eb8919f479f
Diogo Biazus <diogob@gmail.com>
2013-09-04 17:47:08 -0300

    now we use asset_host for ActionMailer in default_url_options initializer

--- config/initializers/default_url_options.rb
+++ config/initializers/default_url_options.rb
@@ -1,4 +1,5 @@
 if Rails.env.production?
+  ActionMailer::Base.asset_host = ::Configuration[:host]
   Rails.application.routes.default_url_options = {host: ::Configuration[:host]} 
 else
   Rails.application.routes.default_url_options = {host: 'localhost:3000'} 
