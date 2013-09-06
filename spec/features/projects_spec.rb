6b711f9cda79e52ec22077c483d1416c674610c2
Diogo Biazus <diogob@gmail.com>
2013-09-06 14:26:01 -0300

    fixing empty route problem and search bug

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -34,7 +34,7 @@ describe "Projects" do
       create(:project, name: 'Foo', state: 'online', online_days: 30, recommended: true)
       create(:project, name: 'Lorem', state: 'online', online_days: 30, recommended: false)
       visit explore_path(locale: :pt)
-      sleep 3
+      sleep 4
     end
     it "should show recommended projects" do
       recommended = all(".results .project")
