6696b2373089d55aa8ea7b000e366531dd67d507
Diogo Biazus <diogob@gmail.com>
2013-09-06 13:13:13 -0300

    used new route system to load user profile tabs

--- app/assets/javascripts/app/user.js
+++ app/assets/javascripts/app/user.js
@@ -1,27 +1,20 @@
 App.addChild('User', _.extend({
   el: '#main_content[data-action="show"][data-controller-name="users"]',
 
-  events: {
-    'click nav#user_profile_menu a' : 'onTabClick'
-  },
-
   //@TODO: Refactor this ugly code into a generic routing generation inside the Skull.Tabs extension
   activate: function(){
-    this.makeRoute('backs');
-    this.makeRoute('projects');
-    this.makeRoute('credits');
-    this.makeRoute('settings');
-    this.makeRoute('unsubscribes');
+    this.route('backs');
+    this.route('projects');
+    this.route('credits');
+    this.route('settings');
+    this.route('unsubscribes');
   },
 
-  makeRoute: function(name){
-    var that = this;
-    this.parent.router.route(name, name, function(){
-      var $tab = that.$('nav#user_profile_menu a[href="#' + name + '"]');
-      if($tab.length > 0){
-        that.onTabClick({ target: $tab });
-      }
-    });
+  followRoute: function(){
+    var $tab = this.$('nav#user_profile_menu a[href="' + window.location.hash + '"]');
+    if($tab.length > 0){
+      this.onTabClick({ target: $tab });
+    }
   },
 
 }, Skull.Tabs));
