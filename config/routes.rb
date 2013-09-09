80bf1f755891b35e64e04e996963e7496f62b83b
Diogo Biazus <diogob@gmail.com>
2013-09-09 18:25:23 -0300

    changed subscribers report to use postgres-copy instead of CSV.

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Catarse::Application.routes.draw do
     namespace :channels, path: '' do
       namespace :adm do
         namespace :reports do
-          resources :subscribers_report, only: [ :index ]
+          resources :subscribers, only: [ :index ]
         end
         resources :statistics, only: [ :index ]
         resources :projects, only: [ :index, :update] do
