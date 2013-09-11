f5a807fa7774c92a1f4cc4f8528583cb783bb863
Diogo Biazus <diogob@gmail.com>
2013-09-10 21:16:22 -0300

    hotfix on login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,6 +41,7 @@ Catarse::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   #config.assets.compile = false
   config.assets.compile = true
+  config.assets.precompile << 'catarse.css'
 
   # Generate digests for assets URLs
   config.assets.digest = true
