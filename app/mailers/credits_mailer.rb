44b3af9ca010df227647172e4e421736b5d224d5
Adriano Benin <adriano.benin@gmail.com>
2013-09-12 15:10:41 -0300

    change from field in credits notification #changelog [Fix #56661930]

--- app/mailers/credits_mailer.rb
+++ app/mailers/credits_mailer.rb
@@ -4,6 +4,6 @@ class CreditsMailer < ActionMailer::Base
   def request_refund_from(backer)
     @backer = backer
     @user = backer.user
-    mail(from: "#{::Configuration[:company_name]} <#{::Configuration[:email_system]}>", to: ::Configuration[:email_payments], subject: I18n.t('credits_mailer.request_refund_from.subject', name: @backer.project.name))
+    mail(from: "#{@user.name} <#{@user.email}>", to: ::Configuration[:email_payments], subject: I18n.t('credits_mailer.request_refund_from.subject', name: @backer.project.name))
   end
 end
