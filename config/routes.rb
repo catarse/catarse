3cbbbe9dd1ec3ed01e1aa4c4f427bf84b92f9e9a
Diogo Biazus <diogob@gmail.com>
2013-09-16 18:25:03 -0300

    made constraint for channel subdomain accept any subdomain except for some special ones used in development or for special purposes

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Catarse::Application.routes.draw do
   mount CatarseMoip::Engine => "/", as: :catarse_moip
 
   # Channels
-  constraints subdomain: 'asas' do
+  constraints subdomain: /^(?!www|secure|test|local)(\w+)/ do
     namespace :channels, path: '' do
       namespace :adm do
         namespace :reports do
