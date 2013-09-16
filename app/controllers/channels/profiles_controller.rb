24067ae6352f25b44702a7401fe095e4c49fc655
Diogo Biazus <diogob@gmail.com>
2013-09-16 18:59:50 -0300

    Added new-channel-home route to make the new channel home coexist with the old one while we are in development. Created a method in ProfilesController to use the new catarse_bootstrap layout in this new home. Created a shared header partial in the catarse_boostrap folder to reimplement the catarse default header in the new bootstrap layout.

--- app/controllers/channels/profiles_controller.rb
+++ app/controllers/channels/profiles_controller.rb
@@ -1,11 +1,14 @@
 class Channels::ProfilesController < Channels::BaseController
+  layout :use_catarse_boostrap
   inherit_resources
   defaults resource_class: Channel, finder: :find_by_permalink! 
   actions :show
-  custom_actions resource: [:how_it_works]
-
-
+  custom_actions resource: [:how_it_works, :new_channel_home]
 
   before_filter{ params[:id] = request.subdomain }
 
+  private
+  def use_catarse_boostrap
+    action_name == 'new_channel_home' ? 'catarse_bootstrap' : 'application'
+  end
 end
