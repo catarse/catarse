0644ecd00fccaac6afa6c8430cb451c67a0e65a8
Diogo Biazus <diogob@gmail.com>
2013-09-24 14:18:06 -0300

    fixed the subscribers report with a workarount. Will make the code prettier after some updates in postgres-copy gem.

--- app/controllers/channels/admin/reports/subscriber_reports_controller.rb
+++ app/controllers/channels/admin/reports/subscriber_reports_controller.rb
@@ -1,7 +1,7 @@
 class Channels::Admin::Reports::SubscriberReportsController < Admin::Reports::BaseController
 
   private
-  def begin_of_association_chain
-    Channel.find_by_permalink!(request.subdomain.to_s)
+  def end_of_association_chain
+    SubscriberReport.where(channel_id: Channel.find_by_permalink!(request.subdomain.to_s).id)
   end
 end
