1dca5a07f5f154ff6d9bb88c2d3be28d4432bfcb
Diogo Biazus <diogob@gmail.com>
2013-09-25 14:33:26 -0300

    fix recommendations after back was created, we need the explicit order in this SQL.

--- app/models/backer.rb
+++ app/models/backer.rb
@@ -79,7 +79,7 @@ class Backer < ActiveRecord::Base
   end
 
   def recommended_projects
-    user.recommended_projects.where("projects.id <> ?", project.id)
+    user.recommended_projects.where("projects.id <> ?", project.id).order("count DESC")
   end
 
   def refund_deadline
