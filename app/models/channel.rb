9ebfae91dcccf3073d76a053797da7dcc20aa0b4
Adriano Benin <adriano.benin@gmail.com>
2013-10-08 16:36:17 -0300

    add how_it_works to edit

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,5 +1,5 @@
 class Channel < ActiveRecord::Base
-  attr_accessible :description, :name, :permalink, :video_url, :twitter, :facebook, :website, :image
+  attr_accessible :description, :name, :permalink, :video_url, :twitter, :facebook, :website, :image, :how_it_works
   schema_associations
 
   validates_presence_of :name, :description, :permalink
