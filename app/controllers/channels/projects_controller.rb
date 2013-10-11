3ba01be4e1741e13994851d78d38cc2d21e03727
Diogo Biazus <diogob@gmail.com>
2013-10-11 15:33:57 -0300

    removed unused code

--- app/controllers/channels/projects_controller.rb
+++ app/controllers/channels/projects_controller.rb
@@ -5,18 +5,8 @@ class Channels::ProjectsController < ProjectsController
   # Inheriting from the original Projects controller
   # With one addition: we include the project into the current channel
   before_filter only: [:create] { params[:project][:channels] = [@channel] }
-  after_filter only: [:create] { notify_trustees }
 
   prepend_before_filter{ params[:profile_id] = request.subdomain }
-
-
-
-
-  # After a project submission through a channel, notify all channel's trustees
-  protected
-    def notify_trustees
-
-    end
 end
 
 
