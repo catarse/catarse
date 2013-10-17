50b238dfe50979e6b96aebe9e6f9a074a16b6d93
Diogo Biazus <diogob@gmail.com>
2013-10-17 14:22:41 -0300

    moved new channel home and new how it works overwriting the old ones

--- app/controllers/channels/profiles_controller.rb
+++ app/controllers/channels/profiles_controller.rb
@@ -1,16 +1,11 @@
 class Channels::ProfilesController < Channels::BaseController
-  layout :use_catarse_boostrap
+  layout 'catarse_bootstrap'
   inherit_resources
   enable_authorization only: [:edit, :update]
   actions :show, :edit, :update
-  custom_actions resource: [:how_it_works, :new_how_it_works, :new_channel_home]
+  custom_actions resource: [:how_it_works]
 
   def resource
     @profile ||= channel
-  end
-
-  private
-  def use_catarse_boostrap
-    action_name == 'new_channel_home' || action_name == 'new_how_it_works' ? 'catarse_bootstrap' : 'application'
   end
 end
