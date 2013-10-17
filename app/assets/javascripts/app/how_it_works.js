85b4a4f2900ecb6a56500cfeea024f4adf92119f
Diogo Biazus <diogob@gmail.com>
2013-10-17 12:34:32 -0300

    added methods to build the sidebar in the new how it works [fix #57686712]

--- app/assets/javascripts/app/how_it_works.js
+++ app/assets/javascripts/app/how_it_works.js
@@ -2,9 +2,21 @@ App.addChild('HowItWorks', {
   el: '.how-it-works-wrapper',
 
   activate: function(){
+    this.body = this.$('.how-it-works');
+    this.sidebar = this.$('.how-it-works-sidebar');
+    this.$('#side-menu').append(this.generateMenu());
   },
 
   generateMenu: function(){
+    return _.map(this.getHeaders(), function(el){ 
+      return $('<li>').append($('<a>').prop('href', '#' + el.prop('name')).html(el.html()));
+    });
+  },
+
+  getHeaders: function(){
+    return this.body.find('h3').map(function(i, el){
+      return $(el).prop('id', 'topic_' + i);
+    });
   }
 });
 
