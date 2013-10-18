f1676047f384a8f7347891c0cb801850e763ff12
Adriano Benin <adriano.benin@gmail.com>
2013-10-18 13:04:26 -0300

    add new search options

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -1,9 +1,8 @@
 class Admin::ProjectsController < Admin::BaseController
   menu I18n.t("admin.projects.index.menu") => Rails.application.routes.url_helpers.admin_projects_path
 
-  has_scope :by_id, :pg_search, :user_name_contains, :with_state
+  has_scope :by_id, :pg_search, :user_name_contains, :with_state, :by_online_date, :by_expires_at, :by_category_id, :by_goal
   has_scope :between_created_at, using: [ :start_at, :ends_at ], allow_blank: true
-  has_scope :order_table, default: 'created_at'
 
   before_filter do
     @total_projects = Project.count
@@ -26,7 +25,11 @@ class Admin::ProjectsController < Admin::BaseController
     redirect_to admin_projects_path
   end
 
+  protected
   def collection
-    @projects = apply_scopes(end_of_association_chain).without_state('deleted').page(params[:page])
+    order = params[:order_by].blank? ? 'created_at' : params[:order_by].split(' ')[0]
+    scope= apply_scopes(end_of_association_chain).without_state('deleted').sort_by{|p| p.send(order)}
+    scope = scope.reverse if !params[:order_by].blank? && (params[:order_by].split(' ')[1] == 'DESC')
+    @projects ||= Kaminari.paginate_array(scope).page(params[:page])
   end
 end
