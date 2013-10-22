5b546da468226ca71c5fc784a0e941de12fcf4e3
Antônio Roberto Silva <forevertonny@gmail.com>
2013-10-22 10:12:12 -0200

    move backer state machine to a concern

--- app/models/backer.rb
+++ app/models/backer.rb
@@ -3,6 +3,8 @@ class Backer < ActiveRecord::Base
   schema_associations
 
   include Shared::StateMachineHelpers
+  include BackerStateMachineHandler
+
   delegate :display_value, :display_confirmed_at, to: :decorator
 
   validates_presence_of :project, :user, :value
@@ -65,12 +67,6 @@ class Backer < ActiveRecord::Base
   def self.between_values(start_at, ends_at)
     return scoped unless start_at.present? && ends_at.present?
     where("value between ? and ?", start_at, ends_at)
-  end
-
-  def self.state_names
-    self.state_machine.states.map do |state|
-      state.name if state.name != :deleted
-    end.compact!
   end
 
   def decorator
@@ -140,62 +136,6 @@ class Backer < ActiveRecord::Base
       phone_number: address_phone_number,
       cpf: payer_document
     })
-  end
-
-  state_machine :state, initial: :pending do
-    state :pending, value: 'pending'
-    state :waiting_confirmation, value: 'waiting_confirmation'
-    state :confirmed, value: 'confirmed'
-    state :canceled, value: 'canceled'
-    state :refunded, value: 'refunded'
-    state :requested_refund, value: 'requested_refund'
-    state :refunded_and_canceled, value: 'refunded_and_canceled'
-    state :deleted, value: 'deleted'
-
-    event :push_to_trash do
-      transition all => :deleted
-    end
-
-    event :pendent do
-      transition all => :pending
-    end
-
-    event :waiting do
-      transition pending: :waiting_confirmation
-    end
-
-    event :confirm do
-      transition all => :confirmed
-    end
-
-    event :cancel do
-      transition all => :canceled
-    end
-
-    event :request_refund do
-      transition confirmed: :requested_refund, if: ->(backer){
-        backer.user.credits >= backer.value && !backer.credits
-      }
-    end
-
-    event :refund do
-      transition [:requested_refund, :confirmed] => :refunded
-    end
-
-    event :hide do
-      transition all => :refunded_and_canceled
-    end
-
-    after_transition confirmed: :requested_refund, do: :after_transition_from_confirmed_to_requested_refund
-    after_transition confirmed: :canceled, do: :after_transition_from_confirmed_to_canceled
-  end
-
-  def after_transition_from_confirmed_to_canceled
-    notify_observers :notify_backoffice_about_canceled
-  end
-
-  def after_transition_from_confirmed_to_requested_refund
-    notify_observers :notify_backoffice
   end
 
   # Used in payment engines
