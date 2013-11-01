8883dbcad5520c7d90de7b4bd869045bc96e8118
Antônio Roberto Silva <forevertonny@gmail.com>
2013-11-01 09:53:26 -0200

    set default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,8 @@ Catarse::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_mailer.asset_host = "http://catarse.me"
+  config.asset_host = ::Configuration[:base_url]
+  config.action_mailer.default_url_options = { host: ::Configuration[:host] }
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
