d5ce397eaf00d1b61f03628a3193ebf23df66dc8
Diogo Biazus <diogob@gmail.com>
2013-11-11 11:26:02 -0200

    just tidying up a bit the notifications_mailer spec

--- spec/mailers/notifications_mailer_spec.rb
+++ spec/mailers/notifications_mailer_spec.rb
@@ -1,19 +1,21 @@
 require 'spec_helper'
 
 describe NotificationsMailer do
-  let(:notification){ create(:notification, template_name: 'template', user: create(:user), origin_name: 'Catarse', origin_email: 'contact@foo.bar') }
-  subject{ NotificationsMailer.notify(notification) }
+  describe ".notify" do
+    let(:notification){ create(:notification, template_name: 'template', user: create(:user), origin_name: 'Catarse', origin_email: 'contact@foo.bar') }
+    subject{ NotificationsMailer.notify(notification) }
 
-  before do
-    notification
-    Mail::Message.any_instance.stub(:deliver)
-    NotificationsMailer.any_instance.should_receive(:mail).with({
-      from: "Catarse <contact@foo.bar>",
-      to: notification.user.email,
-      subject: '',
-      template_name: 'template'
-    })
+    before do
+      notification
+      Mail::Message.any_instance.stub(:deliver)
+      NotificationsMailer.any_instance.should_receive(:mail).with({
+        from: "Catarse <contact@foo.bar>",
+        to: notification.user.email,
+        subject: '',
+        template_name: 'template'
+      })
+    end
+
+    it("should satisfy expectations"){ subject }
   end
-
-  it("should satisfy expectations"){ subject }
 end
