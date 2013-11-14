b6276a29f343a9ba1e692d7a5bc271937170d4ac
Diogo Biazus <diogob@gmail.com>
2013-11-14 10:19:47 -0200

    renaming single char variable

--- app/controllers/unsubscribes_controller.rb
+++ app/controllers/unsubscribes_controller.rb
@@ -3,11 +3,11 @@ class UnsubscribesController < ApplicationController
   belongs_to :user
 
   def create
-    params[:user][:unsubscribes_attributes].each_value do |u|
-      if u[:subscribed] == '1' && !u[:id].nil? #change from unsubscribed to subscribed
-        parent.unsubscribes.where(project_id: u[:project_id]).destroy_all
-      elsif u[:subscribed] == '0' && u[:id].nil? #change from subscribed to unsubscribed
-        parent.unsubscribes.create!(project_id: u[:project_id])
+    params[:user][:unsubscribes_attributes].each_value do |subscription|
+      if subscription[:subscribed] == '1' && !subscription[:id].nil? #change from unsubscribed to subscribed
+        parent.unsubscribes.where(project_id: subscription[:project_id]).destroy_all
+      elsif subscription[:subscribed] == '0' && subscription[:id].nil? #change from subscribed to unsubscribed
+        parent.unsubscribes.create!(project_id: subscription[:project_id])
       end
     end
     flash[:notice] = t('users.current_user_fields.updated')
