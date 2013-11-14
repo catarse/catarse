0f77140a51055f5a0009309e78fc4a35e5ebc9fc
Antônio Roberto Silva <forevertonny@gmail.com>
2013-11-14 03:40:39 -0200

    remove module Services, because rails has a weird bug with this module :/

--- app/services/project_downloader.rb
+++ app/services/project_downloader.rb
@@ -1,24 +1,22 @@
-module Services
-  class ProjectDownloader
+class ProjectDownloader
 
-    def initialize(resource)
-      @resource = resource
-    end
-
-    def start!
-      update_video_embed_url
-      download_video_thumbnail
-    end
-
-    def download_video_thumbnail
-      @resource.video_thumbnail = open(@resource.video.thumbnail_large) if @resource.video_url.present? && @resource.video
-    rescue OpenURI::HTTPError, TypeError => e
-      Rails.logger.info "-----> #{e.inspect}"
-    end
-
-    def update_video_embed_url
-      @resource.video_embed_url = @resource.video.embed_url if @resource.video
-    end
-
+  def initialize(resource)
+    @resource = resource
   end
+
+  def start!
+    update_video_embed_url
+    download_video_thumbnail
+  end
+
+  def download_video_thumbnail
+    @resource.video_thumbnail = open(@resource.video.thumbnail_large) if @resource.video_url.present? && @resource.video
+  rescue OpenURI::HTTPError, TypeError => e
+    Rails.logger.info "-----> #{e.inspect}"
+  end
+
+  def update_video_embed_url
+    @resource.video_embed_url = @resource.video.embed_url if @resource.video
+  end
+
 end
