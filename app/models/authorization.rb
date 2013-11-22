a886f514c481961ccbf364c3f8a9ad21438943ec
Diogo Biazus <diogob@gmail.com>
2013-11-22 08:39:25 -0200

    added basic validations and specs

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,4 +1,6 @@
 class Authorization < ActiveRecord::Base
   attr_accessible :oauth_provider, :oauth_provider_id, :uid, :user_id
   schema_associations
+  validates_presence_of :oauth_provider, :user, :uid
+
 end
