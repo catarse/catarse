b08a54b302484261d1794f6a32c245d098a412a7
Diogo Biazus <diogob@gmail.com>
2013-11-22 09:57:26 -0200

    created methods to find authorization from oauth hash and to create a new authorization based on the same hash

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,6 +1,19 @@
 class Authorization < ActiveRecord::Base
-  attr_accessible :oauth_provider, :oauth_provider_id, :uid, :user_id
+  attr_accessible :oauth_provider, :oauth_provider_id, :uid, :user_id, :user
   schema_associations
   validates_presence_of :oauth_provider, :user, :uid
 
+  scope :from_hash, ->(hash){
+    joins(:oauth_provider).
+    where("oauth_providers.name = :name AND uid = :uid", {name: hash['provider'], uid: hash['uid']})
+  }
+
+  def self.find_from_hash(hash)
+    from_hash(hash).first
+  end
+
+  def self.create_from_hash(hash, user = nil)
+    user ||= User.create_from_hash(hash)
+    create!(user: user, uid: hash['uid'], oauth_provider: OauthProvider.find_by_name(hash['provider']))
+  end
 end
--- spec/models/authorizations_spec.rb
+++ spec/models/authorizations_spec.rb
@@ -1,6 +1,48 @@
 require 'spec_helper'
 
 describe Authorization do
+    let(:oauth_data){
+    Hashie::Mash.new({
+      credentials: {
+        expires: true,
+        expires_at: 1366644101,
+        token: "AAAHuZCwF61OkBAOmLTwrhv52pZCriPnTGIasdasdasdascNhZCZApsZCSg6POZCQqolxYjnqLSVH67TaRDONx72fXXXB7N7ZBByLZCV7ldvagm"
+      },
+      extra: {
+        raw_info: {
+          bio: "I, simply am not there",
+          email: "diogob@gmail.com",
+          first_name: "Diogo",
+          gender: "male",
+          id: "547955110",
+          last_name: "Biazus",
+          link: "http://www.facebook.com/diogo.biazus",
+          locale: "pt_BR",
+          name: "Diogo, Biazus",
+          timezone: -3,
+          updated_time: "2012-08-01T18:22:50+0000",
+          username: "diogo.biazus",
+          verified: true
+        },
+      },
+      info: {
+        description: "I, simply am not there",
+        email: "diogob@gmail.com",
+        first_name: "Diogo",
+        image: "http://graph.facebook.com/547955110/picture?type:, square",
+        last_name: "Biazus",
+        name: "Diogo, Biazus",
+        nickname: "diogo.biazus",
+        urls: {
+          Facebook: "http://www.facebook.com/diogo.biazus"
+        },
+        verified: true
+      },
+      provider: "facebook",
+      uid: "547955110"
+    })
+  }
+
   describe "Associations" do
     it{ should belong_to :user }
     it{ should belong_to :oauth_provider }
@@ -10,5 +52,35 @@ describe Authorization do
     it{ should validate_presence_of :oauth_provider } 
     it{ should validate_presence_of :user } 
     it{ should validate_presence_of :uid } 
+  end
+
+  describe ".find_from_hash" do
+    before do
+      provider = create(:oauth_provider, name: oauth_data[:provider])
+      @authotization = create(:authorization, oauth_provider: provider, uid: oauth_data[:uid])
+      create(:authorization, oauth_provider: provider)
+    end
+    subject{ Authorization.find_from_hash(oauth_data) }
+    it{ should == @authotization }
+  end
+
+  describe ".create_from_hash" do
+    before do
+      create(:oauth_provider, name: oauth_data[:provider])
+    end
+    subject{ Authorization.create_from_hash(oauth_data, user) }
+    context "when user exists" do
+      let(:user){ create(:user, email: oauth_data['info']['email']) }
+      it{ should be_persisted }
+      its(:uid){ should == oauth_data['uid'] }
+      its(:user){ should == user }
+    end
+
+    context "when user is new" do
+      let(:user){}
+      it{ should be_persisted }
+      its(:uid){ should == oauth_data['uid'] }
+      its(:user){ should be_persisted }
+    end
   end
 end
