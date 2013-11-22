c9039bd60831803fe9234c1194775a916cc9ba41
Diogo Biazus <diogob@gmail.com>
2013-11-22 10:47:18 -0200

    fixing project video iframe url

--- app/decorators/project_decorator.rb
+++ app/decorators/project_decorator.rb
@@ -39,7 +39,7 @@ class ProjectDecorator < Draper::Decorator
 
   def display_video_embed_url
     if source.video_embed_url
-      "//#{source.video_embed_url}?title=0&byline=0&portrait=0&autoplay=0"
+      "#{source.video_embed_url}?title=0&byline=0&portrait=0&autoplay=0"
     end
   end
 
