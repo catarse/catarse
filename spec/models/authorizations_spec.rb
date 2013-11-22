cac5c1ee7dba4b6b5044cba54468cc53bae7b8bf
Diogo Biazus <diogob@gmail.com>
2013-11-22 08:39:25 -0200

    added basic validations and specs

--- spec/models/authorizations_spec.rb
+++ spec/models/authorizations_spec.rb
@@ -1,6 +1,14 @@
 require 'spec_helper'
 
 describe Authorization do
-  it{ should belong_to :user }
-  it{ should belong_to :oauth_provider }
+  describe "Associations" do
+    it{ should belong_to :user }
+    it{ should belong_to :oauth_provider }
+  end
+
+  describe "Validations" do
+    it{ should validate_presence_of :oauth_provider } 
+    it{ should validate_presence_of :user } 
+    it{ should validate_presence_of :uid } 
+  end
 end
