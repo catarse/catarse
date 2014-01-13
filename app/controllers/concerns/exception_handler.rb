bfe303199a2685863f9049756f88b3586820f402
Diogo Biazus <diogob@gmail.com>
2014-01-12 20:26:20 -0500

    Changed exception handler to handle Pundit and CanCan exceptions using the same method. Let's try a smooth migration.

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -7,17 +7,21 @@ module Concerns
       rescue_from ActionController::UnknownController, with: :render_404
       rescue_from ActiveRecord::RecordNotFound, with: :render_404
 
-      rescue_from CanCan::Unauthorized do |exception|
-        session[:return_to] = request.env['REQUEST_URI']
-        message = exception.message
+      rescue_from Pundit::NotAuthorizedError, with: :auth_error
+      rescue_from CanCan::Unauthorized, with: :auth_error
 
-        if current_user.nil?
-          redirect_to new_user_registration_path, alert: I18n.t('devise.failure.unauthenticated')
-        elsif request.env["HTTP_REFERER"]
-          redirect_to :back, alert: message
-        else
-          redirect_to root_path, alert: message
-        end
+    end
+
+    def auth_error(exception)
+      session[:return_to] = request.env['REQUEST_URI']
+      message = exception.message
+
+      if current_user.nil?
+        redirect_to new_user_registration_path, alert: I18n.t('devise.failure.unauthenticated')
+      elsif request.env["HTTP_REFERER"]
+        redirect_to :back, alert: message
+      else
+        redirect_to root_path, alert: message
       end
     end
 
