9a216435e70d19ce6e988ccd332fcb08d6d58dd7
Antônio Roberto Silva <forevertonny@gmail.com>
2014-01-14 23:23:18 -0200

    show only visible posts

--- app/controllers/channels/posts_controller.rb
+++ app/controllers/channels/posts_controller.rb
@@ -2,13 +2,13 @@ class Channels::PostsController < Channels::BaseController
   layout 'catarse_bootstrap'
   inherit_resources
 
-  def index
-    @post = ChannelPost.new(channel_id: channel.id, user_id: current_user.id) if current_user
-  end
-
   private
 
   def begin_of_association_chain
     channel
+  end
+
+  def end_of_association_chain
+    begin_of_association_chain.channel_posts.visible
   end
 end
