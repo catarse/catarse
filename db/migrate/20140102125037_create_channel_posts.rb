df70615cdb4bce2c82f234a126634ffd575d6cf4
Antônio Roberto Silva <forevertonny@gmail.com>
2014-01-16 10:04:06 -0200

    added not null into channel posts table

--- db/migrate/20140102125037_create_channel_posts.rb
+++ db/migrate/20140102125037_create_channel_posts.rb
@@ -1,12 +1,12 @@
 class CreateChannelPosts < ActiveRecord::Migration
   def change
     create_table :channel_posts do |t|
-      t.text :title
-      t.text :body
-      t.text :body_html
-      t.references :channel, index: true
-      t.references :user, index: true
-      t.boolean :visible
+      t.text :title, null: false
+      t.text :body, null: false
+      t.text :body_html, null: false
+      t.references :channel, index: true, null: false
+      t.references :user, index: true, null: false
+      t.boolean :visible, null: false, default: false
 
       t.timestamps
     end
