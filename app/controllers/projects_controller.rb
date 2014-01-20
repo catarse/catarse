64116db98b15e35af9a54992d51d97c25cb261a6
Diogo Biazus <diogob@gmail.com>
2014-01-20 06:36:55 -0500

    using shorter array of symbols constant

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 class ProjectsController < ApplicationController
-  after_filter :verify_authorized, except: [:index, :video, :video_embed, :embed, :embed_panel]
+  after_filter :verify_authorized, except: %i[index video video_embed embed embed_panel]
   inherit_resources
   has_scope :pg_search, :by_category_id, :near_of
   has_scope :recent, :expiring, :successful, :recommended, :not_expired, type: :boolean
