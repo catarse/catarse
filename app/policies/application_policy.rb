b36a141b3df6755c6a716b78a96e5fd26daa3e73
Diogo Biazus <diogob@gmail.com>
2014-01-24 11:56:34 -0500

    Added method to policy to check if an attribute is permitted. Added a spec on project policy for it.

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -38,6 +38,10 @@ class ApplicationPolicy
     Pundit.policy_scope!(user, record.class)
   end
 
+  def permitted?(field)
+    permitted_attributes.values.first.include? field
+  end
+
   protected
   def done_by_onwer_or_admin?
     is_owned_by?(user) || user.try(:admin?)
