a233ff65f2b633fbac95ad4478c7944c09b574b2
Adriano Benin <adriano.benin@gmail.com>
2014-01-29 12:32:44 -0200

    remove unneeded scopes

--- app/models/user.rb
+++ app/models/user.rb
@@ -160,10 +160,6 @@ class User < ActiveRecord::Base
     Project.contributed_by(self.id)
   end
 
-  def created_projects
-    Project.created_by(self.id)
-  end
-
   def fix_twitter_user
     self.twitter.gsub!(/@/, '') if self.twitter
   end
