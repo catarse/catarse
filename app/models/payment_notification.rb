6be66f0368a5d42e2a8d12438ade37e8b7868a88
Antônio Roberto Silva <forevertonny@gmail.com>
2014-02-06 09:22:53 -0200

    create deliver_process_notification in payment notification and removed the observer [#62727374]

--- app/models/payment_notification.rb
+++ app/models/payment_notification.rb
@@ -1,4 +1,15 @@
 class PaymentNotification < ActiveRecord::Base
   schema_associations
   serialize :extra_data, JSON
+
+  # This method should be called by payments engines
+  # when user payment status is processing
+  def deliver_process_notification
+    Notification.notify_once(:processing_payment,
+      self.contribution.user,
+      {contribution_id: self.contribution.id},
+      contribution: self.contribution,
+      project_name: self.contribution.project.name,
+      payment_method: self.contribution.payment_method)
+  end
 end
