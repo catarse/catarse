2a4f99f67dda6110a95bdfc638ef21e2b783eb9c
Diogo Biazus <diogob@gmail.com>
2014-02-07 14:58:50 -0500

    added user policy to the project

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,23 @@
+class UserPolicy < ApplicationPolicy
+  def update?
+    done_by_onwer_or_admin?
+  end
+
+  def update_password?
+    done_by_onwer_or_admin?
+  end
+  
+  def unsubscribe_notifications?
+    done_by_onwer_or_admin?
+  end
+
+  def uservoice_gadget?
+    done_by_onwer_or_admin?
+  end
+
+  protected
+  def done_by_onwer_or_admin?
+    record == user || user.try(:admin?)
+  end
+end
+
