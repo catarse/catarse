dda8ab9247d1f93bc7e0c6e477146fd8abd46518
Diogo Biazus <diogob@gmail.com>
2014-02-24 23:38:26 -0500

    Moved channel profile video controls to a Skull View [#65577788]

---
+++ app/assets/javascripts/app/channel_profile.js
@@ -0,0 +1,35 @@
+App.addChild('ChannelProfile', _.extend({
+  el: '#channel-profile',
+
+  events: {
+    'click .video-thumb' : 'clickVideoThumb',
+    'click .big-video-close' : 'clickBigVideoClose'
+  },
+
+  clickBigVideoClose: function(){
+    var that = this;
+    this.bigVideo.slideUp( "slow", function() {
+      that.bigVideoClose.toggle();
+      that.channelBio.slideDown();
+      $('html,body').animate({scrollTop: 0},'slow');
+    });
+  },
+
+  clickVideoThumb: function(){
+    var that = this;
+    this.channelBio.toggle();
+    this.bigVideo.slideDown( "slow", function() {
+      $('html,body').animate({scrollTop: that.bigVideo.offset().top},'slow');
+    });
+    this.bigVideoClose.toggle("easein");
+  },
+
+  activate: function(){
+    this.bigVideo = this.$('.big-video');
+    this.bigVideoClose = this.$('.big-video-close');
+    this.channelBio = this.$('.channel-bio');
+  }
+
+}));
+
+
