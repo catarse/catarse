2ca4f516d4b1bd57eba6ee1ea45db7e7e504816e
Antônio Roberto Silva <forevertonny@gmail.com>
2014-03-13 17:30:22 -0300

    added direct refund methods into contribution payment engine [#67474192]

--- spec/models/concerns/contribution/payment_engine_handler_spec.rb
+++ spec/models/concerns/contribution/payment_engine_handler_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Contribution::PaymentEngineHandler do
-  let(:engine){ { name: 'moip', review_path: ->(contribution){ "/#{contribution}" }, locale: 'en', refund_path: ->(contribution){ "/refund/#{contribution}" } } }
+  let(:engine){ { name: 'moip', review_path: ->(contribution){ "/#{contribution}" }, locale: 'en' } }
   let(:contribution){ create(:contribution, payment_method: 'MoIP') }
 
   before do
@@ -23,17 +23,44 @@ describe Contribution::PaymentEngineHandler do
     end
   end
 
-  describe "#refund_path" do
-    subject { contribution.refund_path }
+  describe "#can_do_refund?" do
+    subject { contribution.can_do_refund? }
 
-    context "when contribution has a payment engine" do
-      before { PaymentEngines.register engine }
+    context "when contribution has a payment engine with direct refund enabled" do
+      before do
+        PaymentEngines.register(engine.merge!({ can_do_refund?: true }))
+        contribution.stub(:direct_refund).and_return(true)
+      end
 
-      it { should eq(engine[:refund_path].call(contribution)) }
+      it { should be_true }
     end
 
-    context "when contribution don't have a payment engine" do
-      it { should eq(nil) }
+    context "when contribution has a payment engine without direct refund enabled" do
+      before do
+        PaymentEngines.register(engine)
+      end
+
+      it { should be_false }
+    end
+  end
+
+  describe "direct_refund" do
+    subject { contribution.direct_refund }
+
+    context "when contribution has a payment engine with direct refund enabled" do
+      before do
+        PaymentEngines.register(engine.merge!({ direct_refund: ->(contribution) { true } }))
+      end
+
+      it { should be_true }
+    end
+
+    context "when contribution has a payment engine without direct refund enabled" do
+      before do
+        PaymentEngines.register(engine)
+      end
+
+      it { should be_false }
     end
   end
 
