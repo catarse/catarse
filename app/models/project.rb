12dc8a902575f64d53022cf66317c4f569e1c5cb
Diogo Biazus <diogob@gmail.com>
2014-03-13 14:46:39 -0400

    included timezone conversion in between dates filter for project model (it's needed for the expires_at filter) [fix #58264386]

--- app/models/project.rb
+++ app/models/project.rb
@@ -186,7 +186,7 @@ class Project < ActiveRecord::Base
   private
   def self.between_dates(attribute, starts_at, ends_at)
     return scoped unless starts_at.present? && ends_at.present?
-    where("projects.#{attribute}::date between to_date(?, 'dd/mm/yyyy') and to_date(?, 'dd/mm/yyyy')", starts_at, ends_at)
+    where("(projects.#{attribute} AT TIME ZONE coalesce((SELECT c.value FROM configurations c WHERE c.name = 'timezone'), 'America/Sao_Paulo'))::date between to_date(?, 'dd/mm/yyyy') and to_date(?, 'dd/mm/yyyy')", starts_at, ends_at)
   end
 
   def self.get_routes
