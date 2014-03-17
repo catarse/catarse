5eda2d081f2be320d525368c154bf21151143720
Diogo Biazus <diogob@gmail.com>
2014-03-17 11:14:34 -0400

    Moved menu creation to menu handler included this should close #608 and makes it easier to handle access control.

--- app/controllers/admin/financials_controller.rb
+++ app/controllers/admin/financials_controller.rb
@@ -3,8 +3,6 @@ class Admin::FinancialsController < Admin::BaseController
   inherit_resources
   defaults  resource_class: Project, collection_name: 'projects', instance_name: 'project'
 
-  add_to_menu "admin.financials.index.menu", :admin_financials_path
-
   has_scope :by_permalink, :name_contains, :user_name_contains, :financial, :with_state, :by_progress
   has_scope :between_expires_at, using: [ :start_at, :ends_at ], allow_blank: true
 
