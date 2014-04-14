77a8645494fdddfabfda75c9f9fd5d62c25bb970
Diogo Biazus <diogob@gmail.com>
2014-04-14 15:03:06 -0400

    using secret key on devise config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.secret_key = CatarseSettings[:secret_key_base]
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
