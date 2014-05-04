f8c80cd1c2918c27520c53c78b01dbf73a097fc2
Diogo Biazus <diogob@gmail.com>
2014-05-04 17:05:56 -0400

    added mixpanel tracking in a separate observer (in case someone wants to remove mixpanel tracking) and added tracking events for contribution confirmation

---
+++ spec/observers/mixpanel_observer_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe MixpanelObserver do
+  let(:contribution){ create(:contribution, key: 'should be updated', payment_method: 'should be updated', state: 'confirmed', confirmed_at: nil) }
+  let(:tracker){ double('mixpanel-ruby tracker', {track: nil}) }
+
+  before do
+    MixpanelObserver.any_instance.stub(tracker: tracker)
+    tracker.should_receive(:track).with(contribution.user.id.to_s, "Contribution confirmed")
+  end
+
+  describe "#from_waiting_confirmation_to_confirmed" do
+    it "should send tracker a track call with the user id of the contribution" do
+      contribution.notify_observers :from_waiting_confirmation_to_confirmed
+    end
+  end
+
+  describe "#from_pending_to_confirmed" do
+    it "should send tracker a track call with the user id of the contribution" do
+      contribution.notify_observers :from_pending_to_confirmed
+    end
+  end
+end
+
+
