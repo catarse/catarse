bbc66165d3a7dfffabdd14b131cd457faa87e750
Diogo Biazus <diogob@gmail.com>
2014-05-05 20:59:57 -0400

    bring current_date to ruby to avoid time zone problems generating current_date (the DB is UTC always).

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -25,7 +25,7 @@ class Contribution < ActiveRecord::Base
   scope :anonymous, -> { where(anonymous: true) }
   scope :credits, -> { where("credits OR lower(payment_method) = 'credits'") }
   scope :not_anonymous, -> { where(anonymous: false) }
-  scope :confirmed_today, -> { with_state('confirmed').where("contributions.confirmed_at::date = current_date ") }
+  scope :confirmed_today, -> { with_state('confirmed').where("contributions.confirmed_at::date = to_date(?, 'yyyy-mm-dd')", Time.now.strftime('%Y-%m-%d')) }
 
   scope :can_cancel, -> { where("contributions.can_cancel") }
 
