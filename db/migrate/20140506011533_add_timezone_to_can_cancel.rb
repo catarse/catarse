3e503336ab2e55349a1469764011a6670dbdeb23
Diogo Biazus <diogob@gmail.com>
2014-05-05 21:19:26 -0400

    fixed database function can_cancel to take into account the timezone

---
+++ db/migrate/20140506011533_add_timezone_to_can_cancel.rb
@@ -0,0 +1,29 @@
+class AddTimezoneToCanCancel < ActiveRecord::Migration
+  def change
+    execute "
+CREATE OR REPLACE FUNCTION public.can_cancel(contributions)
+ RETURNS boolean
+ LANGUAGE sql
+AS $function$
+        select
+          $1.state = 'waiting_confirmation' and
+          (
+            ((
+              select count(1) as total_of_days
+              from generate_series($1.created_at::date, (current_timestamp AT TIME ZONE coalesce((SELECT value FROM settings WHERE name = 'timezone'), 'America/Sao_Paulo'))::date, '1 day') day
+              WHERE extract(dow from day) not in (0,1)
+            )  > 4)
+            OR
+            (
+              lower($1.payment_choice) = lower('DebitoBancario')
+              AND
+                (
+                  select count(1) as total_of_days
+                  from generate_series($1.created_at::date, (current_timestamp AT TIME ZONE coalesce((SELECT value FROM settings WHERE name = 'timezone'), 'America/Sao_Paulo'))::date, '1 day') day
+                  WHERE extract(dow from day) not in (0,1)
+                )  > 1)
+          )
+      $function$
+      "
+  end
+end
