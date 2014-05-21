b528a70e92aa9305c4795b78bc917bd2986811af
Antônio Roberto Silva <forevertonny@gmail.com>
2014-05-21 15:45:15 -0300

    added payment method on Finished contribution tracker [12436035]

--- app/observers/mixpanel_observer.rb
+++ app/observers/mixpanel_observer.rb
@@ -6,6 +6,21 @@ class MixpanelObserver < ActiveRecord::Observer
   end
   alias :from_pending_to_confirmed :from_waiting_confirmation_to_confirmed
 
+  def from_pending_to_waiting_confirmation(contribution)
+    user = contribution.user
+
+    tracker.track(user.id.to_s, "Finished contribution", {
+      user_id: user.id.to_s,
+      created: user.created_at,
+      last_login: user.last_sign_in_at,
+      contributions: user.total_contributed_projects,
+      has_contributions: (user.total_contributed_projects > 0),
+      project: contribution.project.name,
+      payment_method: contribution.payment_method,
+      payment_choice: contribution.payment_choice
+    });
+  end
+
   private
   def tracker
     @tracker ||= Mixpanel::Tracker.new(CatarseSettings[:mixpanel_token])
