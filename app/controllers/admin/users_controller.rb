dbd7ef83655d6a435f849c19125929b8bc8ce4a4
Diogo Biazus <diogob@gmail.com>
2014-05-23 12:10:43 -0400

    fixing admin users title

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,15 +1,9 @@
 class Admin::UsersController < Admin::BaseController
   layout 'catarse_bootstrap'
   inherit_resources
-  before_filter :set_title
-
   has_scope :by_id, :by_name, :by_email, :by_payer_email, :by_key, :has_credits, :has_credits_difference, only: :index
 
   protected
-  def set_title
-    @title = t("admin.users.index.title")
-  end
-
   def collection
     @users ||= apply_scopes(end_of_association_chain).order_by(params[:order_by] || 'coalesce(user_totals.sum, 0) DESC').includes(:user_total).page(params[:page])
   end
