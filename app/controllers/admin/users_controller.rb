fdc843d4393142c69d367819eb2a86ad97919ba9
Diogo Biazus <diogob@gmail.com>
2014-06-10 19:46:42 -0400

    fixing admin/users [12851158]

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,12 @@
 class Admin::UsersController < Admin::BaseController
   layout 'catarse_bootstrap'
   inherit_resources
-  has_scope :by_id, :by_name, :by_email, :by_payer_email, :by_key, :has_credits, :has_credits_difference, only: :index
+  has_scope :by_id, :by_name, :by_email, :by_payer_email, :by_key, only: :index
+  has_scope :has_credits, type: :boolean, only: :index
 
   protected
   def collection
-    @users ||= apply_scopes(end_of_association_chain).order_by(params[:order_by] || 'coalesce(user_totals.sum, 0) DESC').includes(:user_total).page(params[:page])
+    @users ||= apply_scopes(end_of_association_chain.joins(:user_total)).order_by(params[:order_by] || 'coalesce(user_totals.sum, 0) DESC').includes(:user_total).page(params[:page])
   end
 end
 
