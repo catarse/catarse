8cb3045e00ccc922fee09b9027812f60ee54a3e4
Diogo Biazus <diogob@gmail.com>
2014-06-16 16:39:22 -0400

    adding sidekiq initializer to solve heroku redis connection

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,7 @@
+Sidekiq.configure_server do |config|
+    config.redis = { url: ENV["REDISTOGO_URL"] || "redis://localhost:6379/" }
+end
+
+Sidekiq.configure_client do |config|
+    config.redis = { url: ENV["REDISTOGO_URL"] || "redis://localhost:6379/" }
+end
