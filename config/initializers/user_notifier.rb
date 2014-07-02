0076a57063dec3b6555b6f48d5335c067ab9c5bc
Diogo Biazus <diogob@gmail.com>
2014-07-02 14:11:55 -0400

    added user_notifier gem and its configuration

---
+++ config/initializers/user_notifier.rb
@@ -0,0 +1,22 @@
+UserNotifier.configure do |config|
+  # The system email will be used in all 'from' fields in your emails
+  # This avoids issues with the sender autenticity, 
+  # we always use the reply_to to set a return address
+  config.system_email     = CatarseSettings[:email_system]
+
+  # The name of the email template in your views/layouts
+  config.email_layout     = 'email'
+
+  # The class name of your model representing system users that will receive the notifications
+  # The model should have an email attribute containing the email address of the user
+  config.user_class_name  = 'User'
+
+  # This is the default email for reply_to 
+  # in case the notification does not have a variable from_email
+  config.from_email       = CatarseSettings[:email_contact]
+
+  # This is the default name used in from and reply_to 
+  # in case the notification does not have a variable from_name
+  config.from_name        = CatarseSettings[:company_name]
+end
+
