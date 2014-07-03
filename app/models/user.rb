cf4a568d0933719fab7a870caa4964da76b72bc6
Diogo Biazus <diogob@gmail.com>
2014-07-03 17:22:16 -0400

    adding user notification using the UserNotifier gem and moving some templates to be used by the gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 class User < ActiveRecord::Base
   include User::OmniauthHandler
+  has_notifications
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   # :validatable
@@ -99,13 +100,13 @@ class User < ActiveRecord::Base
     end
   end
 
-  def notify(template_name, params = {})
-    Notification.notify(
-      template_name,
-      self,
-      params
-    )
-  end
+  #def notify(template_name, params = {})
+  #  Notification.notify(
+  #    template_name,
+  #    self,
+  #    params
+  #  )
+  #end
 
   def active_for_authentication?
     super && deactivated_at.nil?
