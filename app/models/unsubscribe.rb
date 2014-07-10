3463aa263cf6bad72efd0b003aa93bae42783de4
Antônio Roberto Silva <forevertonny@gmail.com>
2014-07-10 17:42:02 -0300

    turn unsubscribe controller more readble

--- app/models/unsubscribe.rb
+++ app/models/unsubscribe.rb
@@ -3,7 +3,13 @@ class Unsubscribe < ActiveRecord::Base
 
   attr_accessor :subscribed
 
+  scope :by_project_id, ->(project_id) { where(project_id: project_id) }
+
   def self.updates_unsubscribe project_id
     find_or_initialize_by(project_id: project_id)
+  end
+
+  def self.drop_all_for_project(project_id)
+    by_project_id(project_id).destroy_all
   end
 end
