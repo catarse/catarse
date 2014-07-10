dc1863c1263980144dabdf9f09200e5f9703fd01
Diogo Biazus <diogob@gmail.com>
2014-07-09 21:32:36 -0400

    created contribution_notifications table

---
+++ db/migrate/20140709163624_create_contribution_notifications.rb
@@ -0,0 +1,13 @@
+class CreateContributionNotifications < ActiveRecord::Migration
+  def change
+    create_table :contribution_notifications do |t|
+      t.integer :user_id, null: false
+      t.integer :contribution_id, null: false
+      t.text :from_email, null: false
+      t.text :from_name, null: false
+      t.text :template_name, null: false
+      t.text :locale, null: false
+      t.timestamp :sent_at
+    end
+  end
+end
