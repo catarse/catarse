ca749fe6081f11145c25180c0399f14b34eb61f8
Diogo Biazus <diogob@gmail.com>
2014-07-11 09:55:44 -0400

    fixed worker specs using database checks instead of expectations

--- spec/workers/pending_contribution_worker_spec.rb
+++ spec/workers/pending_contribution_worker_spec.rb
@@ -11,50 +11,41 @@ describe PendingContributionWorker do
   end
 
   context "when contribution is pending" do
-    before do
-      Notification.should_receive(:notify_once).with(
-        :pending_payment,
-        contribution.user,
-        { contribution_id: contribution.id },
-        contribution: contribution
-      )
-    end
-
     it "should create a pending payment notification" do
       PendingContributionWorker.perform_async(contribution.id)
+      expect(ContributionNotification.where({
+        template_name: 'pending_payment', 
+        user: contribution.user, 
+        contribution: contribution
+      }).count(:all)).to eq 1
     end
   end
 
   context "when contribution is pending but we have a confirmed contribution on the same project" do
     before do
       confirmed_contribution
-      Notification.should_not_receive(:notify_once).with(
-        :pending_payment,
-        contribution.user,
-        { contribution_id: contribution.id },
-        contribution: contribution
-      )
     end
 
     it "should not create a pending payment notification" do
       PendingContributionWorker.perform_async(contribution.id)
+      expect(ContributionNotification.where({
+        template_name: 'pending_payment', 
+        user: contribution.user, 
+        contribution: contribution
+      }).count(:all)).to eq 0
     end
   end
 
   context "whe contribution is not pending" do
-    before do
-      contribution.stub(:pending?).and_return(false)
-
-      Notification.any_instance.should_not_receive(:notify_once).with(
-        :pending_payment,
-        contribution.user,
-        { contribution_id: contribution.id },
-        contribution: contribution
-      )
-    end
+    let(:contribution) { create(:contribution, state: 'confirmed', project_id: project.id, user_id: user.id) }
 
     it "should not create a pending payment notification" do
       PendingContributionWorker.perform_async(contribution.id)
+      expect(ContributionNotification.where({
+        template_name: 'pending_payment', 
+        user: contribution.user, 
+        contribution: contribution
+      }).count(:all)).to eq 0
     end
   end
 
