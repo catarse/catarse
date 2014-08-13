b34bc28d5659d890cd9a95d96fea5989e5d5fed2
Antônio Roberto Silva <forevertonny@gmail.com>
2014-08-13 19:34:07 -0300

    pagarme config

--- config/initializers/pagarme.rb
+++ config/initializers/pagarme.rb
@@ -1,4 +1,6 @@
 CatarsePagarme.configure do |config|
   config.api_key = CatarseSettings[:pagarme_api_key]
-  slip_tax = CatarseSettings[:pagarme_slip_tax]
+  config.slip_tax = CatarseSettings[:pagarme_slip_tax]
+  config.credit_card_tax = CatarseSettings[:pagarme_credit_card_tax]
+  config.interest_rate = CatarseSettings[:pagarme_interest_rate]
 end
