e5d9696a73d95c9f58be9c6cf4a6698b59021e9c
Diogo Biazus <diogob@gmail.com>
2014-08-14 17:56:03 -0400

    errors alert in new contribution form and JS tricks to make the value compatible with reward

--- app/assets/javascripts/app/contribution.js
+++ app/assets/javascripts/app/contribution.js
@@ -3,7 +3,8 @@ App.addChild('Contribution', {
 
   events: {
     'click label.back-reward-radio-reward' : 'clickReward',
-    'click button#submit' : 'submitForm'
+    'click button#submit' : 'submitForm',
+    'blur #contribution_value' : 'resetReward'
   },
 
   submitForm: function(){
@@ -13,11 +14,24 @@ App.addChild('Contribution', {
 
   activate: function(){
     this.$('input[type=radio]:checked').parent().addClass('selected');
+    this.$value = this.$('#contribution_value');
+  },
+
+  resetReward: function(event){
+    if(parseInt('0' + this.$value.val()) < this.minimumValue()){
+      this.$('label.back-reward-radio-reward:first').click();
+    }
+  },
+
+  minimumValue: function(){
+    return this.$('label.back-reward-radio-reward.selected').find('label[data-minimum-value]').data('minimum-value');
   },
 
   clickReward: function(event){
+    $currentTarget = $(event.currentTarget);
     this.$('label.back-reward-radio-reward').removeClass('selected');
-    $(event.currentTarget).addClass('selected');
+    $currentTarget.addClass('selected');
+    this.$value.val(this.minimumValue());
   }
 });
 
--- app/decorators/reward_decorator.rb
+++ app/decorators/reward_decorator.rb
@@ -16,11 +16,11 @@ class RewardDecorator < Draper::Decorator
   def name
     deliver = %{
         <div class="back-reward-delivery-date caption">
-          Estimativa de entrega:&nbsp;#{source.display_deliver_estimate}
+          Estimativa de entrega:&nbsp;#{source.display_deliver_estimate || I18n.t('projects.contributions.no_estimate')}
         </div>
     }
     %{
-      <label class="w-form-label headline" for="contribution_reward#{source.id && "_#{source.id}"}">#{source.minimum_value > 0 ? source.display_minimum+'+' : I18n.t('rewards.index.dont_want')}</label>
+      <label data-minimum-value="#{source.minimum_value > 0 ? source.minimum_value.to_i : '10'}" class="w-form-label headline" for="contribution_reward#{source.id && "_#{source.id}"}">#{source.minimum_value > 0 ? source.display_minimum+'+' : I18n.t('rewards.index.dont_want')}</label>
       <div class="back-reward-selected-badge caption no-mobile">#{I18n.t('projects.contributions.you_selected')}</div>
       <div class="back-reward-reward-description">
         <p class="body-medium">
