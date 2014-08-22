e9b111653b2c92e5cbf3ceea5116ffa8565bf6c4
Antônio Roberto Silva <forevertonny@gmail.com>
2014-08-22 17:35:06 -0300

    added spec to check associations in bank_account

--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe BankAccount do
+  describe "associations" do
+    it{ should belong_to :user }
+  end
+
   describe "Validations" do
     it{ should validate_presence_of(:name) }
     it{ should validate_presence_of(:agency) }
