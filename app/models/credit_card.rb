6710a987cac005fe3add28d787974b2a71b46a6d
Antônio Roberto Silva <forevertonny@gmail.com>
2014-08-25 11:05:15 -0300

    rename column on credit_cards object_id -> subscription_id

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,5 +1,5 @@
 class CreditCard < ActiveRecord::Base
   belongs_to :user
 
-  validates :user, :last_digits, :card_brand, :object_id, presence: true
+  validates :user, :last_digits, :card_brand, :subscription_id, presence: true
 end
