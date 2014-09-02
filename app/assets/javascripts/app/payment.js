76c9aa073d85f46c4e6175ed5fffaf76f2a31885
Diogo Biazus <diogob@gmail.com>
2014-09-02 16:48:45 -0400

    added payment engines tabs to edit contribution form

--- app/assets/javascripts/app/payment.js
+++ app/assets/javascripts/app/payment.js
@@ -1,22 +1,21 @@
 App.addChild('Payment', _.extend({
-  el: '#project_review #payment',
+  el: '#payment-engines',
 
   events: {
-    'click #payment_menu a' : 'onTabClick'
+    'click .tab' : 'onTabClick'
   },
 
   activate: function(){
     _this = this;
 
     this.loadPaymentChoices();
-
     $('#live_in_brazil').on('change', function(){
       _this.loadPaymentChoices();
     });
   },
 
   updatePaymentMethod: function() {
-    var $selected_tab = this.$('#payment_menu a.selected');
+    var $selected_tab = this.$('.tab.selected');
     $.ajax({
       url: this.$el.data('update-info-path'),
       type: 'PUT',
@@ -25,12 +24,12 @@ App.addChild('Payment', _.extend({
   },
 
   hideNationalPayment: function() {
-    this.$('#payment_menu a#MoIP').hide();
+    this.$('#MoIP').hide();
     this.$('.payments_type#MoIP_payment').hide();
   },
 
   selectInternationalPayment: function() {
-    this.onTabClick({currentTarget: this.$('#payment_menu a#PayPal')});
+    this.onTabClick({currentTarget: this.$('#PayPal')});
   },
 
   loadPaymentChoices: function() {
@@ -38,8 +37,8 @@ App.addChild('Payment', _.extend({
       this.hideNationalPayment();
       this.selectInternationalPayment();
     } else {
-      this.$('#payment_menu a#MoIP').show();
-      this.onTabClick({currentTarget: this.$('#payment_menu a:first')});
+      this.$('#payment-engines #MoIP').show();
+      this.onTabClick({currentTarget: this.$('.tabs:first')});
     }
 
     this.on('selectTab', this.updatePaymentMethod);
