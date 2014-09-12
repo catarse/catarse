d22b70745baef8f4019a88cecadbd91fcf97d68d
Antônio Roberto <forevertonny@gmail.com>
2014-09-12 19:26:23 -0300

    non non non comments

--- app/assets/javascripts/app/project/project_metrics.js
+++ app/assets/javascripts/app/project/project_metrics.js
@@ -6,20 +6,6 @@ App.views.Project.addChild('ProjectMetrics', {
     this.$loaderDiv = this.$("#metrics-loading");
     this.$results = this.$(".results");
     this.path = this.$el.data('path');
-
-    //this.$('.project-metrics').css({width: '980px'});
-    //)this.parent.on('selectTab', this.parent.loadTab);
-  },
-
-  //loadMetrics: function() {
-  //  var that = this;
-
-  //  if($('a#metrics_link').hasClass('selected')) {
-  //    $.get(this.path, function(data) {
-  //      that.$results.html(data);
-  //    });
-  //  }
-  //}
-
+  }
 });
 
