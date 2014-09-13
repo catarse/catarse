c8de17dc2827d0bf46bb6f14ef7ba6dc850fdcec
Antônio Roberto <forevertonny@gmail.com>
2014-09-12 22:30:02 -0300

    added methods to check if user is in queue for reminder :D
    we just forgot to look inside the scheduler set to check if user is
    present ;D

--- app/models/concerns/project/reminders_handler.rb
+++ app/models/concerns/project/reminders_handler.rb
@@ -3,7 +3,29 @@ module Project::RemindersHandler
 
   included do
     def user_already_in_reminder?(user_id)
-      notifications.where(template_name: 'reminder', user_id: user_id).present?
+      notifications.where(template_name: 'reminder', user_id: user_id).present? ||
+      user_in_reminder_queue?(user_id)
+    end
+
+    def user_in_reminder_queue?(user_id)
+      scheduler_set.any? do |job|
+        job['class'] == 'ReminderProjectWorker' &&
+        job.args[0] == user_id &&
+        job.args[1] == self.id
+      end
+    end
+
+    def delete_from_reminder_queue(user_id)
+      job = scheduler_set.select do |job|
+        job['class'] == 'ReminderProjectWorker' &&
+        job.args[0] == user_id &&
+        job.args[1] == self.id
+      end
+      job.each(&:delete)
+    end
+
+    def scheduler_set
+      @queue_set ||= Sidekiq::ScheduledSet.new
     end
   end
 end
