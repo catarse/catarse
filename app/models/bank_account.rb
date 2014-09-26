dc48e51d5e7e10c5e7e39386d6c1967aec753af1
Antônio Roberto <forevertonny@gmail.com>
2014-09-26 11:53:54 -0300

    added method to return the bank code from bank name

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -2,4 +2,8 @@ class BankAccount < ActiveRecord::Base
   belongs_to :user
 
   validates :name, :agency, :agency_digit, :account, :user_name, :user_document, :account_digit, presence: true
+
+  def bank_code
+    $1 if name.match(/(\w+) \- .*/)
+  end
 end
--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -8,9 +8,33 @@ describe BankAccount do
   describe "Validations" do
     it{ should validate_presence_of(:name) }
     it{ should validate_presence_of(:agency) }
+    it{ should validate_presence_of(:agency_digit) }
     it{ should validate_presence_of(:account) }
     it{ should validate_presence_of(:account_digit) }
     it{ should validate_presence_of(:user_name) }
     it{ should validate_presence_of(:user_document) }
+  end
+
+  describe "#bank_code" do
+    let(:bank_account) { BankAccount.new }
+    context "when bank have the number" do
+      before do
+        bank_account.name = '237 - Banco do Bradesco S.A.'
+      end
+
+      it "should return the bank code" do
+        expect(bank_account.bank_code).to eq("237")
+      end
+    end
+
+    context "when bank don't have number" do
+      before do
+        bank_account.name = 'Banco do Bradesco S.A.'
+      end
+
+      it "should return nil" do
+        expect(bank_account.bank_code).to eq(nil)
+      end
+    end
   end
 end
