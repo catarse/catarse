b1d962d7920cab37f4446fc4d7e9f839125db967
Antônio Roberto <forevertonny@gmail.com>
2014-09-26 16:53:30 -0300

    adjust some migrations :P

--- db/migrate/20140818185729_add_bank_account_into_contributions.rb
+++
@@ -1,5 +0,0 @@
-class AddBankAccountIntoContributions < ActiveRecord::Migration
-  def change
-    add_column :contributions, :bank_account_id, :integer
-  end
-end
--- db/migrate/20140822202511_create_credit_cards.rb
+++ db/migrate/20140822202511_create_credit_cards.rb
@@ -2,9 +2,9 @@ class CreateCreditCards < ActiveRecord::Migration
   def change
     create_table :credit_cards do |t|
       t.references :user, index: true
-      t.text :last_digits
-      t.text :card_brand
-      t.text :object_id, foreign_key: false
+      t.text :last_digits, null: false
+      t.text :card_brand, null: false
+      t.text :object_id, foreign_key: false, null: false
 
       t.timestamps
     end
