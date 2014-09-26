ded15f8261d1e28bf459bbabe3589255b6378170
Antônio Roberto <forevertonny@gmail.com>
2014-09-26 16:56:26 -0300

    more DRY <3

--- app/policies/credit_card_policy.rb
+++ app/policies/credit_card_policy.rb
@@ -2,9 +2,4 @@ class CreditCardPolicy < ApplicationPolicy
   def destroy?
     done_by_owner_or_admin?
   end
-
-  protected
-  def is_owned_by?(user)
-    user.present? && record.user == user
-  end
 end
