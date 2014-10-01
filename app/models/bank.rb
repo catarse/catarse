0a9fd503ce7291b98b53f7ee88a706d68c9d7f71
Antônio Roberto <forevertonny@gmail.com>
2014-10-01 16:04:18 -0300

    added .to_collection into banks to generate a list of banks for select

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -2,4 +2,14 @@ class Bank < ActiveRecord::Base
   has_many :bank_accounts
 
   validates :name, :code, presence: true
+
+  def self.to_collection
+    self.order(:name).map do |bank|
+      [bank.to_s, bank.id]
+    end
+  end
+
+  def to_s
+    [code, name].join(' . ')
+  end
 end
