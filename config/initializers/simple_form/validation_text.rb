2a965d744526a1a1294598539cefca0134d31c68
Diogo Biazus <diogob@gmail.com>
2014-10-03 11:17:04 -0400

    generating error text for fields using data-error-for

--- config/initializers/simple_form/validation_text.rb
+++ config/initializers/simple_form/validation_text.rb
@@ -4,7 +4,7 @@ module SimpleForm
     module ValidationTexts
       # Name of the component method
       def validation_text(wrapper_options = nil)
-        options[:validation_text_html] = {data: {for: "#{object_name}_#{attribute_name}"}}
+        options[:validation_text_html] = {data: {error_for: "#{object_name}_#{attribute_name}"}}
         @validation_text ||= begin
            " " + I18n.t("simple_form.validation_texts.#{object_name}.#{attribute_name}").html_safe if options[:validation_text]
         end
