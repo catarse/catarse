99a3f19bc299a5a2644631ee511b3c8160f66a36
Mauro George <maurogot@gmail.com>
2014-10-12 10:14:43 -0300

    Remove infer_spec_type_from_file_location! on RSpec config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,7 +25,6 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = false
-  config.infer_spec_type_from_file_location!
 
   config.include ActionView::Helpers::TextHelper
   config.include FactoryGirl::Syntax::Methods
