7d8d6f1395259d501f8975f33512c57626d00d96
Antônio Roberto <forevertonny@gmail.com>
2014-10-13 14:24:02 -0300

    show only online projects and search on category too

--- app/models/project.rb
+++ app/models/project.rb
@@ -34,7 +34,11 @@ class Project < ActiveRecord::Base
 
   catarse_auto_html_for field: :about, video_width: 600, video_height: 403
 
-  pg_search_scope :search_on_name, against: [[:name, 'A'], [:headline, 'B']],
+  pg_search_scope :search_on_name,
+    against: [[:name, 'A'], [:headline, 'B']],
+    associated_against: {
+      category: [:name_pt, :name_en]
+    },
     using: {tsearch: {dictionary: "portuguese"}},
     ignoring: :accents
 
