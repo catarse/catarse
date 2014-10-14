a202f47347a06af57875500e9f954c598d5b48c0
Antônio Roberto <forevertonny@gmail.com>
2014-10-14 19:51:44 -0300

    category follower specs

--- spec/models/category_follower_spec.rb
+++ spec/models/category_follower_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe CategoryFollower, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations" do
+    before do
+      create(:category_follower)
+    end
+
+    it{ is_expected.to belong_to :user }
+    it{ is_expected.to belong_to :category }
+  end
 end
