23869dc9f9417ce1902eed7e5adec9dd2754f754
Antônio Roberto <forevertonny@gmail.com>
2014-10-15 17:33:56 -0300

    adjusts

--- db/migrate/20141014213748_create_category_followers.rb
+++ db/migrate/20141014213748_create_category_followers.rb
@@ -1,8 +1,8 @@
 class CreateCategoryFollowers < ActiveRecord::Migration
   def change
     create_table :category_followers do |t|
-      t.references :category, index: true
-      t.references :user, index: true
+      t.references :category, index: true, null: false
+      t.references :user, index: true, null: false
 
       t.timestamps
     end
