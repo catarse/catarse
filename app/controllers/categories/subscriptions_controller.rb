686d7fd58dcf3c9efb7536ed2b208702897c7143
Antônio Roberto <forevertonny@gmail.com>
2014-10-15 17:11:06 -0300

    adjust copys

--- app/controllers/categories/subscriptions_controller.rb
+++ app/controllers/categories/subscriptions_controller.rb
@@ -5,14 +5,14 @@ class Categories::SubscriptionsController < ApplicationController
   def create
     parent.users << current_user
 
-    flash[:notice] = I18n.t('explore.index.success_follow')
+    flash[:notice] = I18n.t('explore.index.success_follow', name: parent.name_pt)
     redirect_to explore_path(anchor: "by_category_id/#{parent.id}")
   end
 
   def destroy
     parent.users.delete(current_user)
 
-    flash[:notice] = I18n.t('explore.index.success_unfollow')
+    flash[:notice] = I18n.t('explore.index.success_unfollow', name: parent.name_pt)
     redirect_to explore_path(anchor: "by_category_id/#{parent.id}")
   end
 
