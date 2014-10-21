541da125dbb48ba805d7dbb0950e25f0960dac7f
Antônio Roberto <forevertonny@gmail.com>
2014-10-21 10:10:05 -0200

    back to old scopes on adm/contribution for users

--- app/controllers/admin/contributions_controller.rb
+++ app/controllers/admin/contributions_controller.rb
@@ -1,6 +1,7 @@
 class Admin::ContributionsController < Admin::BaseController
   layout 'catarse_bootstrap'
   has_scope :project_name_contains, :search_on_payment_data, :with_state, :search_on_user, :search_on_acquirer
+  has_scope :user_name_contains, :user_email_contains
   has_scope :credits, type: :boolean
   has_scope :between_values, using: [ :start_at, :ends_at ], allow_blank: true
   before_filter :set_title
