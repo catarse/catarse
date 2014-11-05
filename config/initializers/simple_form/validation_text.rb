20a398ae210bdef929189c9e50b80f4dd14c7c4b
Diogo Biazus <diogob@gmail.com>
2014-11-05 10:05:14 -0500

    fixing simple_form initializer

--- config/initializers/simple_form/validation_text.rb
+++ config/initializers/simple_form/validation_text.rb
@@ -4,7 +4,7 @@ module SimpleForm
     module ValidationTexts
       # Name of the component method
       def validation_text(wrapper_options = nil)
-        for_id = object_name.gsub(/\[/, '_').gsub(/\]/, '')
+        for_id = object_name.to_s.gsub(/\[/, '_').gsub(/\]/, '')
         options[:validation_text_html] = {data: {error_for: "#{for_id}_#{attribute_name}"}}
         @validation_text ||= begin
           " " + I18n.t("simple_form.validation_texts.#{object.class.model_name.singular}.#{attribute_name}").html_safe if options[:validation_text]
