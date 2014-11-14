d107ff38984b40166790cc8d961c3d45eaf7169d
adriano benin <adriano.benin@gmail.com>
2014-11-14 16:43:20 -0200

    add twitter and fb share buttons on mobile layout

--- app/controllers/concerns/social_helpers_handler.rb
+++ app/controllers/concerns/social_helpers_handler.rb
@@ -5,7 +5,7 @@ module Concerns
     # We use this method only to make stubing easier
     # and remove FB templates from acceptance tests
     included do
-      helper_method :fb_admins, :render_facebook_sdk, :render_facebook_like, :render_twitter
+      helper_method :fb_admins, :render_facebook_sdk, :render_facebook_like, :render_twitter, :render_twitter_mobile, :render_facebook_share, :render_facebook_share_mobile
 
       before_filter do
         @fb_admins = [100000428222603, 547955110]
@@ -32,9 +32,20 @@ module Concerns
       render_to_string(partial: 'layouts/twitter', locals: options).html_safe
     end
 
+    def render_twitter_mobile options={}
+      render_to_string(partial: 'layouts/twitter_mobile', locals: options).html_safe
+    end
+
     def render_facebook_like options={}
       render_to_string(partial: 'layouts/facebook_like', locals: options).html_safe
     end
 
+    def render_facebook_share options={}
+      render_to_string(partial: 'layouts/facebook_share', locals: options).html_safe
+    end
+
+    def render_facebook_share_mobile options={}
+      render(partial: 'layouts/facebook_share_mobile', locals: options)[0].html_safe
+    end
   end
 end
