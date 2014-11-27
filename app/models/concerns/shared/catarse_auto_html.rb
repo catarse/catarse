813392c61eaf13139d2d40ea3a1a600ad6292c4b
Diogo Biazus <diogob@gmail.com>
2014-11-27 16:07:38 -0500

    fixed regex of named_link to use a more strict version

--- app/models/concerns/shared/catarse_auto_html.rb
+++ app/models/concerns/shared/catarse_auto_html.rb
@@ -14,7 +14,7 @@ module Shared::CatarseAutoHtml
     end
 
     AutoHtml.add_filter(:named_link) do |text, options|
-      text.gsub(/"(.+)":([^\s]+)/) do |match|
+      text.gsub(/"(.+?)":http([^\s]+)/) do |match|
         "<a target=\"_blank\" href=\"#{$2}\">#{$1}</a>"
       end
     end
@@ -28,10 +28,12 @@ module Shared::CatarseAutoHtml
           '"' => '"'
         }
         image
+        named_link
         youtube width: options[:video_width], height: options[:video_height], wmode: "opaque"
         vimeo width: options[:video_width], height: options[:video_height]
         redcarpet target: :_blank
         link target: :_blank
+        add_alt_link_class
       end
     end
 
