6283cde28a8ae2987787bc3fb8547385683f23d5
Diogo Biazus <diogob@gmail.com>
2014-11-28 13:04:48 -0500

    Adds country_id to address data that is updated in every contribution

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
     :image_url, :uploaded_image, :bio, :newsletter, :full_name, :address_street, :address_number,
     :address_complement, :address_neighbourhood, :address_city, :address_state, :address_zip_code, :phone_number,
     :cpf, :state_inscription, :locale, :twitter, :facebook_link, :other_link, :moip_login, :deactivated_at, :reactivate_token,
-    :bank_account_attributes
+    :bank_account_attributes, :country_id
 
   mount_uploader :uploaded_image, UserUploader
 
