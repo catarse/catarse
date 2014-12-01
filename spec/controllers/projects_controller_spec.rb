7eded480b58c75bf34cb6ccea5dff43eb2462d33
adriano benin <adriano.benin@gmail.com>
2014-12-01 16:46:44 -0200

    fix specs

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe ProjectsController, type: :controller do
 
     context "when user is logged in" do
       let(:current_user){ create(:user) }
-      it{ is_expected.to redirect_to edit_project_path(Project.last, anchor: 'basics') }
+      it{ is_expected.to redirect_to edit_project_path(Project.last, anchor: 'home') }
     end
   end
 
