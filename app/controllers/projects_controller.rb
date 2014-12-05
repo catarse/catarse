4bd54a449d64dad4c3dbd0a747859b2344f53dc6
Diogo Biazus <diogob@gmail.com>
2014-12-05 13:08:09 -0500

    using filter to detect mobile browsers in project show and contribution controllers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,7 @@ class ProjectsController < ApplicationController
   inherit_resources
   has_scope :pg_search, :by_category_id, :near_of
   has_scope :recent, :expiring, :successful, :in_funding, :recommended, :not_expired, type: :boolean
+  before_filter :detect_mobile_browsers, only: [:show]
 
   respond_to :html
   respond_to :json, only: [:index, :show, :update]
