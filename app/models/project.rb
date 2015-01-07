a6a73eaa7940c11ae0c08b4c687f49619c40edd9
adriano benin <adriano.benin@gmail.com>
2015-01-07 14:46:23 -0200

    only show account link starting on approved projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -170,6 +170,10 @@ class Project < ActiveRecord::Base
     self.using_pagarme(permalinks)
   end
 
+  def can_show_account_link?
+    ['online', 'waiting_funds', 'successful', 'approved'].include? state
+  end
+
   def can_show_funding_period?
     ['online', 'waiting_funds', 'successful', 'failed'].include? state
   end
