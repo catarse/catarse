f9852982a122f1954e7ec252f7b1ca566944197d
adriano benin <adriano.benin@gmail.com>
2015-01-12 10:17:52 -0200

    drop missing constraint

--- db/migrate/20150112121053_drop_project_constraints.rb
+++ db/migrate/20150112121053_drop_project_constraints.rb
@@ -3,6 +3,7 @@ class DropProjectConstraints < ActiveRecord::Migration
     execute <<-SQL
       ALTER TABLE projects DROP CONSTRAINT projects_about_not_blank;
       ALTER TABLE projects DROP CONSTRAINT projects_headline_not_blank;
+      ALTER TABLE projects DROP CONSTRAINT projects_headline_length_within;
     SQL
   end
 end
