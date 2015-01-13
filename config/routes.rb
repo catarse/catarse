aa854936f366ba9e14bc468caa065ea54cc66b0d
Thiago Maia <thiago@catarse.me>
2015-01-13 12:56:04 +1100

    Jobs page

--- config/routes.rb
+++ config/routes.rb
@@ -84,6 +84,8 @@ Catarse::Application.routes.draw do
   get "/terms-of-use" => 'high_voltage/pages#show', id: 'terms_of_use'
   get "/privacy-policy" => 'high_voltage/pages#show', id: 'privacy_policy'
   get "/start" => 'high_voltage/pages#show', id: 'start'
+  get "/jobs" => 'high_voltage/pages#show', id: 'jobs'
+
 
 
   # Channels
