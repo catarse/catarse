5146cb97f035d30841767eaa021bd4a29487578b
Antônio Roberto <forevertonny@gmail.com>
2015-01-19 14:49:18 -0200

    small adjusts on migration

--- db/migrate/20150119151029_create_project_budgets.rb
+++ db/migrate/20150119151029_create_project_budgets.rb
@@ -3,7 +3,7 @@ class CreateProjectBudgets < ActiveRecord::Migration
     create_table :project_budgets do |t|
       t.integer :project_id, null: false
       t.text :name, null: false
-      t.decimal :value, precision: 2, null: false
+      t.decimal :value, precision: 8, scale: 2, null: false
 
       t.timestamps
     end
