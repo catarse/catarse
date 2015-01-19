49799c15b802440061bc6a53a531d34ed0f9c244
Antônio Roberto <forevertonny@gmail.com>
2015-01-19 15:22:53 -0200

    remove pending

--- spec/models/project_budget_spec.rb
+++ spec/models/project_budget_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ProjectBudget, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations" do
+    it { is_expected.to belong_to(:project) }
+  end
+
+  describe "Validations" do
+    it{ is_expected.to validate_presence_of(:name) }
+    it{ is_expected.to validate_presence_of(:value) }
+  end
 end
