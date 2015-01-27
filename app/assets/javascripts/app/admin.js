feea4cb94f6774b6c86a7726f88c0ca3744d8bf6
Diogo Biazus <diogob@gmail.com>
2015-01-27 13:58:59 -0200

    fixing dropdown menus

--- app/assets/javascripts/app/admin.js
+++ app/assets/javascripts/app/admin.js
@@ -5,7 +5,7 @@ App.addChild('Admin', {
     'click .project-admin-menu' : "toggleAdminMenu",
   },
 
-  toggleAdminMenu: function(){
+  toggleAdminMenu: function(event){
     var link = $(event.target);
     this.$dropdown = link.parent().next('nav');
     $('w--open').not(this.$dropdown).removeClass('w--open');
