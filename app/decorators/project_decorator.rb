8d8788d3ef169c2f543ee0c32721abbfc0bb7eab
Antônio Roberto <forevertonny@gmail.com>
2015-01-27 23:59:38 -0200

    use can_show_preview_link to show or not icons at dashboard nav

--- app/decorators/project_decorator.rb
+++ app/decorators/project_decorator.rb
@@ -76,18 +76,20 @@ class ProjectDecorator < Draper::Decorator
   end
 
   def status_icon_for group_name
-    if source.errors.present?
-      has_error = source.errors.any? do |error|
-        source.error_included_on_group?(error, group_name)
-      end
+    if source.can_show_preview_link?
+      if source.errors.present?
+        has_error = source.errors.any? do |error|
+          source.error_included_on_group?(error, group_name)
+        end
 
-      if has_error
-        content_tag(:span, '', class: 'fa fa-exclamation-circle text-error')
+        if has_error
+          content_tag(:span, '', class: 'fa fa-exclamation-circle text-error')
+        else
+          content_tag(:span, '', class: 'fa fa-check-circle text-success')
+        end
       else
-        content_tag(:span, '', class: 'fa fa-check-circle text-success')
+        content_tag(:span, '', class: 'fa fa-check-circle fontcolor-terciary')
       end
-    else
-      content_tag(:span, '', class: 'fa fa-check-circle fontcolor-terciary')
     end
   end
 
