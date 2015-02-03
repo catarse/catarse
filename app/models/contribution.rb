62b2f04fd89b4a9b78d8cb435403b7829ea69a60
adriano benin <adriano.benin@gmail.com>
2015-02-03 17:29:59 -0200

    fix payment_id filter

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -42,7 +42,7 @@ class Contribution < ActiveRecord::Base
   scope :available_to_display, ->{ with_states(['confirmed', 'requested_refund', 'refunded', 'waiting_confirmation']) }
   scope :by_id, ->(id) { where(id: id) }
   scope :by_key, ->(key) { where(key: key) }
-  scope :by_payment_id, ->(payment_id) { where(payment_id: payment_id) }
+  scope :by_payment_id, ->(term) { where("payment_id = ? OR key = ? OR acquirer_tid = ?", term, term, term) }
   scope :by_user_id, ->(user_id) { where(user_id: user_id) }
   scope :user_name_contains, ->(term) { joins(:user).where("unaccent(upper(users.name)) LIKE ('%'||unaccent(upper(?))||'%')", term) }
   scope :user_email_contains, ->(term) { joins(:user).where("unaccent(upper(users.email)) LIKE ('%'||unaccent(upper(?))||'%')", term) }
