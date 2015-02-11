da83f4cffc6c8f4869a62eebe61a847a780c40b5
adriano benin <adriano.benin@gmail.com>
2015-02-11 12:58:27 -0200

    create migration

--- app/models/project_account.rb
+++ app/models/project_account.rb
@@ -1,3 +1,11 @@
 class ProjectAccount < ActiveRecord::Base
   belongs_to :project
+  belongs_to :bank
+  belongs_to :user
+
+  delegate   :display_bank_account, :display_bank_account_owner, :display_address, to: :decorator
+
+  def decorator
+    @decorator ||= ProjectAccountDecorator.new(self)
+  end
 end
