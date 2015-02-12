bd5ae8a62a4bf3c3f8ef1ab838c2645cec96aae8
Antônio Roberto <forevertonny@gmail.com>
2015-02-12 17:38:15 -0200

    adjust workers

--- app/workers/project_base_worker.rb
+++ app/workers/project_base_worker.rb
@@ -1,7 +1,6 @@
 class ProjectBaseWorker
-  def resource_action id, action_name
+  def resource id
     resource = Project.find id
-    Rails.logger.info "[#{action_name} on resource_id -> #{resource.id}] - #{resource.name}"
-    resource.send(action_name)
+    Rails.logger.info "[loading resource_id -> #{resource.id}] - #{resource.name}"
   end
 end
