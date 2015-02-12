e928839af7f47fc55252a3b185f50b0a8c4a17f5
Antônio Roberto <forevertonny@gmail.com>
2015-02-12 18:14:54 -0200

    fixed project posts

--- app/controllers/projects/posts_controller.rb
+++ app/controllers/projects/posts_controller.rb
@@ -14,7 +14,12 @@ class Projects::PostsController < ApplicationController
   def destroy
     authorize resource
     resource.destroy
+
     flash[:notice] = t('project.delete.posts')
     redirect_to edit_project_path(parent, anchor: 'posts')
+  end
+
+  def resource
+    @post ||= parent.posts.find params[:id]
   end
 end
