1c2ad5e76f130fbda983c2ed13173bd4b5bdd20e
adriano benin <adriano.benin@gmail.com>
2015-02-12 11:34:48 -0200

    remove delegate methods

--- app/models/project_account.rb
+++ app/models/project_account.rb
@@ -3,8 +3,6 @@ class ProjectAccount < ActiveRecord::Base
   belongs_to :bank
   belongs_to :user
 
-  delegate   :display_bank_account, :display_bank_account_owner, :display_address, to: :decorator
-
   def decorator
     @decorator ||= ProjectAccountDecorator.new(self)
   end
