fac11e5de515f90b70deee61033ed910772642c1
adriano benin <adriano.benin@gmail.com>
2015-02-13 10:09:36 -0200

    remove useless user field

--- app/models/project_account.rb
+++ app/models/project_account.rb
@@ -1,9 +1,5 @@
 class ProjectAccount < ActiveRecord::Base
   belongs_to :project
   belongs_to :bank
-  belongs_to :user
 
-  def decorator
-    @decorator ||= ProjectAccountDecorator.new(self)
-  end
 end
