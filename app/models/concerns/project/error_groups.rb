6dd2138d0312989eed2b83ca14c68638d5dd6e6d
adriano benin <adriano.benin@gmail.com>
2015-02-21 14:18:29 -0200

    add account type to project account

--- app/models/concerns/project/error_groups.rb
+++ app/models/concerns/project/error_groups.rb
@@ -10,8 +10,8 @@ module Project::ErrorGroups
       user_settings: [ :'project_account.full_name', :'project_account.email', :'project_account.cpf', :'project_account.address_street', :'project_account.address_number',
                        :'project_account.address_city', :'project_account.address_state', :'project_account.address_zip_code', :'project_account.phone_number',
                        :'project_account.bank', :'project_account.agency', :'project_account.agency_digit',
-                       :'project_account.project_account' ,:'project_account.project_account_digit', :'project_account.owner_name',
-                       :'project_account.owner_document']
+                       :'project_account.project_account' ,:'project_account.account_digit', :'project_account.owner_name',
+                       :'project_account.owner_document', :'project_account.account_type']
     }
 
     def error_included_on_group? error_attr, group_name
