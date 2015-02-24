78d2654e28ea00ddc489c456212cbf26a16caebf
Diogo Biazus <diogob@gmail.com>
2015-02-24 14:24:52 -0500

    Adds created_projects and has_online_project to user tracked properties

--- app/observers/mixpanel_observer.rb
+++ app/observers/mixpanel_observer.rb
@@ -56,6 +56,7 @@ class MixpanelObserver < ActiveRecord::Observer
 
   def track_event(user, event, properties={}, ip=nil)
     tracker.track(user.id.to_s, event, properties, user.current_sign_in_ip)
+    tracker.people.set(user.id.to_s, properties)
   end
 
   def user_properties(user)
