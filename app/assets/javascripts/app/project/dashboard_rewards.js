e4648c3254a0a6894ccd62fd0d6474790da8382a
adriano benin <adriano.benin@gmail.com>
2015-02-25 15:04:20 -0300

    fix rewards ordering

--- app/assets/javascripts/app/project/dashboard_rewards.js
+++ app/assets/javascripts/app/project/dashboard_rewards.js
@@ -26,7 +26,7 @@ App.addChild('DashboardRewards', {
       },
       update: function(e, ui) {
         var csrfToken, position;
-        position = that.$('#dashboard-rewards .reward-card').index(ui.item);
+        position = that.$('#dashboard-rewards .nested-fields').index(ui.item);
         csrfToken = $("meta[name='csrf-token']").attr("content");
         update_url = that.$(ui.item).find('.card-persisted').data('update_url');
         return $.ajax({
