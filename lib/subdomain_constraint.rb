6755dfc7a3abc686fe0b1065a9fbbc3d15a24f9b
Antônio Roberto <forevertonny@gmail.com>
2015-02-27 11:28:29 -0300

    check if subdomain is present

--- lib/subdomain_constraint.rb
+++ lib/subdomain_constraint.rb
@@ -1,5 +1,7 @@
 class SubdomainConstraint
   def self.matches?(request)
-    User.with_permalink.where("lower(permalink) = lower(?)", request.subdomain).exists?
+    if request.subdomain.present?
+      User.with_permalink.where("lower(permalink) = lower(?)", request.subdomain).exists?
+    end
   end
 end
