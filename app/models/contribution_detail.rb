b7106df42fcec5d38360fa5dc26196ba8febc9a3
Diogo Biazus <diogob@gmail.com>
2015-03-06 12:03:45 -0500

    Creates contribution details view to migrate admin views based on contribution to a model that joins contribution and payment. Using schema 1 to later migrate to Postgrest and use schema as API version number.

---
+++ app/models/contribution_detail.rb
@@ -0,0 +1,2 @@
+class ContributionDetail < ActiveRecord::Base
+end
---
+++ db/migrate/20150306163559_create_contribution_details.rb
@@ -0,0 +1,39 @@
+class CreateContributionDetails < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+    CREATE OR REPLACE VIEW "1".contribution_details AS
+    SELECT
+      pa.id,
+      c.id AS contribution_id,
+      pa.id AS payment_id,
+      c.user_id,
+      c.project_id,
+      c.reward_id,
+      p.permalink,
+      p.name AS project_name,
+      u.name AS user_name,
+      u.email,
+      u.uploaded_image,
+      pa.key,
+      pa.value,
+      c.anonymous,
+      c.payer_email,
+      pa.gateway_id,
+      c.state AS contribution_state,
+      pa.state AS payment_state,
+      EXISTS(SELECT 1 FROM rewards r WHERE r.id = c.reward_id) AS "has_rewards"
+    FROM
+      projects p
+      JOIN contributions c ON c.project_id = p.id
+      JOIN contributions_payments cp ON cp.contribution_id = c.id
+      JOIN payments pa ON cp.payment_id = pa.id
+      JOIN users u ON c.user_id = u.id;
+    SQL
+  end
+
+  def down
+    execute '
+    DROP VIEW "1".contribution_details;
+    '
+  end
+end
