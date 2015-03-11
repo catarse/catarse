7ade5b73f9e79fbb94f2f6fd4a522171f2b0947f
Vinícius Andrade <vchavesster@gmail.com>
2015-03-11 19:09:29 -0300

    Adds the load more behavior to the project news tab

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -14,7 +14,6 @@ RSpec.describe "Projects", type: :feature do
     CatarseSettings[:company_name] = 'Catarse'
   }
 
-
   describe "home" do
     before do
       create(:project, state: 'online', online_days: 30, online_date: Time.now)
@@ -35,10 +34,12 @@ RSpec.describe "Projects", type: :feature do
       visit explore_path(locale: :pt)
       sleep FeatureHelpers::TIME_TO_SLEEP
     end
+
     it "should show recommended projects" do
       recommended = all(".results .card-project")
       expect(recommended.size).to eq(6)
     end
+
     it "should load 6 more projects after clicking load more and then hide it" do
       click_on("load-more")
       sleep FeatureHelpers::TIME_TO_SLEEP
@@ -51,15 +52,18 @@ RSpec.describe "Projects", type: :feature do
   describe "view" do
     before do
       20.times{ create(:contribution, value: 10.00, credits: true, project: project, state: 'confirmed') }
+      6.times{ create(:project_post, project: project) }
       visit project_path(project)
       sleep FeatureHelpers::TIME_TO_SLEEP
     end
-    it "should show 20 contributions when clicking contributors tab" do
+
+    it "should show 20 contributions when clicking on the contributors tab" do
       click_on("contributions_link")
       sleep FeatureHelpers::TIME_TO_SLEEP
       contributors = all(".results .u-marginbottom-20")
       expect(contributors.size).to eq(20)
     end
+
     it "should load 20 more contributions after click load more and then hide it" do
       click_on("contributions_link")
       sleep FeatureHelpers::TIME_TO_SLEEP
@@ -67,6 +71,23 @@ RSpec.describe "Projects", type: :feature do
       sleep FeatureHelpers::TIME_TO_SLEEP
       contributors = all(".results .u-marginbottom-20")
       expect(contributors.size).to eq(40)
+      expect(page.evaluate_script('$("#load-more:visible").length')).to eq(0)
+    end
+
+    it "should show 3 posts when clicking on the posts tab" do
+      click_on("posts_link")
+      sleep FeatureHelpers::TIME_TO_SLEEP
+      posts = all(".posts .project-news")
+      expect(posts.size).to eq(3)
+    end
+
+    it "should load 3 more contributions after click load more and then hide it" do
+      click_on("posts_link")
+      sleep FeatureHelpers::TIME_TO_SLEEP
+      click_on("load-more")
+      sleep FeatureHelpers::TIME_TO_SLEEP
+      posts = all(".posts .project-news")
+      expect(posts.size).to eq(6) 
       expect(page.evaluate_script('$("#load-more:visible").length')).to eq(0)
     end
   end
@@ -78,20 +99,19 @@ RSpec.describe "Projects", type: :feature do
       visit explore_path(pg_search: 'Lorem')
       sleep FeatureHelpers::TIME_TO_SLEEP
     end
+
     it "should show matching projects" do
       recommended = all(".results .card-project")
       expect(recommended.size).to eq(1)
     end
   end
 
-
   describe "new and create" do
     pending "NEED TEST ON CREATING PROJECT"
   end
 
-
   describe "edit" do
     pending 'NEED TEST ON EDIT PROJECT'
   end
- 
+
 end
