6994d7192c07193e858b8150d1b1e94951d8c33e
Antônio Roberto <forevertonny@gmail.com>
2015-03-19 18:55:07 -0300

    add card_key inside credit_cards and set encryption key of pagarme

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,7 +1,7 @@
 class CreditCard < ActiveRecord::Base
   belongs_to :user
 
-  validates :user, :last_digits, :card_brand, :subscription_id, presence: true
+  validates :user, :last_digits, :card_brand, presence: true
 
   def decorator
     CreditCardDecorator.new(self)
--- config/initializers/pagarme.rb
+++ config/initializers/pagarme.rb
@@ -1,5 +1,6 @@
 CatarsePagarme.configure do |config|
   config.api_key = CatarseSettings.get_without_cache(:pagarme_api_key)
+  config.ecr_key = CatarseSettings.get_without_cache(:pagarme_encryption_key)
   config.slip_tax = CatarseSettings.get_without_cache(:pagarme_slip_tax)
   config.credit_card_tax = CatarseSettings.get_without_cache(:pagarme_credit_card_tax)
   config.interest_rate = CatarseSettings.get_without_cache(:pagarme_interest_rate)
