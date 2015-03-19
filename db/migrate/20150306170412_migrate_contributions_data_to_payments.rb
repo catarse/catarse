0471398f07b0f70b870ce099971707ed6d9bb57a
Diogo Biazus <diogob@gmail.com>
2015-03-19 18:18:33 -0400

    Migrate data to payments table

--- db/migrate/20150306170412_migrate_contributions_data_to_payments.rb
+++ db/migrate/20150306170412_migrate_contributions_data_to_payments.rb
@@ -1,13 +1,14 @@
 class MigrateContributionsDataToPayments < ActiveRecord::Migration
   def up
-    execute "
+    execute <<-SQL
     INSERT INTO payments (
+      id,
       state,
       key,
       gateway,
       gateway_id,
       gateway_fee,
-      geteway_data,
+      gateway_data,
       method,
       value,
       installments,
@@ -15,7 +16,48 @@ class MigrateContributionsDataToPayments < ActiveRecord::Migration
       created_at,
       updated_at
     )
-    "
+    SELECT
+      c.id,
+      CASE c.state
+        WHEN 'pending' THEN 'pending'
+        WHEN 'waiting_confirmation' THEN 'pending'
+        WHEN 'refunded' THEN 'refunded'
+        WHEN 'refunded_and_canceled' THEN 'refunded'
+        WHEN 'requested_refund' THEN 'pending_refund'
+        WHEN 'requested_refund' THEN 'pending_refund'
+        WHEN 'canceled' THEN 'refused'
+        WHEN 'invalid_payment' THEN 'refused'
+        WHEN 'confirmed' THEN 'paid'
+        ELSE c.state
+      END,
+      coalesce(c.key, md5(id::text || current_timestamp::text)),
+      CASE 
+        WHEN c.credits THEN 'Credits'
+        WHEN c.payment_method = 'Credits' THEN 'Credits'
+        WHEN c.payment_method = 'PayPal' THEN 'PayPal'
+        WHEN c.payment_method = 'Pagarme' THEN 'Pagarme'
+        ELSE 'MoIP'
+      END,
+      c.payment_id,
+      c.payment_service_fee,
+      '{}',
+      CASE
+        WHEN c.credits OR c.payment_method = 'Credits' THEN 'Creditos'
+        WHEN c.payment_choice IS NULL THEN 'Desconhecido'
+        ELSE c.payment_choice
+      END,
+      c.value,
+      c.installments,
+      COALESCE(c.installment_value, c.value),
+      c.created_at,
+      c.updated_at
+    FROM
+      contributions c
+    WHERE
+      c.state <> 'deleted';
+    UPDATE payment_notifications SET payment_id = contribution_id WHERE EXISTS (SELECT true FROM payments p WHERE p.id = contribution_id);
+    SELECT setval('payments_id_seq', (SELECT max(id) FROM payments));
+    SQL
   end
 
   def down
