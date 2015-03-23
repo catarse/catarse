8e4915d293205f996638fc8f6953398942bafc76
Antônio Roberto <forevertonny@gmail.com>
2015-03-23 13:49:07 -0300

    local dbhero

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,8 @@ Catarse::Application.routes.draw do
   mount CatarseMoip::Engine => "/", as: :catarse_moip
   mount CatarsePagarme::Engine => "/", as: :catarse_pagarme
   mount CatarseApi::Engine => "/api", as: :catarse_api
-#  mount CatarseWepay::Engine => "/", as: :catarse_wepay
+ #mount CatarseWepay::Engine => "/", as: :catarse_wepay
+  mount Dbhero::Engine => "/dbhero", as: :dbhero
 
   get '/post_preview' => 'post_preview#show', as: :post_preview
   resources :categories, only: [] do
