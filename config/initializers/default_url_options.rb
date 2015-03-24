636c6259de28b62696e0fe181b0f578551575ecb
Diogo Biazus <diogob@gmail.com>
2015-03-24 11:39:00 -0400

    Base.asset_host should be base_url and not host setting

--- config/initializers/default_url_options.rb
+++ config/initializers/default_url_options.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  ActionMailer::Base.asset_host = ::CatarseSettings.get_without_cache(:host)
+  ActionMailer::Base.asset_host = ::CatarseSettings.get_without_cache(:base_url)
   Rails.application.routes.default_url_options = {host: ::CatarseSettings.get_without_cache(:host), protocol: 'https'} 
 else
   Rails.application.routes.default_url_options = {host: 'localhost:3000'} 
