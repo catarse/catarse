d9b7233b78f2b95531661ce127b603b500d20129
Diogo Biazus <diogob@gmail.com>
2015-03-26 15:48:29 -0400

    Validates that payment minimum value is value pledged in contribution

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,9 +4,14 @@ class Payment < ActiveRecord::Base
   has_and_belongs_to_many :contributions
 
   validates_presence_of :state, :key, :gateway, :method, :value, :installments, :installment_value
+  validate :value_should_be_equal_or_greater_than_pledge
 
   before_validation do
     self.key ||= SecureRandom.uuid
+  end
+
+  def value_should_be_equal_or_greater_than_pledge
+    errors.add(:value, I18n.t("activerecord.errors.models.payment.attributes.value.invalid")) if self.contribution && self.value < self.contribution.value
   end
 
   def contribution
