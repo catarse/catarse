c8ef076eac82e6729aec28473930edaca033a731
Diogo Biazus <diogob@gmail.com>
2015-03-26 15:48:29 -0400

    Validates that payment minimum value is value pledged in contribution

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -22,8 +22,19 @@ RSpec.describe Payment, type: :model do
   end
 
   describe "#valid?" do
+
     subject{ payment.valid? }
-    
+
+    context "when payment value is equal than what was pledged" do
+      let(:payment){ build(:payment, value: 10, contributions: [create(:contribution, value: 10)]) }
+      it{ is_expected.to eq true }
+    end
+
+    context "when payment value is lower than what was pledged" do
+      let(:payment){ build(:payment, value: 9, contributions: [create(:contribution, value: 10)]) }
+      it{ is_expected.to eq false }
+    end
+
     it "should set key" do
       expect(payment.key).to_not be_nil
     end
