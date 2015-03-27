e8379cb97b01d9e294fda3268f40f312bd20635f
Vinícius Andrade <vchavesster@gmail.com>
2015-03-27 00:02:33 -0300

    Updates card reward hover element to link and removes unecessary javascript

--- app/assets/javascripts/app/project/project_sidebar.js
+++ app/assets/javascripts/app/project/project_sidebar.js
@@ -1,26 +1,19 @@
 App.addChild('ProjectSidebar', {
+  
   el: '#project-sidebar',
 
-  events:{
-    "click .card-reward" : "selectReward"
-  },
-
-  selectReward: function(event){
-    var url = this.$(event.currentTarget).data('new_contribution_url');
-    this.navigate(url);
-    return false;
-  },
-
   activate: function() {
-    this.$rewards = this.$('#rewards');
-    this.reloadRewards();
-  },
 
-  reloadRewards: function() {
     var that = this;
+    
+    this.$rewards = this.$('#rewards');
+    
     $.get(that.$rewards.data('index_path')).success(function(data){
+    
       that.$rewards.html(data);
+    
     });
+  
   }
 
 });
