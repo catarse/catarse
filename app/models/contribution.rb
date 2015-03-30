e933b852ed3308a08ea3776fc58e510c3a6a8621
Diogo Biazus <diogob@gmail.com>
2015-03-30 15:42:41 -0400

    Fixes project dashboard to use new Contribution.pending scope to look for pending contributions

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -33,6 +33,9 @@ class Contribution < ActiveRecord::Base
   scope :not_anonymous, -> { where(anonymous: false) }
   scope :confirmed_today, -> { where("EXISTS(SELECT true FROM payments p WHERE p.contribution_id = contributions.id AND p.state = 'paid' AND p.paid_at::date = to_date(?, 'yyyy-mm-dd'))", Time.now.strftime('%Y-%m-%d')) }
   scope :confirmed_last_day, -> { where("EXISTS(SELECT true FROM payments p WHERE p.contribution_id = contributions.id AND p.state = 'paid' AND (current_timestamp - p.paid_at) < '1 day'::interval)") }
+  scope :pending, ->{
+    where("EXISTS (SELECT true FROM payments p WHERE p.contribution_id = contributions.id AND p.state = 'pending') AND NOT contributions.was_confirmed")
+  }
   scope :avaiable_to_automatic_refund, -> {
     where('contributions.is_confirmed')
   }
