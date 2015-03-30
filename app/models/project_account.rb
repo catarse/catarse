06715331597f812139f0fb9c91bf47ea5ac78212
adriano benin <adriano.benin@gmail.com>
2015-03-30 20:28:57 -0300

    remove useless accessor

--- app/models/project_account.rb
+++ app/models/project_account.rb
@@ -2,8 +2,6 @@ class ProjectAccount < ActiveRecord::Base
   belongs_to :project
   belongs_to :bank
 
-  attr_accessor :person_type
-
   def entity_type
     cpf.length > 14 ? 'Pessoa Jurídica' : 'Pessoa Física'
   end
