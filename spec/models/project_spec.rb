74cc2e2d9777f9ab87e830a79dc3c668d9558f3f
Diogo Biazus <diogob@gmail.com>
2015-03-31 08:53:06 -0400

    Fixes by_expires_at spec

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -226,8 +226,8 @@ RSpec.describe Project, type: :model do
     subject { Project.by_expires_at('10/10/2013') }
 
     before do
-      @project_01 = create(:project, online_date: '10/10/2013', online_days: 1)
-      @project_02 = create(:project, online_date: '09/10/2013', online_days: 1)
+      @project_01 = create(:project, online_date: '2013-10-10', online_days: 1)
+      @project_02 = create(:project, online_date: '2013-10-09', online_days: 1)
     end
 
     it { is_expected.to eq [@project_02] }
