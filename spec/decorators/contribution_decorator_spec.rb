5befd1a2a35f50868c418e9c37e0dca21cce599f
Diogo Biazus <diogob@gmail.com>
2015-04-01 11:45:04 -0400

    Fixes contribution decorator to work with payments

--- spec/decorators/contribution_decorator_spec.rb
+++ spec/decorators/contribution_decorator_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe ContributionDecorator do
   include Draper::LazyHelpers
+
+  let(:contribution){ create(:confirmed_contribution) }
+  let(:payment){ contribution.payments.last }
+
   before do
     I18n.locale = :pt
   end
@@ -9,51 +13,48 @@ RSpec.describe ContributionDecorator do
   describe "#display_installments_details" do
     subject { contribution.decorate.display_installment_details }
     context "when I have 1 installment" do
-      let(:contribution){ create(:contribution, installments: 1) }
+      before do
+        payment.update_attributes installments: 1
+      end
       it{ is_expected.to eq "" }
     end
 
     context "when I have >1 installment" do
-      let(:contribution){ create(:contribution, installments: 2, installment_value: '10') }
-      it{ is_expected.to eq "#{contribution.installments} x #{number_to_currency contribution.installment_value}" }
-    end
-
-    context "when I have >1 installment and nil in installment value" do
-      let(:contribution){ create(:contribution, installments: 2, installment_value: nil) }
-      it{ is_expected.to eq "#{contribution.installments} x #{number_to_currency contribution.installment_value}" }
+      before do
+        payment.update_attributes installments: 2, installment_value: 10
+      end
+      it{ is_expected.to eq "#{payment.installments} x #{number_to_currency payment.installment_value}" }
     end
   end
 
   describe "#display_payment_details" do
     subject { contribution.decorate.display_payment_details }
     context "when contribution is made with credits" do
-      let(:contribution){ create(:contribution, credits: true) }
+      before do
+        payment.update_attributes gateway: 'Credits'
+      end
       it{ is_expected.to eq I18n.t("contribution.payment_details.creditos") }
     end
 
-    context "when contribution is not made with credits and choice is null" do
-      let(:contribution){ create(:contribution, credits: false, payment_choice: nil) }
-      it{ is_expected.to eq "" }
-    end
-
     context "when contribution is not made with credits" do
-      let(:contribution){ create(:contribution, credits: false, payment_choice: 'CartaoDeCredito') }
+      before do
+        payment.update_attributes gateway: 'Pagarme', payment_method: 'CartaoDeCredito'
+      end
       it{ is_expected.to eq I18n.t("contribution.payment_details.cartao_de_credito") }
     end
   end
 
   describe "#display_date" do
-    [:confirmed_at, :refunded_at, :requested_refund_at].each do |field|
+    [:paid_at, :refunded_at, :pending_refund_at].each do |field|
       context "displaying #{field.to_s}" do
         subject { contribution.decorate.display_date(field)}
-
-        let(:contribution) do
-          c = build(:contribution)
-          c[field] = Time.now
-          c
+        before do
+          attributes = {}
+          attributes[field] = Time.now
+          payment.update_attributes attributes
         end
 
-        it{ is_expected.to eq(I18n.l(contribution.send(field).to_date)) }
+        it{ is_expected.to eq(I18n.l(payment.send(field).to_date)) }
       end
     end
   end
