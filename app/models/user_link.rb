3c5da5187f6e523bb39195b6a7d728e71cd6a050
adriano benin <adriano.benin@gmail.com>
2015-04-02 11:15:20 -0300

    show user links on user card

--- app/models/user_link.rb
+++ app/models/user_link.rb
@@ -1,3 +1,13 @@
 class UserLink < ActiveRecord::Base
+  before_save :fix_link
   belongs_to :user
+
+  def link_without_protocol
+    self.link.sub(/^https?\:\/\//, '').sub(/^www./,'')
+  end
+
+  def fix_link
+    self.link = ('http://' + self.link) unless self.link[/^https?:\/\//]
+  end
+
 end
