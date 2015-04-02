3c0c9e0907d7a40bed38d5385e688b9de4e4e877
adriano benin <adriano.benin@gmail.com>
2015-04-02 11:24:38 -0300

    add specs

--- spec/models/user_link_spec.rb
+++ spec/models/user_link_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe UserLink, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user){ create(:user) }
+
+  describe ".link_without_protocol" do
+    subject {UserLink.last.link_without_protocol}
+    context "when link has no protocol" do
+      before do
+        create(:user_link, link: 'foo.com')
+      end
+      it{ is_expected.to eq('foo.com') }
+    end
+    context "when link has protocol" do
+      before do
+        create(:user_link, link: 'http://bar.com')
+      end
+      it{ is_expected.to eq('bar.com') }
+    end
+  end
+
 end
