bbdca9530ec5924ad2ad1cc5303eb7ef6ff2b169
Vinícius Andrade <vchavesster@gmail.com>
2015-04-03 00:48:08 -0300

    Improves Iframe responsive JS solution

--- app/assets/javascripts/app/guides.js
+++ app/assets/javascripts/app/guides.js
@@ -2,12 +2,16 @@ App.addChild('Guides', _.extend({
   el: '#page-guides',
 
   activate: function(){
+    this.$container = this.$('.w-iframe');
+
     this.route('starting');
     this.route('you_history');
     this.route('goals');
     this.route('rewards');
     this.route('social');
     this.route('after_project');
+
+    this.setupResponsiveIframes(this.$container);
 
     if(this.$('.dashboard-nav-link.selected').length < 1 &&  window.location.hash == '') {
       window.location.hash = 'starting'
@@ -30,6 +34,6 @@ App.addChild('Guides', _.extend({
       });
     }
   }
-}, Skull.Tabs));
+}, Skull.Tabs, Skull.UI_helper));
 
 
