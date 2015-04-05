7677553f63268c57d9bb4a4be4844fa753b532b7
Vinícius Chaves de Andrade <vchavesster@gmail.com>
2015-04-04 22:47:01 -0300

    Updates code style

--- app/assets/javascripts/lib/ui_helper.js
+++ app/assets/javascripts/lib/ui_helper.js
@@ -1,69 +1,42 @@
 Skull.UI_helper = {
+  setupResponsiveIframes: function($container) {
+    this.$container = $container;
+    this.$iframes = $("iframe[src^='//player.vimeo.com'], iframe[src^='//www.youtube.com']");
+    this.$iframes.each(function() {
+      if (!$(this).data('aspectRatio')) {
+        $(this)
+          .data('aspectRatio', this.height / this.width)
+          .removeAttr('height')
+          .removeAttr('width');
+      }
+    });
+    this.updateIframeSize();
+    this.windowResize();
+  },
 
-	setupResponsiveIframes: function($container){
-	    
-		this.$container = $container;
+  windowResize: function() {
+    var that = this;        
+    $(window).resize(function() {
+      that.updateIframeSize();
+    });
+  },
 
-		this.$iframes = $("iframe[src^='//player.vimeo.com'], iframe[src^='//www.youtube.com']");
-
-	    this.$iframes.each(function() {
-
-			if(!$(this).data('aspectRatio')){
-			
-				$(this)
-					.data('aspectRatio', this.height / this.width)
-					.removeAttr('height')
-					.removeAttr('width');
-			
-			}
-	    
-	    });
-
-	    this.updateIframeSize();
-	    
-	    this.windowResize();
-  	
-  	},
-
-  	windowResize: function(){
-  		
-  		var that = this;
-  		
-		$(window).resize(function() {
-
-		  that.updateIframeSize();
-
-		});
-
-  	},
-
-	updateIframeSize: function(){
-
-		var newWidth = this.$container.width();
-
-		if(newWidth){
-		  
-			this.$iframes.each(function() {
-			
-				var $el = $(this);
-
-		  		$el
-					.width(newWidth)
-					.height(newWidth * $el.data('aspectRatio'));
-			
-			});
-
-		}else{
-
-			this.$iframes.each(function() {
-				
-				$(this)
-					.removeAttr('height')
-					.removeAttr('width');
-
-			});
-		}
-
-	}
-	
+  updateIframeSize: function() {
+    var newWidth = this.$container.width();
+    if (newWidth) {
+      this.$iframes.each(function() {
+        var $el = $(this);
+        $el
+          .width(newWidth)
+          .height(newWidth * $el.data('aspectRatio'));
+      });
+    } else {
+      this.$iframes.each(function() {
+        $(this)
+          .removeAttr('height')
+          .removeAttr('width');
+      });
+    }
+  }
+  
 }
