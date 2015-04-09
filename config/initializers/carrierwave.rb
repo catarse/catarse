eca52b3e7609e89f5207e76d6ef9991c3f90d90e
Diogo Biazus <diogob@gmail.com>
2015-04-09 17:37:04 -0400

    Uncomments process quality of image in carrierwave uploader and changes the carrierwave initializer to use MiniMagick

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,15 +16,13 @@ CarrierWave.configure do |config|
 end
 
 module CarrierWave
-  module RMagick
-
+  module MiniMagick
     def quality(percentage)
       manipulate! do |img|
-        img.write(current_path){ self.quality = percentage } unless img.quality == percentage
+        img.quality(percentage.to_s)
         img = yield(img) if block_given?
         img
       end
     end
-
   end
 end
