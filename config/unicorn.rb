b37b1d60e3c5cddafdd1e29e5c4aa11e15cfe4b2
Antônio Roberto <forevertonny@gmail.com>
2015-04-16 00:14:40 -0300

    manual start newrelic

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -14,6 +14,13 @@ timeout 30
 # Preload entire app for fast forking.
 preload_app true
 
+# Ensure the agent is started using Unicorn.
+# This is needed when using Unicorn and preload_app is not set to true.
+# See https://docs.newrelic.com/docs/ruby/no-data-with-unicorn
+if defined?(Unicorn) && File.basename($0).start_with?('unicorn')
+  ::NewRelic::Agent.manual_start()
+  ::NewRelic::Agent.after_fork(:force_reconnect => true)
+end
 
 before_fork do |server, worker|
 
