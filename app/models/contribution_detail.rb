6c8cea7d72cddb16eaa4769094f39fd4caafaf68
Diogo Biazus <diogob@gmail.com>
2015-04-16 10:53:32 -0400

    Fixes search by payment id in admin

--- app/models/contribution_detail.rb
+++ app/models/contribution_detail.rb
@@ -18,7 +18,7 @@ class ContributionDetail < ActiveRecord::Base
   scope :project_name_contains, ->(term) {
     joins(:project).merge(Project.pg_search(term))
   }
-  scope :by_payment_id, ->(term) { where("? IN (payment_id, key, acquirer_tid)", term) }
+  scope :by_payment_id, ->(term) { where("? IN (payment_id, key, (gateway_data->'acquirer_tid')::text)", term) }
   scope :by_user_id, ->(user_id) { where(user_id: user_id) }
   scope :by_gateway, ->(gateway) { where(gateway: gateway) }
   scope :by_payment_method, ->(payment_method) { where(payment_method: payment_method ) }
