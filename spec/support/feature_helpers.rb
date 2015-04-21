3d6c6e0259cbffeef5b583b6f35ad9abdf58593f
Vinícius Andrade <vchavesster@gmail.com>
2015-04-20 23:44:50 -0300

    Create country before user to avoid foreign key active record error

--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -12,6 +12,7 @@ module FeatureHelpers
   end
 
   def current_user
+    FactoryGirl.create(:country)
     @user ||= FactoryGirl.create(:user, password: 'test123', password_confirmation: 'test123')
   end
 end
