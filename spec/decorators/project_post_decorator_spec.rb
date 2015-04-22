7dc89e2771cb9bfdd10ce0abd840bdb69c33a901
adriano benin <adriano.benin@gmail.com>
2015-04-22 09:17:22 -0300

    add spec

---
+++ spec/decorators/project_post_decorator_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe ProjectPostDecorator do
+
+  describe "#email_comment_html" do
+    subject{ project_post.email_comment_html }
+    context "when there is a video iframe" do
+      let(:project_post){ create(:project_post, comment_html: '<iframe src="//www.foo.com"></iframe> <p>Bar</p>') }
+      it{ is_expected.to eq('<a href="http://www.foo.com">www.foo.com</a> <p>Bar</p>') }
+    end
+
+    context "when project there is no video iframe" do
+      let(:project_post){ create(:project_post, comment_html: '<p>Bar</p>') }
+      it{ is_expected.to eq('<p>Bar</p>') }
+    end
+  end
+
+end
+
