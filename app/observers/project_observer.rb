71df2569ea522cf6412d9d7222057943fe67fd5a
Diogo Biazus <diogob@gmail.com>
2015-04-23 22:11:32 -0400

    Add notification to redbooth in case of project turning to waiting funds having reached the goal. Small refactor in redbooth email creation

--- app/observers/project_observer.rb
+++ app/observers/project_observer.rb
@@ -30,6 +30,7 @@ class ProjectObserver < ActiveRecord::Observer
 
   def from_online_to_waiting_funds(project)
     project.notify_owner(:project_in_waiting_funds, { from_email: CatarseSettings[:email_projects] })
+    notify_admin_project_will_succeed(project) if project.reached_goal?
   end
 
   def from_waiting_funds_to_successful(project)
@@ -73,9 +74,20 @@ class ProjectObserver < ActiveRecord::Observer
   end
 
   private
+  def redbooth_user
+    User.find_by(email: CatarseSettings[:email_redbooth])
+  end
+
+  def notify_redbooth(project, template)
+    project.notify_once(template, redbooth_user, project, { from_email: CatarseSettings[:redbooth_from_email] }) if redbooth_user
+  end
+
   def notify_admin_that_project_is_successful(project)
-    redbooth_user = User.find_by(email: CatarseSettings[:email_redbooth])
-    project.notify_once(:redbooth_task, redbooth_user) if redbooth_user
+    notify_redbooth project, :redbooth_task
+  end
+
+  def notify_admin_project_will_succeed(project)
+    notify_redbooth project, :redbooth_task_project_will_succeed
   end
 
   def notify_admin_that_project_reached_deadline(project)
