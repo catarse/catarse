0bb802c0ae726731e0b83d2609db7ae93edfa558
Diogo Biazus <diogob@gmail.com>
2015-04-27 13:10:49 -0400

    Tidy up the code

--- app/assets/javascripts/app/project/project_contributions.js
+++ app/assets/javascripts/app/project/project_contributions.js
@@ -2,7 +2,6 @@ App.views.Project.addChild('ProjectContributions', _.extend({
   el: '#project_contributions',
 
   activate: function(){
-    
     this.filter = {available_to_count: true};
     this.setupPagination(
       this.$("#contributions-loading img"),
@@ -19,26 +18,18 @@ App.views.Project.addChild('ProjectContributions', _.extend({
   },
 
   showContributions: function(){
-    
     var state = $('input:radio[name=contribution_state]:checked').val();
-
     this.filter = { page: 1 };
 
     if(state == 'waiting_confirmation'){
-    
       this.filter.pending = true;
-    
-    }else{
-
+    }
+    else{
       this.filter.available_to_count = true;
-    
     }
 
     this.$('.results').empty();
-    
     this.fetchPage();
-
   }
-
 }, Skull.Pagination));
 
