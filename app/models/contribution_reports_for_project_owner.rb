a9acc77caf58070c1333f95b3715b24b63811102
Antônio Roberto <forevertonny@gmail.com>
2015-04-30 13:30:47 -0300

    small refactor to use has_scope

--- app/models/contribution_reports_for_project_owner.rb
+++ app/models/contribution_reports_for_project_owner.rb
@@ -1,3 +1,22 @@
 class ContributionReportsForProjectOwner < ActiveRecord::Base
   acts_as_copy_target
+
+  scope :project_id, -> (project_id) { where(project_id: project_id) }
+  scope :reward_id, -> (reward_id) { where(reward_id: reward_id) }
+  scope :state, -> (state = 'paid') { where(state: state) }
+  scope :project_owner_id, -> (project_owner_id) { where(project_owner_id: project_owner_id) }
+
+  def self.report
+    report_sql = ""
+    I18n.t('contribution_report_to_project_owner').keys[0..-2].each{
+      |column| report_sql << "#{column} AS \"#{I18n.t("contribution_report_to_project_owner.#{column}")}\","
+    }
+
+    self.select(%Q{
+        #{report_sql}
+        CASE WHEN anonymous='t' THEN '#{I18n.t('yes')}'
+            WHEN anonymous='f' THEN '#{I18n.t('no')}'
+        END as "#{I18n.t('contribution_report_to_project_owner.anonymous')}"
+      })
+  end
 end
