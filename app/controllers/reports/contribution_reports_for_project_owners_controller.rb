f7ca4bccdcf90f06e32946e7de4d98779a7dc7f6
Antônio Roberto <forevertonny@gmail.com>
2015-04-30 13:30:47 -0300

    small refactor to use has_scope

--- app/controllers/reports/contribution_reports_for_project_owners_controller.rb
+++ app/controllers/reports/contribution_reports_for_project_owners_controller.rb
@@ -1,42 +1,33 @@
 class Reports::ContributionReportsForProjectOwnersController < ApplicationController
-  inherit_resources
+  #before_filter :verify_authorized
+
   respond_to :csv, :xls
-  actions :index
 
-  #TODO Refact to use pudint
+  has_scope :project_id, :reward_id, :state
+
   def index
-    @report = end_of_association_chain.to_xls( columns: I18n.t('contribution_report_to_project_owner').values )
-    @project = Project.find params[:project_id]
+    respond_to do |format|
+      format.csv do
+        send_data collection.copy_to_string, filename: "#{project.name}.csv"
+      end
 
-    super do |format|
-      format.csv { send_data end_of_association_chain.copy_to_string, filename: "#{@project.name}.csv" }
-      format.xls { send_data @report, filename: "#{@project.name}.xls" }
+      format.xls do
+        send_data collection.to_xls(
+          columns: I18n.t('contribution_report_to_project_owner').values
+        ), filename: "#{project.name}.xls"
+      end
     end
   end
 
-  def end_of_association_chain
-    report_sql = ""
-    I18n.t('contribution_report_to_project_owner').keys[0..-2].each{
-      |column| report_sql << "#{column} AS \"#{I18n.t("contribution_report_to_project_owner.#{column}")}\","
-    }
+  protected
 
-    super.
-      select(%Q{
-        #{report_sql}
-        CASE WHEN anonymous='t' THEN '#{I18n.t('yes')}'
-            WHEN anonymous='f' THEN '#{I18n.t('no')}'
-        END as "#{I18n.t('contribution_report_to_project_owner.anonymous')}"
-      }).
-      where(get_conditions)
+  def collection
+    @collection ||= apply_scopes(ContributionReportsForProjectOwner.report)
+    @collection.project_owner_id(current_user.id) unless current_user.admin?
+    @collection
   end
 
-  def get_conditions
-    conditions = { project_id: params[:project_id] }
-
-    conditions.merge!(reward_id: params[:reward_id]) if params[:reward_id].present?
-    conditions.merge!(state: (params[:state].present? ? params[:state] : 'paid'))
-    conditions.merge!(project_owner_id: current_user.id) unless current_user.admin
-
-    conditions
+  def project
+    @project ||= Project.find params[:project_id]
   end
 end
