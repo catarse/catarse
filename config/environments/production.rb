a2ebfa26d2b179c6aa956147ddc4393545cc6bb4
Diogo Biazus <diogob@gmail.com>
2015-05-18 16:16:57 -0400

    Sets default_locale only in config/initializers/locale, not in production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,6 @@ Catarse::Application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
-  I18n.default_locale = :pt
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
