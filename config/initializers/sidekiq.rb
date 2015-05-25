fe881a5c53f4a1c93c2724674f9fb96050a419df
Diogo Biazus <diogob@gmail.com>
2015-05-25 12:03:38 -0400

    Fixes redis_url config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 require 'sidekiq/api'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV["REDISTOGO_URL"] || "redis://localhost:6379/" }
+  config.redis = { url: ENV["REDIS_URL"] || "redis://localhost:6379/" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV["REDISTOGO_URL"] || "redis://localhost:6379/" }
+  config.redis = { url: ENV["REDIS_URL"] || "redis://localhost:6379/" }
 end
