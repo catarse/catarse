709fdff8de7bc6d32eb76096d0672790f97c4f19
Diogo Biazus <diogob@gmail.com>
2015-06-02 16:47:08 -0400

    Stores just the first referral link in session

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,32 @@
+#encoding:utf-8
+require 'rails_helper'
+
+RSpec.describe ApplicationController, type: :controller do
+  let(:params){ {ref: 'foo'} }
+
+  before do
+    allow(controller).to receive(:params).and_return(params)
+  end
+
+  describe "#referral_it!" do
+    before do
+      session[:referral_link] = initial_session_value
+      controller.referral_it!
+    end
+
+    context "when we already have a referral link in session" do
+      let(:initial_session_value){ 'test' }
+      it "should store ref in session" do
+        expect(session[:referral_link]).to eq initial_session_value
+      end
+    end
+
+    context "when we still do not have a referral link in session" do
+      let(:initial_session_value){ nil }
+      it "should store ref in session" do
+        expect(session[:referral_link]).to eq params[:ref]
+      end
+    end
+  end
+end
+
