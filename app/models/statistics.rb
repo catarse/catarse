e6a1c64a9c8c656077f6da4581829613e1ef469d
Antônio Roberto <forevertonny@gmail.com>
2015-06-04 02:35:52 -0300

    need to use default order because statistics does not have id column

--- app/models/statistics.rb
+++ app/models/statistics.rb
@@ -1,2 +1,6 @@
 class Statistics < ActiveRecord::Base
+  default_scope { order('total_users DESC') }
+  def self.refresh_view
+    connection.execute('REFRESH MATERIALIZED VIEW statistics')
+  end
 end
