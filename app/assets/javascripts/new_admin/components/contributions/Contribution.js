ea8d0f8ca06062c53f3a14b39545803f4cd79621
Vinícius Andrade <vchavesster@gmail.com>
2015-06-10 18:14:35 -0300

    Improves factory to help testing the component model

--- app/assets/javascripts/new_admin/components/contributions/Contribution.js
+++ app/assets/javascripts/new_admin/components/contributions/Contribution.js
@@ -1,39 +1,25 @@
 //Contributions.model.js
-var Contribution = function(data){
+var Contributions = function(data){
   data = data || {}
-    this.id = m.prop(data.id)
-    this.name = m.prop(data.name)
-    this.email = m.prop(data.email)
+  this.id = m.prop(data.id)
+  this.name = m.prop(data.name)
+  this.email = m.prop(data.email)
 }
-Contribution.list = function(data) {
-  return [
-    {
-      id: '1',
-      name: 'Contribuidor 1',
-      email: 'teste@teste.com'
-    },
-    {
-      id: '2',
-      name: 'Contribuidor 2',
-      email: 'teste@teste.com'
-    },
-    {
-      id: '3',
-      name: 'Contribuidor 3',
-      email: 'teste@teste.com'
-    },
-    {
-      id: '4',
-      name: 'Contribuidor 4',
-      email: 'teste@teste.com'
-    },
-    {
-      id: '5',
-      name: 'Contribuidor 5',
-      email: 'teste@teste.com'
-    }
-  ]
+Contributions.list = function(data) {
+
+  var data = [];
+  
+  //quick manual factory
+  for(var i = 0; i < 5; i++){
+    data[i] = {
+      id: i,
+      name: 'Contribuidor_'+i,
+      email: 'teste'+i+'@teste.com'
+    };
+  }
+
+  return data;
   //return m.request({method: "GET", url: "http://api.catarse.me/contributions", data: data});
 }
 
-app.Contribution = Contribution;
+app.Contributions = Contributions;
