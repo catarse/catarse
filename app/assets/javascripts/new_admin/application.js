1e2ac71718e589125754040364f449035487f22b
Vinícius Andrade <vchavesster@gmail.com>
2015-06-11 18:45:15 -0300

    Requires mithril dev to help debugging and adds global app helper

--- app/assets/javascripts/new_admin/application.js
+++ app/assets/javascripts/new_admin/application.js
@@ -1,7 +1,11 @@
-//= require mithril
+//= require mithril/mithril
 //= require_self
 //= require_tree ./components
 //= require ./router
 //= require ./init
 
 var app = window.app = {};
+
+app.submodule = function(module, args) {
+    return module.view.bind(this, new module.controller(args))
+}
