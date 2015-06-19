e2b4c8a6930afd82c6c24192ba9331eaa631433a
Antônio Roberto <forevertonny@gmail.com>
2015-06-19 01:36:29 -0300

    added specs for bank_accounts/new controller action

--- spec/controllers/bank_accounts_controller_spec.rb
+++ spec/controllers/bank_accounts_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe BankAccountsController, type: :controller do
   subject{ response }
+  let(:project) { create(:project, state: 'online', goal: 1000 )}
   let(:user){ create(:user) }
 
   before do
@@ -9,12 +10,45 @@ RSpec.describe BankAccountsController, type: :controller do
   end
 
   describe "GET new" do
-    before do
-      get :new
+    context "when user does not have pending refund payments" do
+      before do
+        get :new, locale: :pt
+      end
+      it{ is_expected.to redirect_to root_path }
     end
 
-    context "when user does not have pending refunds" do
-      it{ is_expected.to redirect_to root_path }
+    context "when user have pending refund payments" do
+      let(:contribution) do
+        create(:confirmed_contribution, {
+          project: project,
+          value: 10,
+          user: user
+        })
+      end
+
+      before do
+        payment = contribution.payments.first
+        payment.update_column(:gateway, 'Pagarme')
+        project.update_column(:state, 'failed')
+      end
+
+      context "when user have a bank_account" do
+        before { get :new, locale: :pt }
+        it "should redirect to bank_account edit" do
+          is_expected.to redirect_to edit_bank_account_path(user.bank_account)
+        end
+      end
+
+      context "when user not have a bank_account" do
+        let(:user) { create(:user, bank_account: nil) }
+
+        before do
+          get :new, locale: :pt
+        end
+        it "should render new form" do
+          is_expected.to have_http_status(200)
+        end
+      end
     end
   end
 end
