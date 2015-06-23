528db1576bda4e606ae0723f066eaaeca6b9fb30
Vinícius Andrade <vchavesster@gmail.com>
2015-06-23 19:03:58 -0300

    Fixes new_admin dependency problems

--- app/assets/javascripts/new_admin/application.js
+++ app/assets/javascripts/new_admin/application.js
@@ -1,9 +1,15 @@
 //= require mithril
 //= require underscore
 //= require mithril.postgrest
+//= require new_admin/init
 //= require catarse_admin
 //= require_self
 
 var adminRoot = document.getElementById("new-admin")
+<<<<<<< HEAD
 m.postgrest.init(adminRoot.getAttribute('data-api'), {method: "GET", url: "/api_token"});
 m.module(adminRoot, adminApp.AdminContributions);
+=======
+
+m.module(adminRoot, adminApp.AdminContributions);
+>>>>>>> Fixes new_admin dependency problems
