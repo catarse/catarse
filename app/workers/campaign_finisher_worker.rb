e5b7fdab0409a2fb9baa54cc62e02eba520399ff
Antônio Roberto <forevertonny@gmail.com>
2015-06-26 13:05:41 -0300

    before finish a project should check on pagarme the
    current transaction status

--- app/workers/campaign_finisher_worker.rb
+++ app/workers/campaign_finisher_worker.rb
@@ -3,6 +3,11 @@ class CampaignFinisherWorker < ProjectBaseWorker
   sidekiq_options retry: false
 
   def perform id
+    resource(id).payments.each do |payment|
+      payment.pagarme_delegator.update_transaction
+      payment.change_status_from_transaction
+    end
+
     resource(id).finish
   end
 end
