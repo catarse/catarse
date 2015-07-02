40f196f03f7806261d2f9796f304e4083f6f4f06
Antônio Roberto <forevertonny@gmail.com>
2015-07-02 13:30:55 -0300

    added Bank.most_popular_collection method [#98058686]
    this method return a list of 10 most popular banks used by users on
    database.

--- spec/models/bank_spec.rb
+++ spec/models/bank_spec.rb
@@ -19,4 +19,31 @@ RSpec.describe Bank, type: :model do
       end
     end
   end
+
+  describe '.most_popular_collection' do
+    subject { Bank.most_popular_collection }
+
+    let(:user_01) { create(:user, :without_bank_data) }
+    let(:bank_01) { create(:bank, name: "Foo") }
+    let(:bank_02) { create(:bank, name: "Foo bar", code: "001") }
+    let(:bank_03) { create(:bank, name: "Foo bar 2", code: "002") }
+
+    let!(:bank_account01) { create(:bank_account, user: user_01, bank: bank_01) }
+    let!(:bank_account02) { create(:bank_account, user: user_01, bank: bank_01) }
+    let!(:bank_account03) { create(:bank_account, user: user_01, bank: bank_02) }
+
+    before do
+      stub_const("Bank::MOST_POPULAR_LIMIT", 2)
+    end
+
+    context "when we have a current_bank injected" do
+      subject { Bank.most_popular_collection(bank_03) }
+      it "returns a collection with banks in order of most used" do
+        is_expected.to eq([[bank_01.to_s, bank_01.id],
+                           [bank_02.to_s, bank_02.id],
+                           [bank_03.to_s, bank_03.id],
+                           [I18n.t('shared.no_bank_label'), 0]])
+      end
+    end
+  end
 end
