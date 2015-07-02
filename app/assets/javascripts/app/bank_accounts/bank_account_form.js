0a8d41662d6a06acca99639150b040f8a1a174c3
Antônio Roberto <forevertonny@gmail.com>
2015-07-02 16:36:13 -0300

    added function to show bank number form instead select when
    user not found the bank in most popular banks

--- app/assets/javascripts/app/bank_accounts/bank_account_form.js
+++ app/assets/javascripts/app/bank_accounts/bank_account_form.js
@@ -4,12 +4,23 @@ App.addChild('BankAccountForm', _.extend({
   events: {
     'blur input' : 'checkInput',
     'blur select' : 'checkInput',
+    'change select#bank_account_bank_id': 'ShowBankNumberForm'
   },
 
   activate: function(){
     this.setupForm();
     this.$('.field_with_errors .text-error').slideDown('slow');
     this.$('#bank_account_owner_name').data('custom-validation', this.validateName);
+  },
+
+  ShowBankNumberForm: function(event) {
+    $target = this.$(event.currentTarget);
+    $bank_select = this.$('#bank_select');
+    $bank_search = this.$('#bank_search');
+    if($target.val() == 0){
+      $bank_select.hide();
+      $bank_search.show();
+    }
   },
 
   validateName: function(field) {
