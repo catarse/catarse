8dcb9474912bf32a922a6d4c8daa25929888fc80
Antônio Roberto <forevertonny@gmail.com>
2015-07-02 17:02:37 -0300

    added bank list and actions to show and hide the bank list

--- app/assets/javascripts/app/bank_accounts/bank_account_form.js
+++ app/assets/javascripts/app/bank_accounts/bank_account_form.js
@@ -4,23 +4,31 @@ App.addChild('BankAccountForm', _.extend({
   events: {
     'blur input' : 'checkInput',
     'blur select' : 'checkInput',
-    'change select#bank_account_bank_id': 'ShowBankNumberForm'
+    'change select#bank_account_bank_id': 'showBankNumberForm',
+    'click a#show_bank_list': 'toggleBankList'
   },
 
   activate: function(){
     this.setupForm();
     this.$('.field_with_errors .text-error').slideDown('slow');
     this.$('#bank_account_owner_name').data('custom-validation', this.validateName);
+    //this.$('#bank_account_owner_name').data('custom-validation', this.validateName);
   },
 
-  ShowBankNumberForm: function(event) {
+  showBankNumberForm: function(event) {
     $target = this.$(event.currentTarget);
     $bank_select = this.$('#bank_select');
     $bank_search = this.$('#bank_search');
-    if($target.val() == 0){
+
+    if($target.val() == 0) {
       $bank_select.hide();
       $bank_search.show();
     }
+  },
+
+  toggleBankList: function(event) {
+    $bank_list = this.$('#bank_search_list');
+    $bank_list.slideToggle('slow');
   },
 
   validateName: function(field) {
