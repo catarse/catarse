0c5d43f0babd1d6696d29691f67539fd5e238646
Antônio Roberto <forevertonny@gmail.com>
2015-07-16 22:05:31 -0300

    added public.is_second_clip(contributions) that returns a boolean
    when contributions ia second slip

---
+++ db/migrate/20150717000403_added_second_slip_function_to_contributions.rb
@@ -0,0 +1,21 @@
+class AddedSecondSlipFunctionToContributions < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+      DROP FUNCTION public.is_second_slip(contributions);
+      CREATE OR REPLACE FUNCTION public.is_second_slip(contributions) RETURNS boolean
+        LANGUAGE sql AS $_$
+            select true where (
+              select count(1) from payments p
+                where p.contribution_id = $1.id
+                and lower(p.payment_method) = 'boletobancario'
+            ) >= 2
+        $_$ STABLE;
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      DROP FUNCTION public.is_second_slip(contributions);
+    SQL
+  end
+end
