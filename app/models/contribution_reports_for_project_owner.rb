686403324183ee9cb3bf3872c996ce6fc3aa21ed
Diogo Biazus <diogob@gmail.com>
2015-07-24 12:35:52 -0400

    Adds waiting_payment attribute to contributors report and use it to generate pending contributions in project dashboard

--- app/models/contribution_reports_for_project_owner.rb
+++ app/models/contribution_reports_for_project_owner.rb
@@ -1,9 +1,12 @@
 class ContributionReportsForProjectOwner < ActiveRecord::Base
   acts_as_copy_target
 
+  belongs_to :payment
+
   scope :project_id, -> (project_id) { where(project_id: project_id) }
   scope :reward_id, -> (reward_id) { where(reward_id: reward_id) }
   scope :state, -> (state) { where(state: state) }
+  scope :waiting_payment, -> { where(waiting_payment: true) }
   scope :project_owner_id, -> (project_owner_id) { where(project_owner_id: project_owner_id) }
 
   def self.report
