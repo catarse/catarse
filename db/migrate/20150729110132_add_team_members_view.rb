eb0aa7f72413bc10a972b0401eacb9740bb861cd
Antônio Roberto <forevertonny@gmail.com>
2015-07-31 09:10:27 -0300

    added team related views
    - team_totals that returns the totals of our team, (total countries,
    cities, backed…)
    - team_members that return a list of users that is admin on database
    - added SECURITY DEFINER on functions to anonymous can execute in the
    same scope

---
+++ db/migrate/20150729110132_add_team_members_view.rb
@@ -0,0 +1,95 @@
+class AddTeamMembersView < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+      CREATE OR REPLACE FUNCTION was_confirmed(contributions) RETURNS boolean
+          LANGUAGE sql STABLE SECURITY DEFINER
+          AS $_$
+            SELECT EXISTS (
+              SELECT true
+              FROM
+                payments p
+              WHERE p.contribution_id = $1.id AND p.state = ANY(confirmed_states())
+            );
+          $_$;
+
+      CREATE OR REPLACE FUNCTION public.profile_img_thumbnail(users) RETURNS text AS $$
+      SELECT
+        'https://' || (SELECT value FROM settings WHERE name = 'aws_host') ||
+        '/' || (SELECT value FROM settings WHERE name = 'aws_bucket') ||
+        '/uploads/user/uploaded_image/' || $1.id::text ||
+        '/thumb_avatar_' || $1.uploaded_image
+      $$ LANGUAGE SQL STABLE SECURITY DEFINER;
+
+      CREATE OR REPLACE VIEW "1".team_members AS (
+        select
+          u.id,
+          u.name,
+          u.profile_img_thumbnail as img,
+          COALESCE(ut.total_contributed_projects, 0) as total_contributed_projects,
+          COALESCE(ut.sum, 0) as total_amount_contributed
+        from users u
+        left join user_totals ut on ut.user_id = u.id
+        where u.admin
+        order by u.name asc
+      );
+
+      GRANT SELECT on "1".team_members to admin;
+      GRANT SELECT on "1".team_members to web_user;
+      GRANT SELECT on "1".team_members to anonymous;
+
+      drop index if exists user_totals_user_id_ix;
+      drop index if exists user_admin_id_ix;
+      create index user_totals_user_id_ix on user_totals(user_id);
+      create index user_admin_id_ix on users(id) where admin;
+
+      CREATE OR REPLACE VIEW "1".team_totals as (
+        select
+          count(DISTINCT u.id) as member_count,
+          array_to_json(array_agg(DISTINCT country.name)) as countries,
+          count(DISTINCT c.project_id)
+            FILTER (WHERE c.was_confirmed)
+            as total_contributed_projects,
+          count(DISTINCT lower(unaccent(u.address_city))) as total_cities,
+          sum(c.value)
+            FILTER (WHERE c.was_confirmed)
+            as total_amount
+        from users u
+        left join contributions c on c.user_id = u.id
+        left join countries country on country.id = u.country_id
+        where u.admin
+      );
+
+      GRANT SELECT on "1".team_totals to admin;
+      GRANT SELECT on "1".team_totals to web_user;
+      GRANT SELECT on "1".team_totals to anonymous;
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      drop view "1".team_members;
+      drop view if exists "1".team_totals;
+      drop index if exists user_totals_user_id_ix;
+      drop index if exists user_admin_id_ix;
+
+      CREATE OR REPLACE FUNCTION was_confirmed(contributions) RETURNS boolean
+          LANGUAGE sql
+          AS $_$
+            SELECT EXISTS (
+              SELECT true
+              FROM
+                payments p
+              WHERE p.contribution_id = $1.id AND p.state = ANY(confirmed_states())
+            );
+          $_$;
+
+      CREATE OR REPLACE FUNCTION public.profile_img_thumbnail(users) RETURNS text AS $$
+      SELECT
+        'https://' || (SELECT value FROM settings WHERE name = 'aws_host') ||
+        '/' || (SELECT value FROM settings WHERE name = 'aws_bucket') ||
+        '/uploads/user/uploaded_image/' || $1.id::text ||
+        '/thumb_avatar_' || $1.uploaded_image
+      $$ LANGUAGE SQL STABLE;
+    SQL
+  end
+end
