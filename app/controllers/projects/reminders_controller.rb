e7a05999af73639e0b389bc6057d9f0a81320162
adriano benin <adriano.benin@gmail.com>
2015-08-19 18:10:18 -0300

    fix project reminders preventing user to save [#101500236]

--- app/controllers/projects/reminders_controller.rb
+++ app/controllers/projects/reminders_controller.rb
@@ -12,7 +12,7 @@ class Projects::RemindersController < ApplicationController
   end
 
   def destroy
-    project.notifications.where(template_name: 'reminder', user_id: current_user.id).destroy_all
+    project.delete_from_reminder_queue(current_user.id)
     redirect_to project_by_slug_path(project.permalink)
   end
 
