13cd87d6deeda1fd4146f3c8d0da0869bcc84d74
adriano benin <adriano.benin@gmail.com>
2015-09-18 12:25:54 -0300

    remove contribution_canceled_after_confirmed notification [#102779642]

--- app/observers/payment_observer.rb
+++ app/observers/payment_observer.rb
@@ -43,8 +43,6 @@ class PaymentObserver < ActiveRecord::Observer
 
   def from_paid_to_refused(payment)
     contribution = payment.contribution
-    contribution.notify_to_backoffice(:contribution_canceled_after_confirmed,
-                                      { from_email: contribution.user.email, from_name: contribution.user.name })
     contribution.notify_to_contributor( :contribution_canceled ) if !payment.slip_payment?
   end
   alias :from_pending_to_refused :from_paid_to_refused
