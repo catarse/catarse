b6666807b99675d97080df6038831c4ace2c73af
Diogo Biazus <diogob@gmail.com>
2015-09-22 18:18:41 -0400

    Fix problem on delete project reminders

--- db/migrate/20150916230353_add_reminder_handlers_to_project_details.rb
+++ db/migrate/20150916230353_add_reminder_handlers_to_project_details.rb
@@ -145,7 +145,11 @@ class AddReminderHandlersToProjectDetails < ActiveRecord::Migration
       create or replace function public.delete_project_reminder() returns trigger
       language plpgsql as $$
         begin
-          delete from public.project_notifications where user_id = current_setting('user_vars.user_id')::integer and project_id = OLD.project_id;
+          delete from public.project_notifications 
+          where
+            template_name = 'reminder'
+            and user_id = current_setting('user_vars.user_id')::integer 
+            and project_id = OLD.project_id;
           return old;
         end;
       $$;
