0e515ca33fc586de160a34954dc03c75c094216d
Antônio Roberto <forevertonny@gmail.com>
2015-09-29 01:04:21 -0300

    use find_each instead each on workers

--- app/workers/campaign_finisher_worker.rb
+++ app/workers/campaign_finisher_worker.rb
@@ -3,7 +3,7 @@ class CampaignFinisherWorker < ProjectBaseWorker
   sidekiq_options retry: false
 
   def perform id
-    resource(id).payments.where('gateway_id IS NOT NULL').with_states(%w(paid pending pending_refund)).each do |payment|
+    resource(id).payments.where('gateway_id IS NOT NULL').with_states(%w(paid pending pending_refund)).find_each(batch_size: 100) do |payment|
       payment.pagarme_delegator.update_transaction
       payment.change_status_from_transaction
     end
