fb953d69c86b932fac0c541ea5777634e81a5728
Adriano Benin <adriano.benin@gmail.com>
2015-10-01 22:02:51 -0300

    undo mistake

--- config/environment.rb
+++ config/environment.rb
@@ -6,4 +6,4 @@ Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 Catarse::Application.initialize!
 
-ActiveRecord::Base.connection.execute('set statement_timeout to 59500')
+ActiveRecord::Base.connection.execute('set statement_timeout to 5500')
