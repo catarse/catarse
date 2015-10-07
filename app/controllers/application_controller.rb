07eb12a5ba248e8a9c158b56dd39edb6420b8990
Adriano Benin <adriano.benin@gmail.com>
2015-10-07 15:26:33 -0300

    update donation screen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,9 @@ class ApplicationController < ActionController::Base
   end
 
   def can_display_pending_refund_alert?
-    @can_display_alert ||= (current_user && current_user.pending_refund_payments.present? && controller_name.to_sym != :bank_accounts)
+    @can_display_alert ||= (current_user && current_user.pending_refund_payments.present? &&
+                            controller_name.to_sym != :bank_accounts &&
+                            action_name.to_sym != :no_account_refund )
   end
 
   def render_projects collection, ref, locals = {}
