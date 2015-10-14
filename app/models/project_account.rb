585bab6f42b20699d77c2d120a79468fd5a38a8f
Diogo Biazus <diogob@gmail.com>
2015-10-14 15:03:21 -0400

    Hotfix for bank account saving

--- app/models/project_account.rb
+++ app/models/project_account.rb
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 class ProjectAccount < ActiveRecord::Base
   belongs_to :project
   belongs_to :bank
 
-  validates_presence_of :email, :address_street, :address_number, :address_city, :address_state, :address_zip_code, :phone_number, :bank, :agency, :account, :account_digit, :owner_name, :owner_document, :account_type
+  validates_presence_of :email, :address_street, :address_number, :address_city, :address_state, :address_zip_code, :phone_number, :bank, :agency, :account, :account_digit, :owner_name, :owner_document
 
   def entity_type
     if owner_document
