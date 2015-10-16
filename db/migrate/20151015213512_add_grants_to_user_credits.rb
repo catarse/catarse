02d2dcffdf248c9ddd196d06bf40dfa0c04cf977
Adriano Benin <adriano.benin@gmail.com>
2015-10-16 14:25:57 -0300

    revoke grant from anonymous

--- db/migrate/20151015213512_add_grants_to_user_credits.rb
+++ db/migrate/20151015213512_add_grants_to_user_credits.rb
@@ -2,7 +2,7 @@ class AddGrantsToUserCredits < ActiveRecord::Migration
   def change
     execute <<-SQL
 
-      grant select on "1".user_credits to anonymous;
+      revoke select on "1".user_credits from anonymous;
       grant select on "1".user_credits to admin;
       grant select on "1".user_credits to web_user;
     SQL
