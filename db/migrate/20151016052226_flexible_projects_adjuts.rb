146f45200196648aafd36068783d69aeeae48c86
Antônio Roberto <forevertonny@gmail.com>
2015-10-19 18:02:50 -0200

    small fixes :p

--- db/migrate/20151016052226_flexible_projects_adjuts.rb
+++ db/migrate/20151016052226_flexible_projects_adjuts.rb
@@ -1,11 +1,9 @@
 class FlexibleProjectsAdjuts < ActiveRecord::Migration
   def up
-    change_column_null :projects, :category_id, true
     add_index :flexible_projects, :project_id, unique: true
   end
 
   def down
-    change_column_null :projects, :category_id, false
     remove_index :flexible_projects, :project_id
   end
 end
