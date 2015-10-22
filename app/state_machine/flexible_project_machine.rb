a1ae6bfa67a89455b7ab69e95e2b05b75354524f
Antônio Roberto <forevertonny@gmail.com>
2015-10-21 22:07:23 -0200

    adjust flexible project machine to use setup_machine
    with this method we can reuse the guards and callbacks already defined
    on flexible

--- app/state_machine/flexible_project_machine.rb
+++ app/state_machine/flexible_project_machine.rb
@@ -1,60 +1,81 @@
 class FlexibleProjectMachine
   include Statesman::Machine
-  BASIC_VALIDATION_STATES = %i(online waiting_funds successful).freeze
 
-  state :draft, initial: true
-  state :rejected
-  state :online
-  state :successful
-  state :waiting_funds
-  state :deleted
-
-  transition from: :draft, to: [:rejected, :deleted, :online]
-  transition from: :rejected, to: [:draft, :deleted]
-  transition from: :online, to: [:waiting_funds, :successful]
-  transition from: :waiting_funds, to: [:successful]
-
-  # Ensure that project is valid when try change
-  # the project state
-  guard_transition(to: BASIC_VALIDATION_STATES) do |project|
-    project.valid?
+  def self.basic_validation_states
+    %i(online waiting_funds successful).freeze
   end
 
-  # Ensure that project already expired to enter on waiting_funds or successful
-  guard_transition(to: %i(waiting_funds successful)) do |project|
-    project.expired?
+  def self.need_expiration_states
+    %i(waiting_funds successful).freeze
   end
 
-  # Ensure that project has pending contributions before enter on waiting_funds
-  guard_transition(to: :waiting_funds) do |project|
-    project.in_time_to_wait?
+  def self.finished_states
+    %i(successful).freeze
   end
 
-  # Ensure that project has not more pending contributions
-  # before enter on sucessful
-  guard_transition(to: :successful) do |project|
-    !project.in_time_to_wait?
+  def self.setup_machine
+    state :draft, initial: true
+    state :rejected
+    state :online
+    state :successful
+    state :waiting_funds
+    state :deleted
+
+
+    # this block receive all transition
+    # definitions
+    yield self if block_given?
+    
+    # Ensure that project is valid when try change
+    # the project state
+    guard_transition(to: basic_validation_states) do |project|
+      project.valid?
+    end
+
+    # Ensure that project already expired to enter on waiting_funds or successful
+    guard_transition(to: need_expiration_states) do |project|
+      project.expired?
+    end
+
+    # Ensure that project has pending contributions before enter on waiting_funds
+    guard_transition(to: :waiting_funds) do |project|
+      project.in_time_to_wait?
+    end
+
+    # Ensure that project has not more pending contributions
+    # before enter on sucessful
+    guard_transition(to: finished_states) do |project|
+      !project.in_time_to_wait?
+    end
+
+    # Before transition, change the state to trigger validations
+    before_transition do |project, transition|
+      transition.metadata[:from_state] = project.state
+      project.state = transition.to_state
+    end
+
+    # After transition to successful should notify_observers
+    after_transition to: finished_states do |project| 
+      project.notify_observers :sync_with_mailchimp
+    end
+
+    # After transition run, persist the current state
+    # into model.state field.
+    after_transition do |project, transition|
+      project.save
+      from_state = transition.metadata["from_state"]
+
+      project.notify_observers :"from_#{from_state}_to_#{transition.to_state}"
+    end
   end
 
-  # Before transition, change the state to trigger validations
-  before_transition do |project, transition|
-    transition.metadata[:from_state] = project.state
-    project.state = transition.to_state
+  setup_machine do
+    transition from: :rejected, to: %i(draft deleted)
+    transition from: :draft, to: %i(rejected deleted online)
+    transition from: :online, to: %i(waiting_funds successful)
+    transition from: :waiting_funds, to: %i(successful)
   end
 
-  # After transition to successful should notify_observers
-  after_transition to: :successful do |project| 
-    project.notify_observers :sync_with_mailchimp
-  end
-
-  # After transition run, persist the current state
-  # into model.state field.
-  after_transition do |project, transition|
-    project.save
-    from_state = transition.metadata["from_state"]
-
-    project.notify_observers :"from_#{from_state}_to_#{transition.to_state}"
-  end
 
   # put project into draft state
   def push_to_draft
