23bb7c66288b15ca7453ce57bb70867beb27ae9b
Antônio Roberto <forevertonny@gmail.com>
2015-10-22 23:40:13 -0200

    adjust flexible migrations

--- db/migrate/20151015224506_create_flexible_projects.rb
+++ db/migrate/20151015224506_create_flexible_projects.rb
@@ -1,9 +1,17 @@
 class CreateFlexibleProjects < ActiveRecord::Migration
-  def change
+  def up
     create_table :flexible_projects do |t|
       t.references :project
+      t.text :state
 
       t.timestamps
     end
+
+    add_index :flexible_projects, :project_id, unique: true
+  end
+
+  def down
+    drop_table :flexible_projects
+    remove_index :flexible_projects, :project_id
   end
 end
