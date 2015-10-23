e8d6cdee8cd09bc56fc52c460f3e9929e786b4a3
Antônio Roberto <forevertonny@gmail.com>
2015-10-23 15:23:10 -0200

    fix typo error on inverse_of

--- app/models/flexible_project_transition.rb
+++ app/models/flexible_project_transition.rb
@@ -1,6 +1,5 @@
 class FlexibleProjectTransition < ActiveRecord::Base
   include Statesman::Adapters::ActiveRecordTransition
 
-
-  belongs_to :flexible_project, inverse_of: :flexible_project_transitions
+  belongs_to :flexible_project, inverse_of: :transitions
 end
