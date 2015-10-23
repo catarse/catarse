a6e5ec9db87e465cf4881bbfdb10acc5206d0507
Antônio Roberto <forevertonny@gmail.com>
2015-10-22 23:40:13 -0200

    adjust flexible migrations

--- db/migrate/20151016052226_flexible_projects_adjuts.rb
+++
@@ -1,9 +0,0 @@
-class FlexibleProjectsAdjuts < ActiveRecord::Migration
-  def up
-    add_index :flexible_projects, :project_id, unique: true
-  end
-
-  def down
-    remove_index :flexible_projects, :project_id
-  end
-end
