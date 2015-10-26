3c2dcac5dda60e988c25ada57cd8bbf357d2d46e
Diogo Biazus <diogob@gmail.com>
2015-10-26 14:55:38 -0400

    Fixes refresh materialized view with statement_timeout [fix #106328942]

--- app/models/concerns/shared/materialized_view.rb
+++ app/models/concerns/shared/materialized_view.rb
@@ -4,6 +4,7 @@ module Shared
 
     included do
       def self.refresh_view
+        connection.execute("SET statement_timeout TO 0;")
         connection.execute("REFRESH MATERIALIZED VIEW #{self.table_name}")
       end
     end
