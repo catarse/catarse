266c66c8e76173df31b18efd7bfddbf22223a31c
Diogo Biazus <diogob@gmail.com>
2015-11-04 18:37:59 -0500

    Makes flexible project state and project_id not null. Default initial state is draft

--- db/migrate/20151015224506_create_flexible_projects.rb
+++ db/migrate/20151015224506_create_flexible_projects.rb
@@ -1,8 +1,8 @@
 class CreateFlexibleProjects < ActiveRecord::Migration
   def up
     create_table :flexible_projects do |t|
-      t.references :project
-      t.text :state
+      t.references :project, null: false
+      t.text :state, null: false, default: 'draft'
 
       t.timestamps
     end
