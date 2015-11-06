471342a12dba26667ccf8385b72df9f01c6a5b79
Antônio Roberto <forevertonny@gmail.com>
2015-11-06 17:38:23 -0200

    adjust projects / project_details endpoint to handle with flexible project columns (state, state_order, mode)

---
+++ db/migrate/20151106163309_adjust_project_details_to_handle_with_flexible.rb
@@ -0,0 +1,237 @@
+class AdjustProjectDetailsToHandleWithFlexible < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+SET statement_timeout TO 0;
+    SQL
+
+    execute <<-SQL
+SELECT deps_save_and_drop_dependencies('1', 'project_details');
+DROP VIEW "1".project_details;
+CREATE VIEW "1".project_details AS 
+ SELECT p.id AS project_id,
+    p.id,
+    p.user_id,
+    p.name,
+    p.headline,
+    p.budget,
+    p.goal,
+    p.about_html,
+    p.permalink,
+    p.video_embed_url,
+    p.video_url,
+    c.name_pt AS category_name,
+    c.id AS category_id,
+    original_image(p.*) AS original_image,
+    thumbnail_image(p.*, 'thumb'::text) AS thumb_image,
+    thumbnail_image(p.*, 'small'::text) AS small_image,
+    thumbnail_image(p.*, 'large'::text) AS large_image,
+    thumbnail_image(p.*, 'video_cover'::text) AS video_cover_image,
+    COALESCE(pt.progress, 0::numeric) AS progress,
+    COALESCE(pt.pledged, 0::numeric) AS pledged,
+    COALESCE(pt.total_contributions, 0::bigint) AS total_contributions,
+    COALESCE(fp.state, p.state) as state,
+    p.mode,
+    p.state_order,
+    p.expires_at,
+    zone_expires_at(p.*) AS zone_expires_at,
+    p.online_date,
+    p.sent_to_analysis_at,
+    is_published(p.*) AS is_published,
+    is_expired(p.*) AS is_expired,
+    open_for_contributions(p.*) AS open_for_contributions,
+    p.online_days,
+    remaining_time_json(p.*) AS remaining_time,
+    ( SELECT count(pp_1.*) AS count
+           FROM project_posts pp_1
+          WHERE pp_1.project_id = p.id) AS posts_count,
+    json_build_object('city', COALESCE(ct.name, u.address_city), 'state_acronym', COALESCE(st.acronym, u.address_state::character varying), 'state', COALESCE(st.name, u.address_state::character varying)) AS address,
+    json_build_object('id', u.id, 'name', u.name) AS "user",
+    count(DISTINCT pn.*) FILTER (WHERE pn.template_name = 'reminder'::text) AS reminder_count,
+    is_owner_or_admin(p.user_id) AS is_owner_or_admin,
+    user_signed_in() AS user_signed_in,
+    current_user_already_in_reminder(p.*) AS in_reminder,
+    count(pp.*) AS total_posts,
+    "current_user"() = 'admin'::name AS is_admin_role
+   FROM projects p
+     JOIN categories c ON c.id = p.category_id
+     JOIN users u ON u.id = p.user_id
+     LEFT JOIN public.flexible_projects fp on fp.project_id = p.id
+     LEFT JOIN project_posts pp ON pp.project_id = p.id
+     LEFT JOIN "1".project_totals pt ON pt.project_id = p.id
+     LEFT JOIN cities ct ON ct.id = p.city_id
+     LEFT JOIN states st ON st.id = ct.state_id
+     LEFT JOIN project_notifications pn ON pn.project_id = p.id
+  GROUP BY p.id, c.id, u.id, c.name_pt, ct.name, u.address_city, st.acronym, u.address_state, st.name, pt.progress, pt.pledged, pt.total_contributions, p.state, p.expires_at, p.sent_to_analysis_at, pt.total_payment_service_fee, fp.state;
+
+select deps_restore_dependencies('1', 'project_details');
+
+grant select on "1".project_details to admin;
+grant select on "1".project_details to web_user;
+grant select on "1".project_details to anonymous;
+
+grant select on public.flexible_projects to admin;
+grant select on public.flexible_projects to web_user;
+grant select on public.flexible_projects to anonymous;
+
+grant select on public.flexible_project_states to admin;
+grant select on public.flexible_project_states to web_user;
+grant select on public.flexible_project_states to anonymous;
+
+grant select on public.project_states to admin;
+grant select on public.project_states to web_user;
+grant select on public.project_states to anonymous;
+
+SELECT deps_save_and_drop_dependencies('1', 'projects');
+DROP VIEW "1".projects CASCADE;
+CREATE VIEW "1".projects AS
+ SELECT p.id AS project_id,
+    p.name AS project_name,
+    p.headline,
+    p.permalink,
+    p.mode,
+    COALESCE(fp.state, p.state) as state,
+    p.state_order,
+    p.online_date,
+    p.recommended,
+    public.thumbnail_image(p.*, 'large'::text) AS project_img,
+    public.remaining_time_json(p.*) AS remaining_time,
+    p.expires_at,
+    COALESCE(( SELECT pt.pledged
+           FROM "1".project_totals pt
+          WHERE (pt.project_id = p.id)), (0)::numeric) AS pledged,
+    COALESCE(( SELECT pt.progress
+           FROM "1".project_totals pt
+          WHERE (pt.project_id = p.id)), (0)::numeric) AS progress,
+    COALESCE(s.acronym, (pa.address_state)::character varying(255)) AS state_acronym,
+    u.name AS owner_name,
+    COALESCE(c.name, pa.address_city) AS city_name
+   FROM ((((public.projects p
+     JOIN public.users u ON ((p.user_id = u.id)))
+     LEFT JOIN public.flexible_projects fp ON fp.project_id = p.id
+     LEFT JOIN public.project_accounts pa ON ((pa.project_id = p.id)))
+     LEFT JOIN public.cities c ON ((c.id = p.city_id)))
+     LEFT JOIN public.states s ON ((s.id = c.state_id)));
+
+select deps_restore_dependencies('1', 'projects');
+grant select on "1".projects to admin;
+grant select on "1".projects to web_user;
+grant select on "1".projects to anonymous;
+
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+SET statement_timeout TO 0;
+    SQL
+
+    execute <<-SQL
+SELECT deps_save_and_drop_dependencies('1', 'project_details');
+DROP VIEW "1".project_details;
+CREATE VIEW "1".project_details AS 
+ SELECT p.id AS project_id,
+    p.id,
+    p.user_id,
+    p.name,
+    p.headline,
+    p.budget,
+    p.goal,
+    p.about_html,
+    p.permalink,
+    p.video_embed_url,
+    p.video_url,
+    c.name_pt AS category_name,
+    c.id AS category_id,
+    original_image(p.*) AS original_image,
+    thumbnail_image(p.*, 'thumb'::text) AS thumb_image,
+    thumbnail_image(p.*, 'small'::text) AS small_image,
+    thumbnail_image(p.*, 'large'::text) AS large_image,
+    thumbnail_image(p.*, 'video_cover'::text) AS video_cover_image,
+    COALESCE(pt.progress, 0::numeric) AS progress,
+    COALESCE(pt.pledged, 0::numeric) AS pledged,
+    COALESCE(pt.total_contributions, 0::bigint) AS total_contributions,
+    p.state,
+    p.expires_at,
+    zone_expires_at(p.*) AS zone_expires_at,
+    p.online_date,
+    p.sent_to_analysis_at,
+    is_published(p.*) AS is_published,
+    is_expired(p.*) AS is_expired,
+    open_for_contributions(p.*) AS open_for_contributions,
+    p.online_days,
+    remaining_time_json(p.*) AS remaining_time,
+    ( SELECT count(pp_1.*) AS count
+           FROM project_posts pp_1
+          WHERE pp_1.project_id = p.id) AS posts_count,
+    json_build_object('city', COALESCE(ct.name, u.address_city), 'state_acronym', COALESCE(st.acronym, u.address_state::character varying), 'state', COALESCE(st.name, u.address_state::character varying)) AS address,
+    json_build_object('id', u.id, 'name', u.name) AS "user",
+    count(DISTINCT pn.*) FILTER (WHERE pn.template_name = 'reminder'::text) AS reminder_count,
+    is_owner_or_admin(p.user_id) AS is_owner_or_admin,
+    user_signed_in() AS user_signed_in,
+    current_user_already_in_reminder(p.*) AS in_reminder,
+    count(pp.*) AS total_posts,
+    "current_user"() = 'admin'::name AS is_admin_role
+   FROM projects p
+     JOIN categories c ON c.id = p.category_id
+     JOIN users u ON u.id = p.user_id
+     LEFT JOIN project_posts pp ON pp.project_id = p.id
+     LEFT JOIN "1".project_totals pt ON pt.project_id = p.id
+     LEFT JOIN cities ct ON ct.id = p.city_id
+     LEFT JOIN states st ON st.id = ct.state_id
+     LEFT JOIN project_notifications pn ON pn.project_id = p.id
+  GROUP BY p.id, c.id, u.id, c.name_pt, ct.name, u.address_city, st.acronym, u.address_state, st.name, pt.progress, pt.pledged, pt.total_contributions, p.state, p.expires_at, p.sent_to_analysis_at, pt.total_payment_service_fee;
+
+select deps_restore_dependencies('1', 'project_details');
+
+grant select on "1".project_details to admin;
+grant select on "1".project_details to web_user;
+grant select on "1".project_details to anonymous;
+
+revoke select on public.flexible_projects from admin;
+revoke select on public.flexible_projects from web_user;
+revoke select on public.flexible_projects from anonymous;
+
+revoke select on public.flexible_project_states from admin;
+revoke select on public.flexible_project_states from web_user;
+revoke select on public.flexible_project_states from anonymous;
+
+revoke select on public.project_states from admin;
+revoke select on public.project_states from web_user;
+revoke select on public.project_states from anonymous;
+
+SELECT deps_save_and_drop_dependencies('1', 'projects');
+DROP VIEW "1".projects CASCADE;
+CREATE VIEW "1".projects AS
+ SELECT p.id AS project_id,
+    p.name AS project_name,
+    p.headline,
+    p.permalink,
+    p.state,
+    p.online_date,
+    p.recommended,
+    public.thumbnail_image(p.*, 'large'::text) AS project_img,
+    public.remaining_time_json(p.*) AS remaining_time,
+    p.expires_at,
+    COALESCE(( SELECT pt.pledged
+           FROM "1".project_totals pt
+          WHERE (pt.project_id = p.id)), (0)::numeric) AS pledged,
+    COALESCE(( SELECT pt.progress
+           FROM "1".project_totals pt
+          WHERE (pt.project_id = p.id)), (0)::numeric) AS progress,
+    COALESCE(s.acronym, (pa.address_state)::character varying(255)) AS state_acronym,
+    u.name AS owner_name,
+    COALESCE(c.name, pa.address_city) AS city_name
+   FROM ((((public.projects p
+     JOIN public.users u ON ((p.user_id = u.id)))
+     LEFT JOIN public.project_accounts pa ON ((pa.project_id = p.id)))
+     LEFT JOIN public.cities c ON ((c.id = p.city_id)))
+     LEFT JOIN public.states s ON ((s.id = c.state_id)));
+
+grant select on "1".projects to admin;
+grant select on "1".projects to web_user;
+grant select on "1".projects to anonymous;
+select deps_restore_dependencies('1', 'projects');
+
+    SQL
+  end
+end
