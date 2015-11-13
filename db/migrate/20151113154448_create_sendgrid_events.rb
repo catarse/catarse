d569eb770fa147f77501d2ba83eae1313e20e455
Adriano Benin <adriano.benin@gmail.com>
2015-11-13 18:56:47 -0200

    normalize sendgrid_events table

--- db/migrate/20151113154448_create_sendgrid_events.rb
+++ db/migrate/20151113154448_create_sendgrid_events.rb
@@ -1,8 +1,15 @@
 class CreateSendgridEvents < ActiveRecord::Migration
   def change
     create_table :sendgrid_events do |t|
-      t.json :sendgrid_data
+      t.integer :notification_id, null: false, foreign_key: false
+      t.integer :notification_user, null: false
+      t.text  :notification_type, null: false
+      t.text :template_name, null: false
+      t.text :event, null: false
+      t.text :email, null: false
+      t.text :useragent
 
+      t.json :sendgrid_data, null: false
       t.timestamps
     end
   end
