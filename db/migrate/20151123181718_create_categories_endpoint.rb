150da70e30b32f8d4cfc3abe76b1a3d723f93923
Diogo Biazus <diogob@gmail.com>
2015-11-24 11:48:18 -0500

    Fixes categories endpoint and project_search function to take flex projects into account

--- db/migrate/20151123181718_create_categories_endpoint.rb
+++ db/migrate/20151123181718_create_categories_endpoint.rb
@@ -5,11 +5,11 @@ class CreateCategoriesEndpoint < ActiveRecord::Migration
     SELECT
         c.id,
         name_pt as name,
-        (SELECT count(*) from projects p where p.state = 'online' AND p.category_id = c.id) as online_projects,
+        (SELECT count(*) from projects p where p.open_for_contributions AND p.category_id = c.id) as online_projects,
         (SELECT count(DISTINCT user_id) from category_followers cf where cf.category_id = c.id) as followers
         FROM categories c
         WHERE
-        exists(select true from projects p where p.category_id = c.id and p.state not in('draft', 'rejected'));
+        exists(select true from projects p where p.category_id = c.id);
     grant select on "1".categories to admin;
     grant select on "1".categories to web_user;
     grant select on "1".categories to anonymous;
