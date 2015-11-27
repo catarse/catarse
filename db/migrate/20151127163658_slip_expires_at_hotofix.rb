ffa5feda515d7144f132b1145f353cba98f45890
Diogo Biazus <diogob@gmail.com>
2015-11-27 12:25:23 -0500

    When payment is new we should bring slip_expiration_date from database using current_timestamp

--- db/migrate/20151127163658_slip_expires_at_hotofix.rb
+++ db/migrate/20151127163658_slip_expires_at_hotofix.rb
@@ -1,18 +1,34 @@
 class SlipExpiresAtHotofix < ActiveRecord::Migration
   def up
     execute <<-SQL
-CREATE OR REPLACE FUNCTION public.slip_expires_at(payments)
+CREATE OR REPLACE FUNCTION public.slip_expiration_weekdays()
+ RETURNS int
+ LANGUAGE sql
+ STABLE
+AS $function$
+    SELECT 2;
+    $function$;
+
+CREATE OR REPLACE FUNCTION public.weekdays_from(weekdays int, from_ts timestamp)
  RETURNS timestamp without time zone
  LANGUAGE sql
  STABLE
 AS $function$
     SELECT max(day) FROM (
       SELECT day
-      FROM generate_series($1.created_at, $1.created_at + '1 month'::interval, '1 day') day
+      FROM generate_series(from_ts, from_ts + '1 year'::interval, '1 day') day
       WHERE extract(dow from day) not in (0,1)
       ORDER BY day
-      LIMIT 3
+      LIMIT (weekdays + 1)
     ) a;
+    $function$;
+
+CREATE OR REPLACE FUNCTION public.slip_expires_at(payments)
+ RETURNS timestamp without time zone
+ LANGUAGE sql
+ STABLE
+AS $function$
+SELECT weekdays_from(public.slip_expiration_weekdays(), $1.created_at);
     $function$;
     SQL
   end
