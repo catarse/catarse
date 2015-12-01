754dd536a06cf3e6de883ad1ec7e564862c44324
Antônio Roberto <forevertonny@gmail.com>
2015-12-01 20:13:42 -0200

    added can_deliver scope to project_reminder model

--- app/models/project_reminder.rb
+++ app/models/project_reminder.rb
@@ -4,4 +4,6 @@ class ProjectReminder < ActiveRecord::Base
 
   validates :user_id, uniqueness: { scope: :project_id }
   validates :user_id, :project_id, presence: true
+
+  scope :can_deliver, -> { where("project_reminders.can_deliver") }
 end
--- spec/models/project_reminder_spec.rb
+++ spec/models/project_reminder_spec.rb
@@ -15,4 +15,30 @@ RSpec.describe ProjectReminder, type: :model do
     it { is_expected.to validate_presence_of :project_id }
     it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(:project_id) }
   end
+
+  describe ".can_deliver" do
+    context "when project is expiring" do
+      let(:project) { create(:project, state: 'online', online_days: 1, expires_at: 1.hour.from_now) }
+
+      before do
+        4.times { create(:project_reminder, project: project) }
+      end
+
+      subject { ProjectReminder.can_deliver.count }
+
+      it { is_expected.to eq(4) }
+    end
+
+    context "when project is not expiring" do
+      let(:project) { create(:project, state: 'online', online_days: 10, expires_at: 60.hours.from_now) }
+
+      before do
+        4.times { create(:project_reminder, project: project) }
+      end
+
+      subject { ProjectReminder.can_deliver.count }
+
+      it { is_expected.to eq(0) }
+    end
+  end
 end
