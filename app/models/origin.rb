8bfcfab22163e6c040f1b3c43b69fb64d53a5994
Antônio Roberto <forevertonny@gmail.com>
2015-12-03 15:29:16 -0200

    added process to search or store a origin

--- app/models/origin.rb
+++ app/models/origin.rb
@@ -1,4 +1,25 @@
 class Origin < ActiveRecord::Base
   has_many :projects
   has_many :contributions
+
+  validates :domain, presence: true
+  validates :domain, uniqueness: { scope: :referral }
+
+  # Process a given ref and http referrer
+  # until found or create a new Origin
+  def self.process referral, http_referrer
+    find_or_create_by(
+      referral: referral,
+      domain: get_domain_from_url(http_referrer)
+    ) if http_referrer.present?
+  end
+
+  protected
+
+  def self.get_domain_from_url(url)
+    uri = URI.parse(url)
+    uri = URI.parse("http://#{url}") if uri.scheme.nil?
+    host = uri.host.downcase
+    host.start_with?('www.') ? host[4..-1] : host
+  end
 end
--- spec/models/origin_spec.rb
+++ spec/models/origin_spec.rb
@@ -5,4 +5,54 @@ RSpec.describe Origin, type: :model do
     it { is_expected.to have_many(:projects) }
     it { is_expected.to have_many(:contributions) }
   end
+
+  describe "validations" do
+    it{ is_expected.to validate_presence_of(:domain) }
+    it { is_expected.to validate_uniqueness_of(:domain).scoped_to(:referral) }
+  end
+
+  describe "#process" do
+    let(:domain) { nil }
+    let(:referral) { nil }
+
+    subject { Origin.process(referral, domain) }
+
+    context "with ref" do
+      context "when referral already exists into database with the same origin domain" do
+        let(:domain) { 'www.catarse.me' }
+        let(:referral) { 'explore' }
+        let!(:origin) { create(:origin, domain: 'catarse.me', referral: referral)}
+        it "should return the already created origin" do
+          is_expected.to eq origin
+        end
+      end
+
+      context "when referral should not exists" do
+        let(:domain) { 'http://m.facebook.com/posts/123123/lorem' }
+        let(:referral) { 'fb_test' }
+        let!(:origin) { create(:origin, domain: 'lorem.com', referral: referral)}
+
+        it "should store and return a new origin" do
+          expect(subject.domain).to eq 'm.facebook.com'
+          expect(subject).to_not eq(origin)
+        end
+      end
+    end
+
+    context "without ref" do
+      let(:domain) { 'google.com' }
+      let(:referral) { nil }
+      it "should store without ref" do
+        is_expected.to_not eq(nil)
+        expect(subject.domain).to eq(domain)
+      end
+    end
+
+    context "without origin domain" do
+      let(:domain) { nil }
+      it "should return nil without domain" do
+        is_expected.to eq(nil)
+      end
+    end
+  end
 end
