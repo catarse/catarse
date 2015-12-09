bd2c4d314c6644aae35c9aba5f25ab15df274db5
Antônio Roberto <forevertonny@gmail.com>
2015-12-09 18:21:24 -0200

    remove drops column from this PR

--- db/migrate/20151208161500_bye_unsued_fields_into_projects.rb
+++ db/migrate/20151208161500_bye_unsued_fields_into_projects.rb
@@ -33,12 +33,6 @@ CREATE OR REPLACE VIEW "1".projects AS
      LEFT JOIN public.cities c ON ((c.id = p.city_id)))
      LEFT JOIN public.states s ON ((s.id = c.state_id)));
 
-ALTER TABLE projects
-    DROP COLUMN sent_to_analysis_at,
-    DROP COLUMN rejected_at,
-    DROP COLUMN online_date,
-    DROP COLUMN referral_link,
-    DROP COLUMN sent_to_draft_at
     SQL
   end
 
@@ -77,35 +71,6 @@ CREATE OR REPLACE VIEW "1".projects AS
      LEFT JOIN public.project_accounts pa ON ((pa.project_id = p.id)))
      LEFT JOIN public.cities c ON ((c.id = p.city_id)))
      LEFT JOIN public.states s ON ((s.id = c.state_id)));
-
-ALTER TABLE projects
-    ADD COLUMN referral_link text,
-    ADD COLUMN sent_to_analysis_at timestamp without time zone,
-    ADD COLUMN rejected_at timestamp without time zone,
-    ADD COLUMN online_date timestamp without time zone,
-    ADD COLUMN sent_to_draft_at timestamp without time zone;
-
-UPDATE projects p
-    SET referral_link = (
-        SELECT COALESCE(o.referral, o.domain)
-            FROM origins o
-            WHERE o.id = p.origin_id ),
-    online_date = p.online_at,
-    sent_to_analysis_at = p.in_analysis_at,
-    rejected_at = (
-        SELECT pt.created_at
-            FROM "1".project_transitions pt
-            WHERE pt.project_id = p.id
-                AND pt.state = 'rejected'),
-    sent_to_draft_at = p.created_at,
-    uploaded_image = (
-        CASE WHEN video_thumbnail is not null THEN
-            uploaded_image
-        ELSE
-            COALESCE(uploaded_image, 'missing_image')
-        END),
-    about_html = COALESCE(about_html, name),
-    headline = COALESCE(headline, name);
     SQL
   end
 end
