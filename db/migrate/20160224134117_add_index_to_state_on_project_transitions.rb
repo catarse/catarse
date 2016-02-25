037bc8d0d29bd0b54d49db32b016e5f6e1a50eb8
Antônio Roberto <forevertonny@gmail.com>
2016-02-24 21:28:34 -0300

    can't use concurrently inside transaction block

--- db/migrate/20160224134117_add_index_to_state_on_project_transitions.rb
+++ db/migrate/20160224134117_add_index_to_state_on_project_transitions.rb
@@ -1,8 +1,8 @@
 class AddIndexToStateOnProjectTransitions < ActiveRecord::Migration
   def up
     execute <<-SQL
-CREATE INDEX CONCURRENTLY to_state_project_tran_idx ON public.project_transitions ( to_state );
-CREATE INDEX CONCURRENTLY to_state_flex_project_tran_idx ON public.flexible_project_transitions ( to_state );
+CREATE INDEX to_state_project_tran_idx ON public.project_transitions ( to_state );
+CREATE INDEX to_state_flex_project_tran_idx ON public.flexible_project_transitions ( to_state );
     SQL
   end
 
