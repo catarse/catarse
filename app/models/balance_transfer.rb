1c6a644896fe323b4a922fef6bacfcb9238b1ebb
Antônio Roberto Silva <forevertonny@gmail.com>
2016-03-17 17:07:02 -0300

    basic state machine for balance_transfer

--- app/models/balance_transfer.rb
+++ app/models/balance_transfer.rb
@@ -1,2 +1,25 @@
 class BalanceTransfer < ActiveRecord::Base
+  belongs_to :project
+  belongs_to :user
+
+  has_many :transitions,
+    class_name: 'BalanceTransferTransition',
+    autosave: false
+
+  def state_machine
+    @stat_machine ||= BalanceTransferMachine.new(self, {
+      transition_class: 'BalanceTransferTransition',
+      association_name: :transitions
+    })
+  end
+
+  def state
+    state_machine.current_state || 'pending'
+  end
+
+  %w(pending authorized processing transfered error rejected).each do |st|
+    define_method "#{st}?" do
+      self.state == st
+    end
+  end
 end
