c53e1298bb5dc808602d55fc7b792a176a041207
Antônio Roberto Silva <forevertonny@gmail.com>
2016-04-18 13:56:15 -0300

    added notification when transfers is authorized [Fix #104349978]
    notify project owner about started transfer

--- app/state_machine/balance_transfer_machine.rb
+++ app/state_machine/balance_transfer_machine.rb
@@ -9,8 +9,10 @@ class BalanceTransferMachine
   state :rejected
 
   transition from: :pending, to: %i(authorized rejected)
+  transition from: :processing, to: %i(error rejected transferred)
 
   after_transition(from: :pending, to: :authorized) do |bt|
     bt.pagarme_delegator.transfer_funds
+    bt.project.notify(:project_balance_transferred, bt.project.user,bt.project) if bt.project.present?
   end
 end
