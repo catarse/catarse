44545a52b12a105cbcc3c7f8947ccd39270caddb
Antônio Roberto Silva <forevertonny@gmail.com>
2016-05-09 18:26:09 -0300

    financial scopre dont run on ci

--- db/migrate/20160509162912_remove_pcc.rb
+++ db/migrate/20160509162912_remove_pcc.rb
@@ -19,44 +19,6 @@ CREATE VIEW "1".project_transfers AS
      LEFT JOIN "1".project_totals pt ON ((pt.project_id = p.id)))
   WHERE (public.is_owner_or_admin(p.user_id) OR ("current_user"() = 'catarse'::name));
 
-DROP VIEW financial.project_transfers;
-CREATE VIEW financial.project_transfers AS
- SELECT p.id AS project_id,
-    p.service_fee,
-    p.goal,
-    pt.paid_pledged AS pledged,
-    public.zone_timestamp(p.expires_at) AS expires_at,
-    public.zone_timestamp(COALESCE(public.successful_at(p.*), public.failed_at(p.*))) AS finished_at,
-    pt.paid_total_payment_service_fee AS gateway_fee,
-    public.total_catarse_fee(p.*) AS catarse_fee,
-    public.total_catarse_fee_without_gateway_fee(p.*) AS catarse_fee_without_gateway,
-    (pt.pledged - public.total_catarse_fee(p.*)) AS amount_without_catarse_fee,
-    public.irrf_tax(p.*) AS irrf_tax,
-    (((pt.paid_pledged - public.total_catarse_fee(p.*)) + public.irrf_tax(p.*))) AS total_amount
-   FROM (public.projects p
-     LEFT JOIN "1".project_totals pt ON ((pt.project_id = p.id)));
-
-DROP MATERIALIZED VIEW financial.repasses;
-CREATE MATERIALIZED VIEW financial.repasses AS
- SELECT p.id AS project_id,
-    p.service_fee,
-    p.goal,
-    pt.pledged,
-    public.zone_timestamp(p.expires_at) AS expires_at,
-    public.zone_timestamp(COALESCE(public.successful_at(p.*), public.failed_at(p.*))) AS finished_at,
-    pt.total_payment_service_fee AS gateway_fee,
-    public.total_catarse_fee(p.*) AS catarse_fee,
-    public.total_catarse_fee_without_gateway_fee(p.*) AS catarse_fee_without_gateway,
-    (pt.pledged - public.total_catarse_fee(p.*)) AS amount_without_catarse_fee,
-    public.irrf_tax(p.*) AS irrf_tax,
-    (((pt.pledged - public.total_catarse_fee(p.*)) + public.irrf_tax(p.*))) AS total_amount
-   FROM ((public.projects p
-     LEFT JOIN "1".project_totals pt ON ((pt.project_id = p.id)))
-     LEFT JOIN public.flexible_projects fp ON ((fp.project_id = p.id)))
-  WHERE (COALESCE(fp.state, (p.state)::text) = 'successful'::text)
-  ORDER BY p.expires_at DESC
-  WITH NO DATA;
-
 
 CREATE OR REPLACE FUNCTION public.approve_project_account() RETURNS trigger
     LANGUAGE plpgsql
@@ -151,46 +113,6 @@ CREATE VIEW "1".project_transfers AS
    FROM (public.projects p
      LEFT JOIN "1".project_totals pt ON ((pt.project_id = p.id)))
   WHERE (public.is_owner_or_admin(p.user_id) OR ("current_user"() = 'catarse'::name));
-
-DROP VIEW financial.project_transfers;
-CREATE VIEW financial.project_transfers AS
- SELECT p.id AS project_id,
-    p.service_fee,
-    p.goal,
-    pt.paid_pledged AS pledged,
-    public.zone_timestamp(p.expires_at) AS expires_at,
-    public.zone_timestamp(COALESCE(public.successful_at(p.*), public.failed_at(p.*))) AS finished_at,
-    pt.paid_total_payment_service_fee AS gateway_fee,
-    public.total_catarse_fee(p.*) AS catarse_fee,
-    public.total_catarse_fee_without_gateway_fee(p.*) AS catarse_fee_without_gateway,
-    (pt.pledged - public.total_catarse_fee(p.*)) AS amount_without_catarse_fee,
-    public.irrf_tax(p.*) AS irrf_tax,
-    public.pcc_tax(p.*) AS pcc_tax,
-    (((pt.paid_pledged - public.total_catarse_fee(p.*)) + public.irrf_tax(p.*)) + public.pcc_tax(p.*)) AS total_amount
-   FROM (public.projects p
-     LEFT JOIN "1".project_totals pt ON ((pt.project_id = p.id)));
-
-DROP MATERIALIZED VIEW financial.repasses;
-CREATE MATERIALIZED VIEW financial.repasses AS
- SELECT p.id AS project_id,
-    p.service_fee,
-    p.goal,
-    pt.pledged,
-    public.zone_timestamp(p.expires_at) AS expires_at,
-    public.zone_timestamp(COALESCE(public.successful_at(p.*), public.failed_at(p.*))) AS finished_at,
-    pt.total_payment_service_fee AS gateway_fee,
-    public.total_catarse_fee(p.*) AS catarse_fee,
-    public.total_catarse_fee_without_gateway_fee(p.*) AS catarse_fee_without_gateway,
-    (pt.pledged - public.total_catarse_fee(p.*)) AS amount_without_catarse_fee,
-    public.irrf_tax(p.*) AS irrf_tax,
-    public.pcc_tax(p.*) AS pcc_tax,
-    (((pt.pledged - public.total_catarse_fee(p.*)) + public.irrf_tax(p.*)) + public.pcc_tax(p.*)) AS total_amount
-   FROM ((public.projects p
-     LEFT JOIN "1".project_totals pt ON ((pt.project_id = p.id)))
-     LEFT JOIN public.flexible_projects fp ON ((fp.project_id = p.id)))
-  WHERE (COALESCE(fp.state, (p.state)::text) = 'successful'::text)
-  ORDER BY p.expires_at DESC
-  WITH NO DATA;
 
 CREATE OR REPLACE FUNCTION public.approve_project_account() RETURNS trigger
     LANGUAGE plpgsql
