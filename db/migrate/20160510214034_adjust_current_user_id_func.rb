ec92f900c56711671923e595a3059bfc4d1c90f9
Antônio Roberto Silva <forevertonny@gmail.com>
2016-05-10 18:43:46 -0300

    always return null::integer when postgrest.claims.user_id not setted, avoid use set in stable function

---
+++ db/migrate/20160510214034_adjust_current_user_id_func.rb
@@ -0,0 +1,38 @@
+class AdjustCurrentUserIdFunc < ActiveRecord::Migration
+  def up
+    execute %Q{
+CREATE OR REPLACE FUNCTION public.current_user_id()
+ RETURNS integer
+ LANGUAGE plpgsql
+ STABLE
+AS $function$
+BEGIN
+  RETURN nullif(current_setting('postgrest.claims.user_id'), '')::integer;
+EXCEPTION
+WHEN others THEN
+  RETURN NULL::integer;
+END
+    $function$
+;
+    }
+  end
+
+  def down
+    execute %Q{
+CREATE OR REPLACE FUNCTION public.current_user_id()
+ RETURNS integer
+ LANGUAGE plpgsql
+ STABLE
+AS $function$
+BEGIN
+  RETURN nullif(current_setting('postgrest.claims.user_id'), '')::integer;
+EXCEPTION
+WHEN others THEN
+  SET LOCAL postgrest.claims.user_id TO '';
+  RETURN NULL::integer;
+END
+    $function$
+;
+    }
+  end
+end
