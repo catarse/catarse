7f5352255312562604c4ca248c940880dda0de99
Adriano Benin <adriano.benin@gmail.com>
2016-05-10 13:25:46 -0300

    fix successful_projects view

--- db/migrate/20160505150034_refactor_views.rb
+++ db/migrate/20160505150034_refactor_views.rb
@@ -340,6 +340,46 @@ class RefactorViews < ActiveRecord::Migration
 
       GRANT SELECT ON "1".category_totals TO admin, anonymous, web_user;
 
+      DROP MATERIALIZED VIEW "1".successful_projects;
+      CREATE MATERIALIZED VIEW "1".successful_projects AS
+        SELECT p.id AS project_id,
+      p.category_id,
+      p.name AS project_name,
+      p.headline,
+      p.permalink,
+      p.mode AS mode,
+      p.state::text AS state,
+      so.so AS state_order,
+      od.od AS online_date,
+      p.recommended,
+      thumbnail_image(p.*, 'large'::text) AS project_img,
+      remaining_time_json(p.*) AS remaining_time,
+      p.expires_at,
+      COALESCE(( SELECT pt.pledged
+             FROM project_totals pt
+            WHERE pt.project_id = p.id), 0::numeric) AS pledged,
+      COALESCE(( SELECT pt.progress
+             FROM project_totals pt
+            WHERE pt.project_id = p.id), 0::numeric) AS progress,
+      s.acronym AS state_acronym,
+      u.name AS owner_name,
+      c.name AS city_name,
+      p.full_text_index,
+      is_current_and_online(p.expires_at, p.state::text) AS open_for_contributions
+     FROM projects p
+       JOIN users u ON p.user_id = u.id
+       JOIN cities c ON c.id = p.city_id
+       JOIN states s ON s.id = c.state_id
+       JOIN LATERAL zone_timestamp(online_at(p.*)) od(od) ON true
+       JOIN LATERAL state_order(p.*) so(so) ON true
+      WHERE (EXISTS ( SELECT true AS bool
+             FROM "1".project_transitions
+            WHERE project_transitions.state::text = 'successful'::text AND project_transitions.most_recent AND project_transitions.project_id = p.id));
+
+      CREATE UNIQUE INDEX successful_projects_idx ON "1".successful_projects(project_id);
+
+      GRANT SELECT ON "1".successful_projects TO anonymous, web_user, admin;
+
       DROP FUNCTION mode(projects);
 
     SQL
