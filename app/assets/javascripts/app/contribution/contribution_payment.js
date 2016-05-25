1217ee35a237fa81825ec06dfd1279bc60194335
Bruno Grossi <bruno@grossi.com.br>
2016-05-25 18:13:37 -0300

    Finished contribution events

--- app/assets/javascripts/app/contribution/contribution_payment.js
+++ app/assets/javascripts/app/contribution/contribution_payment.js
@@ -1,6 +1,14 @@
 App.addChild('Payment', {
   el: '#payment-engines',
 
+  events: {
+    'change #payment_card_number' : 'payment_card_number_change'
+  },
+
+  payment_card_number_change: function(event) {
+    CatarseAnalytics.oneTimeEvent({cat:'contribution_finish',act:'contribution_cc_edit'});
+  },
+
   activate: function(){
     var that = this;
     $.get(this.$("#engine").data('path')).success(function(data){
@@ -13,4 +21,3 @@ App.addChild('Payment', {
   }
 
 });
-
