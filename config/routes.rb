6bbe7d74d0c0fedf3decfbe5152ca82199573269
Antônio Roberto Silva <forevertonny@gmail.com>
2016-05-30 11:13:16 -0300

    /flex redirects to crowdfunding.catarse.me [Fix #112494187]

--- config/routes.rb
+++ config/routes.rb
@@ -123,7 +123,7 @@ Catarse::Application.routes.draw do
   get "/new-admin" => 'high_voltage/pages#show', id: 'new_admin'
   get "/explore" => 'high_voltage/pages#show', id: 'explore'
   get "/team" => 'high_voltage/pages#show', id: 'team'
-  get "/flex" => 'high_voltage/pages#show', id: 'flex'
+  get '/flex' => redirect('http://crowdfunding.catarse.me')
   get "/projects_dashboard" => 'high_voltage/pages#show', id: 'projects_dashboard'
 
 
