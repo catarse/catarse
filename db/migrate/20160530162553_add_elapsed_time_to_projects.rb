f1164f0a9985775ef97bf20b6a4e94983b7351db
Adriano Benin <adriano.benin@gmail.com>
2016-05-31 11:57:14 -0300

    fix migration

--- db/migrate/20160530162553_add_elapsed_time_to_projects.rb
+++ db/migrate/20160530162553_add_elapsed_time_to_projects.rb
@@ -1,7 +1,7 @@
 class AddElapsedTimeToProjects < ActiveRecord::Migration
   def change
     execute <<-SQL
-    CREATE OR REPLACE VIEW "1".project AS
+    CREATE OR REPLACE VIEW "1".projects AS
     SELECT p.id AS project_id,
     p.category_id,
     p.name AS project_name,
@@ -14,7 +14,6 @@ class AddElapsedTimeToProjects < ActiveRecord::Migration
     p.recommended,
     thumbnail_image(p.*, 'large'::text) AS project_img,
     remaining_time_json(p.*) AS remaining_time,
-    elapsed_time_json(p.*) AS elapsed_time,
     p.expires_at,
     COALESCE(( SELECT
                 CASE
@@ -30,7 +29,8 @@ class AddElapsedTimeToProjects < ActiveRecord::Migration
     u.name AS owner_name,
     c.name AS city_name,
     p.full_text_index,
-    is_current_and_online(p.expires_at, p.state::text) AS open_for_contributions
+    is_current_and_online(p.expires_at, p.state::text) AS open_for_contributions,
+    elapsed_time_json(p.*) AS elapsed_time
    FROM projects p
      JOIN users u ON p.user_id = u.id
      JOIN cities c ON c.id = p.city_id
