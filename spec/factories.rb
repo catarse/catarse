92c43edae160c95761fde7f4a58a7530eab1d159
Adriano Benin <adriano.benin@gmail.com>
2016-06-03 16:18:56 -0300

    fix specs

--- spec/factories.rb
+++ spec/factories.rb
@@ -132,7 +132,6 @@ FactoryGirl.define do
     f.goal 10000
     f.online_days 5
     f.more_links 'Ipsum dolor'
-    f.first_contributions 'Foo bar'
     f.video_url 'http://vimeo.com/17298435'
     f.budget '1000'
     f.uploaded_image File.open("#{Rails.root}/spec/support/testimg.png")
