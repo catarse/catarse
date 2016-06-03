6f62f0c390f341c375bd34898fa465bee64aa334
Antônio Roberto Silva <forevertonny@gmail.com>
2016-06-03 10:10:46 -0300

    should validate presence of city and name size on both project modes

--- app/models/concerns/project/all_or_nothing_state_validator.rb
+++ app/models/concerns/project/all_or_nothing_state_validator.rb
@@ -5,12 +5,6 @@ module Project::AllOrNothingStateValidator
 
   included do
     with_options if: ->(x) { x.mode == 'aon' } do |wg| 
-      # Validation for online? only state
-      wg.with_options if: :online? do |wo|
-        wo.validates_presence_of :city
-        wo.validates_length_of :name, maximum: Project::NAME_MAXLENGTH
-      end
-
       # Start validations when project state
       # is included on ON_ANALYSIS_TO_END_STATE
       wg.with_options if: -> (x) { ( Project::ON_ANALYSIS_TO_END_STATES.include? x.state ) && ( x.mode == 'aon' ) } do |wo| 
