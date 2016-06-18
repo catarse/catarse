e1c8806cfc45550a82c11e0a90a4328c0a82d493
Antônio Roberto Silva <forevertonny@gmail.com>
2016-06-18 00:53:38 -0300

    use sendgrid_port conf

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -2,7 +2,7 @@ begin
   if Rails.env.production?
     ActionMailer::Base.smtp_settings = {
       address: 'smtp.sendgrid.net',
-      port: '587',
+      port: CatarseSettings.get_without_cache(:sendgrid_port),
       authentication: :plain,
       user_name: CatarseSettings.get_without_cache(:sendgrid_user_name),
       password: CatarseSettings.get_without_cache(:sendgrid),
