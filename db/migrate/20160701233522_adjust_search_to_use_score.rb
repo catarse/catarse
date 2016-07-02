022719e986ca7d931c16605dd7de9e3eeb609dd6
Antônio Roberto Silva <forevertonny@gmail.com>
2016-07-01 22:08:26 -0300

    adjust search to order by score

---
+++ db/migrate/20160701233522_adjust_search_to_use_score.rb
@@ -0,0 +1,29 @@
+class AdjustSearchToUseScore < ActiveRecord::Migration
+  def change
+    execute %{
+CREATE OR REPLACE FUNCTION "1".project_search(query text)
+ RETURNS SETOF "1".projects
+ LANGUAGE sql
+ STABLE
+AS $function$
+        SELECT
+            p.*
+        FROM
+            "1".projects p
+        WHERE
+            (
+                p.full_text_index @@ plainto_tsquery('portuguese', unaccent(query))
+                OR
+                p.project_name % query
+            )
+            AND p.state_order >= 'published'
+        ORDER BY
+            p.score DESC,
+            p.open_for_contributions DESC,
+            p.state_order,
+            ts_rank(p.full_text_index, plainto_tsquery('portuguese', unaccent(query))) DESC,
+            p.project_id DESC;
+     $function$;
+    }
+  end
+end
