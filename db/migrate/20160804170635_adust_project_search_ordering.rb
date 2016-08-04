f7051769448c49d25a2764d27923ffdcbe020b90
Antônio Roberto Silva <forevertonny@gmail.com>
2016-08-04 14:09:44 -0300

    use nulls last to order online projects at first on search result [Fix #125505161]

---
+++ db/migrate/20160804170635_adust_project_search_ordering.rb
@@ -0,0 +1,58 @@
+class AdustProjectSearchOrdering < ActiveRecord::Migration
+  def up
+    %Q{
+CREATE OR REPLACE FUNCTION "1".project_search(query text)
+ RETURNS SETOF "1".projects
+ LANGUAGE sql
+ STABLE
+AS $function$
+        SELECT
+            p.*
+        FROM
+            "1".projects p
+        WHERE
+            (
+                p.full_text_index @@ plainto_tsquery('portuguese', unaccent(query))
+                OR
+                p.project_name % query
+            )
+            AND p.state_order >= 'published'
+        ORDER BY
+            p.score DESC NULLS LAST,
+            p.open_for_contributions DESC,
+            p.state_order,
+            ts_rank(p.full_text_index, plainto_tsquery('portuguese', unaccent(query))) DESC,
+            p.project_id DESC;
+     $function$
+    }
+
+  end
+
+  def down
+    %Q{
+CREATE OR REPLACE FUNCTION "1".project_search(query text)
+ RETURNS SETOF "1".projects
+ LANGUAGE sql
+ STABLE
+AS $function$
+        SELECT
+            p.*
+        FROM
+            "1".projects p
+        WHERE
+            (
+                p.full_text_index @@ plainto_tsquery('portuguese', unaccent(query))
+                OR
+                p.project_name % query
+            )
+            AND p.state_order >= 'published'
+        ORDER BY
+            p.score DESC,
+            p.open_for_contributions DESC,
+            p.state_order,
+            ts_rank(p.full_text_index, plainto_tsquery('portuguese', unaccent(query))) DESC,
+            p.project_id DESC;
+     $function$
+    }
+  end
+end
