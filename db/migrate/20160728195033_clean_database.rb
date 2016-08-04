2922fdea95e2a479469cb25a20b50be2657e84a4
Adriano Benin <adriano.benin@gmail.com>
2016-08-04 12:04:23 -0300

    simplify state_order function

--- db/migrate/20160728195033_clean_database.rb
+++ db/migrate/20160728195033_clean_database.rb
@@ -1,34 +1,14 @@
 class CleanDatabase < ActiveRecord::Migration
   def change
     execute <<-SQL
-
   CREATE  OR REPLACE FUNCTION state_order(project_id integer) RETURNS project_state_order
       LANGUAGE sql STABLE
       AS $_$
-  SELECT
-  CASE p.mode
-  WHEN 'flex' THEN
-      (
       SELECT state_order
-      FROM
-      public.project_states ps
-      WHERE
-      ps.state = p.state
-      )
-  ELSE
-      (
-      SELECT state_order
-      FROM
-      public.project_states ps
-      WHERE
-      ps.state = p.state
-      )
-  END
-  FROM public.projects p
-  WHERE p.id = $1;
+      FROM projects p
+      JOIN project_states ps ON p.state = ps.state
+      where p.id = $1;
   $_$;
-
-
 
    drop table flexible_project_transitions;
    drop table flexible_projects;
