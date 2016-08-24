84eaa439c6251e564eca9a34a9a5ad6efd22e32a
Antônio Roberto Silva <forevertonny@gmail.com>
2016-08-24 09:08:39 -0300

    setup db pool on sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,6 +2,11 @@ require 'sidekiq/api'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV["REDIS_URL"] || "redis://localhost:6379/" }
+
+  database_url = ENV['DATABASE_URL']
+  if database_url
+    ActiveRecord::Base.establish_connection("#{database_url}?pool=#{ENV['DB_POOL']}")
+  end
 end
 
 Sidekiq.configure_client do |config|
