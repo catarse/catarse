642630fc98e4bae7ae3f81a57183bbe25da33195
Adriano Benin <adriano.benin@gmail.com>
2016-09-01 10:50:34 -0300

    only allow numbers on account number field [#126158201]

--- app/assets/javascripts/app/project/project_dashboard_user.js
+++ app/assets/javascripts/app/project/project_dashboard_user.js
@@ -51,11 +51,32 @@ App.addChild('DashboardUserSettingsForm', _.extend({
 
   events: {
     'blur input' : 'checkInput',
-    'click input[type="submit"]' : 'validate'
+    'click input[type="submit"]' : 'validate',
+    "keydown #project_account_attributes_account": "allowOnlyNumbers"
   },
 
   activate: function(){
     this.setupForm();
+  },
+
+  allowOnlyNumbers: function (e) {
+    // Allow: backspace, delete, tab, escape and enter
+    if ($.inArray(e.keyCode, [ 8, 9, 27, 13, 110]) !== -1 ||
+       // Allow: Ctrl+A
+      (e.keyCode == 65 && e.ctrlKey === true) ||
+       // Allow: Ctrl+C
+      (e.keyCode == 67 && e.ctrlKey === true) ||
+       // Allow: Ctrl+X
+      (e.keyCode == 88 && e.ctrlKey === true) ||
+       // Allow: home, end, left, right
+      (e.keyCode >= 35 && e.keyCode <= 39)) {
+           // let it happen, don't do anything
+           return;
+    }
+    // Ensure that it is a number and stop the keypress
+    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+      e.preventDefault();
+    }
   }
 
 }, Skull.Form));
