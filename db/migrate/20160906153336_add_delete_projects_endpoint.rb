01c928393d22d0616fe94f66f8cfa70ada234b73
Adriano Benin <adriano.benin@gmail.com>
2016-09-13 12:03:32 -0300

    add generate_project_full_text_index

--- db/migrate/20160906153336_add_delete_projects_endpoint.rb
+++ db/migrate/20160906153336_add_delete_projects_endpoint.rb
@@ -1,6 +1,26 @@
 class AddDeleteProjectsEndpoint < ActiveRecord::Migration
   def change
     execute <<-SQL
+
+    CREATE OR REPLACE FUNCTION public.generate_project_full_text_index(project public.projects) RETURNS tsvector
+    LANGUAGE plpgsql
+    STABLE
+    AS $$
+        DECLARE
+            full_text_index tsvector;
+        BEGIN
+
+            full_text_index :=  setweight(to_tsvector('portuguese', unaccent(coalesce(project.name::text, ''))), 'A') || 
+                                setweight(to_tsvector('portuguese', unaccent(coalesce(project.permalink::text, ''))), 'C') || 
+                                setweight(to_tsvector('portuguese', unaccent(coalesce(project.headline::text, ''))), 'B') || 
+                                setweight(to_tsvector('portuguese', unaccent(coalesce((SELECT c.name_pt FROM categories c WHERE c.id = project.category_id)::text, ''))), 'B') || 
+                                setweight(to_tsvector('portuguese', unaccent(coalesce((select array_agg(t.name)::text from public.taggings ta join public_tags t on t.id = ta.public_tag_id where ta.project_id = project.id)::text, ''))), 'B') || 
+                                setweight(to_tsvector('portuguese', unaccent(coalesce((SELECT u.name FROM users u WHERE u.id = project.user_id)::text, ''))), 'C');
+
+          RETURN full_text_index;
+        END
+    $$;
+
     CREATE OR REPLACE FUNCTION update_full_text_index() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
@@ -11,6 +31,7 @@ class AddDeleteProjectsEndpoint < ActiveRecord::Migration
       RETURN NEW;
     END;
     $$;
+
 
 
       CREATE OR REPLACE FUNCTION "1".delete_project(_project_id integer) RETURNS void
