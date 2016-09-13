c4501aee9bdbe827c2546386ec149499a5886210
Adriano Benin <adriano.benin@gmail.com>
2016-09-13 10:25:25 -0300

    fix permissions

--- db/migrate/20160906153336_add_delete_projects_endpoint.rb
+++ db/migrate/20160906153336_add_delete_projects_endpoint.rb
@@ -4,7 +4,15 @@ class AddDeleteProjectsEndpoint < ActiveRecord::Migration
       CREATE OR REPLACE FUNCTION "1".delete_project(_project_id integer) RETURNS void
           LANGUAGE plpgsql
           AS $$
-              begin
+            declare
+                v_project public.projects;
+            begin
+                select * from public.projects where id = _project_id into v_project;
+
+                if _project_id is null or not public.is_owner_or_admin(v_project.user_id) or v_project.state <> 'draft' then
+                    raise exception 'invalid project permission';
+                end if;
+
                 update project_transitions pt set most_recent = false where pt.project_id = _project_id;
                 insert into public.project_transitions (to_state, metadata, sort_key, project_id, most_recent, created_at, updated_at) 
                 values ('deleted', '{"to_state":"deleted", "from_state":' || (select p.state from projects p where id = _project_id) || '}', 0, _project_id, true, current_timestamp, current_timestamp);
