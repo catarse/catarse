c97101939285a5dfbe05e43042866334bcbd5827
Antônio Roberto Silva <forevertonny@gmail.com>
2016-09-27 12:17:01 -0300

    failed project can go to trash too :P [Fix #131118627]

--- app/state_machine/aon_project_machine.rb
+++ app/state_machine/aon_project_machine.rb
@@ -5,6 +5,7 @@ class AonProjectMachine < FlexProjectMachine
     transition from: :draft, to: %i(rejected deleted online)
     transition from: :online, to: %i(draft rejected deleted waiting_funds successful failed)
     transition from: :waiting_funds, to: %i(successful failed)
+    transition from: :failed, to: %i(deleted)
 
     guard_transition(to: :failed) do |project|
       !project.reached_goal?
