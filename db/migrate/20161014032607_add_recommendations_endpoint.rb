54cb505dd4f4644436f9ec9e489417ae50a69d0d
Vinicius Andrade <vini.andrade.dev@gmail.com>
2016-10-18 10:54:43 -0200

    Fixes view permissions

--- db/migrate/20161014032607_add_recommendations_endpoint.rb
+++ db/migrate/20161014032607_add_recommendations_endpoint.rb
@@ -2,9 +2,10 @@ class AddRecommendationsEndpoint < ActiveRecord::Migration
   def up
     execute <<-SQL
     CREATE OR REPLACE VIEW "1".recommended_projects AS
-    SELECT   recommendations.user_id,
-         recommendations.project_id,
-         Sum(recommendations.count)::bigint AS count
+    SELECT
+        recommendations.user_id,
+        recommendations.project_id,
+        Sum(recommendations.count)::bigint AS count
     FROM     (
                       SELECT   b.user_id,
                                recommendations_1.id                 AS project_id,
@@ -16,7 +17,8 @@ class AddRecommendationsEndpoint < ActiveRecord::Migration
                       ON       recommenders.user_id = backers_same_projects.user_id
                       JOIN     projects recommendations_1
                       ON       recommendations_1.id = recommenders.project_id
-                      WHERE    was_confirmed(b.*)
+                      WHERE    public.is_owner_or_admin(b.user_id)
+                      AND      was_confirmed(b.*)
                       AND      was_confirmed(backers_same_projects.*)
                       AND      was_confirmed(recommenders.*)
                       AND      b.updated_at > (now()            - '6 mons'::interval)
@@ -56,7 +58,7 @@ class AddRecommendationsEndpoint < ActiveRecord::Migration
     GROUP BY recommendations.user_id,
              recommendations.project_id
     ORDER BY sum(recommendations.count)::bigint DESC;
-    GRANT SELECT ON "1".recommended_projects to admin, web_user, anonymous;
+    GRANT SELECT ON "1".recommended_projects to admin, web_user;
     SQL
   end
   def down
