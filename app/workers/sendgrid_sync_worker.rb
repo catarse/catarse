028e46f26738f73cbea1b4ebf77b5e4bf929c8b2
Antônio Roberto <forevertonny@gmail.com>
2016-11-30 12:50:19 -0200

    Added update / create on sendgrid sync worker
    Basic code to worker update / find / create a recipient for user

--- app/workers/sendgrid_sync_worker.rb
+++ app/workers/sendgrid_sync_worker.rb
@@ -1,4 +1,69 @@
 class SendgridSyncWorker
+  attr_accessor :user
   include Sidekiq::Worker
   sidekiq_options queue: 'actions'
+
+  def perform user_id
+    @user = User.find user_id
+
+    if user.sendgrid_recipient_id.present?
+      update_recipient
+    else
+      find_or_create_recipient
+    end
+
+    # TODO: add support to remove and put recipient on newsletter list
+    # remove_from_newsletter if recipient_on_newsletter? && !user.newsletter
+    # put_on_newsletter if !recipient_on_newsletter? && user.newsletter
+  end
+
+  private
+
+  def find_or_create_recipient
+    if search_recipient.present?
+      user.update_column(:sendgrid_recipient_id, search_recipient[:id])
+    else
+      persisted_recipient = create_recipient
+      user.update_column(:sendgrid_recipient_id, persisted_recipient)
+    end
+  end
+
+  def update_recipient
+    params = { request_body: [prepare_user_to_sendgrid] }
+    parse_recipients_from_response sendgrid_recipients.patch(params)
+  end
+
+  def create_recipient
+    params = { request_body: [prepare_user_to_sendgrid] }
+    parse_recipients_from_response sendgrid_recipients.post(params)
+  end
+
+  def search_recipient
+    params = { email: user.email }
+    response = sendgrid_recipients.search.get(query_params: params)
+    JSON.parse(response.body).try(:[], 'recipients').try(:first).try(:deep_symbolize_keys)
+  end
+
+  def parse_recipients_from_response response
+    JSON.parse(response.body).try(:[], 'persisted_recipients').first
+  end
+
+  def prepare_user_to_sendgrid
+    name_mask = user.name.split ' '
+    {
+      email: user.email,
+      last_name: name_mask.pop,
+      first_name: name_mask.join(' '),
+      'Cidade': user.address_city,
+      'Estado': user.address_state
+    }
+  end
+
+  def sendgrid
+    @sendgrid ||= SendGrid::API.new(api_key: CatarseSettings[:sendgrid_mkt_api_key])
+  end
+
+  def sendgrid_recipients
+    sendgrid.client.contactdb.recipients
+  end
 end
--- spec/workers/sendgrid_sync_worker_spec.rb
+++ spec/workers/sendgrid_sync_worker_spec.rb
@@ -1,4 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe SendgridSyncWorker do
+  let(:user) { create(:user) }
+  let(:srmock) do
+    double(
+      post: double( body: { persisted_recipients: ['xxx'] }.to_json ),
+      patch: double( body: { persisted_recipients: ['xxx'] }.to_json ),
+      search: double(
+        get: double(
+          body: {
+            recipients: []
+          }.to_json
+        )
+      )
+    )
+  end
+
+
+  before do
+    CatarseSettings[:sendgrid_mkt_api_key] = 'key'
+    Sidekiq::Testing.inline!
+    allow(User).to receive(:find).with(user.id).and_return(user)
+    allow_any_instance_of(SendgridSyncWorker).to receive(:sendgrid_recipients).and_return(srmock)
+  end
+
+  subject { SendgridSyncWorker.new }
+
+  describe 'when user sendgrid recipient id is null' do
+    describe 'when user exists on sendgrid recipients' do
+      before do
+        allow(subject).to receive(:search_recipient).and_return({id: 'xxx'})
+        expect(subject).to_not receive(:create_recipient).and_call_original
+        expect(user).to receive(:update_column).with(:sendgrid_recipient_id, 'xxx')
+      end
+
+      it { subject.perform(user.id) }
+    end
+
+    describe 'when user does not exists on sendgrid recipients' do
+      before do
+        expect(subject).to receive(:find_or_create_recipient).and_call_original
+        expect(subject).to receive(:create_recipient).and_call_original
+        expect(user).to receive(:update_column).with(:sendgrid_recipient_id, 'xxx')
+      end
+
+      it { subject.perform(user.id) }
+    end
+  end
+
+  describe 'when user sengrid recipient id aleady filled' do
+    describe 'just refreshing' do
+      before do
+        allow(user).to receive(:sendgrid_recipient_id).and_return('xxx')
+        expect(subject).to receive(:update_recipient)
+      end
+
+      it { subject.perform(user.id) }
+    end
+  end
 end
