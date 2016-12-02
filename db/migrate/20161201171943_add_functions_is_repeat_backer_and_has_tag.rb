544646994653b6757dc911acc335ba2fba9917a7
Bruno Grossi <bruno@grossi.com.br>
2016-12-02 10:05:48 -0200

    Changing has_tag to has_admin_tag

--- db/migrate/20161201171943_add_functions_is_repeat_backer_and_has_tag.rb
+++ db/migrate/20161201171943_add_functions_is_repeat_backer_and_has_tag.rb
@@ -8,7 +8,7 @@ $BODY$SELECT EXISTS (
     ON c.id<>$1.id and c.user_id=$1.user_id and p.contribution_id=c.id and p.state=ANY(confirmed_states()) AND c.created_at<$1.created_at LIMIT 1
 );$BODY$
 LANGUAGE sql;
-CREATE OR REPLACE FUNCTION public.has_tag(projects, text) RETURNS boolean AS
+CREATE OR REPLACE FUNCTION public.has_admin_tag(projects, text) RETURNS boolean AS
 $BODY$SELECT EXISTS (
   SELECT true FROM taggings tgg JOIN tags t ON tgg.project_id=$1.id AND tgg.tag_id=t.id AND t.slug=$2 LIMIT 1
 );$BODY$
