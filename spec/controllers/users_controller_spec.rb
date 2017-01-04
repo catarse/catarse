3ecc75e99a57f68c2faf53f084c185cf3fa24fa3
Adriano Benin <adriano.benin@gmail.com>
2017-01-04 13:53:18 -0200

    remove outdated spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -222,7 +222,6 @@ RSpec.describe UsersController, type: :controller do
       context "with wrong current password" do
         let(:current_password){ 'wrong_password' }
         it{ expect(user.errors).not_to be_nil }
-        it{ is_expected.not_to redirect_to edit_user_path(user) }
       end
 
       context "with right current password and right confirmation" do
