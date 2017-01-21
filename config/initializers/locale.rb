ee3dafb20005924ab9fb46d705406f8058fcc0bb
sushant12 <sussyoung9@gmail.com>
2017-01-21 12:38:30 +0545

    translated header menu, and homepage

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -6,4 +6,4 @@ I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 I18n.available_locales = [:pt, :en]
 
 # set default locale to something other than :en
-I18n.default_locale = :pt
+I18n.default_locale = :en
