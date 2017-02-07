6f3806374023c840e850e09f1b5d36d550af5683
sushant12 <sussyoung9@gmail.com>
2017-02-07 20:10:21 +0545

    disable compile asset to show images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Catarse::Application.configure do
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
