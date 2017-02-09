907aaaf8e55ad64aee5e7149995870b92718a60b
Antônio Roberto <forevertonny@gmail.com>
2017-02-09 08:56:30 -0200

    use alias weekdays_from when payment id is nil

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -23,7 +23,7 @@ class Payment < ActiveRecord::Base
   def slip_expiration_date
     # If payment does not exist gives expiration date based on current_timestamp
     if self.id.nil?
-      self.class.connection.select_one("SELECT public.weekdays_from(public.slip_expiration_weekdays(), current_timestamp::timestamp) at time zone 'America/Sao_Paulo'")['weekdays_from'].try(:to_datetime);
+      self.class.connection.select_one("SELECT public.weekdays_from(public.slip_expiration_weekdays(), current_timestamp::timestamp) at time zone 'America/Sao_Paulo' as weekdays_from")['weekdays_from'].try(:to_datetime)
     else
       pluck_from_database("slip_expires_at at time zone 'America/Sao_Paulo'").try(:to_datetime)
     end
