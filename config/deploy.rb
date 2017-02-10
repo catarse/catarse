8df277d5e7570f2821a6cca65a7085ebf7b8a81e
sushant12 <sussyoung9@gmail.com>
2017-02-10 20:10:40 +0545

    removed the bin directory from linked_dirs because I was not able to run rails console

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@ set :branch, :master
 set :deploy_to, '/home/deploy/jvn'
 set :pty, true
 set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
 set :rvm_ruby_version, 'ruby-2.3.1' # Edit this if you are using MRI Ruby
