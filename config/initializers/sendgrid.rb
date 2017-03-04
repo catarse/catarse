ef50d79341ebb265ae6ad6bcb2e2fa77ab23b49f
sushant12 <sussyoung9@gmail.com>
2017-03-04 08:58:04 +0545

    updated domain for sendgrid setting

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -6,7 +6,7 @@ begin
       authentication: :plain,
       user_name: CatarseSettings.get_without_cache(:sendgrid_user_name),
       password: CatarseSettings.get_without_cache(:sendgrid),
-      domain: 'heroku.com'
+      domain: 'myjvn.com'
     }
     ActionMailer::Base.delivery_method = :smtp
   end
