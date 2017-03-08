6fd7a7762cecff2a15c9e963f4c31cdf6cbcadba
Antônio Roberto <forevertonny@gmail.com>
2017-03-08 20:09:59 -0300

    only validate birth_date when user account_type is pf

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,7 @@ class User < ActiveRecord::Base
   mount_uploader :cover_image, CoverUploader
 
  validates :bank_account, :name, :about_html, :public_name, :cpf, :address_zip_code, :phone_number, :address_state, :country_id, :address_city, :address_street, :address_number, :address_neighbourhood, presence: true, if: -> (user) { user.published_projects.present? || user.publishing_project }
- validates :birth_date,  presence: true, if: -> (user) { user.publishing_project }
+ validates :birth_date,  presence: true, if: -> (user) { user.publishing_project && user.account_type == 'pf' }
 
 
   validates_presence_of :email
