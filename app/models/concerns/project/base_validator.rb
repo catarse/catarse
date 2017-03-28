91fde1d65f708c986909ede2a9d310a6ede76c8d
sushant12 <sussyoung9@gmail.com>
2017-03-28 19:29:17 +0545

    disabled brazil bank payment validator and use jvn platform fee

--- app/models/concerns/project/base_validator.rb
+++ app/models/concerns/project/base_validator.rb
@@ -20,25 +20,25 @@ module Project::BaseValidator
       wo.validates_presence_of :uploaded_image,
         unless: ->(project) { project.video_thumbnail.present? }
 
-      wo.validate do
-        # unless self.user.bank_account
-        #   self.errors.add(:bank_account, 'Invalid bank details')
-        # end
-
-        self.user.publishing_project = true
-        self.user.valid?
-
-        if self.user.bank_account
-          self.user.bank_account.valid?
-        else
-          self.user.build_bank_account
-          self.user.bank_account.valid?
-        end
-
-        self.user.errors.each do |error, error_message|
-          self.errors.add('user.' + error.to_s, error_message)
-        end
-      end
+      # wo.validate do
+      #   unless self.user.bank_account
+      #     self.errors.add(:bank_account, 'Invalid bank details')
+      #   end
+      #
+      #   self.user.publishing_project = true
+      #   self.user.valid?
+      #
+      #   if self.user.bank_account
+      #     self.user.bank_account.valid?
+      #   else
+      #     self.user.build_bank_account
+      #     self.user.bank_account.valid?
+      #   end
+      #
+      #   self.user.errors.each do |error, error_message|
+      #     self.errors.add('user.' + error.to_s, error_message)
+      #   end
+      # end
 
       # wo.validates_presence_of :budget
 
