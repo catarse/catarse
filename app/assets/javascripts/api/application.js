36f08d4b1fd31a66c14f26f9397c711754ff295a
sushant12 <sussyoung9@gmail.com>
2017-04-23 11:39:50 +0545

    using jvnjs 1.0.32

--- app/assets/javascripts/api/application.js
+++ app/assets/javascripts/api/application.js
@@ -8,9 +8,8 @@
 //= require i18n/translations
 //= require ../analytics
 //= require api/init
-//= require catarse.js/dist/catarse.js
+//= require jvnjs/dist/catarse.js
 //= require_self
-
 (function(m, c, Chart, analytics){
   //Chart.defaults.global.responsive = true;
   Chart.defaults.global.responsive = false;
