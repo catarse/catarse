4a72255b7f02d49bf6f2d5c1206b32e08e5f484f
Antônio Roberto <forevertonny@gmail.com>
2017-05-04 14:38:38 -0300

    Adjust new transitions on balance transfers machine

--- app/state_machine/balance_transfer_machine.rb
+++ app/state_machine/balance_transfer_machine.rb
@@ -6,16 +6,18 @@ class BalanceTransferMachine
   state :processing
   state :transferred
   state :error
+  state :gateway_error
   state :rejected
 
   transition from: :pending, to: %i(authorized rejected)
-  transition from: :authorized, to: %i(error rejected processing pending)
+  transition from: :authorized, to: %i(gateway_error error rejected processing pending)
   transition from: :processing, to: %i(error transferred)
-  transition from: :error, to: %i(authorized)
+  transition from: :gateway_error, to: %i(processing transferred error)
 
   after_transition(from: :pending, to: :authorized) do |bt|
     #bt.pagarme_delegator.transfer_funds
-    bt.refresh_project_amount if bt.project && bt.project_amount_changed?
+    # TODO: should refresh giving another balance transaction if has diff
+    #bt.refresh_project_amount if bt.project && bt.project_amount_changed?
   end
 
   after_transition(from: :processing, to: :transferred) do |bt| 
@@ -25,6 +27,10 @@ class BalanceTransferMachine
   end
 
   after_transition(to: :error) do |bt| 
+    bt.refund_balance
+  end
+
+  after_transition(to: :rejected) do |bt|
     bt.refund_balance
   end
 end
