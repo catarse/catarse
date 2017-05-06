faedc500ce57659f9c1711eb27e54576b204c8cc
sushant12 <sussyoung9@gmail.com>
2017-05-06 13:09:16 +0545

    fixed the email subscription again, updated the banner txt,link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -108,8 +108,9 @@ class ApplicationController < ActionController::Base
     email = params['EMAIL']
     if email =~ EMAIL_REGEX
       list_id = CatarseSettings[:sendgrid_newsletter_list_id]
-      client = sendgrid_api.client.contactdb.recipients
-      rr = client.post(request_body: [{email: email}])
+      client = sendgrid_api.client
+
+      rr = client.contactdb.recipients.post(request_body: [{email: email}])
       recipient = JSON.parse(rr.body).try(:[], 'persisted_recipients').try(:first)
       client.contactdb.lists._(list_id).recipients.post(request_body: [recipient])
     end
