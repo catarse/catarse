06f6c949b8d3c0dfc2fa7f3a2b8ce9079272368c
Adriano Benin <adriano.benin@gmail.com>
2017-05-10 11:20:18 -0300

    add more relations

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,5 +1,6 @@
 class Survey < ActiveRecord::Base
   belongs_to :reward
   has_many :survey_open_questions
+  has_many :survey_multiple_choice_questions
 
 end
--- app/models/survey_open_question_answer.rb
+++ app/models/survey_open_question_answer.rb
@@ -1,4 +1,5 @@
 class SurveyOpenQuestionAnswer < ActiveRecord::Base
   belongs_to :survey_open_question
+  belongs_to :user
 
 end
