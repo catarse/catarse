3cc223c4f1d5e8c2bfcf9b85b3885e7c90e73f06
sushant12 <sussyoung9@gmail.com>
2017-05-12 09:58:43 +0545

    using local as activerecord timezone and using grasruts email to send forgot passowrd no tificaiotn

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "Catarse <system@catarse.me>"
+  config.mailer_sender = "Grasruts <contact@grasruts.com>"
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
