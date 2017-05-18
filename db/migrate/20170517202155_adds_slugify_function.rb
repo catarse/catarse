f572fa38bef4f7be4dfd9d7b0491e4bb8da91065
Bruno Grossi <bruno@grossi.com.br>
2017-05-18 13:41:37 -0300

    fix

--- db/migrate/20170517202155_adds_slugify_function.rb
+++ db/migrate/20170517202155_adds_slugify_function.rb
@@ -1,23 +1,13 @@
-class AlterTagsEndpoint < ActiveRecord::Migration
+class AddsSlugifyFunction < ActiveRecord::Migration
   def up
     execute <<-SQL
-    CREATE OR REPLACE FUNCTION "1".tag_search(query text, count integer)
-      RETURNS SETOF "1".public_tags AS
+    CREATE OR REPLACE FUNCTION public.slugify(val text)
+    RETURNS text AS
     $BODY$
-        SELECT t.*
-        FROM "1".public_tags t
-        WHERE  query is not null and query<>'' and (
-              t.name % query
-              OR
-              t.slug % query
-              OR
-              t.slug % regexp_replace(regexp_replace(lower(unaccent(query)),'[^a-z0-9]+','-','g'),'^-|-$','','g')
-        )
-        ORDER BY uses DESC, name ASC
-        LIMIT COALESCE(count, 10)
-     $BODY$
-  LANGUAGE sql STABLE;
-      GRANT SELECT ON "1".public_tags TO anonymous, web_user, admin;
+        SELECT regexp_replace(regexp_replace(lower(unaccent(val)),'[^a-z0-9]+','-','g'),'^-|-$','','g');
+    $BODY$
+    LANGUAGE sql IMMUTABLE;
+    GRANT SELECT ON "1".public_tags TO anonymous, web_user, admin;
     SQL
   end
   def down
