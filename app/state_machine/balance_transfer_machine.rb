1894e8138491160015ee2ff14216a58b0dab66b3
Antônio Roberto <forevertonny@gmail.com>
2017-05-18 13:38:14 -0300

    adjust balance transfer transitions

--- app/state_machine/balance_transfer_machine.rb
+++ app/state_machine/balance_transfer_machine.rb
@@ -11,7 +11,7 @@ class BalanceTransferMachine
 
   transition from: :pending, to: %i(authorized rejected)
   transition from: :authorized, to: %i(gateway_error error rejected processing pending)
-  transition from: :processing, to: %i(error transferred)
+  transition from: :processing, to: %i(error transferred gateway_error)
   transition from: :gateway_error, to: %i(processing transferred error)
 
   after_transition(from: :processing, to: :transferred) do |bt| 
