1d1eda65fd849d98da20eb5333e1623f49e75459
sushant12 <sussyoung9@gmail.com>
2017-05-19 12:18:48 +0545

    fixing memory bloat

--- config/puma.rb
+++ config/puma.rb
@@ -45,3 +45,20 @@ preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+
+before_fork do
+  require 'puma_worker_killer'
+
+  PumaWorkerKiller.config do |config|
+	  config.ram           = 1024 # mb
+	  config.frequency     = 5    # seconds
+	  config.percent_usage = 0.98
+	  config.rolling_restart_frequency = 12 * 3600 # 12 hours in seconds
+	  config.reaper_status_logs = true # setting this to false will not log lines like:
+	  # PumaWorkerKiller: Consuming 54.34765625 mb with master and 2 workers.
+
+	  config.pre_term = -> (worker) { puts "Worker #{worker.inspect} being killed" }
+  end
+  PumaWorkerKiller.start
+end
