f674acc167d84b10b4a87d349ed6dc3a2b64d1d6
Adriano Benin <adriano.benin@gmail.com>
2017-08-08 13:30:54 -0300

    validate shipping fee value

--- app/models/concerns/contribution/custom_validators.rb
+++ app/models/concerns/contribution/custom_validators.rb
@@ -15,7 +15,9 @@ module Contribution::CustomValidators
 
     def value_must_be_at_least_rewards_value
       return unless reward
-      errors.add(:value, I18n.t('contribution.value_must_be_at_least_rewards_value', minimum_value: reward.display_minimum)) unless value.to_f >= reward.minimum_value
+      value_to_validate = reward.minimum_value
+      value_to_validate += shipping_fee.value if reward.shipping_fees.present?
+      errors.add(:value, I18n.t('contribution.value_must_be_at_least_rewards_value', minimum_value: reward.display_minimum)) unless value.to_f >= value_to_validate
     end
 
     def should_not_contribute_if_maximum_contributions_been_reached
