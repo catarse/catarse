79a3b70fbeb0c74b64fc63a7d6ae4b925931ebd3
Antônio Roberto <forevertonny@gmail.com>
2017-08-30 11:32:01 -0300

    Adjust specs

--- spec/observers/user_observer_spec.rb
+++ spec/observers/user_observer_spec.rb
@@ -4,7 +4,9 @@ require 'rails_helper'
 
 RSpec.describe UserObserver do
   describe 'after_create' do
+    let(:list) { create(:mail_marketing_list)}
     before do
+      CatarseSettings[:sendgrid_newsletter_list_id] = list.list_id
       expect_any_instance_of(UserObserver).to receive(:after_create).and_call_original
     end
 
@@ -31,12 +33,24 @@ RSpec.describe UserObserver do
   context 'after_save' do
     subject { create(:user, newsletter: false, facebook_link: '') }
 
-    context 'when user change the newsletter option' do
+    context 'when user not have marketing lists unsynced' do
+      let(:list) { create(:mail_marketing_list) }
       before do
-        expect(SendgridSyncWorker).to receive(:perform_async).with(subject.id)
+        expect(SendgridSyncWorker).not_to receive(:perform_async).with(subject.id)
+        subject.mail_marketing_users.create(mail_marketing_list: list, last_sync_at: DateTime.now) 
       end
 
-      it { subject.update_attribute(:newsletter, true) }
+      it { subject.save! }
+    end
+
+    context 'when user has marketing lists unsynced' do
+      let(:list) { create(:mail_marketing_list) }
+      before do
+        expect(SendgridSyncWorker).to receive(:perform_async).with(subject.id)
+        subject.mail_marketing_users.create(mail_marketing_list: list)
+      end
+
+      it { subject.save! }
     end
   end
 end
