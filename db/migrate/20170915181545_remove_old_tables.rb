031f5fa0f2c6b0e1d66890dec57d3b64e9ac5945
Adriano Benin <adriano.benin@gmail.com>
2017-10-02 17:06:45 -0300

    fix migration

--- db/migrate/20170915181545_remove_old_tables.rb
+++ db/migrate/20170915181545_remove_old_tables.rb
@@ -1,9 +1,9 @@
 class RemoveOldTables < ActiveRecord::Migration
   def change
     execute <<-SQL
-      DROP VIEW  IF EXISTS flexible_project_transitions ;
+      DROP TABLE IF EXISTS flexible_project_transitions ;
 
-      DROP VIEW  IF EXISTS flexible_projects ;
+      DROP TABLE IF EXISTS flexible_projects ;
     SQL
   end
 end
