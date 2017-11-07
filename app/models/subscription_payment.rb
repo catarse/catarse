c7c422f907714f80de1812db0143daf9e463891e
Antônio Roberto <forevertonny@gmail.com>
2017-11-07 17:20:11 -0200

    Added method to check if subscription_payment already in balance

--- app/models/subscription_payment.rb
+++ app/models/subscription_payment.rb
@@ -2,4 +2,7 @@ class SubscriptionPayment < ActiveRecord::Base
   belongs_to :subscription
   has_many :balance_transactions
 
+  def already_in_balance?
+    balance_transactions.where(event_name: %i[subscription_fee subscription_payment]).present?
+  end
 end
