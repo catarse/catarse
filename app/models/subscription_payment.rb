8d59097dae456d1d46c857de5b465cde326d3e11
Antônio Roberto <forevertonny@gmail.com>
2017-12-06 00:20:42 -0200

    Added relations to subscription_payment

--- app/models/subscription_payment.rb
+++ app/models/subscription_payment.rb
@@ -1,5 +1,8 @@
 class SubscriptionPayment < ActiveRecord::Base
   self.table_name = 'common_schema.catalog_payments'
+  belongs_to :user, primary_key: :common_id
+  belongs_to :project, primary_key: :common_id
+  belongs_to :reward, primary_key: :common_id
   belongs_to :subscription
   has_many :balance_transactions, foreign_key: :subscription_payment_uuid
 
