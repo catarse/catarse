df6668dc5c83545c55bb7463594b1da8401647c7
Adriano Benin <adriano.benin@gmail.com>
2018-07-25 16:57:58 -0300

    fix weekday_from function

---
+++ db/migrate/20180725195716_fix_weekdays_from_function.rb
@@ -0,0 +1,18 @@
+class FixWeekdaysFromFunction < ActiveRecord::Migration
+  def change
+    execute <<-SQL
+    CREATE OR REPLACE FUNCTION public.weekdays_from(weekdays integer, from_ts timestamp without time zone)
+     RETURNS timestamp without time zone
+     LANGUAGE sql
+     STABLE
+    AS $function$
+            SELECT day FROM (
+              SELECT day
+              FROM generate_series(from_ts + '1 day'::interval, from_ts + '1 year'::interval, '1 day') day
+              WHERE extract(dow from day) not in (0,6)
+              ORDER BY day
+            ) a LIMIT 1 OFFSET (weekdays - 1);
+            $function$
+    SQL
+  end
+end
