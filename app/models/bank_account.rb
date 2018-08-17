46de702141c7d0c3f3e9d1264a7762292509cac0
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2018-08-17 11:45:10 -0300

    added bank account custom validations for correct errors response #159584273
    Signed-off-by: Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -8,34 +8,116 @@ class BankAccount < ActiveRecord::Base
   belongs_to :user
   belongs_to :bank
 
-  validates :bank_id, :agency, :account, :account_digit, :account_type, presence: true
+  validates :bank_id, :account_digit, :account_type, presence: true
   validates :account_type, inclusion: { in: %w[conta_corrente conta_poupanca conta_corrente_conjunta conta_poupanca_conjunta] }
 
   attr_accessor :input_bank_number
   validate :input_bank_number_validation
-  validates :agency, length: { is: 4 }, if: :bank_code_in_validation_table?
-  validates :account_digit, length: { is: 1 }, if: :bank_code_in_validation_table?
   validates :agency_digit, length: { is: 1 }, if: ->(ba) {
     %w[237 001].include?(ba.bank_code.to_s)
   }
-  validates :account, length: { maximum: 7 }, if: ->(ba) {
-    %w[237].include?(ba.bank_code.to_s)
-  }
-  validates :account, length: { maximum: 8 }, if: ->(ba) {
-    %w[001 033].include?(ba.bank_code.to_s)
-  }
-  validates :account, length: { is: 5 }, if: ->(ba) {
-    %w[341].include?(ba.bank_code.to_s)
-  }
-  validates :account, length: { maximum: 11 }, if: ->(ba) {
-    %w[104].include?(ba.bank_code.to_s)
-  }
-  validates :account, length: { is: 6 }, if: ->(ba) {
-    %w[399].include?(ba.bank_code.to_s)
-  }
-  validates :account, length: { is: 7 }, if: ->(ba) {
-    %w[745].include?(ba.bank_code.to_s)
-  }
+
+  validate :agency, :agency_validation
+  validate :account, :account_validation
+  validate :account_digit, :account_digit_validation
+
+  def agency_validation
+    
+    errors.delete :agency
+
+    if agency == nil || agency.length == 0
+      errors.add(:agency, :blank) 
+      return false
+    end
+
+    if agency.length != 4
+      errors.add(:agency, :format) 
+      return false
+    end
+
+    if !agency.match? /\d{4}/
+      errors.add(:agency, :invalid)
+      return false
+    end
+
+    bank_code_in_validation_table?
+  end
+
+  def account_validation
+
+    # setup locale custom access
+    account_locale = 'activerecord.errors.models.bank_account.attributes.account'
+    account_equal_locale = I18n.t("#{account_locale}.equal")
+    account_maximum_locale = I18n.t("#{account_locale}.maximum")
+    account_invalid_locale = I18n.t("#{account_locale}.invalid")
+
+    errors.delete :account
+
+    if account == nil || account.length == 0
+      errors.add(:account, :blank)
+      return false
+    end
+
+    if !account.match? /\d+/
+      errors.add(:account, :format)
+      return false
+    end
+
+    if %w[237].include?(bank_code.to_s) && account.length > 7
+      account_error = (account_invalid_locale + account_maximum_locale) % [7]
+      errors.add(:account, account_error)
+      return false
+    end 
+
+    if %w[001 033].include?(bank_code.to_s) && account.length > 8
+      account_error = (account_invalid_locale + account_maximum_locale) % [8]
+      errors.add(:account, account_error)
+      return false
+    end 
+
+    if %w[341].include?(bank_code.to_s) && account.length != 5
+      account_error = (account_invalid_locale + account_equal_locale) % [5]
+      errors.add(:account, account_error)
+      return false
+    end 
+
+    if %w[104].include?(bank_code.to_s) && account.length > 11
+      account_error = (account_invalid_locale + account_maximum_locale) % [11]
+      errors.add(:account, account_error)
+      return false
+    end 
+
+    if %w[399].include?(bank_code.to_s) && account.length != 6
+      account_error = (account_invalid_locale + account_equal_locale) % [6]
+      errors.add(:account, account_error)
+      return false
+    end 
+
+    if %w[745].include?(bank_code.to_s) && account.length != 7
+      account_error = (account_invalid_locale + account_equal_locale) % [7]
+      errors.add(:account, account_error)
+      return false
+    end 
+    
+    return true
+  end
+
+  def account_digit_validation
+
+    errors.delete :account_digit
+
+    if account_digit == nil || account_digit.length == 0
+      errors.add(:account_digit, :blank)
+      return false
+    end
+
+    if account_digit.length != 1
+      errors.add(:account_digit, :format)
+      return false
+    end
+
+    bank_code_in_validation_table?
+  end
 
   # before validate bank account we inject the founded
   # bank account via input_bank_number
