e203e1931da821d472eba1a89f32fc5052056d71
Bruno E. Grossi <bruno@grossi.com.br>
2018-09-27 01:07:05 +0000

    Project fiscal data as a table to not change project/user data

--- app/models/project_fiscal_inform.rb
+++ app/models/project_fiscal_inform.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ProjectFiscalInform < ActiveRecord::Base
-  self.table_name = 'public.project_fiscal_informs'
+  self.table_name = 'public.project_fiscal_informs_view'
   belongs_to :project
   belongs_to :user
 
@@ -9,3 +9,6 @@ class ProjectFiscalInform < ActiveRecord::Base
     self
   end
 end
+
+#dont know why self.table_name didn't work
+ProjectFiscalInform.table_name = 'public.project_fiscal_informs_view'
