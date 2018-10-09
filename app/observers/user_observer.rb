1b27bd3bd2b18c7fd7400a29cf9083f96efa58cf
Adriano Benin <adriano.benin@gmail.com>
2018-10-09 18:10:26 -0300

    circleci test

--- app/observers/user_observer.rb
+++ app/observers/user_observer.rb
@@ -22,7 +22,7 @@ class UserObserver < ActiveRecord::Observer
     user.fix_twitter_user
     user.fix_facebook_link
     user.nullify_permalink
-    user.cache_uploaded_image_url
+    # user.cache_uploaded_image_url
   end
 
   def after_save(user)
