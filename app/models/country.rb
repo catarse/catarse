e78e9e972f0373e551df9f641dd5562326d20d9b
Adriano Benin <adriano.benin@gmail.com>
2018-10-30 11:29:00 -0300

    state controller

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,22 @@
 # frozen_string_literal: true
 
 class Country < ActiveRecord::Base
+  include Shared::CommonWrapper
+  after_save :index_on_common
+
+  def common_index
+    id_hash = common_id.present? ? {id: common_id} : {}
+
+    {
+      external_id: id,
+      name: name,
+      name_en: name_en,
+      created_at: created_at.try(:strftime, "%FT%T"),
+      updated_at: updated_at.try(:strftime, "%FT%T")
+    }.merge!(id_hash)
+  end
+
+  def index_on_common
+    common_wrapper.index_state(self) if common_wrapper
+  end
 end
