4c8209f4e7cf0d6c1841f3191d3eb72600b24fe4
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2018-11-13 14:41:08 -0200

    fixing specs
    Signed-off-by: Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>

--- spec/models/post_reward_spec.rb
+++ spec/models/post_reward_spec.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe PostReward, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'associations' do
+    it { is_expected.to belong_to :project_post }
+    it { is_expected.to belong_to :reward }
+  end
 end
