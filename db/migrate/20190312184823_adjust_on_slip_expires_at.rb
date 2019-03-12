9f7ffa5735412fa4e4a8d4a7f966e631f6755038
Antonio Roberto Silva <forevertonny@gmail.com>
2019-03-12 19:09:45 +0000

    slip_expires_at should use gateway data expiration when exists

---
+++ db/migrate/20190312184823_adjust_on_slip_expires_at.rb
@@ -0,0 +1,25 @@
+class AdjustOnSlipExpiresAt < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+create function slip_expires_at(payments) returns timestamp without time zone
+  stable
+  language sql
+as
+$$
+SELECT weekdays_from(public.slip_expiration_weekdays(), public.zone_timestamp(coalesce(($1.gateway_data->>'boleto_expiration_date'::text)::timestamp, $1.created_at)))::date + 1 - '1 second'::interval;
+$$;
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+create or replace function slip_expires_at(payments) returns timestamp without time zone
+  stable
+  language sql
+as
+$$
+SELECT weekdays_from(public.slip_expiration_weekdays(), public.zone_timestamp($1.created_at))::date + 1 - '1 second'::interval;
+$$;
+    SQL
+  end
+end
