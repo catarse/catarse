7617adee6b24caa9bea7b9c6dc5f48b7aa1c8729
Antonio Roberto Silva <forevertonny@gmail.com>
2019-06-09 23:24:29 +0000

    env rack timeout

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,8 +9,8 @@ else
   worker_processes 3
 end
 
-# Requests with more than 30 sec will be killed
-timeout 30
+# Requests with more than will be killed
+timeout (ENV['RACK_TIMEOUT'] || 30).to_i
 
 # Preload entire app for fast forking.
 preload_app true
