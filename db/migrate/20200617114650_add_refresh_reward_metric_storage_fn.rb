9561e9a097df3d812c84590b7ddccd43b88b44c9
Antonio Roberto Silva <forevertonny@gmail.com>
2020-06-17 13:16:08 +0000

    add fn to refresh reward metric storage

---
+++ db/migrate/20200617114650_add_refresh_reward_metric_storage_fn.rb
@@ -0,0 +1,32 @@
+class AddRefreshRewardMetricStorageFn < ActiveRecord::Migration
+  def change
+    execute <<-SQL
+
+CREATE OR REPLACE FUNCTION public.refresh_reward_metric_storage(arg_r rewards)
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+declare
+    v_data jsonb;
+begin
+    select
+        json_build_object(
+            'paid_count', coalesce(public.paid_count(arg_r), 0),
+            'waiting_payment_count', coalesce(public.waiting_payment_count(arg_r), 0)
+        )::jsonb
+    into v_data;
+    
+    insert into public.reward_metric_storages (reward_id, data, refreshed_at, created_at, updated_at)
+        values (arg_r.id, v_data, now(), now(), now())
+    on conflict (reward_id) 
+        do update set
+            data = excluded.data,
+            refreshed_at = excluded.refreshed_at,
+            updated_at = excluded.updated_at;
+    
+    return;
+end;
+$function$;
+    SQL
+  end
+end
