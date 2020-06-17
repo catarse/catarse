fbd765fa46ef25dc54f81eb15ec154e3b67bf8fd
Antonio Roberto Silva <forevertonny@gmail.com>
2020-06-17 17:48:42 +0000

    fix typo

--- db/migrate/20200601191633_add_antecipation_fee.rb
+++ db/migrate/20200601191633_add_antecipation_fee.rb
@@ -1,6 +1,6 @@
 class AddAntecipationFee < ActiveRecord::Migration
   def change
-    add_column :projects, :antecipation_fee, default: 0.025, null: false
+    add_column :projects, :antecipation_fee, :numeric, default: 0.025, null: false
     add_index :balance_transactions, [:event_name, :project_id, :contribution_id], where: "event_name = 'antecipation_fee'", name: 'balance_antecipation_fee_evt_uniq', unique: true
     add_index :balance_transactions, [:event_name, :project_id, :contribution_id], where: "event_name = 'contribution_payment'", name: 'balance_contribution_payment_evt_uniq', unique: true
   end
