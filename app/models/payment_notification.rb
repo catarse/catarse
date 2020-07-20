bc11085ad2a111591c06722377f071c001ed83ab
Antonio Roberto Silva <forevertonny@gmail.com>
2020-07-20 22:05:29 +0000

    refresh reward metric storages after create a payment_notification

--- app/models/payment_notification.rb
+++ app/models/payment_notification.rb
@@ -4,6 +4,14 @@ class PaymentNotification < ActiveRecord::Base
   belongs_to :contribution
   serialize :extra_data, JSON
 
+  after_commit :schedule_refresh_metric_storages, on: :create
+
+  def schedule_refresh_metric_storages
+    return unless self.contribution.present?
+    return unless self.contribution.reward.present?
+    RewardMetricStorageRefreshWorker.perform_in(5.seconds, contribution.reward_id)
+  end
+
   # This methods should be called by payments engines
   def deliver_process_notification
     deliver_contributor_notification(:processing_payment)
