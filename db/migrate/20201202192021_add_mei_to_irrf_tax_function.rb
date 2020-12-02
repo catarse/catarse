69c3ae8008c4277f5fb1c70d45ff120fa537429e
stephannv <3025661+stephannv@users.noreply.github.com>
2020-12-02 16:28:59 -0300

    🐞 Adiciona tipo MEI no cálculo de IRRF

---
+++ db/migrate/20201202192021_add_mei_to_irrf_tax_function.rb
@@ -0,0 +1,39 @@
+class AddMeiToIrrfTaxFunction < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+      CREATE OR REPLACE FUNCTION public.irrf_tax(project projects)
+      RETURNS numeric
+      LANGUAGE sql
+      STABLE
+      AS $function$
+        SELECT
+            CASE
+            WHEN u.account_type IN ('pj', 'mei') AND p.total_catarse_fee_without_gateway_fee >= 666.66 THEN
+                0.015 * p.total_catarse_fee_without_gateway_fee
+            ELSE 0 END
+        FROM public.projects p
+        LEFT JOIN public.users u on u.id = p.user_id
+        WHERE p.id = project.id;
+      $function$;
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      CREATE OR REPLACE FUNCTION public.irrf_tax(project projects)
+      RETURNS numeric
+      LANGUAGE sql
+      STABLE
+      AS $function$
+        SELECT
+            CASE
+            WHEN u.account_type = 'pj' AND p.total_catarse_fee_without_gateway_fee >= 666.66 THEN
+                0.015 * p.total_catarse_fee_without_gateway_fee
+            ELSE 0 END
+        FROM public.projects p
+        LEFT JOIN public.users u on u.id = p.user_id
+        WHERE p.id = project.id;
+      $function$;
+    SQL
+  end
+end
