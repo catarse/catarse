7b9820d093d3d5af3953711852c22cee8aa8c4dd
stephannv <3025661+stephannv@users.noreply.github.com>
2020-12-21 11:48:20 -0300

    🔧 Configura inicialização do Sentry

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module Catarse
 
       Raven.configure do |config|
         config.dsn = CatarseSettings.get_without_cache(:sentry_dsn) || ''
+        config.secret_key = config.public_key
         config.environments = %w[sandbox production]
         config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
       end
