21ee07eb30d6849cdf9fca31c6f042d40db6e9d3
Stephann <3025661+stephannv@users.noreply.github.com>
2021-03-15 10:59:53 -0300

    🐞 Ajusta download de thumbnail dos vídeos dos projetos

--- spec/models/concerns/project/video_handler_spec.rb
+++ spec/models/concerns/project/video_handler_spec.rb
@@ -8,14 +8,17 @@ RSpec.describe Project::VideoHandler, type: :model do
   describe '#download_video_thumbnail' do
     before do
       expect(project).to receive(:download_video_thumbnail).and_call_original
-      expect(project).to receive(:open).and_return(File.open('spec/fixtures/files/image.png'))
+
       stub_request(:any, 'https://vimeo.com/17298435')
           .to_return(body: file_fixture('vimeo_default_request.txt'))
+      stub_request(:any, 'https://i.vimeocdn.com/video/107328495_640.jpg')
+          .to_return(body: file_fixture('image.png').read)
+
       project.download_video_thumbnail
     end
 
     it 'should open the video_url and store it in video_thumbnail' do
-      expect(project.video_thumbnail.url).to eq("/uploads/project/video_thumbnail/#{project.id}/image.png")
+      expect(project.video_thumbnail.url).to match("/uploads/project/video_thumbnail/#{project.id}/")
     end
   end
 end
