41a2148264eca4f200a96f3c472bcad88e12688d
Stephann Vasconcelos <3025661+stephannv@users.noreply.github.com>
2021-06-17 09:57:03 -0300

    🐞 Corrige migração para recriar index do balance_transactions

--- db/migrate/20210520200349_index_subscription_payment_refunded_uniq_adjustment.rb
+++ db/migrate/20210520200349_index_subscription_payment_refunded_uniq_adjustment.rb
@@ -1,14 +1,15 @@
 class IndexSubscriptionPaymentRefundedUniqAdjustment < ActiveRecord::Migration[6.1]
   def up
     execute %Q{
-      drop index idx_subscription_payment_refunded_evt_uniq;
+      drop index if exists idx_subscription_payment_refunded_evt_uniq;
       create unique index idx_subscription_payment_refunded_evt_uniq on balance_transactions(event_name, subscription_payment_uuid, user_id) where event_name = 'subscription_payment_refunded';
     }
   end
 
   def down
     execute %Q{
-      drop index idx_subscription_payment_refunded_evt_uniq;
+      drop index if exists idx_subscription_payment_refunded_evt_uniq;
+      create unique index idx_subscription_payment_refunded_evt_uniq on balance_transactions(event_name, subscription_payment_uuid) where event_name = 'subscription_payment_refunded';
     }
   end
 end
