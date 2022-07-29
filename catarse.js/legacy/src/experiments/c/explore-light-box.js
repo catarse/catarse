common-group/services-core

💚 Altera o nome do selo para Projetos de Destaque

services/catarse/catarse.js/legacy/src/experiments/c/explore-light-box.js
@@ -19,7 +19,7 @@ export class ExploreLightBox {
     /**
      * @param {{attrs: Attrs}} vnode
      */
-    view({attrs}) {
+    view({ attrs }) {
         const onClose = attrs.onClose;
         const categories = attrs.categories;
         const closePreventRedirect = (/** @type {Event} */ event) => {
@@ -29,7 +29,7 @@ export class ExploreLightBox {
 
         const filters = [
             {
-                name: 'Projetos que amamos',
+                name: 'Projetos de destaques',
                 query: {
                     filter: 'projects_we_love',
                 }
@@ -139,7 +139,7 @@ class ExploreLightBoxList {
     /**
      * @param {{ attrs: Attrs }} vnode
      */
-    view({attrs}) {
+    view({ attrs }) {
 
         const title = attrs.title;
         const items = attrs.items;
@@ -176,7 +176,7 @@ class ExploreLightBoxListItem {
     /**
      * @param {{ attrs: Attrs }} vnode
      */
-    view({attrs}) {
+    view({ attrs }) {
         const label = attrs.label;
         const url = attrs.url;
         const onSelect = attrs.onSelect;

services/catarse/catarse.js/legacy/src/vms/project-filters-vm.js
@@ -42,7 +42,7 @@ const projectFiltersVM = () => {
         }).open_for_contributions('true'),
 
         active_saved_projects = filtersVM({
-          active_saved_projects: 'eq'
+            active_saved_projects: 'eq'
         }).active_saved_projects(true),
 
         contributed_by_friends = filtersVM({
@@ -72,18 +72,18 @@ const projectFiltersVM = () => {
 
         filters = {
             projects_we_love_not_sub: {
-                title: 'Projetos que amamos',
+                title: 'Projetos de destaques',
                 filter: projects_we_love_not_sub,
                 mode: 'not_sub',
-                nicename: 'Projetos que amamos',
+                nicename: 'Projetos de destaques',
                 isContextual: false,
                 keyName: 'projects_we_love',
                 header_badges: ['badge-aon-h-margin', 'badge-flex-h-margin']
             },
             projects_we_love: {
-                title: 'Projetos que amamos',
+                title: 'Projetos de destaques',
                 filter: projects_we_love,
-                nicename: 'Projetos que amamos',
+                nicename: 'Projetos de destaques',
                 isContextual: false,
                 keyName: 'projects_we_love',
                 header_badges: ['badge-aon-h-margin', 'badge-flex-h-margin']
@@ -191,10 +191,10 @@ const projectFiltersVM = () => {
         };
 
     const setContextFilters = (contextFilters) => {
-            _.map(contextFilters, filterKey => filters[filterKey].isContextual = true);
+        _.map(contextFilters, filterKey => filters[filterKey].isContextual = true);
 
-            return filters;
-        },
+        return filters;
+    },
         getContextFilters = () => _.filter(filters, filter => filter.isContextual),
         removeContextFilter = (filter) => {
             filters[filter.keyName].isContextual = false;

catarse/catarse

catarse.js/legacy/src/experiments/c/explore-light-box.js
@@ -19,7 +19,7 @@ export class ExploreLightBox {
     /**
      * @param {{attrs: Attrs}} vnode
      */
-    view({attrs}) {
+    view({ attrs }) {
         const onClose = attrs.onClose;
         const categories = attrs.categories;
         const closePreventRedirect = (/** @type {Event} */ event) => {
@@ -29,7 +29,7 @@ export class ExploreLightBox {
 
         const filters = [
             {
-                name: 'Projetos que amamos',
+                name: 'Projetos de destaque',
                 query: {
                     filter: 'projects_we_love',
                 }
@@ -139,7 +139,7 @@ class ExploreLightBoxList {
     /**
      * @param {{ attrs: Attrs }} vnode
      */
-    view({attrs}) {
+    view({ attrs }) {
 
         const title = attrs.title;
         const items = attrs.items;
@@ -176,7 +176,7 @@ class ExploreLightBoxListItem {
     /**
      * @param {{ attrs: Attrs }} vnode
      */
-    view({attrs}) {
+    view({ attrs }) {
         const label = attrs.label;
         const url = attrs.url;
         const onSelect = attrs.onSelect;

catarse.js/legacy/src/vms/project-filters-vm.js
@@ -42,7 +42,7 @@ const projectFiltersVM = () => {
         }).open_for_contributions('true'),
 
         active_saved_projects = filtersVM({
-          active_saved_projects: 'eq'
+            active_saved_projects: 'eq'
         }).active_saved_projects(true),
 
         contributed_by_friends = filtersVM({
@@ -72,18 +72,18 @@ const projectFiltersVM = () => {
 
         filters = {
             projects_we_love_not_sub: {
-                title: 'Projetos que amamos',
+                title: 'Projetos de destaque',
                 filter: projects_we_love_not_sub,
                 mode: 'not_sub',
-                nicename: 'Projetos que amamos',
+                nicename: 'Projetos de destaque',
                 isContextual: false,
                 keyName: 'projects_we_love',
                 header_badges: ['badge-aon-h-margin', 'badge-flex-h-margin']
             },
             projects_we_love: {
-                title: 'Projetos que amamos',
+                title: 'Projetos de destaque',
                 filter: projects_we_love,
-                nicename: 'Projetos que amamos',
+                nicename: 'Projetos de destaque',
                 isContextual: false,
                 keyName: 'projects_we_love',
                 header_badges: ['badge-aon-h-margin', 'badge-flex-h-margin']
@@ -191,10 +191,10 @@ const projectFiltersVM = () => {
         };
 
     const setContextFilters = (contextFilters) => {
-            _.map(contextFilters, filterKey => filters[filterKey].isContextual = true);
+        _.map(contextFilters, filterKey => filters[filterKey].isContextual = true);
 
-            return filters;
-        },
+        return filters;
+    },
         getContextFilters = () => _.filter(filters, filter => filter.isContextual),
         removeContextFilter = (filter) => {
             filters[filter.keyName].isContextual = false;